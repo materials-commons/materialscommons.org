927384ffd5babe1245bfb95c1ac97a7fcf2bc50a
V. Glenn Tarcea <gtarcea@umich.edu>
2013-08-01 16:32:31 -0400

    Tags by various lookup pages.

--- assets/js/materialscommons/controllers/labcontroller.js
+++ assets/js/materialscommons/controllers/labcontroller.js
@@ -1,15 +1,13 @@
-function LabController($scope, Mcdb){
+function LabController($scope, $http) {
     $scope.mcdb = Mcdb.db();
 
-
-
-    $scope.get_lab_data = function(value){
-        $scope.lab_data = $scope.mcdb.query("materialscommons-app", "datagroup_by_usergroups", {key:value});
+    $scope.get_lab_data = function (value) {
+        $scope.lab_data = $scope.mcdb.query("materialscommons-app", "datagroup_by_usergroups", {key: value});
 
     }
+
     $scope.get_utc_obj = function (utc_in_sec) {
         var d = new Date(utc_in_sec * 1000);
         return d;
     }
-
 }
--- assets/js/materialscommons/controllers/tags/tags.js
+++ assets/js/materialscommons/controllers/tags/tags.js
@@ -1,53 +1,43 @@
-function TagListController($scope, $routeParams, $location, cornercouch, User)
-{
-    $scope.server = cornercouch();
-    $scope.server.session();
+function TagListController($scope, $routeParams, $location, $http, User) {
     $scope.listtype = $routeParams.listtype;
-    $scope.mcdb = $scope.server.getDB("materialscommons");
-    if ($routeParams.listtype == "all")
-    {
+    if ($routeParams.listtype == "all") {
         $scope.tagHeader = "All Tags";
-        $scope.mcdb.query("materialscommons-app", "tags_by_count", {group_level:1});
+        $http.jsonp(mcurljsonp('/tags/count'))
+            .success(function (data) {
+                $scope.tags = data;
+            });
     }
-    else if ($routeParams.listtype == "mytags")
-    {
+    else if ($routeParams.listtype == "mytags") {
         $scope.tagHeader = "My Tags";
-        var username = User.get_username();
-        $scope.mcdb.query("materialscommons-app", "tags_by_user", {group_level:2, startkey:[username], endkey:[username, {}]});
-
+        $http.jsonp(mcurljsonpu('/tags/count', User))
+            .success(function (data) {
+                $scope.tags = data;
+            });
     }
 
-//    $scope.listDataForTag = function(tag) {
-  //      $location.path("/tags/data/bytag/" + tag + "/");
-
-    $scope.listDataForTag = function(key) {
-        var tag = $scope.listtype == "mytags" ? key[1] : key[0];
-        $location.path("/tags/data/bytag/" + tag +"/" + User.get_username());
+    $scope.listDataForTag = function (tag) {
+        $location.path("/tags/data/bytag/" + tag + "/" + User.get_username());
     }
 
-    if ($routeParams.tag_name){
-       // alert($routeParams.tag_name);
-        $scope.tag_name = $routeParams.tag_name;
-        $scope.all_tags_by_tag_name = $scope.mcdb.query("materialscommons-app", "docs_by_tag", {key:$routeParams.tag_name});
-
-
-    }
-
-
+//    if ($routeParams.tag_name){
+//       // alert($routeParams.tag_name);
+//        $scope.tag_name = $routeParams.tag_name;
+//        $scope.all_tags_by_tag_name = $scope.mcdb.query("materialscommons-app", "docs_by_tag", {key:$routeParams.tag_name});
+//    }
 }
 
 function TagDataController($scope, $routeParams, $location, $http, User) {
     $scope.tag = $routeParams.tag;
     $http.jsonp(mcurljsonpu2('/data/tag', $routeParams.tag, User))
-        .success(function(data) {
+        .success(function (data) {
             $scope.docs = data;
         });
 
-    $scope.editData = function(id) {
+    $scope.editData = function (id) {
         $location.path("/data/edit/" + id);
     }
 
-    $scope.get_alldata_for_tag = function(tag){
+    $scope.get_alldata_for_tag = function (tag) {
         $location.path("/tags/tag_info/" + tag);
     }
 
