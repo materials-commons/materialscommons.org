7599fe926715c458aebf7842fd0bed8b10cf72b2
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-08-10 15:01:25 -0400

    Removed unused modules.

--- app/materialscommons-app.js
+++ app/materialscommons-app.js
@@ -1,11 +1,12 @@
-var app = angular.module('materialscommons', ['CornerCouch', 'ui', 'Filter', 'materialsCommonsServices', 'materialsdirective','treedirective', 'jqyoui', 'AngularStomp',
-                            'ui.bootstrap', 'smartTable.table', 'smartTable.column', 'flash']);
+var app = angular.module('materialscommons',
+    ['ui', 'Filter', 'materialsCommonsServices', 'materialsdirective', 'jqyoui', 'AngularStomp',
+        'ui.bootstrap', 'flash']);
 
-app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
     Stomp.WebSocketClass = SockJS;
 
     /*
-    ** CORS support
+     ** CORS support
      */
     //$httpProvider.defaults.useXDomain = true;
     //delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -15,7 +16,8 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($r
         when('/data/data/:id', {templateUrl: 'partials/data/data.html', controller: DataSearchController}).
         when('/models', {templateUrl: 'partials/under-construction.html', controller: ModelsSearchController}).
         when('/mylab/:tab/:subpage/:id', {templateUrl: 'partials/mylab/mylab.html', controller: MyLabTabController}).
-        when('/searchindex/:subpage/:name', {templateUrl: 'partials/search_repository.html', controller: SearchIndexController}).
+        when('/searchindex/:subpage/:name',
+        {templateUrl: 'partials/search_repository.html', controller: SearchIndexController}).
         when('/login', {templateUrl: 'partials/login.html', controller: LoginController}).
         when('/logout', {templateUrl: 'partials/about.html', controller: LogOutController}).
         when('/explore', {templateUrl: 'partials/tree_view.html', controller: ExploreController}).
@@ -23,14 +25,17 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($r
         when('/contact', {templateUrl: 'partials/contact.html', controller: ContactController}).
         when('/help', {templateUrl: 'partials/help.html', controller: HelpController}).
         when('/search', {templateUrl: 'partials/search_repository.html', controller: SearchIndexController}).
-        when('/datagroups', {templateUrl: 'partials/datagroups/my_data_groups.html', controller: MyDataGroupsController}).
+        when('/datagroups',
+        {templateUrl: 'partials/datagroups/my_data_groups.html', controller: MyDataGroupsController}).
         when('/data_by_user', {templateUrl: 'partials/datagroups/user_data.html', controller: MyDataController}).
         when('/my_lab', {templateUrl: 'partials/datagroups/my_lab.html', controller: LabController}).
-        when('/results_by_date', {templateUrl: 'partials/datagroups/results_by_date.html', controller: SearchByDateController}).
+        when('/results_by_date',
+        {templateUrl: 'partials/datagroups/results_by_date.html', controller: SearchByDateController}).
         when('/user_functions', {templateUrl: 'partials/user_functions.html', controller: FrontPageController}).
         when('/create-account', {templateUrl: 'partials/create-account.html', controller: AccountController}).
         when('/tags/list/:listtype', {templateUrl: 'partials/tags/tags-list.html', controller: TagListController}).
-        when('/tags/data/bytag/:tag/:user', {templateUrl: 'partials/tags/data-for-tag.html', controller: TagDataController}).
+        when('/tags/data/bytag/:tag/:user',
+        {templateUrl: 'partials/tags/data-for-tag.html', controller: TagDataController}).
         when('/data/edit/:id', {templateUrl: 'partials/data/data-edit.html', controller: DataEditController}).
         when('/tree_view', {templateUrl: 'partials/tree_view.html', controller: TreeController}).
         when('/tags/tag_info/:tag_name', {templateUrl: 'partials/tags/tag_info.html', controller: TagListController}).
@@ -64,11 +69,11 @@ app.run(function ($rootScope, $location, User) {
 //            setActiveMainNav("#findmodels");
 //        }
 
-        if (mcglobals.bypasslogin){
+        if (mcglobals.bypasslogin) {
             if (mcglobals.username) {
                 User.setAuthenticated(true, mcglobals.username);
             }
-            return ;
+            return;
         }
 
         if (!User.isAuthenticated()) {
