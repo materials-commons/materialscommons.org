50835de87359b2d6440ccadfe3246cf3c1479285
Glenn Tarcea <glenn.tarcea@gmail.com>
2013-08-10 20:14:06 -0400

    Fix up allowing test configuration.

--- app/js/config/config.js
+++ app/js/config/config.js
@@ -1,10 +1,9 @@
 if (window.location.hostname != "materialscommons.org")
 {
-    document.write('<script type="text/javascript" src="' + "app/js/config/testconfig.js" + '"></script>');
     if (typeof testconfig != 'undefined') {
         mcglobals.bypasslogin = testconfig.bypasslogin;
         mcglobals.username = testconfig.username;
-        mcglobals.apihost = testconfig.apihost
+        mcglobals.apihost = testconfig.apihost;
     }
 }
 
