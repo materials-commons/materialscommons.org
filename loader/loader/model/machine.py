a2954ebeabc1623fc6164bccf71a078c4139fac3
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-08 16:24:59 -0400

    ISSUE#98 Rename shortname to name (the default name). We were also ignoring items passed in the constructor.

--- loader/loader/model/machine.py
+++ loader/loader/model/machine.py
@@ -1,9 +1,9 @@
 import rethinkdb as r
 
 class Machine(object):
-    def  __init__(self, longname, shortname, contact):
-        self.shortname = ""
-        self.longname = ""
+    def  __init__(self, name, longname, contact):
+        self.name = name
+        self.longname = longname
         self.birthtime = r.now()
         self.contact = contact
         self.notes = []
