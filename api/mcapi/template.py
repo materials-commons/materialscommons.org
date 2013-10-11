7603bb0af82e86bf34d1d562c6fae774295c48ea
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-11 11:49:07 -0400

    ISSUE#98 Change format of the model for a template. Add condition templates.

--- api/mcapi/template.py
+++ api/mcapi/template.py
@@ -1,6 +1,6 @@
 from mcapp import app
 from decorators import crossdomain, apikey, jsonp
-from flask import g, request
+from flask import request
 import rethinkdb as r
 from utils import error_response
 import dmutil
@@ -29,9 +29,11 @@ def create_template():
     return create_template_for_type(template_type, j)
 
 def create_template_for_type(template_type, j):
+    print "create_template_for_type: %s" %(template_type)
     template_table = {\
                       "process": create_process_template,\
-                      "machine": create_machine_template
+                      "machine": create_machine_template,\
+                      "condition": create_condition_template\
     }
     if template_type in template_table:
         template_func = template_table[template_type]
@@ -43,38 +45,56 @@ def create_template_for_type(template_type, j):
 
 def create_process_template(j):
     template = common_template_elements("process", j)
-    template['model']['name'] = ""
-    template['model']['owner'] = ""
-    template['model']['description'] = ""
-    template['model']['birthtime'] = ""
-    template['model']['mtime'] = ""
-    template['model']['machine'] = ""
-    template['model']['process_type'] = ""
-    template['model']['version'] = ""
-    template['model']['parent'] = ""
-    template['model']['notes'] = []
-    template['model']['inputs'] = []
-    template['model']['outputs'] = []
-    template['model']['runs'] = []
-    template['model']['citations'] = []
-    template['model']['status'] = ""
+    required_conditions = dmutil.get_optional('required_conditions', j, [])
+    add_model_item(template, 'required_conditions', required_conditions)
+    add_model_item(template, 'name', "")
+    add_model_item(template, 'owner', "")
+    add_model_item(template, 'description', "")
+    add_model_item(template, 'birthtime', "")
+    add_model_item(template, 'mtime', "")
+    add_model_item(template, 'machine', "")
+    add_model_item(template, 'process_type', "")
+    add_model_item(template, 'version', "")
+    add_model_item(template, 'parent', "")
+    add_model_item(template, 'notes', [])
+    add_model_item(template, 'inputs', [])
+    add_model_item(template, 'outputs', [])
+    add_model_item(template, 'runs', [])
+    add_model_item(template, 'citations', [])
+    add_model_item(template, 'status', "")
+    return template
 
 def create_machine_template(j):
     template = common_template_elements("machine", j)
-    template['model']['shortname'] = ""
-    template['model']['longname'] = ""
-    template['model']['birthtime'] = ""
-    template['model']['contact'] = ""
-    template['model']['notes'] = []
+    add_model_item(template, 'shortname', "")
+    add_model_item(template, 'longname', "")
+    add_model_item(template, 'birthtime', "")
+    add_model_item(template, 'contact', "")
+    add_model_item(template, 'description', "")
+    add_model_item(template, 'notes', [])
+    return template
+
+def create_condition_template(j):
+    template = common_template_elements("condition", j)
+    properties = j['properties']
+    for prop in properties:
+        prop_name = prop['name']
+        prop_value = prop['value']
+        add_model_item(template, prop_name, prop_value)
+        add_model_item(template, prop_name + '_note', "")
+    return template
 
 def common_template_elements(template_type, j):
     template = dict()
     template['template_type'] = template_type
-    template['template_name'] = dmutil.get_required('name', j)
-    template['template_owner'] = dmutil.get_required('owner', j)
+    template['template_name'] = dmutil.get_required('template_name', j)
+    template['owner'] = dmutil.get_required('owner', j)
     template['template_description'] = dmutil.get_required('template_description', j)
     now = r.now()
     template['template_birthtime'] = now
     template['template_mtime'] = now
-    template['model'] = dict()
+    template['model'] = list()
     return template
+
+def add_model_item(template, name, value):
+    template['model'].append({'name':name, 'value':value})
