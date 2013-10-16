7cce436c4e4f6b3c6e0551aa46edde6c0151788c
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-16 16:46:21 -0400

    ISSUE#98 Create place holder for getting all project files.

--- api/mcapi/user/projects.py
+++ api/mcapi/user/projects.py
@@ -1,9 +1,10 @@
 from ..mcapp import app
-from ..decorators import crossdomain, apikey, jsonp
-from flask import request, g
+from ..decorators import apikey, jsonp
+from flask import g
 import rethinkdb as r
-from .. import dmutil
+#from .. import dmutil
 from .. import args
+from ..utils import error_response
 
 @app.route('/v1.0/user/<user>/projects', methods=['GET'])
 @apikey
@@ -13,3 +14,14 @@ def get_all_projects(user):
     rr = args.add_all_arg_options(rr)
     items = list(rr.run(g.conn, time_format='raw'))
     return args.json_as_format_arg(items)
+
+@app.route('/v1.0/user/<user>/project/<project_id>/datafiles')
+@apikey
+@jsonp
+def get_all_datafiles_for_project(user, project_id):
+    project = r.table('projects').get(project_id).run(g.conn)
+    if project is None:
+        return error_response(400)
+    if project['owner'] != user:
+        return error_response(400)
+    return ""
