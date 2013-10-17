42869823b4af024906606c5caf50e5dc5de69a57
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-17 16:11:36 -0400

    Add call to get datadirs for a project.

--- api/mcapi/user/projects.py
+++ api/mcapi/user/projects.py
@@ -15,7 +15,7 @@ def get_all_projects(user):
     items = list(rr.run(g.conn, time_format='raw'))
     return args.json_as_format_arg(items)
 
-@app.route('/v1.0/user/<user>/project/<project_id>/datafiles')
+@app.route('/v1.0/user/<user>/projects/<project_id>/datafiles')
 @apikey
 @jsonp
 def get_all_datafiles_for_project(user, project_id):
@@ -25,3 +25,15 @@ def get_all_datafiles_for_project(user, project_id):
     if project['owner'] != user:
         return error_response(400)
     return ""
+
+@app.route('/v1.0/user/<user>/projects/<project_id>/datadirs')
+@apikey
+@jsonp
+def get_datadirs_for_project(user, project_id):
+    rr = r.table('project2datadir').filter({'project_id': project_id})
+    rr = rr.eq_join('project_id', r.table('projects')).zip()
+    rr = rr.eq_join('datadir_id', r.table('datadirs')).zip()
+    selection = list(rr.run(g.conn, time_format='raw'))
+    if len(selection) > 0 and selection[0]['owner'] == user:
+        return args.json_as_format_arg(selection)
+    return args.json_as_format_arg([])
