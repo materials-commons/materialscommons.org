5075d44e4a74469c47ef5fc73cd3bc68ce9396f7
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-21 16:57:01 -0400

    Add in call to build tree from a project.

--- api/mcapi/user/projects.py
+++ api/mcapi/user/projects.py
@@ -5,6 +5,9 @@ import rethinkdb as r
 #from .. import dmutil
 from .. import args
 from ..utils import error_response
+from datadirs import DItem, DEncoder
+from os.path import dirname
+import json
 
 @app.route('/v1.0/user/<user>/projects', methods=['GET'])
 @apikey
@@ -37,3 +40,23 @@ def get_datadirs_for_project(user, project_id):
     if len(selection) > 0 and selection[0]['owner'] == user:
         return args.json_as_format_arg(selection)
     return args.json_as_format_arg([])
+
+@app.route('/v1.0/user/<user>/projects/<project_id>/datadirs/tree')
+#@apikey
+@jsonp
+def get_datadirs_as_tree_for_project(user, project_id):
+    rr = r.table('project2datadir').filter({'project_id': project_id})
+    rr = rr.eq_join('project_id', r.table('projects')).zip()
+    rr = rr.eq_join('datadir_id', r.table('datadirs')).zip()
+    rr = rr.pluck('id', 'name').order_by('name')
+    selection = list(rr.run(g.conn, time_format='raw'))
+    all_data_dirs = {}
+    for item in selection:
+        name = item['name']
+        if name not in all_data_dirs:
+            all_data_dirs[name] = DItem(item['id'], name, "datadir")
+        dname = dirname(name)
+        if dname in all_data_dirs:
+            dir_to_add_to = all_data_dirs[dname]
+            dir_to_add_to.children.append(item)
+    return json.dumps(all_data_dirs, indent=4, cls=DEncoder)
