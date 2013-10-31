0e7fb169978f461ab6d0f24f36f1d8fbba309972
V. Glenn Tarcea <gtarcea@umich.edu>
2013-10-31 14:40:24 -0400

    ISSUE#132 Add calls to delete a state item, or to delete all for a user.
    Fix up create to return the object rather than just the id. Add
    some security checks around items.

--- backend/mcapi/stater/stater.py
+++ backend/mcapi/stater/stater.py
@@ -10,12 +10,13 @@ from .. import error
 from .. import args
 
 class State(object):
-    def __init__(self, owner, name, description):
+    def __init__(self, owner, name, description, type):
         self.owner = owner
         self.birthtime = r.now()
         self.mtime = self.birthtime
         self.name = name
         self.description = description
+        self.type = type
 
 class StateEncoder(json.JSONEncoder):
     def default(self, o):
@@ -28,9 +29,10 @@ def create_new_state():
     j = request.get_json()
     user = access.get_user()
     name = dmutil.get_optional('name', j, user + "_state_" + utils.now_str())
+    type = dmutil.get_required('type', j)
     description = dmutil.get_optional('description', j, "Save State for " + user)
-    s = State(user, name, description)
-    return dmutil.insert_entry('state', s.__dict__)
+    s = State(user, name, description, type)
+    return dmutil.insert_entry('state', s.__dict__, return_created=True)
 
 @app.route('/stater/<state_id>', methods=['PUT'])
 @crossdomain(origin='*')
@@ -51,10 +53,36 @@ def update_state(state_id):
 @apikey
 @jsonp
 def get_state_for_id(state_id):
-    return dmutil.get_single_from_table('state', state_id)
+    state = dmutil.get_single_from_table('state', state_id, raw=True)
+    if state['owner'] != access.get_user():
+        return error.not_authorized("You are not authorized to access this state: %s" % (state_id))
+    return args.json_as_format_arg(state)
 
 @app.route('/stater', methods=['GET'])
 @apikey
 @jsonp
 def get_all_state():
-    return dmutil.get_all_from_table('state')
+    return dmutil.get_all_from_table('state', filter_by={'owner': access.get_user()})
+
+@app.route('/stater/<state_id>', methods=['DELETE'])
+@apikey
+def delete_state(state_id):
+    user = access.get_user()
+    state = dmutil.get_single_from_table('state', state_id, raw=True)
+    if state['owner'] != user:
+        return error.not_authorized("You are not authorized to delete this state: %s" % (state_id))
+    rv = r.table('state').get(state_id).delete().run(g.conn)
+    if rv['deleted'] == 0:
+        return error.database_error("Unable to delete state %s" % (state_id))
+    return args.json_as_format_arg(state)
+
+@app.route('/stater', methods=['DELETE'])
+@apikey
+def delete_all_state_for_user():
+    user = access.get_user()
+    rr = r.table('state').filter({'owner':user})
+    rr = rr.for_each(lambda state: r.table('state').get(state['id']).delete())
+    rv = rr.run(g.conn)
+    if rv['errors'] != 0:
+        return error.database_error("Unable to delete all items, the database returned an error")
+    return json.dumps({'deleted': rv['deleted']})
