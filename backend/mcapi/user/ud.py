a91ff9851567fc25db80f1d330f660ee58bf4fa8
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-18 16:33:16 -0500

    Remove debugging statements from import.

--- backend/mcapi/user/ud.py
+++ backend/mcapi/user/ud.py
@@ -46,52 +46,30 @@ def upload_file():
 @apikey
 @crossdomain(origin='*')
 def import_file():
-    print "import_file"
     user = access.get_user()
-    print "about to do request.form"
-    for key in request.files.keys():
-        print "request.files key = %s" %(key)
-    print request.form
-    print request.files
     if 'file' not in request.files:
-        print "no files to import"
         return error.bad_request("No files to import")
     elif 'project' not in request.form:
-        print "no project"
         return error.bad_request('No project specified')
     elif 'datadir' not in request.form:
-        print "no datadir"
         return error.bad_request('No datadir specified')
-    print "getting project"
     project = request.form['project']
     datadir = request.form['datadir']
     mkdirp('/tmp/uploads')
-    print "calling request.files"
     file = request.files['file']
-    print "Printing files for file"
-    #print file.read()
-    print "calling make_datafile"
     dfid = make_datafile(datadir, user, file.filename)
     filepath = os.path.join('/tmp/uploads', dfid)
-    print "filepath = %s" % (filepath)
     file.save(filepath)
-    print "Wrote file to path %s" % (filepath)
     #load_data_file.delay(df, project, datadir)
     return jsonify({'id': dfid})
 
 def make_datafile(datadir, user, filename):
-    print "make_datafile: datadir %s, %s, %s" %(datadir, user, filename)
     df = datafile.DataFile(filename, "private", user)
-    print "   1"
     dfid = dmutil.insert_entry_id('datafiles', df.__dict__)
-    print "   2"
     ddir = r.table('datadirs').get(datadir).run(g.conn)
-    print "   3"
     dfiles = ddir['datafiles']
     dfiles.append(dfid)
-    print "   4"
     r.table('datadirs').get(datadir).update({'datafiles': dfiles}).run(g.conn)
-    print "   5"
     return dfid
 
 @app.route('/download/<path:datafile>')
