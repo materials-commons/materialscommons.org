c0a67b1501560b25b5a904e63cc1e4fb67c9b31f
tammasr <tammasr@umich.edu>
2013-11-18 15:27:51 -0500

    route changes

--- website/app/js/controllers/controllers.js
+++ website/app/js/controllers/controllers.js
@@ -32,6 +32,18 @@ function ContactController($scope) {
 
 function HelpController($scope) {
     $scope.pageDescription = "Help";
+
+}
+
+function PhoneListController($scope) {
+    $scope.phones = [
+        {'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.'}
+    ];
 }
 
 
