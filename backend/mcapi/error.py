655949e3f8353ebd9bb886afdaa7cf1aea6aff78
V. Glenn Tarcea <gtarcea@umich.edu>
2013-11-19 16:40:56 -0500

    PEP formatting.

--- backend/mcapi/error.py
+++ backend/mcapi/error.py
@@ -1,5 +1,6 @@
 from flask import make_response, jsonify
 
+
 class HttpStatusCode(object):
     #
     # Client Errors
@@ -18,35 +19,47 @@ class HttpStatusCode(object):
     STATUS_SERVER_INTERNAL_ERROR = 500
     STATUS_SERVER_SERVICE_UNAVAILABLE = 503
 
+
 def error_response(message, status_code):
     return make_response(jsonify({'error': message}), status_code)
+
 
 def bad_request(message):
     return error_response(message, HttpStatusCode.STATUS_BAD_REQUEST)
 
+
 def not_authorized(message):
     return error_response(message, HttpStatusCode.STATUS_NOT_AUTHORIZED)
+
 
 def forbidden(message):
     return error_response(message, HttpStatusCode.STATUS_FORBIDDEN)
 
+
 def not_found(message):
     return error_response(message, HttpStatusCode.STATUS_NOT_FOUND)
+
 
 def method_not_allowed(message):
     return error_response(message, HttpStatusCode.STATUS_METHOD_NOT_ALLOWED)
 
+
 def not_acceptable(message):
     return error_response(message, HttpStatusCode.STATUS_NOT_ACCEPTABLE)
+
 
 def already_exists(message):
     return error_response(message, HttpStatusCode.STATUS_ALREADY_EXISTS)
 
+
 def server_internal_error(message):
     return error_response(message, HttpStatusCode.STATUS_SERVER_INTERNAL_ERROR)
 
+
 def server_service_unavailable(message):
-    return error_response(message, HttpStatusCode.STATUS_SERVER_SERVICE_UNAVAILABLE)
+    return error_response(message,
+                          HttpStatusCode.STATUS_SERVER_SERVICE_UNAVAILABLE)
+
 
 def database_error(message):
     return server_internal_error(message)
