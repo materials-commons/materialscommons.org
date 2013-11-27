55c382ce0c2e7600f2ea1b0a4755dad9f5a01776
tammasr <tammasr@umich.edu>
2013-11-27 15:25:22 -0500

    process provenance - svaing to state table

--- website/app/js/controllers/mytools/ProjectController.js
+++ website/app/js/controllers/mytools/ProjectController.js
@@ -33,5 +33,72 @@ function ListProjectsController($scope, mcapi) {
         return flatTree;
     };
 
+    $scope.active = function(tab){
+        $scope.selected = tab;
+
+    }
+    $scope.isSelected = function(tab){
+        return $scope.selected === tab;
+    }
 
 }
+
+function ProcessStepController($scope, mcapi, watcher, Stater){
+    mcapi('/templates')
+        .success(function(data) {
+            $scope.process_templates = data;
+        })
+        .error(function(data) {
+
+        }).jsonp();
+
+    $scope.selected_template_type = function (id) {
+        mcapi('/processes/template/%', id)
+            .success(function(data) {
+                $scope.processes = data;
+            })
+            .error(function(data) {
+
+            }).jsonp();
+    }
+
+    watcher.watch($scope, 'selected_process', function (name) {
+        if (name == "new"){
+            $scope.process = name;
+        }
+        else{
+            $scope.process = JSON.parse(name);
+        }
+    });
+
+    $scope.add_notes = function () {
+        $scope.process.notes.push($scope.new_note);
+        $scope.new_note = "";
+    }
+    $scope.add_error_msg = function () {
+        $scope.process.runs.push({'started': $scope.start_run ,'stopped': $scope.stop_run, 'error_messages': $scope.new_err_msg});
+        $scope.new_err_msg = "";
+        $scope.start_run = "";
+        $scope.stop_run = "";
+    }
+
+    $scope.add_citations = function(){
+
+    }
+
+    $scope.save_process = function(){
+        console.dir($scope.process)
+        Stater.newId("setting_provenance1", "Creating provenance1", "upload1", function (status, state) {
+            if (status) {
+                $scope.state = state;
+                $scope.state.attributes.process = {};
+                $scope.state.attributes.process = $scope.process;
+                Stater.persist($scope.state);
+            }
+        });
+    $scope.is_saved = true;
+    }
+
+}
+
+
