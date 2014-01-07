1d5d4ba31b458c70b08750e5ad591da85f662a5f
stamma <sravyatamma@Sravyas-MacBook-Air.local>
2014-01-07 16:59:33 -0500

    provenance flow bug fixed and tried graph layout raphael

--- website/app/js/config/config.js
+++ website/app/js/config/config.js
@@ -4,9 +4,15 @@ function doConfig() {
          * test.materialscommons.org doesn't conform to the naming convention so
          * explicitly check for it and set the api path appropriately.
          */
-        mcglobals.apihost = "https://apitest.materialscommons.org";
+         mcglobals.apihost = "https://apitest.materialscommons.org";
     } else {
-        mcglobals.apihost = window.location.protocol + "//api." + window.location.hostname;
+        console.log('in else part '+ window.location.hostname)
+        mcglobals.apihost = 'http://localhost:5000';
+        //mcglobals.apikey = 'a74c165e52ba11e3bccaac162d80f1bf';
+
+        //mcglobals.apihost = window.location.protocol + "//api." + window.location.hostname;
+
     }
 }
+
 
