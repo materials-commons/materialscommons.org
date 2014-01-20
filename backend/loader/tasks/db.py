2dbaff7d694d1c2b47a7a797c7282c4e2dfe1460
tammasr <tammasr@umich.edu>
2014-01-20 09:52:33 -0500

    current sprint updated

--- backend/loader/tasks/db.py
+++ backend/loader/tasks/db.py
@@ -73,7 +73,7 @@ def load_data_dir(user, directory, state_id):
 def load_data_dir_1(user, state_id):
     state_saver = StateCreateSaver()
     try:
-        r.connect('localhost', 28015, db='materialscommons').repl()
+        r.connect('localhost', 30815, db='materialscommons').repl()
         load_data_1(user, state_id, state_saver)
     except mcexceptions.RequiredAttributeException as rae:
         traceback.print_exc()
