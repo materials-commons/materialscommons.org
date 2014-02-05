a705e6dd6c197cea95f81646b721f0f09e9425c0
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-02-05 11:38:44 -0500

    Add support to show images.

--- website/app/js/controllers/mytools/DataControllers.js
+++ website/app/js/controllers/mytools/DataControllers.js
@@ -1,12 +1,16 @@
-function DataEditController($scope, $window, mcapi, alertService, $stateParams, pubsub) {
+function DataEditController($scope, $window, mcapi, alertService, $stateParams, pubsub, User) {
     $scope.count = 0;
     $scope.grid_options = [];
     $scope.id = $stateParams.id;
 
     $scope.setupAccessToUserFile = function () {
-        $scope.fileType = determineFileType($scope.doc.mediatype);
-        $scope.fileSrc = filePath($scope.fileType, $scope.doc.mediatype, $scope.doc.location, $scope.doc.name);
-        $scope.originalFileSrc = originalFilePath($scope.doc.location, $scope.doc.name);
+        if (isImage($scope.doc.name)) {
+            $scope.fileType = "image";
+        } else {
+            $scope.fileType = "other";
+        }
+        $scope.fileSrc = "datafiles/static/" + $scope.doc.id+"?apikey=" + User.apikey();
+        $scope.originalFileSrc = "datafiles/static/" + $scope.doc.id+"?apikey=" + User.apikey();
         $scope.fileName = $scope.doc.name;
     }
 
