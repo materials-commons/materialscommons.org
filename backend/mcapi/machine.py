11578126aba82fe86295feff54c6ae3c72044102
tammasr <tammasr@umich.edu>
2014-02-17 14:13:10 -0500

    Added machine to new layout

--- backend/mcapi/machine.py
+++ backend/mcapi/machine.py
@@ -7,6 +7,7 @@ import dmutil
 import json
 import args
 
+
 @app.route('/machines', methods=['GET'])
 @jsonp
 def get_all_machines():
@@ -19,30 +20,17 @@ def get_all_machines():
 def get_machine(machine_id):
     return dmutil.get_single_from_table('machines', machine_id)
 
+
 @app.route('/machines/new', methods=['POST'])
 @apikey
 @crossdomain(origin='*')
 def create_machine():
     j = request.get_json()
     machine = dict()
-    machine['name'] = dmutil.get_required('name', j)
-    machine['longname'] = dmutil.get_optional('longname', j)
+    machine['model'] = dmutil.get_required('model', j)
     machine['birthtime'] = r.now()
-    machine['notes'] = dmutil.get_optional('notes', j, [])
-    #contact_id = dmutil.get_required('contact', j)
-    #if not dmutil.entry_exists('contacts', contact_id):
-        #return error.bad_request("You must specify the contacts")
-    #machine['contact'] = contact_id
-    machine['contact_email'] = dmutil.get_optional('contact_email', j)
-    machine['model'] = dmutil.get_optional('model', j)
     return dmutil.insert_entry('machines', machine)
 
-
-@app.route('/add_two_numbers', methods=['GET'])
-def add_numbers():
-    x = request.args.get('a', 0, type=int)
-    y = request.args.get('b', 0, type=int)
-    return jsonify(result=x+y)
 
 @app.route('/materials', methods=['GET'])
 @apikey(shared=True)
