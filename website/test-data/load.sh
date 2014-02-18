2f635f19a71ee0fe419d937fee83977421fc9ff7
V. Glenn Tarcea <gtarcea@umich.edu>
2014-02-18 15:13:43 -0500

    Remove old unused files and directories.

--- website/test-data/load.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rethinkdb import --file news.json --table materialscommons.news --force
-#rethinkdb import --file reviews.json --table materialscommons.reviews --force
-#rethinkdb import --file udqueue.json --table materialscommons.udqueue --force
