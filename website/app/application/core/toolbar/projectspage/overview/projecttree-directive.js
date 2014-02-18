2e0dc4af96c72c839fec2f15f882877c101f1088
tammasr <tammasr@umich.edu>
2014-02-18 13:54:08 -0500

    directive redone

---
+++ website/app/application/core/toolbar/projectspage/overview/projecttree-directive.js
@@ -0,0 +1,79 @@
+Application.Controllers.controller('ProjectTreeController',
+    ["$scope", "mcapi", "Projects", "pubsub", function ($scope, mcapi, Projects, pubsub) {
+        $scope.model = Projects.model;
+
+        $scope.openFolder = function (item) {
+            var e = _.find($scope.trail, function (trailItem) {
+                return trailItem.id === item.id;
+            });
+            if ( typeof e === 'undefined') {
+                // first level is 0 so we need to add 1 to our test
+                if (item.level + 1 <= $scope.trail.length) {
+                    // Remove everything at this level and above
+                    $scope.trail = $scope.trail.splice(0, item.level);
+                }
+                $scope.trail.push(item);
+            }
+            $scope.dir = item.children;
+        };
+
+        $scope.backToFolder = function (item) {
+            $scope.dir = item.children;
+        };
+
+        $scope.selectProject = function (projectId) {
+            $scope.trail = [];
+            $scope.projectId = projectId;
+            $scope.tree_data = [];
+            $scope.loaded = false;
+            if (!(projectId in $scope.model.projects)) {
+                mcapi('/projects/%/tree2', projectId)
+                    .success(function (data) {
+                        if (data[0]) {
+                            $scope.tree_data = data;
+                            $scope.dir = $scope.tree_data[0].children;
+                            var obj = {};
+                            obj.dir = $scope.dir;
+                            $scope.model.projects[projectId] = obj;
+                            $scope.loaded = true;
+                            $scope.trail.push(data[0]);
+                        }
+                    }).jsonp();
+            } else {
+                $scope.loaded = true;
+                $scope.dir = $scope.model.projects[projectId].dir;
+            }
+        };
+
+        $scope.fileSelected = function (entry) {
+            entry.selected = !entry.selected;
+            var channel = Projects.channel;
+            if (channel != null) {
+                pubsub.send(channel, entry);
+            }
+
+        };
+
+        $scope.selectProject($scope.project);
+    }]);
+
+Application.Directives.directive('projectTree',
+    function () {
+        return {
+            restrict: "E",
+            controller: "ProjectTreeController",
+            transclude: false,
+            link: function ($scope, $element, $attrs) {
+                //$scope.project = $attrs.project;
+                //console.log("link project = " + $scope.project);
+            },
+            replace: true,
+            scope: {
+                ngModel: "@",
+                project: "@project"
+            },
+            templateUrl: "../templates/projectTree/projectTree.html"
+        }
+    });
+
+
---
+++ website/app/application/directives/dfproperties-directive.js
@@ -0,0 +1,23 @@
+Application.Directives.directive('dfProperties',
+    function () {
+        return {
+            restrict: "A",
+            scope: {
+                defaultProperties: '='
+            },
+            template: "<span ng-repeat='df in defaultProperties'>" +
+                "<div class='control-group'>" +
+                "<label class='control-label'>{{df.name}}:</label>" +
+                "<div class='controls'>" +
+                "<p ng-if='df.value_choice.length!=0'>" +
+                "<select ng-model='df.value'>" +
+                "<option ng-repeat='each in df.value_choice' value='{{each}}'> {{each}} </option></select>" +
+                "</p>" +
+                "<p ng-if='df.value_choice.length==0'>" +
+                "<input ng-model='df.value' placeholder='{{df.name}} ...' type='text'>" +
+                "</p>" +
+                "</div>" +
+                "</div>" +
+                "</span>"
+        };
+    });
