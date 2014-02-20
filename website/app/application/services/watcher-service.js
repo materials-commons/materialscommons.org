b708d8e012af1eda5c9eaf64baca1ee1f94c0ab1
tammasr <tammasr@umich.edu>
2014-02-20 12:51:38 -0500

    added file

---
+++ website/app/application/services/watcher-service.js
@@ -0,0 +1,19 @@
+Application.Services.factory('watcher',
+    function () {
+        var watcherService = {};
+
+        watcherService.watch = function (scope, variable, fn) {
+            scope.$watch(variable, function (newval, oldval) {
+                if (!newval && !oldval) {
+                    return;
+                }
+                else if (newval == "" && oldval) {
+                    fn(oldval);
+                } else {
+                    fn(newval);
+                }
+            });
+        }
+
+        return watcherService;
+    });
