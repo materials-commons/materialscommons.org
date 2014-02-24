c2181142cbad9fb58dc21931ea6493eebed84717
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-02-24 13:50:16 -0500

    Updates for project overview.

--- website/app/application/core/toolbar/overview/overview-controller.js
+++ website/app/application/core/toolbar/overview/overview-controller.js
@@ -2,7 +2,38 @@ Application.Controllers.controller('toolbarOverview',
     ["$scope",
         function ($scope) {
             $scope.init = function () {
-
+                $scope.projects = [
+                    {
+                        name: "Test_Proj",
+                        id: "c33edab7-a65f-478e-9fa6-9013271c73ea",
+                        directories: 30,
+                        files: 100,
+                        birthtime: "11/12/2013",
+                        description: "An interesting project",
+                        size: "10Gb",
+                        drafts: 5,
+                        reviews: 0,
+                        todos: [
+                            {name: "a", description: "Do something"}
+                        ],
+                        provenance: 3
+                    },
+                    {
+                        name: "WE43 Heat Treatments",
+                        id: "904886a7-ea57-4de7-8125-6e18c9736fd0",
+                        directories: 30,
+                        files: 100,
+                        size: "10Gb",
+                        birthtime: "11/12/2013",
+                        description: "An interesting project",
+                        drafts: 5,
+                        reviews: 0,
+                        todos: [
+                            {name: "a", description: "Do something"}
+                        ],
+                        provenance: 3
+                    }
+                ];
             };
 
             $scope.init();
