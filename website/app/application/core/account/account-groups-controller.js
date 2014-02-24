ee6511d8aa6507e67d667ae0e9c2af3d2a982ce2
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-02-23 21:37:56 -0500

    Update buttons. When creating a group have it show up in the table.

--- website/app/application/core/account/account-groups-controller.js
+++ website/app/application/core/account/account-groups-controller.js
@@ -1,12 +1,25 @@
 Application.Controllers.controller('accountGroups',
-    ["$scope", "mcapi", "User", "$state", function ($scope, mcapi, User, $state) {
-        mcapi('/user/%/usergroups', User.u())
-            .success(function (data) {
-                $scope.user_groups = data;
-            }).jsonp();
+    ["$scope", "mcapi", "User", "$state", "pubsub",
+        function ($scope, mcapi, User, $state, pubsub) {
 
+            pubsub.waitOn($scope, 'usergroups.change', function () {
+                $scope.getGroups();
+            });
 
-        $scope.createGroup = function () {
-            $state.go("account.groupcreate");
-        };
-    }]);
+            $scope.getGroups = function () {
+                mcapi('/user/%/usergroups', User.u())
+                    .success(function (data) {
+                        $scope.user_groups = data;
+                    }).jsonp();
+            };
+
+            $scope.createGroup = function () {
+                $state.go("account.groupcreate");
+            };
+
+            $scope.init = function () {
+                $scope.getGroups();
+            };
+
+            $scope.init();
+        }]);
