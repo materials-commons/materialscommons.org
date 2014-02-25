847bb7871dabcc8d45c19f6d8e8bbcc036aec4db
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-02-25 07:06:38 -0500

    Define tagsCount()

--- website/app/application/core/toolbar/_tags-controller.js
+++ website/app/application/core/toolbar/_tags-controller.js
@@ -1,14 +1,18 @@
 Application.Controllers.controller('_toolbarTags',
     ["$scope", "mcapi", "pubsub", function ($scope, mcapi, pubsub) {
+        $scope.tagsCount = function () {
+            mcapi('/tags/count')
+                .success(function (data) {
+                    $scope.tags = data;
+                }).jsonp();
+        };
+
         pubsub.waitOn($scope, 'tags.change', function () {
             $scope.tagsCount();
         });
 
         $scope.init = function () {
-            mcapi('/tags/count')
-                .success(function (data) {
-                    $scope.tags = data;
-                }).jsonp();
+            $scope.tagsCount();
         };
 
         $scope.init();
