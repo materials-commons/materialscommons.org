8207ab0d6b9c45d56928b29d1158b52def6c3b7b
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-03 13:54:19 -0500

    Clean up comments and extra spaces.

--- website/app/index/config.js
+++ website/app/index/config.js
@@ -6,17 +6,11 @@ function doConfig() {
          */
          mcglobals.apihost = "https://apitest.materialscommons.org";
     } else if (window.location.hostname === 'materialscommons.org') {
-        //console.log('in else part '+ window.location.hostname)
-        //mcglobals.apihost = 'http://localhost:5000';
-        //mcglobals.apikey = 'a74c165e52ba11e3bccaac162d80f1bf';
-
         mcglobals.apihost = window.location.protocol + "//api." + window.location.hostname;
-
     } else if (window.location.hostname == 'localhost') {
         mcglobals.apihost = 'http://localhost:5002';
     } else {
         mcglobals.apihost = window.location.protocol + "//api." + window.location.hostname;
-
     }
 }
 
