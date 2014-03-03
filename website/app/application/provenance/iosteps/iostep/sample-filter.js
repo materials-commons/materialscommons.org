23ceefc9aab9203a29150b5bf8c292e52a015027
tammasr <tammasr@umich.edu>
2014-03-03 13:38:20 -0500

    added back functionality

--- website/app/application/provenance/iosteps/iostep/sample-filter.js
+++ website/app/application/provenance/iosteps/iostep/sample-filter.js
@@ -1,7 +1,7 @@
 Application.Filters.filter('samplefilter', function () {
 
     return function (samples, doc) {
-        if (samples) {
+        if (samples && doc.material) {
             var i, return_samples = [];
             for (i = 0; i < samples.length; i++) {
                 if (samples[i].material.name === doc.material.name) {
