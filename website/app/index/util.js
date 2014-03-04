b62a2aa2a422a4729fa0b9198a37bd6ebb9ba070
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-04 14:59:42 -0500

    Remove unused functions.

--- website/app/index/util.js
+++ website/app/index/util.js
@@ -1,21 +1,9 @@
-function setActiveMainNav(nav) {
-    $('#mc-main-navs li').removeClass("active");
-    $(nav).addClass("active");
-}
-
-function determineFileType(mediaType) {
-    if (mediaType.indexOf('image') != -1) {
-        return "image";
-    } else {
-        return "other";
-    }
-}
-
 var recognized_images = ["jpg", "png", "jpeg", "gif"];
 
 function isImage(name) {
+    var i;
     var s = name.toLowerCase();
-    for (var i = 0; i < recognized_images.length; i++) {
+    for (i = 0; i < recognized_images.length; i++) {
         if (endsWith(s, recognized_images[i])) {
             return true;
         }
@@ -32,28 +20,10 @@ function _add_json_callback(url) {
     return url + "&callback=JSON_CALLBACK";
 }
 
-function filePath(fileType, mediaType, location, name) {
-    var filePath = "assets/materialscommons/";
-    if (fileType == "image" && mediaType == "image/tiff") {
-        var newName = name.substr(0, name.lastIndexOf('.')) || name;
-        newName = newName + '.jpg';
-        return filePath + location + '/.conversion/' + newName;
-    } else if (fileType == "image") {
-        return filePath + location + '/' + name;
-    } else {
-        return filePath + location + '/' + name;
-    }
-
-    return filePath;
-}
-
-function originalFilePath(location, name) {
-    var filePath = "assets/materialscommons/";
-    return filePath + location + '/' + name;
-}
-
-String.prototype.capitalize = function() {
-    return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+String.prototype.capitalize = function () {
+    return this.replace(/(?:^|\s)\S/g, function (a) {
+        return a.toUpperCase();
+    });
 };
 
 // save a reference to the core implementation
@@ -64,7 +34,7 @@ var indexOfValue = _.indexOf;
 _.mixin({
 
     // return the index of the first array element passing a test
-    indexOf: function(array, test) {
+    indexOf: function (array, test) {
         // delegate to standard indexOf if the test isn't a function
         if (!_.isFunction(test)) return indexOfValue(array, test);
         // otherwise, look for the index
