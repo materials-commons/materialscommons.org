5c079f27c33316d5e6338fc5d8214ee96c2a13b3
tammasr <tammasr@umich.edu>
2014-03-07 17:26:01 -0500

    Clone prov half way

--- website/app/application/core/toolbar/drafts/drafts-controller.js
+++ website/app/application/core/toolbar/drafts/drafts-controller.js
@@ -21,9 +21,38 @@ Application.Controllers.controller('toolbarDrafts',
                 $state.go('toolbar.drafts');
             };
 
-            $scope.init = function () {
-                $scope.drafts = ProvDrafts.drafts;
+            $scope.cloneProvenance = function (draft) {
+                var new_draft = {};
+                if (draft.clone_number) {
+                    new_draft = ProvDrafts.prepareClone(draft, draft.clone_number);
+                    console.log(new_draft);
+                } else {
+                    new_draft = ProvDrafts.prepareClone(draft, '');
+                    console.log(new_draft);
+
+                }
+
             };
 
+            $scope.prepareClone = function (old_df, clone_num) {
+
+                if (old_df.clone_number) {
+                    var make_name = '', existing_clone_list = [], split_item = [];
+                    $scope.drafts.forEach(function (df) {
+                        existing_clone_list.push(df.clone_number);
+                    });
+                    split_item = old_df.clone_number.split('-');
+                    make_name = split_item[0] + (split_item[1] + 1);
+                    if (existing_clone_list.indexOf(make_name) === -1) {
+                        ProvDrafts.clone_number = make_name;
+                        //modify n save draft
+                    }
+                }
+            };
+
+            $scope.init = function () {
+                $scope.drafts = ProvDrafts.drafts;
+
+            };
             $scope.init();
         }]);
--- website/app/application/services/provdrafts-service.js
+++ website/app/application/services/provdrafts-service.js
@@ -88,6 +88,71 @@ Application.Provenance.Services.factory('ProvDrafts', ["mcapi", "pubsub",
                     }).jsonp();
             },
 
+            prepareClone: function (df, clone_num) {
+                var new_draft = {}, clones_numbers = [];
+                if (clone_num === '') {
+                    console.log('first one ');
+                    new_draft = df;
+                    new_draft.id = '';
+                    new_draft.clone_number = df.attributes.process.name + '-' + 1;
+                    new_draft.attributes.process.name = new_draft.clone_number;
+                    console.log(new_draft.attributes.input_conditions)
+                    if (new_draft.attributes.input_conditions) {
+                        for(var i = 0; i< new_draft.attributes.input_conditions.length; i++){
+                            console.log(new_draft.attributes.input_conditions[i]);
+                        }
+//                        new_draft.attributes.input_conditions.forEach(function (cond) {
+//                            new_draft.attributes.input_conditions[cond].default[0].value = df.attributes.input_conditions[cond].default[0].value + '-' + 1;
+//                        });
+                    }
+                    if (new_draft.attributes.output_conditions) {
+//                        new_draft.attributes.output_conditions.forEach(function (cond) {
+//                            new_draft.attributes.output_conditions[cond].default[0].value = df.attributes.output_conditions[cond].default[0].value + '-' + 1;
+//                        });
+                    }
+                    new_draft.attributes.input_files = [];
+                    new_draft.attributes.output_files = [];
+                    return new_draft;
+
+                } else {
+                    console.log('second one');
+                    clones_numbers = ProvDrafts.get_existing_clones();
+                    var make_name = '', i = 0, split_item = [];
+                    while (i < 5) {
+                        split_item = df.clone_number;
+                        make_name = split_item[0] + (split_item[1] + 1);
+                        if (clones_numbers.indexOf(make_name) === -1) {
+                            new_draft = df;
+                            new_draft.id = '';
+                            new_draft.clone_number = make_name;
+                            new_draft.attributes.process.name = new_draft.clone_number;
+                            if (new_draft.attributes.input_conditions) {
+                                new_draft.attributes.input_conditions.forEach(function (cond) {
+                                    new_draft.attributes.input_conditions[cond].default[0].value = df.attributes.input_conditions[cond].default[0].value + '-' + (split_item[1] + 1);
+                                });
+                            }
+                            if (new_draft.attributes.output_conditions) {
+                                new_draft.attributes.output_conditions.forEach(function (cond) {
+                                    new_draft.attributes.output_conditions[cond].default[0].value = df.attributes.output_conditions[cond].default[0].value + '-' + (split_item[1] + 1);
+                                });
+                            }
+                            new_draft.attributes.input_files = [];
+                            new_draft.attributes.output_files = [];
+                            return new_draft;
+                        }
+                    };
+
+                }
+            },
+
+            get_existing_clones: function () {
+                var clones = [];
+                service.drafts.forEach(function (item) {
+                    clones.push(item.clone_number);
+                });
+                return clones;
+            },
+
             deleteDraft: function (draft_id) {
                 var i = _.indexOf(service.drafts, function (item) {
                     if (item.id === draft_id) {
