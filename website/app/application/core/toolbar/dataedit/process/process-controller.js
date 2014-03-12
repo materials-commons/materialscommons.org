5203a0103a24ea006e66bad2061365de81378879
tammasr <tammasr@umich.edu>
2014-03-12 13:32:58 -0400

    working on process page

--- website/app/application/core/toolbar/dataedit/process/process-controller.js
+++ website/app/application/core/toolbar/dataedit/process/process-controller.js
@@ -1,13 +1,16 @@
-Application.Controllers.controller('toolbarDataEditDisplayProcess',
+Application.Controllers.controller('toolbarDataEditProcess',
     ["$scope", "mcapi", "User", "$stateParams", "alertService", "watcher",
         function ($scope, mcapi, User, $stateParams, alertService, watcher) {
-
             $scope.showProcessDetails = function (process_id) {
                 mcapi('/processes/%', process_id)
                     .success(function (data) {
                         $scope.process = data;
                     }).jsonp();
+                console.log($scope.process);
+                $state.go('toolbar.dataedit.process');
+
             };
+
 
             $scope.init = function () {
 
