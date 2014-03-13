510fa19c8598353aac46108aaa49129710154118
V. Glenn Tarcea <gtarcea@umich.edu>
2014-03-13 10:13:47 -0400

    Remove commented out code.

--- website/app/application/provenance/iosteps/iostep/iostep-controller.js
+++ website/app/application/provenance/iosteps/iostep/iostep-controller.js
@@ -27,34 +27,6 @@ Application.Provenance.Controllers.controller('provenanceIOStepsIOStep',
                 $scope.doc.model.added_properties.push({'name': $scope.customPropertyName, 'value': $scope.customPropertyValue, 'unit': '', 'value_choice': [], 'unit_choice': []});
             };
 
-//            $scope.loadMaterials = function () {
-//                mcapi('/materials')
-//                    .success(function (data) {
-//                        $scope.materials = data;
-//
-//                        if ($scope.doc.material) {
-//                            var i = _.indexOf($scope.materials, function (item) {
-//                                return (item.name === $scope.doc.material.name);
-//                            });
-//
-//                            if (i !== -1) {
-//                                $scope.doc.material = $scope.materials[i];
-//                                if ($scope.doc.sample) {
-//                                    var j = _.indexOf($scope.samples_list, function (item) {
-//                                        return (item.id === $scope.doc.sample.id);
-//                                    });
-//                                    if (j !== -1) {
-//                                        $scope.doc.sample = $scope.samples_list[j];
-//                                        $scope.doc.model = $scope.doc.sample.model;
-//                                        $scope.doc.owner = $scope.doc.sample.owner;
-//                                        $scope.doc.sample_id = $scope.doc.sample.id;
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }).jsonp();
-//            };
-
             $scope.load_all_samples = function () {
                 mcapi('/samples')
                     .success(function (data) {
