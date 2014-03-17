34762afc1e0c2dd7225e4777bda0cd2d4adb7342
tammasr <tammasr@umich.edu>
2014-03-17 17:13:10 -0400

    added asterick for required fields and working on form submit

--- website/app/application/core/toolbar/machines/machines-controller.js
+++ website/app/application/core/toolbar/machines/machines-controller.js
@@ -1,5 +1,5 @@
 Application.Controllers.controller('toolbarMachines',
-    ["$scope", "mcapi", function ($scope, mcapi) {
+    ["$scope", "mcapi", "$injector", function ($scope, mcapi, $injector) {
         $scope.machine = {
             "additional": []
         };
@@ -30,28 +30,33 @@ Application.Controllers.controller('toolbarMachines',
             };
         };
 
-        $scope.save = function () {
+        $scope.save = function (doc) {
+            var $validationProvider = $injector.get('$validation');
+            var check = $validationProvider.checkValid(doc);
+            console.log(check);
+            $validationProvider.validate($scope.default_properties);
+
             $scope.default_properties.forEach(function (item) {
                 $scope.machine[item.name] = item.value;
             });
-            mcapi('/machines/new')
-                .arg('order_by=birthtime')
-                .success(function (data) {
-                    mcapi('/machines/%', data.id)
-                        .success(function (machine_obj) {
-                            $scope.mach = machine_obj;
-                            $scope.machines_list.unshift(machine_obj);
-                        })
-                        .error(function (e) {
-
-                        }).jsonp();
-                    $scope.machine = {
-                        "additional": []
-                    };
-                })
-                .error(function (e) {
-
-                }).post($scope.machine);
+//            mcapi('/machines/new')
+//                .arg('order_by=birthtime')
+//                .success(function (data) {
+//                    mcapi('/machines/%', data.id)
+//                        .success(function (machine_obj) {
+//                            $scope.mach = machine_obj;
+//                            $scope.machines_list.unshift(machine_obj);
+//                        })
+//                        .error(function (e) {
+//
+//                        }).jsonp();
+//                    $scope.machine = {
+//                        "additional": []
+//                    };
+//                })
+//                .error(function (e) {
+//
+//                }).post($scope.machine);
         };
 
         $scope.add_property_to_machine = function () {
