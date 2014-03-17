09fe192f959cfc0c4c06b3bad4b2344c85a5b1b3
tammasr <tammasr@umich.edu>
2014-03-17 13:01:26 -0400

    templates

--- website/app/application/provenance/iosteps/iostep/iostep-controller.js
+++ website/app/application/provenance/iosteps/iostep/iostep-controller.js
@@ -7,7 +7,8 @@ Application.Provenance.Controllers.controller('provenanceIOStepsIOStep',
             };
 
             $scope.addCustomProperty = function () {
-                $scope.doc.model.added_properties.push({'name': $scope.customPropertyName, 'value': $scope.customPropertyValue, 'unit': '', 'value_choice': [], 'unit_choice': []});
+                $scope.doc.model.added_properties.push({'name': $scope.customPropertyName, 'value': $scope.customPropertyValue, "type": "text", 'unit': '', 'value_choice': [], 'unit_choice': []});
+                console.log($scope.doc.model.added_properties);
             };
 
             $scope.loadMaterials = function () {
