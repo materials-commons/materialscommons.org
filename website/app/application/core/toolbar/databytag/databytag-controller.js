91f12207d375ca612dab8308259d8a12f78d4d21
tammasr <tammasr@umich.edu>
2014-03-21 10:54:42 -0400

    test

--- website/app/application/core/toolbar/databytag/databytag-controller.js
+++
@@ -1,9 +0,0 @@
-Application.Controllers.controller('toolbarDataByTag',
-    ["$scope", "$stateParams", "mcapi", function ($scope, $stateParams, mcapi) {
-        $scope.tag = $stateParams.name;
-        mcapi('/datafiles/tag/%', $scope.tag)
-            .success(function (data) {
-                $scope.docs = data;
-            }).jsonp();
-
-    }]);
