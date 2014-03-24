7636fb710a88a2793a7866738513d7c971297f79
tammasr <tammasr@umich.edu>
2014-03-24 17:02:01 -0400

    added files

---
+++ website/app/application/provenance/iosteps/iostep/clone-service.js
@@ -0,0 +1,15 @@
+Application.Services.factory('Clone',
+    [function () {
+        return {
+            get_clone: function (doc, draft) {
+                var input_sample_copy =  angular.copy(draft.attributes.input_conditions['Pick Sample']);
+                doc.sample.id = '';
+                doc.sample.model =  input_sample_copy.sample.model;
+                doc.sample.owner =  input_sample_copy.sample.owner;
+                doc.model = input_sample_copy.sample.model;
+                doc.model.default[0].value =  doc.model.default[0].value + ' : ' +draft.attributes.process.name;
+                return doc;
+
+            }
+        };
+    }]);
