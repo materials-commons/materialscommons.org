43f4c388b8f5cf7811ce7b81b4ed19d55cc6b073
tammasr <tammasr@umich.edu>
2014-03-24 11:24:59 -0400

    commiting changes to samples

--- website/app/application/provenance/iosteps/iostep/iostep-controller.js
+++ website/app/application/provenance/iosteps/iostep/iostep-controller.js
@@ -1,33 +1,50 @@
 Application.Provenance.Controllers.controller('provenanceIOStepsIOStep',
     ["$scope", "ProvDrafts", "$stateParams", "mcapi",
         function ($scope, ProvDrafts, $stateParams, mcapi) {
+            $scope.model = {
+                additionalProperty: {},
+                pick_sample: {}
+            };
+
+            $scope.pick_sample = function () {
+                var i = _.indexOf($scope.samples_list, function (item) {
+                    return (item.id === $scope.doc.sample.id);
+                });
+
+                if (i !== -1) {
+                    $scope.doc.sample = $scope.samples_list[i];
+                    $scope.doc.model = $scope.doc.sample.model;
+                    $scope.doc.owner = $scope.doc.sample.owner;
+                    $scope.doc.sample_id = $scope.doc.sample.id;
+                }
+            };
 
             $scope.addAdditionalProperty = function () {
-                $scope.doc.model.added_properties.push(JSON.parse($scope.additionalProperty));
+                $scope.doc.model.added_properties.push(JSON.parse($scope.model.additionalProperty));
             };
 
             $scope.addCustomProperty = function () {
                 $scope.doc.model.added_properties.push({'name': $scope.customPropertyName, 'value': $scope.customPropertyValue, "type": "text", 'unit': '', 'value_choice': [], 'unit_choice': []});
             };
 
-            $scope.loadMaterials = function () {
-                mcapi('/materials')
+            $scope.load_all_samples = function () {
+                mcapi('/samples')
                     .success(function (data) {
-                        $scope.materials = data;
-
-                        if ($scope.doc.material) {
-                            var i = _.indexOf($scope.materials, function (item) {
-                                return (item.name === $scope.doc.material.name);
+                        $scope.samples_list = data;
+                        if ($scope.doc.sample) {
+                            var i = _.indexOf($scope.samples_list, function (item) {
+                                return (item.id === $scope.doc.sample.id);
                             });
 
                             if (i !== -1) {
-                                $scope.doc.material = $scope.materials[i];
+                                $scope.doc.sample = $scope.samples_list[i];
                             }
                         }
                     }).jsonp();
+
             };
 
-            $scope.init = function () {
+            function init() {
                 $scope.stepName = $stateParams.iostep;
                 if ($stateParams.iosteps === 'inputs') {
                     $scope.doc = ProvDrafts.current.attributes.input_conditions[$scope.stepName];
@@ -35,15 +52,13 @@ Application.Provenance.Controllers.controller('provenanceIOStepsIOStep',
                     $scope.doc = ProvDrafts.current.attributes.output_conditions[$scope.stepName];
                 }
 
-                if ($scope.doc.template_pick === 'material') {
-                    $scope.loadMaterials();
+                if ($scope.doc.template_pick === 'sample') {
+                    $scope.load_all_samples();
                 }
-
-                $scope.defaultProperties = $scope.doc.model.default;
-                $scope.additionalProperties = [];
-                $scope.useExisting = "yes";
-
-            };
-
-            $scope.init();
+//
+//                $scope.defaultProperties = $scope.doc.model.default;
+//                $scope.additionalProperties = [];
+//                $scope.useExisting = "yes";
+            }
+            init();
         }]);
