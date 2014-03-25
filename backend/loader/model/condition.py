2e0769471e87aab8dbee5f9914416f7a2f67566a
tammasr <tammasr@umich.edu>
2014-03-25 14:29:21 -0400

    co

--- backend/loader/model/condition.py
+++ backend/loader/model/condition.py
@@ -2,7 +2,7 @@ import rethinkdb as r
 
 class Condition(object):
     def __init__(self):
-        self.model = {}
+        self.model = dict()
         self.owner = ''
         self.birthtime = r.now()
         self.sample_id = ''
