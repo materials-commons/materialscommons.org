c8c523521bb54eb0071f9162b261eb5b24dcadb7
tammasr <tammasr@umich.edu>
2014-04-04 17:11:38 -0400

    templates

--- website/app/application/provenance/iosteps/iostep/clone-service.js
+++ website/app/application/provenance/iosteps/iostep/clone-service.js
@@ -4,11 +4,13 @@ Application.Services.factory('Clone',
             get_clone: function (doc, draft) {
                 var input_sample_copy =  angular.copy(draft.attributes.input_conditions['Pick Sample']);
                 doc.sample = {};
-                doc.sample.id = '';
+//                doc = input_sample_copy.sample;
+//                doc.
+//                doc.sample.id = '';
                 doc.sample.model =  input_sample_copy.sample.model;
                 doc.sample.owner =  input_sample_copy.sample.owner;
                 doc.model = input_sample_copy.sample.model;
-                doc.model.default[0].value =  doc.model.default[0].value + ' : ' +draft.attributes.process.name;
+                doc.model.name =  doc.model.name + ' : ' + draft.attributes.process.name;
                 return doc;
 
             }
