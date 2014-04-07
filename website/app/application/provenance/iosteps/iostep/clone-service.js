a22525d9e7d697be9f93db0ba40e7162497e666a
tammasr <tammasr@umich.edu>
2014-04-07 17:10:23 -0400

    transform provenance and add templates accordingly

--- website/app/application/provenance/iosteps/iostep/clone-service.js
+++ website/app/application/provenance/iosteps/iostep/clone-service.js
@@ -4,13 +4,7 @@ Application.Services.factory('Clone',
             get_clone: function (doc, draft) {
                 var input_sample_copy =  angular.copy(draft.attributes.input_conditions['Pick Sample']);
                 doc.sample = {};
-//                doc = input_sample_copy.sample;
-//                doc.
-//                doc.sample.id = '';
-                doc.sample.model =  input_sample_copy.sample.model;
-                doc.sample.owner =  input_sample_copy.sample.owner;
-                doc.model = input_sample_copy.sample.model;
-                doc.model.name =  doc.model.name + ' : ' + draft.attributes.process.name;
+                doc.sample = input_sample_copy.sample;
                 return doc;
             }
         };
