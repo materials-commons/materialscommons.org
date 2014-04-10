e16cffc3f5c16ea647b40dba5f6320716ae5d4fa
tammasr <tammasr@umich.edu>
2014-04-10 16:25:49 -0400

    changed all templates to reflect the correct type

--- website/app/application/provenance/process/process-controller.js
+++ website/app/application/provenance/process/process-controller.js
@@ -28,21 +28,6 @@ Application.Provenance.Controllers.controller('provenanceProcess',
                 $scope.process.added_properties.push({'name': $scope.customPropertyName, 'value': $scope.customPropertyValue, "type": "text", 'unit': '', 'value_choice': [], 'unit_choice': [], 'required': false});
             };
 
-            $scope.add_notes = function () {
-                $scope.process.notes.push({'message': $scope.bk.new_note, 'who': User.u(), 'date': dateGenerate.new_date()});
-                $scope.bk.new_note = "";
-            };
-
-            $scope.add_run = function () {
-                if ($scope.process.template.template_pick === 'experiment') {
-                    $scope.process.runs.push({'started': $scope.bk.exp_run_date, 'stopped': '', 'error_messages': ''});
-                } else {
-                    $scope.process.runs.push({'started': $scope.bk.start_run, 'stopped': $scope.bk.stop_run, 'error_messages': $scope.bk.new_err_msg});
-                    $scope.bk.new_err_msg = "";
-                    $scope.bk.start_run = "";
-                    $scope.bk.stop_run = "";
-                }
-            };
 
             $scope.remove_run = function (index) {
                 $scope.process.runs.splice(index, 1);
