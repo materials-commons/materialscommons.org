f3a514f561e1e156b6c73d97c0a025e47e0cda1f
tammasr <tammasr@umich.edu>
2014-04-16 15:27:00 -0400

    functionality to display inputs and outputs in the process page

--- website/app/application/core/toolbar/process/provenance/provenance-controller.js
+++ website/app/application/core/toolbar/process/provenance/provenance-controller.js
@@ -1,6 +1,27 @@
 Application.Controllers.controller('toolbarProcessProvenance',
-    ["$scope", "mcapi", "User", "$state", "$stateParams",
-        function ($scope, mcapi, User, $state, $stateParams) {
+    ["$scope", "mcapi", "User", "$state", "$stateParams", "$filter",
+        function ($scope, mcapi, User, $state, $stateParams, $filter) {
+
+            $scope.showtab = function (tab, type) {
+                switch (type) {
+                    case "id":
+                        mcapi('/samples/%', tab.properties.id.value)
+                            .success(function (data) {
+                                $scope.item = data;
+                            }).jsonp();
+                        break;
+                    case "condition":
+                        $scope.item = tab;
+                        break;
+                    case "file":
+                        mcapi('/datafile/%', tab.properties.id.value)
+                            .success(function (data) {
+                                $scope.item = data;
+                            }).jsonp();
+                        break;
+                }
+            };
+
 
             $scope.init = function () {
                 $scope.id = $stateParams.id;
@@ -8,11 +29,18 @@ Application.Controllers.controller('toolbarProcessProvenance',
                 $scope.input_properties = [];
                 $scope.input_files = [];
                 $scope.output_files = [];
+
+
                 mcapi('/processes/%', $scope.id)
                     .success(function (data) {
                         $scope.process = data;
-                        $scope.input_files = $scope.process.inputs
+                        $scope.input_files = $filter('processFilter')($scope.process.inputs, 'file');
+                        $scope.output_files = $filter('processFilter')($scope.process.outputs, 'file');
+                        $scope.input_conditions = $filter('processFilter')($scope.process.inputs, 'condition', 'id');
+                        $scope.output_conditions = $filter('processFilter')($scope.process.outputs, 'condition', 'id');
                     }).jsonp();
+
+
             };
             $scope.init();
         }]);
