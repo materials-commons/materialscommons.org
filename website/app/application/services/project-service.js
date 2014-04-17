f40411c5e967bf4d22269431e46a8a7c4c964da4
V. Glenn Tarcea <gtarcea@umich.edu>
2014-04-17 09:24:56 -0400

    Add comments and documentation to the service.

--- website/app/application/services/project-service.js
+++ website/app/application/services/project-service.js
@@ -1,3 +1,8 @@
+/*
+ * The Projects service acts as a cache for projects and their files. It provides routines
+ * for manipulating the selected flag in the list of files in a project, as well as channel
+ * that controllers, directives and services can set to receive events on.
+ */
 Application.Services.factory('Projects',
     [function () {
         var service = {
@@ -6,14 +11,23 @@ Application.Services.factory('Projects',
             },
             channel: null,
 
+            /*
+             * Clears the current set of projects.
+             */
             clear: function () {
                 service.model.projects = {};
             },
 
+            /*
+             * Sets the channel used to publish events to.
+             */
             setChannel: function (what) {
                 service.channel = what;
             },
 
+            /*
+             * Clears the selected flag on all files in the project.
+             */
             clearSelectedFiles: function (projectId) {
                 var treeModel = new TreeModel(),
                     root = treeModel.parse(service.model.projects[projectId].dir);
@@ -22,12 +36,17 @@ Application.Services.factory('Projects',
                 });
             },
 
-            resetSelectedFiles: function(files, projectId) {
+            /*
+             * Walks the projects file tree and sets the selected flag to true for
+             * all files found in the files list all other files selected flag
+             * is set to false.
+             */
+            resetSelectedFiles: function (files, projectId) {
                 var filesHash = {},
                     treeModel = new TreeModel(),
                     root = treeModel.parse(service.model.projects[projectId].dir);
 
-                files.forEach(function(fileEntry) {
+                files.forEach(function (fileEntry) {
                     filesHash[fileEntry.id] = fileEntry;
                 });
 
@@ -40,6 +59,9 @@ Application.Services.factory('Projects',
                 });
             },
 
+            /*
+             * Sets the selected flag to true for all files listed in files.
+             */
             setFilesSelected: function (files, projectId) {
                 if (files.length == 0) {
                     return;
@@ -49,7 +71,7 @@ Application.Services.factory('Projects',
                     treeModel = new TreeModel(),
                     root = treeModel.parse(service.model.projects[projectId].dir);
 
-                files.forEach(function(fileEntry) {
+                files.forEach(function (fileEntry) {
                     filesHash[fileEntry.id] = fileEntry;
                 });
 
