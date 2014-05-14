e5c8345354de2faea355c100b854527b27437670
tammasr <tammasr@umich.edu>
2014-05-14 16:28:49 -0400

    changes to submit prov modal

--- website/app/application/provenance/finish/finish-controller.js
+++ website/app/application/provenance/finish/finish-controller.js
@@ -1,50 +1,78 @@
 Application.Provenance.Controllers.controller('provenanceFinish',
-    ["$scope", "ProvDrafts", "$dialog", "$state", "mcapi", "alertService", "$stateParams",
-        function ($scope, ProvDrafts, $dialog, $state, mcapi, alertService, $stateParams) {
+    ["$scope", "ProvDrafts", "$modal", "$state", "mcapi", "alertService", "$stateParams",
+        function ($scope, ProvDrafts, $modal, $state, mcapi, alertService, $stateParams) {
+
             $scope.saveDraft = function () {
                 ProvDrafts.saveDraft();
                 alertService.sendMessage("Your draft has been saved!");
             };
 
-            $scope.submitProvenance = function () {
-                var title = '',
-                    msg = 'Once you submit provenance you cannot change it. Do you wish to submit?',
-                    buttons = [
-                        {result: "no", label: "No"},
-                        {result: "yes", label: "Yes"}
-                    ];
-                $dialog.messageBox(title, msg, buttons)
-                    .open()
-                    .then(function (result) {
-                        if (result === "no") {
-                            return;
-                        }
-                        ProvDrafts.saveDraft(function () {
-                            mcapi('/provenance')
-                                .success(function (data) {
-                                    $scope.done = true;
-                                    $scope.process_id = data.process;
-                                    ProvDrafts.deleteDraft($scope.doc.id);
-                                    alertService.sendMessage("Your Provenance was Created Successfully.");
-                                    ProvDrafts.current = null;
-                                    $state.go("toolbar.overview");
-                                })
-                                .error(function () {
-                                    $scope.notdone = true;
-                                    alertService.sendMessage("Sorry - Your Provenance upload failed.");
-                                })
-                                .post({draft_id: $scope.doc.id});
 
-                        });
-                    });
-            };
+//            $scope.submitProvenance = function () {
+//                var title = '',
+//                    msg = 'Once you submit provenance you cannot change it. Do you wish to submit?',
+//                    buttons = [
+//                        {result: "no", label: "No"},
+//                        {result: "yes", label: "Yes"}
+//                    ];
+//                $dialog.messageBox(title, msg, buttons)
+//                    .open()
+//                    .then(function (result) {
+//                        if (result === "no") {
+//                            return;
+//                        }
+//                        ProvDrafts.saveDraft(function () {
+//                            mcapi('/provenance')
+//                                .success(function (data) {
+//                                    $scope.done = true;
+//                                    $scope.process_id = data.process;
+//                                    ProvDrafts.deleteDraft($scope.doc.id);
+//                                    alertService.sendMessage("Your Provenance was Created Successfully.");
+//                                    ProvDrafts.current = null;
+//                                    $state.go("toolbar.overview");
+//                                })
+//                                .error(function () {
+//                                    $scope.notdone = true;
+//                                    alertService.sendMessage("Sorry - Your Provenance upload failed.");
+//                                })
+//                                .post({draft_id: $scope.doc.id});
+//
+//                        });
+//                    });
+//            };
 
             $scope.deleteDraft = function () {
                 ProvDrafts.deleteRemoteDraft($scope.doc.id);
                 $state.go("toolbar.projectspage.overview", {id: $stateParams.id});
             };
 
+            $scope.ok = function () {
+                ProvDrafts.saveDraft(function () {
+                    mcapi('/provenance')
+                        .success(function (data) {
+                            $scope.done = true;
+                            $scope.process_id = data.process;
+                            ProvDrafts.deleteDraft($scope.doc.id);
+                            alertService.sendMessage("Your Provenance was Created Successfully.");
+                            ProvDrafts.current = null;
+                            $state.go("toolbar.overview");
+                        })
+                        .error(function () {
+                            $scope.notdone = true;
+                            alertService.sendMessage("Sorry - Your Provenance upload failed.");
+                        })
+                        .post({draft_id: $scope.doc.id});
+
+                });
+            };
+
+            $scope.cancel = function () {
+                return;
+            };
+
+
             $scope.init = function () {
+                console.log('here');
                 $scope.doc = ProvDrafts.current;
                 $scope.process = $scope.doc.process;
                 $scope.inputs = $scope.doc.process.input_conditions;
