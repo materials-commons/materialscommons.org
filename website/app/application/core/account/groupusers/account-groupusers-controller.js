04d0eb13d6c052ca4b8ee8541f2956c8663eef98
tammasr <tammasr@umich.edu>
2014-05-20 15:33:06 -0400

    user group form has been modified

--- website/app/application/core/account/groupusers/account-groupusers-controller.js
+++ website/app/application/core/account/groupusers/account-groupusers-controller.js
@@ -1,41 +1,36 @@
 Application.Controllers.controller('accountGroupUsers',
-    ["$scope", "mcapi", "$state", "$stateParams", "$dialog", "User", "alertService",
-        function ($scope, mcapi, $state, $stateParams, $dialog, User, alertService) {
-            mcapi('/users')
-                .success(function (data) {
-                    $scope.all_users = data;
-                })
-                .error(function () {
-                }).jsonp();
+    ["$scope", "mcapi", "$state", "$stateParams", "User", "alertService", "pubsub",
+        function ($scope, mcapi, $state, $stateParams, User, alertService, pubsub) {
 
-            mcapi('/usergroup/%', $stateParams.id)
-                .success(function (data) {
-                    $scope.user_group = data;
-                    $scope.owner = $scope.user_group.owner
-                    $scope.signed_in_user = User.u();
-                    $scope.ug_name = $scope.user_group.name;
-                    $scope.users_by_usergroup = data.users;
-                }).jsonp();
+            $scope.createUserGroup = function () {
+                var u_group = {};
+                u_group.access = $scope.group.access;
+                u_group.description = $scope.group.description;
+                u_group.name = $scope.group.name;
+                u_group.users = [User.u()];
+                u_group.owner = User.u();
+                mcapi('/usergroups/new', User.u())
+                    .success(function () {
+                        alertService.sendMessage("UserGroup has been created successfully");
+                        pubsub.send('usergroups.change');
+                    })
+                    .error(function (errorMsg) {
+                        alertService.sendMessage(errorMsg.error);
+                    }).post(u_group);
+            };
+
+            pubsub.waitOn($scope, 'usergroups.change', function () {
+                $scope.getGroups();
+            });
 
             $scope.add_user_to_usergroup = function () {
-                var title = '';
-                var msg = 'Do you want to add  ' + $scope.user_name + ' to ' + $scope.ug_name + '?';
-                var btns = [
-                    {result: 'no', label: 'no'},
-                    {result: 'yes', label: 'yes', cssClass: 'btn-primary'}
-                ];
-                $dialog.messageBox(title, msg, btns)
-                    .open()
-                    .then(function (result) {
-                        if (result == 'yes') {
-                            mcapi('/usergroup/%/selected_name/%', $stateParams.id, $scope.user_name)
-                                .success(function (data) {
-                                    $scope.users_by_usergroup.push(data.id);
-                                }).error(function (data) {
-                                    alertService.sendMessage(data.error);
-                                }).put();
-                        }
-                    });
+                mcapi('/usergroup/%/selected_name/%', $stateParams.id, $scope.user_name)
+                    .success(function (data) {
+                        alertService.sendMessage("user has been added.");
+                        $scope.users_by_usergroup.push(data.id);
+                    }).error(function (data) {
+                        alertService.sendMessage(data.error);
+                    }).put();
             };
 
             $scope.done = function () {
@@ -43,23 +38,42 @@ Application.Controllers.controller('accountGroupUsers',
             };
 
             $scope.delete_user_from_usergroup = function (index) {
-                var title = '';
-                var msg = 'Do you want to delete ' + $scope.users_by_usergroup[index] + ' from ' + $scope.ug_name + '?';
-                var btns = [
-                    {result: 'no', label: 'no'},
-                    {result: 'yes', label: 'yes', cssClass: 'btn-primary'}
-                ];
+                mcapi('/usergroup/%/selected_name/%/remove', $stateParams.id, $scope.users_by_usergroup[index])
+                    .success(function (data) {
+                        alertService.sendMessage("User has been deleted");
+                        $scope.users_by_usergroup = data.users;
+                    }).error(function () {
+                    }).put();
 
-                $dialog.messageBox(title, msg, btns)
-                    .open()
-                    .then(function (result) {
-                        if (result == 'yes') {
-                            mcapi('/usergroup/%/selected_name/%/remove', $stateParams.id, $scope.users_by_usergroup[index])
-                                .success(function (data) {
-                                    $scope.users_by_usergroup = data.users;
-                                }).error(function () {
-                                }).put();
-                        }
-                    });
             };
+            function init() {
+                $scope.group = {
+                    name: null,
+                    access: null,
+                    description: null
+                };
+                mcapi('/user/%/usergroups', User.u())
+                    .success(function (data) {
+                        $scope.user_groups = data;
+                    }).jsonp();
+
+                mcapi('/users')
+                    .success(function (data) {
+                        $scope.all_users = data;
+                    })
+                    .error(function () {
+                    }).jsonp();
+
+                mcapi('/usergroup/%', $stateParams.id)
+                    .success(function (data) {
+                        $scope.user_group = data;
+                        $scope.owner = $scope.user_group.owner;
+                        $scope.signed_in_user = User.u();
+                        $scope.ug_name = $scope.user_group.name;
+                        $scope.users_by_usergroup = data.users;
+                    }).jsonp();
+
+            }
+
+            init();
         }]);
