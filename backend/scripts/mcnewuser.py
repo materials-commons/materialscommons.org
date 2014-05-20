1fa91c9a4468cedb5543b811dab1faf2302d6163
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-20 15:46:35 -0400

    Add ability to change user password.

--- backend/scripts/mcnewuser.py
+++ backend/scripts/mcnewuser.py
@@ -6,6 +6,7 @@ from pbkdf2 import crypt
 import uuid
 import sys
 
+
 class User(object):
     def __init__(self, name, email, password):
         self.name = name
@@ -22,9 +23,11 @@ class User(object):
         self.homepage = ""
         self.notes = []
 
+
 def make_password_hash(password):
     salt = uuid.uuid1().hex
     return crypt(password, salt, iterations=4000)
+
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -33,9 +36,10 @@ if __name__ == "__main__":
     parser.add_option("-p", "--password", dest="password",
                       help="user password", type="string")
     parser.add_option("-P", "--port", dest="port", type="int",
-                      help="rethinkdb port")
-    #parser.add_option("-u", "--update", dest="update", type="bool",
-    #                  help="Reset users password")
+                      help="rethinkdb port", default=30815)
+    parser.add_option("-u", "--update", dest="update",
+                      default=False, action='store_true',
+                      help="Reset users password")
 
     (options, args) = parser.parse_args()
     if options.email is None:
@@ -53,10 +57,16 @@ if __name__ == "__main__":
 
     exists = r.table('users').get(options.email).run(conn)
 
-    if exists is None:
+    if options.update:
+        pwhash = make_password_hash(options.password)
+        if exists is None:
+            print "No such user: " + options.email
+        else:
+            r.table('users').get(options.email)\
+                            .update({'password': pwhash}).run(conn)
+            print "Changed password for user: " + options.email
+    elif exists is None:
         pwhash = make_password_hash(options.password)
         u = User(options.email, options.email, pwhash)
         r.table('users').insert(u.__dict__).run(conn)
         print "Add user: " + options.email
-    else:
-        print "Unknown option"
