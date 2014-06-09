16df333f3dd3d67ed959aa401cb217f1c4460bac
tammasr <tammasr@umich.edu>
2014-06-09 10:55:19 -0400

    removed console in object controller

--- website/app/application/core/toolbar/objects/objects-controller.js
+++ website/app/application/core/toolbar/objects/objects-controller.js
@@ -34,7 +34,6 @@ Application.Controllers.controller('toolbarObjects',
             $scope.bk.selected_treatment = '';
             $scope.default_properties = "";
             $scope.doc.treatments.push(o);
-            console.log($scope.doc)
         };
 
         $scope.save = function (form) {
