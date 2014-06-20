dedf08ceeabdb665633c4f3bee45c3d49d022ecb
tammasr <tammasr@umich.edu>
2014-06-20 14:48:59 -0400

    commiting to resolve the project click ui sref

--- website/app/application/core/toolbar/_projects-controller.js
+++ website/app/application/core/toolbar/_projects-controller.js
@@ -1,14 +1,17 @@
 Application.Controllers.controller('_toolbarProjects',
-    ["$scope", "$state", "model.Projects", "ProjectPath",
-        function ($scope, $state, Projects, ProjectPath) {
+    ["$scope", "$state", "model.Projects", "ProjectPath", "watcher",
+        function ($scope, $state, Projects, ProjectPath, watcher) {
+
             $scope.gotoProject = function (projectId) {
                 ProjectPath.set_project(projectId);
                 $state.go("toolbar.projectspage.overview", {id: projectId, draft_id: '', from: ''});
             };
 
             function init() {
+                console.log('init')
                 Projects.getList().then(function (data) {
                     $scope.projects = data;
+                    $state.go("toolbar.projectspage.overview", {id: $scope.projects[0].id})
                 });
             }
 
--- website/app/application/core/toolbar/projectspage/overview/overview-controller.js
+++ website/app/application/core/toolbar/projectspage/overview/overview-controller.js
@@ -2,6 +2,11 @@ Application.Controllers.controller('toolbarProjectsPageOverview',
     ["$scope", "$stateParams", "pubsub", function ($scope, $stateParams, pubsub) {
 
         function init() {
+//            if ($stateParams.id) {
+//
+//            } else {
+//
+//            }
             $scope.project_id = $stateParams.id;
             $scope.from = $stateParams.from;
             $scope.processes = [];
