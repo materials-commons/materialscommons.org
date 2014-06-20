e2f132269f81adf7f87ba7720550518a5aee2425
tammasr <tammasr@umich.edu>
2014-06-20 14:48:59 -0400

    commiting to resolve the project click ui sref

--- website/app/index/_navigation-controller.js
+++ website/app/index/_navigation-controller.js
@@ -3,7 +3,7 @@ Application.Controllers.controller('_indexNavigation',
         function ($scope, User, $state, Nav) {
             $scope.goHome = function () {
                 if (User.isAuthenticated()) {
-                    $state.go("toolbar.overview");
+                    $state.go("toolbar.projectspage");
                 } else {
                     $state.go("home");
                 }
