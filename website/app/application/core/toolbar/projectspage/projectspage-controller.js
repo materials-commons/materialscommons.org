888e5e334c25b8311cdea26644c0c7f4f06e1c12
tammasr <tammasr@umich.edu>
2014-06-20 16:17:51 -0400

    fixed layout and tab changes

--- website/app/application/core/toolbar/projectspage/projectspage-controller.js
+++ website/app/application/core/toolbar/projectspage/projectspage-controller.js
@@ -1,6 +1,6 @@
 Application.Controllers.controller('toolbarProjectsPage',
-    ["$scope", "$stateParams", "mcapi", "$state", "watcher", "ProjectPath", "Nav","pubsub",
-        function ($scope, $stateParams, mcapi, $state, watcher, ProjectPath, Nav, pubsub) {
+    ["$scope", "$stateParams", "mcapi", "$state", "watcher", "ProjectPath", "Nav", "pubsub", "model.Projects",
+        function ($scope, $stateParams, mcapi, $state, watcher, ProjectPath, Nav, pubsub, Projects) {
 
             $scope.project_id = $stateParams.id;
             $scope.model = {
@@ -13,41 +13,50 @@ Application.Controllers.controller('toolbarProjectsPage',
             });
 
             function init() {
-                $scope.list1 = {"title":"Drag Me"}
-
-                pubsub.send("project.tree", true);
-                $scope.project_id = $stateParams.id;
-                $scope.model = {
-                    action: ''
-                };
-                if ($stateParams.from === 'datafile') {
-                    $scope.project_id = ProjectPath.get_project();
-                    mcapi('/projects/%', $scope.project_id)
-                        .success(function (project) {
-                            $scope.project = project;
-                        }).jsonp();
-                } else {
+                Projects.getList().then(function (data) {
+                    $scope.projects = data;
+                    if ($scope.projects.length === 0) {
+                        return;
+                    }
+                    if (!($stateParams.id)) {
+                        console.log($scope.projects[0].id)
+                        $stateParams.id = $scope.projects[0].id;
+                    }
+                    pubsub.send("project.tree", true);
+                    $scope.project_id = $stateParams.id;
+                    $scope.model = {
+                        action: ''
+                    };
+                    if ($stateParams.from === 'datafile') {
+                        $scope.project_id = ProjectPath.get_project();
+                        mcapi('/projects/%', $scope.project_id)
+                            .success(function (project) {
+                                $scope.project = project;
+                            }).jsonp();
+                    } else {
+                        mcapi('/projects/%', $scope.project_id)
+                            .success(function (project) {
+                                $scope.project = project;
+                                if ($stateParams.draft_id !== "") {
+                                    $state.go('toolbar.projectspage.provenance.process');
+                                } else {
+                                    $state.go('toolbar.projectspage.overview', {id: $scope.project_id, 'draft_id': '', from: ''});
+                                }
+                            }).jsonp();
+                    }
+                    Nav.setActiveNav('Projects');
                     mcapi('/projects/%', $scope.project_id)
                         .success(function (project) {
                             $scope.project = project;
                             if ($stateParams.draft_id !== "") {
-                                $state.go('toolbar.projectspage.provenance.process');
+                                $state.go('toolbar.projectspage.provenance');
                             } else {
-                                $state.go('toolbar.projectspage.overview', {id: $scope.project_id, 'draft_id': '', from: ''});
+                                $state.go('toolbar.projectspage.overview', {id: $scope.project_id, 'draft_id': ''});
                             }
                         }).jsonp();
-                }
-                Nav.setActiveNav('Projects');
-                mcapi('/projects/%', $scope.project_id)
-                    .success(function (project) {
-                        $scope.project = project;
-                        if ($stateParams.draft_id !== "") {
-                            $state.go('toolbar.projectspage.provenance');
-                        } else {
-                            $state.go('toolbar.projectspage.overview', {id: $scope.project_id, 'draft_id': ''});
-                        }
-                    }).jsonp();
 
+                });
             }
+
             init();
         }]);
