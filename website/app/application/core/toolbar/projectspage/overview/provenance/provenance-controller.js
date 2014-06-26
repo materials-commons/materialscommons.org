6ae4a608bcf4b0263773c521c001894465cc8e1e
tammasr <tammasr@umich.edu>
2014-06-26 15:32:28 -0400

    Issue-413-sample tree structure with defined set of json data

--- website/app/application/core/toolbar/projectspage/overview/provenance/provenance-controller.js
+++ website/app/application/core/toolbar/projectspage/overview/provenance/provenance-controller.js
@@ -4,6 +4,28 @@ Application.Controllers.controller('toolbarProjectsPageOverviewProvenance',
 
         function init() {
             $scope.project_id = $stateParams.id;
+            $scope.tree_data = [
+                {Name:"Sample1",Processes:"p1, p2",
+                    children:[
+                        {Name:"Sample1.1", Processes:"p3", id: '',
+                            children:[
+                                {Name:"Sample1.1.1", id: ''},
+                                {Name:"Sample1.1.2", Processes:"p8", id: ''}
+                            ]
+                        },
+                        {Name:"Sample1.2", Processes:1,id: '',
+                            children:[
+                                {Name:"Sample1.2.1", Processes:"p3",id: ''}
+                            ]
+                        }
+                    ]
+                },
+                {Name:"Sample2",Processes:"p9"}
+            ];
+            $scope.col_defs = [
+                { field: "Processes"}
+            ];
+
 
         }
 
