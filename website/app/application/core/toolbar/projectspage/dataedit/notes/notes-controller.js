f7d1139fc48adb4535c1f78184c2cde67f95fa26
tammasr <tammasr@umich.edu>
2014-06-27 16:24:15 -0400

    text angular incorporated into the provenance code

--- website/app/application/core/toolbar/projectspage/dataedit/notes/notes-controller.js
+++ website/app/application/core/toolbar/projectspage/dataedit/notes/notes-controller.js
@@ -2,7 +2,6 @@ Application.Controllers.controller('toolbarDataEditNotes',
     ["$scope", "mcapi", "$state", "$stateParams", "User", "dateGenerate", "alertService",
         function ($scope, mcapi, $state, $stateParams, User, dateGenerate, alertService) {
             $scope.add_notes = function () {
-                console.log($scope.model.new_note);
                 $scope.doc.notes.push({'message': $scope.model.new_note, 'who': User.u(), 'date': dateGenerate.new_date()});
                 $scope.saveData();
                 $scope.model.new_note = "";
@@ -17,9 +16,10 @@ Application.Controllers.controller('toolbarDataEditNotes',
             };
 
             function init() {
+                $scope.htmlContent =
                 $scope.id = $stateParams.data_id;
                 $scope.model = {
-                    new_note: ''
+                    new_note: ""
                 };
                 mcapi('/datafile/%', $scope.id)
                     .success(function (data) {
