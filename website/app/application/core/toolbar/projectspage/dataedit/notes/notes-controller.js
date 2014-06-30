96647ace4d6bef682c176ee8eb9f4f3eaf39aaf2
tammasr <tammasr@umich.edu>
2014-06-30 11:23:15 -0400

    update the notes list on saving

--- website/app/application/core/toolbar/projectspage/dataedit/notes/notes-controller.js
+++ website/app/application/core/toolbar/projectspage/dataedit/notes/notes-controller.js
@@ -1,6 +1,8 @@
 Application.Controllers.controller('toolbarDataEditNotes',
-    ["$scope", "mcapi", "$state", "$stateParams", "User", "dateGenerate", "alertService",
-        function ($scope, mcapi, $state, $stateParams, User, dateGenerate, alertService) {
+    ["$scope", "mcapi", "$state", "$stateParams", "User", "dateGenerate", "alertService", "watcher",
+        function ($scope, mcapi, $state, $stateParams, User, dateGenerate, alertService, watcher) {
+
+
             $scope.add_notes = function () {
                 $scope.doc.notes.push({'message': $scope.model.new_note, 'who': User.u(), 'date': dateGenerate.new_date()});
                 $scope.saveData();
@@ -10,21 +12,27 @@ Application.Controllers.controller('toolbarDataEditNotes',
                 mcapi('/datafile/update/%', $scope.id)
                     .success(function (data) {
                         alertService.sendMessage("Notes has been added");
+                        $scope.get_datafile();
                     }).error(function (data) {
                         alertService.sendMessage(data.error);
                     }).put($scope.doc);
             };
 
-            function init() {
-                $scope.htmlContent =
-                $scope.id = $stateParams.data_id;
-                $scope.model = {
-                    new_note: ""
-                };
+            $scope.get_datafile = function () {
                 mcapi('/datafile/%', $scope.id)
                     .success(function (data) {
                         $scope.doc = data;
                     }).jsonp();
+            };
+
+            function init() {
+                $scope.htmlContent =
+                    $scope.id = $stateParams.data_id;
+                $scope.model = {
+                    new_note: ""
+                };
+                $scope.get_datafile();
             }
+
             init();
         }]);
