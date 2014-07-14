855dc36391e290c25bc8f8b6cee359030feb7c9d
tammasr <tammasr@umich.edu>
2014-07-14 14:59:43 -0400

    script to add samples and projects join table entries

---
+++ backend/scripts/sample_projects_join.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rethinkdb as r
+import optparse
+import sys
+
+    
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option("-P", "--port", dest="port",
+                      help="rethinkdb port", default=30815)
+    (options, args) = parser.parse_args()
+    conn = r.connect('localhost', int(options.port), db='materialscommons')
+    samples = r.table('samples').run(conn)
+    for sample in samples:
+        projects = sample['projects']
+        for p in projects:
+            rr = r.table('projects_samples').insert({'sample_id': sample['id'], 'project_id': p['id'], 'project_name': p['name']}).run(conn)
+            
