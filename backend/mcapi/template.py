23844c554e17e02074d1fdfca5a33b71dcc0f062
tammasr <tammasr@umich.edu>
2014-07-17 15:51:55 -0400

    change to template.py

--- backend/mcapi/template.py
+++ backend/mcapi/template.py
@@ -5,7 +5,7 @@ import rethinkdb as r
 import error
 import dmutil
 import json
-import args
+
 
 @app.route('/templates/<template_id>', methods=['GET'])
 @apikey
@@ -30,9 +30,7 @@ def get_template_by_name(template_name):
 @apikey(shared=True)
 @jsonp
 def get_all_templates():
-    rr = r.table('templates').order_by('template_name')
-    selection = list(rr.run(g.conn, time_format='raw'))
-    return args.json_as_format_arg(selection)
+    return dmutil.get_all_from_table('templates')
 
 
 @app.route('/templates/by_pick/<pick>', methods=['GET'])
