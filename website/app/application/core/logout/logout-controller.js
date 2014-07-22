6db0c631d343f5544032e63913ef6b6ec80ddf9f
tammasr <tammasr@umich.edu>
2014-07-22 14:38:14 -0400

    removed Thumbnail Provider

--- website/app/application/core/logout/logout-controller.js
+++ website/app/application/core/logout/logout-controller.js
@@ -1,10 +1,9 @@
 Application.Controllers.controller('logout',
-    ["$rootScope", "$state", "$cookieStore", "User", "Thumbnails", "Projects", "ProvDrafts",
-        function ($rootScope, $state, $cookieStore, User, Thumbnails, Projects, ProvDrafts) {
+    ["$rootScope", "$state", "$cookieStore", "User", "Projects", "ProvDrafts",
+        function ($rootScope, $state, $cookieStore, User,  Projects, ProvDrafts) {
             $rootScope.email_address = '';
             $cookieStore.remove('mcuser');
             User.setAuthenticated(false, '', '');
-            Thumbnails.clear();
             Projects.clear();
             ProvDrafts.clear();
             $state.transitionTo('home');
