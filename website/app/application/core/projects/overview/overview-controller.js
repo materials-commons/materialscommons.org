43e71d06017c203ef2c0b22b2a135a5989fbce6f
V. Glenn Tarcea <gtarcea@umich.edu>
2014-07-24 11:12:37 -0400

    ISSUE#484. Used ProvDrafts service in places where drafts are accessed. Added pubsub to register for changes.

--- website/app/application/core/projects/overview/overview-controller.js
+++ website/app/application/core/projects/overview/overview-controller.js
@@ -1,39 +1,44 @@
 Application.Controllers.controller('projectsOverview',
-    ["$scope", "$stateParams", "pubsub", "$state","mcapi", function ($scope, $stateParams, pubsub, $state, mcapi) {
-        $scope.countDrafts = function() {
-            if ($scope.project_id === "") {
-                return;
-            }
-            mcapi('/drafts/project/%', $scope.project_id)
-                .success(function (drafts) {
-                    $scope.drafts_count = drafts.length;
-                }).jsonp();
-        };
+                                   ["$scope", "$stateParams", "pubsub", "$state", "ProvDrafts",
+                                    function ($scope, $stateParams, pubsub, $state, ProvDrafts) {
+                                        pubsub.waitOn($scope, ProvDrafts.channel, function() {
+                                            $scope.drafts = ProvDrafts.drafts;
+                                        });
 
-        $scope.showTab = function (tab) {
-            switch (tab) {
-                case "files":
-                    $state.go('projects.overview.files');
-                    break;
-                case "provenance":
-                    $state.go('projects.overview.provenance');
-                    break;
-                case "drafts":
-                    $state.go('projects.overview.drafts');
-                    break;
-                case "samples":
-                    $state.go('projects.overview.samples');
-                    break;
+                                        $scope.countDrafts = function() {
+                                            if ($scope.project_id === "") {
+                                                return;
+                                            }
+                                            mcapi('/drafts/project/%', $scope.project_id)
+                                                .success(function (drafts) {
+                                                    $scope.drafts_count = drafts.length;
+                                                }).jsonp();
+                                        };
 
-            }
-        };
+                                        $scope.showTab = function (tab) {
+                                            switch (tab) {
+                                            case "files":
+                                                $state.go('projects.overview.files');
+                                                break;
+                                            case "provenance":
+                                                $state.go('projects.overview.provenance');
+                                                break;
+                                            case "drafts":
+                                                $state.go('projects.overview.drafts');
+                                                break;
+                                            case "samples":
+                                                $state.go('projects.overview.samples');
+                                                break;
 
-        function init() {
-            $scope.project_id = $stateParams.id;
-            $scope.from = $stateParams.from;
-            $scope.processes = [];
-            $scope.countDrafts();
-        }
+                                            }
+                                        };
 
-        init();
-    }]);
+                                        function init() {
+                                            $scope.project_id = $stateParams.id;
+                                            $scope.from = $stateParams.from;
+                                            $scope.processes = [];
+                                            $scope.drafts = ProvDrafts.loadRemoteDrafts($scope.project_id);
+                                        }
+
+                                        init();
+                                    }]);
