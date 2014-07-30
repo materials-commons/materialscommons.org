051fa44652072a1d1b47cdebe644096942d3bd0d
tammasr <tammasr@umich.edu>
2014-07-30 14:13:01 -0400

    commiting notes directive

---
+++ website/app/application/core/projects/overview/notes/notes-controller.js
@@ -0,0 +1,19 @@
+Application.Controllers.controller('projectsOverviewNotes',
+    ["$scope", "mcapi", "$stateParams",
+        function ($scope, mcapi, $stateParams) {
+
+            $scope.getProject = function () {
+                mcapi('/projects/%', $scope.project_id)
+                    .success(function (project) {
+                        $scope.project = project;
+                        console.log($scope.project)
+                    }).jsonp();
+            };
+
+            function init() {
+                $scope.project_id = $stateParams.id;
+                $scope.getProject()
+            }
+
+            init();
+        }]);
