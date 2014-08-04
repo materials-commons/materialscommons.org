4b523acedbbca081a06a076c08981cab08c7ca21
V. Glenn Tarcea <gtarcea@umich.edu>
2014-08-04 11:45:07 -0400

    ISSUE#518. Clear caches, and send apikey on each call, rather than setting it up by default when it isn't defined.

--- website/app/application/core/logout/logout-controller.js
+++ website/app/application/core/logout/logout-controller.js
@@ -1,10 +1,11 @@
 Application.Controllers.controller('logout',
-    ["$rootScope", "$state", "$cookieStore", "User", "Projects", "ProvDrafts",
-        function ($rootScope, $state, $cookieStore, User,  Projects, ProvDrafts) {
+    ["$rootScope", "$state", "$cookieStore", "User", "Projects", "ProvDrafts", "model.Projects",
+        function ($rootScope, $state, $cookieStore, User,  Projects, ProvDrafts, P2) {
             $rootScope.email_address = '';
             $cookieStore.remove('mcuser');
             User.setAuthenticated(false, '', '');
             Projects.clear();
             ProvDrafts.clear();
+            P2.clear();
             $state.transitionTo('home');
         }]);
