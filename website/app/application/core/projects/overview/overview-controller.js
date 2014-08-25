8a9ad29b0a22054a2289284c92658c94876899b1
V. Glenn Tarcea <gtarcea@umich.edu>
2014-08-25 13:48:41 -0400

    ISSUE#554. Moved Settings to bottom and reordered the tabs. Fixed up the javascript code by formatting it and adding missing semicolons.

--- website/app/application/core/projects/overview/overview-controller.js
+++ website/app/application/core/projects/overview/overview-controller.js
@@ -1,94 +1,98 @@
 Application.Controllers.controller('projectsOverview',
-    ["$scope", "$stateParams", "pubsub", "$state", "ProvDrafts", "mcapi", "Tags","User", "$filter",
-        function ($scope, $stateParams, pubsub, $state, ProvDrafts, mcapi, Tags, User, $filter) {
-            pubsub.waitOn($scope, ProvDrafts.channel, function () {
-                $scope.drafts = ProvDrafts.drafts;
-            });
+                                   ["$scope", "$stateParams", "pubsub", "$state",
+                                    "ProvDrafts", "mcapi", "Tags","User", "$filter", projectsOverview]);
 
-            pubsub.waitOn($scope, 'open_reviews.change', function () {
-                $scope.countReviews();
-            });
+function projectsOverview ($scope, $stateParams, pubsub, $state, ProvDrafts, mcapi, Tags, User, $filter) {
+    pubsub.waitOn($scope, ProvDrafts.channel, function () {
+        $scope.drafts = ProvDrafts.drafts;
+    });
 
-            $scope.countReviews = function(){
-                mcapi('/project/%/reviews', $scope.project_id)
-                    .success(function (reviews) {
-                        $scope.open_reviews = $filter('reviewFilter')(reviews, 'open');
-                    }).jsonp();
-            }
+    pubsub.waitOn($scope, 'open_reviews.change', function () {
+        $scope.countReviews();
+    });
 
-            pubsub.waitOn($scope, 'access.change', function () {
-                $scope.getProject();
-            });
+    $scope.countReviews = function(){
+        mcapi('/project/%/reviews', $scope.project_id)
+            .success(function (reviews) {
+                $scope.open_reviews = $filter('reviewFilter')(reviews, 'open');
+            }).jsonp();
+    };
 
-            $scope.countDrafts = function () {
-                if ($scope.project_id === "") {
-                    return;
-                }
-                mcapi('/drafts/project/%', $scope.project_id)
-                    .success(function (drafts) {
-                        $scope.drafts_count = drafts.length;
-                    }).jsonp();
-            };
+    pubsub.waitOn($scope, 'access.change', function () {
+        $scope.getProject();
+    });
 
-            $scope.createTag = function(){
-                mcapi('/user/%/tags', User.u())
-                    .success(function () {
-                        $scope.loadUserTags();
-                    }).put($scope.bk);
-                $scope.bk = {}
-            }
+    $scope.countDrafts = function () {
+        if ($scope.project_id === "") {
+            return;
+        }
+        mcapi('/drafts/project/%', $scope.project_id)
+            .success(function (drafts) {
+                $scope.drafts_count = drafts.length;
+            }).jsonp();
+    };
 
-            $scope.showTab = function (tab) {
-                switch (tab) {
-                    case "files":
-                        $state.go('projects.overview.files');
-                        break;
-                    case "provenance":
-                        $state.go('projects.overview.provenance');
-                        break;
-                    case "drafts":
-                        $state.go('projects.overview.drafts');
-                        break;
-                    case "samples":
-                        $state.go('projects.overview.samples');
-                        break;
-                    case "notes":
-                        $state.go('projects.overview.notes');
-                        break;
-                    case "settings":
-                        $state.go('projects.overview.settings');
-                        break;
-                    case "reviews":
-                        $state.go('projects.overview.reviews');
-                        break;
-
-                }
-            };
-            $scope.getProject = function(){
-                mcapi('/projects/%', $scope.project_id)
-                    .success(function (project) {
-                        $scope.project = project;
-                    }).jsonp();
-            }
-
-            $scope.loadUserTags= function () {
-                mcapi('/user/%/tags', User.u())
-                    .success(function (user) {
-                        $scope.u_tags = user.preferences.tags;
-                        Tags.updateUserTags($scope.u_tags)
-                    }).jsonp();
-            }
-            function init() {
-                $scope.bk = {
-                    name: ''
-                }
-                $scope.project_id = $stateParams.id;
-                $scope.from = $stateParams.from;
-                $scope.processes = [];
-                $scope.drafts = ProvDrafts.loadRemoteDrafts($scope.project_id);
-                $scope.getProject();
+    $scope.createTag = function(){
+        mcapi('/user/%/tags', User.u())
+            .success(function () {
                 $scope.loadUserTags();
-                $scope.countReviews()
-            }
-            init();
-        }]);
+            }).put($scope.bk);
+        $scope.bk = {};
+    };
+
+    $scope.showTab = function (tab) {
+        switch (tab) {
+        case "files":
+            $state.go('projects.overview.files');
+            break;
+        case "provenance":
+            $state.go('projects.overview.provenance');
+            break;
+        case "drafts":
+            $state.go('projects.overview.drafts');
+            break;
+        case "samples":
+            $state.go('projects.overview.samples');
+            break;
+        case "notes":
+            $state.go('projects.overview.notes');
+            break;
+        case "settings":
+            $state.go('projects.overview.settings');
+            break;
+        case "reviews":
+            $state.go('projects.overview.reviews');
+            break;
+
+        }
+    };
+
+    $scope.getProject = function(){
+        mcapi('/projects/%', $scope.project_id)
+            .success(function (project) {
+                $scope.project = project;
+            }).jsonp();
+    };
+
+    $scope.loadUserTags= function () {
+        mcapi('/user/%/tags', User.u())
+            .success(function (user) {
+                $scope.u_tags = user.preferences.tags;
+                Tags.updateUserTags($scope.u_tags);
+            }).jsonp();
+    };
+
+    function init() {
+        $scope.bk = {
+            name: ''
+        };
+        $scope.project_id = $stateParams.id;
+        $scope.from = $stateParams.from;
+        $scope.processes = [];
+        $scope.drafts = ProvDrafts.loadRemoteDrafts($scope.project_id);
+        $scope.getProject();
+        $scope.loadUserTags();
+        $scope.countReviews();
+    }
+    init();
+}
