b544f5cca582bce9b03ecff14d57608ce2ad4d03
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-09-01 15:25:17 -0400

    Layout project with no side tabs. Remove the fixed content. Start working on adding drag and drop.

--- website/app/application/core/projects/directives/action-stack-directive.js
+++ website/app/application/core/projects/directives/action-stack-directive.js
@@ -6,7 +6,7 @@ function actionStackDirective(actionStackTracker, $compile) {
             return function(scope, e) {
                 scope.toggleStackAction = function(action, actionStackID) {
                     var id = 'action-' + action;
-                    var t = '<div id="' + id + '" class="col-lg-12"><hr class="carved"/><div class="col-lg-offset-1"><div action-' + action + '></div></div></div>';
+                    var t = '<div ui-draggable="false" id="' + id + '" class="col-lg-12"><hr class="carved"/><div class="col-lg-offset-1"><div action-' + action + '></div></div></div>';
                     if (!actionStackTracker.actionActive(action)) {
                         actionStackTracker.pushAction(action);
                         $('#' + actionStackID).append($compile(t)(scope));
