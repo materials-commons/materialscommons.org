2704e070848a8ab0b0272b3400fefc4dbe294399
tammasr <tammasr@umich.edu>
2014-09-03 11:55:07 -0400

    Add Sample Directive

--- website/app/application/directives/sample-report-directive.js
+++ website/app/application/directives/sample-report-directive.js
@@ -1,6 +1,7 @@
 Application.Directives.directive('sampleReport',
     function () {
         return {
+            controller: 'actionSampleReportController',
             restrict: "A",
             scope: {
                 sample: '='
@@ -8,3 +9,21 @@ Application.Directives.directive('sampleReport',
             templateUrl: 'application/directives/sample-report.html'
         };
     });
+
+Application.Controllers.controller('actionSampleReportController',
+    ["$scope", "mcapi",  actionSamplesController]);
+
+function actionSamplesController($scope, mcapi) {
+
+    $scope.refreshProjects = function () {
+        mcapi('/samples/project/%', $scope.sample.id)
+            .success(function (data) {
+                $scope.projects_by_sample = data;
+            }).jsonp();
+    };
+
+    function init(){
+        $scope.refreshProjects();
+    }
+    init();
+}
