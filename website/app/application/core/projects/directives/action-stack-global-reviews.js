1c95758d1ed0149a21cd47e305f264bc0d43eaf7
tammasr <tammasr@umich.edu>
2014-09-05 13:42:04 -0400

    reviews functionality :

---
+++ website/app/application/core/projects/directives/action-stack-global-reviews.js
@@ -0,0 +1,126 @@
+Application.Directives.directive('actionGlobalReviews', actionReviewsDirective);
+
+function actionReviewsDirective() {
+    return {
+        controller: "actionGlobalReviewsController",
+        restrict: "A",
+        templateUrl: "application/core/projects/directives/action-stack-global-reviews.html"
+    };
+}
+
+Application.Controllers.controller('actionGlobalReviewsController',
+    ["$scope", "mcapi", "$filter", "$state", "pubsub", "User", "dateGenerate", actionGlobalReviewsController]);
+
+function actionGlobalReviewsController($scope, mcapi, $filter, $state, pubsub, User, dateGenerate) {
+    pubsub.waitOn($scope, 'reviews.change', function () {
+        $scope.retrieveReviewsRequested();
+        $scope.retrieveReviewsToConduct();
+    });
+
+    $scope.retrieveReviewsToConduct = function () {
+        mcapi('/reviews/to_conduct')
+            .success(function (reviews) {
+                $scope.conduct_open_reviews = $filter('reviewFilter')(reviews, 'open');
+                $scope.conduct_closed_reviews = $filter('reviewFilter')(reviews, 'close');
+                $scope.reviewsToConduct = $scope.conduct_open_reviews;
+                $scope.status = 'open';
+            }).jsonp();
+    };
+    $scope.retrieveReviewsRequested = function () {
+        mcapi('/reviews/requested')
+            .success(function (reviews) {
+                $scope.requested_open_reviews = $filter('reviewFilter')(reviews, 'open');
+                $scope.requested_closed_reviews = $filter('reviewFilter')(reviews, 'close');
+                $scope.reviewsRequested = $scope.requested_open_reviews;
+                $scope.status = 'open';
+            }).jsonp();
+    };
+
+
+    $scope.removeReview = function (index) {
+        mcapi('/reviews/%', $scope.reviewsRequested[index].id)
+            .success(function () {
+                $scope.reviewsRequested.splice(index, 1);
+                pubsub.send('reviews.change', 'review.change');
+            }).delete();
+    };
+
+    $scope.viewReview = function (review) {
+        $scope.model = {
+            selected: false,
+            comment: ""
+        };
+        mcapi('/reviews/%', review.id)
+            .success(function (data) {
+                $scope.review = data;
+            }).jsonp();
+    };
+    $scope.editReview = function(index){
+        $scope.edit_index = index;
+    };
+    $scope.saveReview = function(index) {
+        if ($scope.review.messages[index].message === "") {
+            return;
+        }
+        $scope.edit_index = -1;
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+            }).put({'messages': $scope.review.messages});
+    };
+    $scope.addComment = function() {
+        if ($scope.model.comment.length === 0) {
+            return;
+        }
+        $scope.review.messages.push({'message': $scope.model.comment, 'who': User.u(), 'date': dateGenerate.new_date()});
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+            }).put({'messages': $scope.review.messages});
+    };
+    $scope.closeReview = function() {
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+                pubsub.send('reviews.change');
+                $scope.retrieveReviewsRequested();
+                $scope.retrieveReviewsToConduct();
+                $scope.review = ''
+            }).put({'status': 'close'});
+    };
+
+    $scope.reOpenReview = function() {
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+                pubsub.send('reviews.change');
+                $scope.retrieveReviewsRequested();
+                $scope.retrieveReviewsToConduct();
+                $scope.review = ''
+            }).put({'status': 'open'});
+    };
+
+    $scope.showReviews = function (status, type) {
+        $scope.status = status;
+        if (status == 'open') {
+            if(type == 'requested'){
+                $scope.reviewsRequested = $scope.requested_open_reviews;
+            }else{
+                $scope.reviewsToConduct = $scope.conduct_open_reviews;
+            }
+
+        }
+        else if (status == 'close') {
+            if(type == 'requested'){
+                $scope.reviewsRequested = $scope.requested_closed_reviews;
+            }else{
+                $scope.reviewsToConduct = $scope.conduct_closed_reviews;
+
+            }
+        }
+    };
+
+    function init() {
+        $scope.review = '';
+        $scope.retrieveReviewsRequested();
+        $scope.retrieveReviewsToConduct();
+    }
+
+    init();
+}
