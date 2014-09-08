0fe89297475e5650de8f60888ecb95a2c0bd1403
tammasr <tammasr@umich.edu>
2014-09-08 16:12:50 -0400

    issue-review-bug

--- website/app/application/core/projects/projectspage-controller.js
+++ website/app/application/core/projects/projectspage-controller.js
@@ -94,4 +94,5 @@ Application.Controllers.controller('actionStackListController',
 function actionStackListController($scope, actionStackTracker) {
     $scope.actions = actionStackTracker.actions;
     $scope.actionActive = actionStackTracker.actionActive;
+    console.log($scope.actionActive)
 }
