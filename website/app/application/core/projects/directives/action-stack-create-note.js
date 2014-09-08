d40f8787881fbf261fc1d5d1741ee387937a1bfb
V. Glenn Tarcea <gtarcea@umich.edu>
2014-09-08 13:56:58 -0400

    Clean up notes directives to use model.projects rather than mcapi. Removed extraneous extra calls. Added close button to directive.

--- website/app/application/core/projects/directives/action-stack-create-note.js
+++ website/app/application/core/projects/directives/action-stack-create-note.js
@@ -9,30 +9,28 @@ function actionCreateNoteDirective() {
 }
 
 Application.Controllers.controller('actionCreateNoteController',
-    ["$scope", "mcapi", "$stateParams", "User", "dateGenerate", actionCreateNoteController]);
+    ["$scope", "model.projects", "$stateParams", "User", "dateGenerate", actionCreateNoteController]);
 
-function actionCreateNoteController($scope, mcapi, $stateParams, User, dateGenerate) {
+function actionCreateNoteController($scope, Projects, $stateParams, User, dateGenerate) {
 
     $scope.add_notes = function () {
         $scope.project.notes.push({'message': $scope.model.new_note, 'who': User.u(), 'date': dateGenerate.new_date()});
         $scope.saveData();
         $scope.model.new_note = "";
     };
-    $scope.saveData = function () {
-            mcapi('/projects/%/update', $scope.project.id)
-                .success(function (data) {
-                }).put($scope.project);
 
-    }
-    $scope.getProject = function () {
-        mcapi('/projects/%', $scope.project_id)
-            .success(function (project) {
-                $scope.project = project;
-            }).jsonp();
+    $scope.saveData = function () {
+        $scope.project.put(User.keyparam()).then(function() {
+            $scope.toggleStackAction('create-note');
+        });
     };
+
     function init() {
         $scope.project_id = $stateParams.id;
-        $scope.getProject();
+        Projects.get($scope.project_id).then(function(project) {
+            $scope.project = project;
+        });
+
         $scope.model = {
             new_note: ""
         };
