96e982327d1f41af6414166ffcddcbe757c35fc8
stamma <sravyatamma@Sravyas-MacBook-Air.local>
2014-09-10 16:30:08 -0400

    Directive todod list working

--- website/app/application/core/projects/overview/sideboard-directive.js
+++ website/app/application/core/projects/overview/sideboard-directive.js
@@ -9,13 +9,24 @@ function sideboardDirective() {
 }
 
 Application.Controllers.controller('tagsSideboardController',
-                                   ["$scope", "User", "$stateParams", "model.projects", tagsSideboardController]);
+                                   ["$scope", "User",  tagsSideboardController]);
 
-function tagsSideboardController($scope, User, $stateParams, Projects) {
+function tagsSideboardController($scope, User) {
     $scope.tags = User.attr().preferences.tags;
+}
+
+Application.Controllers.controller('todosSideboardController',
+    ["$scope",  "$stateParams", "model.projects", todosSideboardController]);
+
+function todosSideboardController($scope, $stateParams, Projects) {
+    $scope.checkBox = function(index){
+        $scope.todo.checked = true;
+    }
     $scope.project_id = $stateParams.id;
     Projects.get($scope.project_id).then(function(project) {
         $scope.project = project;
         $scope.todos = project.todos;
+        console.log($scope.todos)
     });
+
 }
