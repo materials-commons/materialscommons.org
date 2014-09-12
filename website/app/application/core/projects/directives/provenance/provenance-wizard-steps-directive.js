893b2c12c1b325edd5f16ea1ced453f7b2fd51f0
V. Glenn Tarcea <gtarcea@umich.edu>
2014-09-12 16:50:00 -0400

    More wizard layout.

--- website/app/application/core/projects/directives/provenance/provenance-wizard-steps-directive.js
+++ website/app/application/core/projects/directives/provenance/provenance-wizard-steps-directive.js
@@ -6,7 +6,7 @@ function provenanceWizardStepsDirective() {
             template: "=",
             showOverview: "="
         },
-        controller: "actionShowProvenanceDirectiveController",
+        controller: "provenanceWizardStepsDirectiveController",
         restrict: "EA",
         templateUrl: "application/core/projects/directives/provenance/provenance-wizard-steps.html"
     };
@@ -15,5 +15,19 @@ function provenanceWizardStepsDirective() {
 Application.Controllers.controller('provenanceWizardStepsDirectiveController',
                                    ["$scope", provenanceWizardStepsDirectiveController]);
 function provenanceWizardStepsDirectiveController($scope) {
+    function templateStepsCount() {
+        if (!$scope.template) {
+            return 0;
+        }
+        var count = $scope.template.input_templates.length;
+        count += ($scope.template.required_input_files ? 1 : 0);
+        count += $scope.template.output_templates.length;
+        count += ($scope.template.required_output_files ? 1 : 0);
+        console.log("count = " + count);
+        return count;
+    }
 
+    $scope.offsetSteps = function() {
+        return templateStepsCount() == 4;
+    };
 }
