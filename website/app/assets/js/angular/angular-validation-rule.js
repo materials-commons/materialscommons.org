6cfe6130fed22d2d44bbbffe8860d77319b492f9
tammasr <tammasr@umich.edu>
2014-09-15 12:00:25 -0400

    Issue-568-sample composition validation is fixed

--- website/app/assets/js/angular/angular-validation-rule.js
+++ website/app/assets/js/angular/angular-validation-rule.js
@@ -9,12 +9,14 @@
                     },
                     url: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
                     email: /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
-                    number: /^\d+$/
+                    number: /^$|^[-]?\d+(\.\d+)?$/,
+                    float: /[+-]?([0-9]+\.([0-9]+)?|\.[0-9]+)([eE][+-]?[0-9]+)?/,
+                    int: /^$|^\d+$/
                 };
 
                 var defaultMsg = {
                     required: {
-                        error: 'This should be Required!!',
+                        error: 'Required!!',
                         success: ''
                     },
                     url: {
@@ -28,11 +30,17 @@
                     number: {
                         error: 'This should be Number',
                         success: ''
+                    },
+                    float: {
+                        error: 'This should be floating point number',
+                        success: ''
+                    },
+                    int: {
+                        error: 'This should be integer',
+                        success: ''
                     }
                 };
-
                 $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
-
             }
         ]);
 
