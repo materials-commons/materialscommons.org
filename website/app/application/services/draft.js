041137d1fe06e601e7ab1142c4ea4a33a75a96ef
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-09-29 08:55:18 -0400

    Add notes area to the currentDraft. Start trying to figure out how to display the notes for each of the fields.

--- website/app/application/services/draft.js
+++ website/app/application/services/draft.js
@@ -26,6 +26,7 @@ function draftService() {
                 draft.inputs[t.id].name = "(I) " + t.template_name;
                 draft.inputs[t.id].done = false;
                 draft.inputs[t.id].note = null;
+                draft.inputs[t.id].showNote = false;
                 draft.inputs[t.id].properties = {};
                 draft.inputs[t.id].custom_properties = {};
                 draft.inputs[t.id].additional_properties = {};
@@ -50,6 +51,8 @@ function draftService() {
 
             if (template.required_input_files) {
                 draft.inputs.files = {};
+                draft.inputs.files.showNote = false;
+                draft.inputs.files.note = null;
                 draft.inputs.files.name = "(I) Files" ;
                 draft.inputs.files.files = [];
                 draft.inputs.files.done = false;
@@ -59,6 +62,8 @@ function draftService() {
                 draft.outputs[t.id] = {};
                 draft.outputs[t.id].name = "(O) " + t.template_name;
                 draft.outputs[t.id].done = false;
+                draft.outputs[t.id].note = null;
+                draft.outputs[t.id].showNote = false;
                 draft.outputs[t.id].properties = {};
                 draft.outputs[t.id].custom_properties = {};
                 draft.outputs[t.id].additional_properties = {};
@@ -66,18 +71,26 @@ function draftService() {
                     draft.outputs[t.id].properties[p.attribute] = {
                         name: p.name,
                         value: "",
-                        unit: ""
+                        unit: "",
+                        note: null,
+                        showNote: false
                     };
                 });
 
                 if (t.template_pick == "pick_sample") {
-                    draft.outputs[t.id].properties.sample = null;
+                    draft.outputs[t.id].properties.sample = {
+                        sample: null,
+                        note: null,
+                        showNote: false
+                    };
                 }
             });
 
             if (template.required_output_files) {
                 draft.outputs.files = {};
                 draft.outputs.files.name = "(O) Files";
+                draft.outputs.files.note = null;
+                draft.outputs.files.showNote = false;
                 draft.outputs.files.files = [];
                 draft.outputs.files.done = false;
             }
