5c660be7bbd53d42742d41b53643f7f8b563bae8
tammasr <tammasr@umich.edu>
2014-10-09 14:34:23 -0400

    preserving active state of the links under each tab

--- website/app/application/core/projects/project/reviews/reviews.js
+++ website/app/application/core/projects/project/reviews/reviews.js
@@ -2,6 +2,17 @@ Application.Controllers.controller("projectReviews",
     ["$scope",  projectReviews]);
 
 function projectReviews($scope) {
+    $scope.activeLink = 'View Reviews';
+    $scope.setActiveLink = function(tabID) {
+        $scope.activeLink = tabID;
+    };
+
+    $scope.isActiveLink = function (tabID) {
+        if (tabID == $scope.activeLink){
+            return true
+        }
+        return false
+    };
     $scope.reportsMenu = [
         {
             title:"By Activity",
