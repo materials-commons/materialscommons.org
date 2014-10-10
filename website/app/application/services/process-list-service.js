33a54ef0ada99af4ec5eb67c78c989f5dbbbdd52
tammasr <tammasr@umich.edu>
2014-10-10 16:52:03 -0400

    provenance view

--- website/app/application/services/process-list-service.js
+++ website/app/application/services/process-list-service.js
@@ -7,15 +7,17 @@ Application.Services.factory('ProcessList',
             return {
                 getProcesses: function(project_id){
                     console.log(project_id);
-                    if (project_id in service.processes){
-                        return service.processes[project_id];
-                    }else{
-                        this.loadProcesses(project_id);
-                    }
+                    this.loadProcesses(project_id);
+//                    if (project_id in service.processes){
+//                        return service.processes[project_id];
+//                    }else{
+//                        this.loadProcesses(project_id);
+//                    }
                 },
 
                 loadProcesses: function (project_id) {
-                    Restangular.all('processes').one('project', project_id).getList().then(function(data){
+                    Restangular.one('processes').one('project', project_id).getList().then(function(data){
+                        console.log("here i am");
                         console.log(data);
                         service.processes[project_id] = data;
                         console.log(service.processes);
