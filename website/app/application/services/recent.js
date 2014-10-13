8207714ee4ffe26fdf9da6d557520f41383d414f
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-10-13 17:20:50 -0400

    Add recents to track recently accessed items. Fillout the sidebar and New menu.

---
+++ website/app/application/services/recent.js
@@ -0,0 +1,71 @@
+Application.Services.factory("recent", recentService);
+
+// recent service tracks recently created items by project. It tracks
+// their state information so that their window state can be quickly
+// recovered.
+function recentService() {
+    var self = this;
+    self.recentByProject = {};
+
+    self.initForProject = function(projectID) {
+        self.recentByProject[projectID] = {
+            items: []
+        };
+    };
+
+    self.newItem = function(itemType, itemState, itemName) {
+        return {
+            itype: itemType,
+            state: itemState,
+            name: itemName
+        };
+    };
+
+    return {
+        // getRecent returns the list of recents for a project.
+        getAll: function(projectID) {
+            if (!(projectID in self.recentByProject)) {
+                self.initForProject(projectID);
+            }
+            return self.recentByProject[projectID].items;
+        },
+
+        // updateRecent adds or updates an existing item in a project.
+        update: function(projectID, name, itemType, state) {
+            var item;
+            var recents = self.recentByProject[projectID].items;
+            var i = _.indexOf(recents, function(item) {
+                return item.name === name && item.itype === itemType;
+            });
+
+            if (i !== -1) {
+                // Found existing, remove it from the list so we can add
+                // it to the front. The list is not that large so while
+                // this is an expensive operation, in a small list we
+                // won't notice it. Also need to update it with its
+                // new state.
+                item = recents[i];
+                item.state = state;
+                recents.splice(i, 1);
+                recents.unshift(item);
+            } else {
+                item = self.newItem(itemType, state, name);
+                // Add to beginning of list so the most recently added items
+                // are the first items.
+                recents.unshift(item);
+            }
+        },
+
+        icon: function(itemType) {
+            switch (itemType) {
+            case "process": return "fa-code-fork";
+            case "sample": return "fa-cubes";
+            case "note": return "fa-edit";
+            case "file": return "fa-file";
+            case "directory": return "fa-folder";
+            case "review": return "fa-comment";
+            default: return "";
+            }
+        }
+    };
+}
--- website/app/sidebar.js
+++ website/app/sidebar.js
@@ -11,11 +11,38 @@ function sidebarDirective() {
 }
 
 Application.Controllers.controller("sidebarDirectiveController",
-                                   ["$scope", "model.projects", sidebarDirectiveController]);
+                                   ["$scope", "model.projects", "recent",
+                                    sidebarDirectiveController]);
 
-function sidebarDirectiveController($scope, projects) {
+function sidebarDirectiveController($scope, projects, recent) {
+    function computeProjectSizes(project) {
+        var totalFiles = 0;
+        $scope.project.projectSize = bytesToSizeStr(project.size);
+        for (var key in project.mediatypes) {
+            totalFiles += project.mediatypes[key];
+        }
+        $scope.project.fileCount = numberWithCommas(totalFiles);
+    }
+
+    $scope.showProjects = false;
+    $scope.showAllRecent = false;
+
     projects.getList().then(function(p) {
         $scope.projects = p;
         $scope.project = $scope.projects[0];
+        computeProjectSizes($scope.project);
+        $scope.recents = recent.getAll($scope.project.id);
     });
+
+    $scope.recentIcon = function(itemType) {
+        return recent.icon(itemType);
+    };
+
+    $scope.setProject = function(project) {
+        $scope.project = project;
+        $scope.showProjects = false;
+        if (!('fileCount' in $scope.project)) {
+            computeProjectSizes(project);
+        }
+    };
 }
