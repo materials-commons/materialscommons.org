4ace6c82859cf87936933275de13ac1bf188ab14
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-14 14:00:52 -0400

    Activate menu items and clean up css alignment for font awesome icons.

--- website/app/sidebar.js
+++ website/app/sidebar.js
@@ -11,10 +11,10 @@ function sidebarDirective() {
 }
 
 Application.Controllers.controller("sidebarDirectiveController",
-                                   ["$scope", "model.projects", "recent",
+                                   ["$scope", "model.projects", "recent", "current",
                                     sidebarDirectiveController]);
 
-function sidebarDirectiveController($scope, projects, recent) {
+function sidebarDirectiveController($scope, projects, recent, current) {
     function computeProjectSizes(project) {
         var totalFiles = 0;
         $scope.project.projectSize = bytesToSizeStr(project.size);
@@ -31,6 +31,7 @@ function sidebarDirectiveController($scope, projects, recent) {
     projects.getList().then(function(p) {
         $scope.projects = p;
         $scope.project = $scope.projects[0];
+        current.setProject($scope.project);
         computeProjectSizes($scope.project);
         $scope.recents = recent.getAll($scope.project.id);
     });
@@ -41,6 +42,7 @@ function sidebarDirectiveController($scope, projects, recent) {
 
     $scope.setProject = function(project) {
         $scope.project = project;
+        current.setProject(project);
         $scope.showProjects = false;
         if (!('fileCount' in $scope.project)) {
             computeProjectSizes(project);
