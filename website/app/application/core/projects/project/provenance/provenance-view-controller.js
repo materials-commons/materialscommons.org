7e1f7ff64d23384aa4d6d55d5451ec6c63a99bea
tammasr <tammasr@umich.edu>
2014-10-15 15:39:32 -0400

    commiting the provenance view

--- website/app/application/core/projects/project/provenance/provenance-view-controller.js
+++ website/app/application/core/projects/project/provenance/provenance-view-controller.js
@@ -1,25 +1,59 @@
 Application.Controllers.controller("projectProvenanceView",
-    ["$scope", "mcapi", "$stateParams", "model.projects", "ProcessList", projectProvenanceView]);
+    ["$scope", "mcapi", "$stateParams", "model.projects", "ProcessList", "User", "pubsub", projectProvenanceView]);
 
-function projectProvenanceView($scope, mcapi, $stateParams, Projects, ProcessList){
+function projectProvenanceView($scope, mcapi, $stateParams, Projects, ProcessList, User, pubsub) {
 
-    $scope.getProcessDetails = function(){
-        $scope.processes = ProcessList.getProcesses($stateParams.id);
-       console.log($scope.processes)
+    $scope.openProcess = function (p) {
+        $scope.flag = false;
+        $scope.sample = {}
+        $scope.settings = []
+        $scope.process = p;
     }
-    $scope.createName = function(name) {
+
+    $scope.createName = function (name) {
         if (name.length > 12) {
-            return name.substring(0,10)+"...";
+            return name.substring(0, 20) + "...";
         }
         return name;
     };
-    function init(){
-        Projects.get($stateParams.id).then(function(project)
-        {
+    $scope.expand = function (df) {
+        $scope.flag = false;
+        $scope.active = df.id;
+        $scope.datafile = df;
+    }
+
+    $scope.isActiveList = function (k) {
+        if (k == $scope.active) {
+            return true
+        }
+        return false
+    }
+
+    $scope.showDetails = function (key, values) {
+        $scope.flag = true;
+        $scope.active = key;
+        if (key == 'sample') {
+            $scope.settings = [];
+            mcapi('/objects/%', values[0].value)
+                .success(function (data) {
+                    $scope.sample = data.sample;
+                }).jsonp();
+        } else {
+            $scope.key = key;
+            $scope.settings = values;
+        }
+
+    }
+
+    function init() {
+        $scope.apikey = User.apikey();
+
+        Projects.get($stateParams.id).then(function (project) {
             $scope.project = project;
-            $scope.getProcessDetails();
+            $scope.processes = ProcessList.getProcesses($stateParams.id);
         });
 
     }
+
     init();
 }
