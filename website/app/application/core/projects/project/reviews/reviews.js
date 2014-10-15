cd1e8c7d41289046ccc86f4f81e6292b71f485b2
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-15 15:34:31 -0400

    Port reviews over to new interface.

--- website/app/application/core/projects/project/reviews/reviews.js
+++ website/app/application/core/projects/project/reviews/reviews.js
@@ -1,23 +1,96 @@
-Application.Controllers.controller("projectReviews",
-    ["$scope",  projectReviews]);
+Application.Controllers.controller('projectReviews',
+                                   ["$scope", "mcapi", "$filter", "dateGenerate", "User",
+                                    "model.projects", "project", projectReviews]);
 
-function projectReviews($scope) {
-    $scope.activeLink = 'View Reviews';
-    $scope.setActiveLink = function(tabID) {
-        $scope.activeLink = tabID;
+function projectReviews($scope, mcapi, $filter, dateGenerate, User, Projects, project) {
+    $scope.viewReview = function (review) {
+        $scope.model = {
+            selected: false,
+            comment: ""
+        };
+        mcapi('/reviews/%', review.id)
+            .success(function (data) {
+                $scope.highlight_review = review;
+                $scope.review = data;
+            }).jsonp();
     };
 
-    $scope.isActiveLink = function (tabID) {
-        if (tabID == $scope.activeLink){
-            return true
-        }
-        return false
+    $scope.editReview = function(index){
+        $scope.edit_index = index;
     };
-    $scope.reportsMenu = [
-        {
-            title:"By Activity",
-            action: ""
-        }
-    ];
 
+    $scope.saveReview = function(index) {
+        if ($scope.review.messages[index].message === "") {
+            return;
+        }
+        $scope.edit_index = -1;
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+            }).put({'messages': $scope.review.messages});
+    };
+
+    $scope.addComment = function() {
+        if ($scope.model.comment.length === 0) {
+            return;
+        }
+        $scope.review.messages.push({
+            'message': $scope.model.comment,
+            'who': User.u(),
+            'date': dateGenerate.new_date()
+        });
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function (data) {
+            }).put({'messages': $scope.review.messages});
+    };
+
+    $scope.closeReview = function() {
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function () {
+                $scope.loadProjectReviews(project.id, true);
+                $scope.review = '';
+            }).put({'status': 'close'});
+    };
+
+    $scope.reOpenReview = function() {
+        mcapi('/reviews/%', $scope.review.id)
+            .success(function () {
+                $scope.loadProjectReviews(project.id, true);
+                $scope.review = '';
+            }).put({'status': 'open'});
+    };
+
+    $scope.loadProjectReviews = function (id, reload) {
+        Projects.getList(reload).then(function (projects) {
+            Projects.get(id).then(function (p) {
+                project = p;
+                $scope.project = p;
+                $scope.reviewCount();
+
+            });
+        });
+    };
+
+    $scope.showReviewsperStatus = function (status) {
+        $scope.status = status;
+    };
+
+    $scope.reviewCount = function(){
+        $scope.open_reviews = $filter('byKey')(project.reviews, 'status', 'open');
+        $scope.closed_reviews = $filter('byKey')(project.reviews, 'status', 'close');
+    };
+
+    function init() {
+        $scope.review = '';
+        $scope.status = 'open';
+        $scope.project = project;
+        $scope.reviewCount();
+
+        $scope.model = {
+            new_review: "",
+            assigned_to: "",
+            title: ""
+        };
+    }
+
+    init();
 }
