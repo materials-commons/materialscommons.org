055db906abc56c7867c6e628e30ce54c0a00bfff
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-15 15:34:00 -0400

    Port notes over to new interface.

--- website/app/application/core/projects/project/notes/notes.js
+++ website/app/application/core/projects/project/notes/notes.js
@@ -1,17 +1,31 @@
 Application.Controllers.controller("projectNotes",
-    ["$scope", projectNotes]);
+    ["$scope", "model.projects", "mcapi", "project", projectNotes]);
 
-function projectNotes($scope) {
-    $scope.activeLink = 'View Notes';
-    $scope.setActiveLink = function(tabID) {
-        $scope.activeLink = tabID;
+function projectNotes($scope, Projects, mcapi, project) {
+    $scope.saveData = function () {
+        mcapi('/projects/%', $scope.project.id)
+            .success(function (data) {
+                $scope.reloadProject(true);
+            }).put($scope.project);
     };
 
-    $scope.isActiveLink = function (tabID) {
-        if (tabID == $scope.activeLink){
-            return true
-        }
-        return false
+    $scope.reloadProject = function(reload){
+        Projects.getList(reload).then(function () {
+        });
     };
 
+    $scope.updateNote = function () {
+        $scope.saveData();
+        $scope.edit_index = -1;
+    };
+
+    $scope.editNotes = function (index) {
+        $scope.edit_index = index;
+    };
+
+    function init() {
+        $scope.project = project;
+    }
+
+    init();
 }
