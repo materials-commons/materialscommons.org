0f306d4c7e0096a6962155c1e0a1c68c3499afc3
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-17 11:56:24 -0400

    Add create project to list of projects.

--- website/app/index/sidebar-projects.js
+++ website/app/index/sidebar-projects.js
@@ -11,17 +11,30 @@ function sidebarProjectsDirective() {
 
 Application.Controllers.controller("sidebarProjectsDirectiveController",
                                    ["$scope", "current", "sidebarUtil",
+                                    "mcapi", "model.projects",
                                     sidebarProjectsDirectiveController]);
 
-function sidebarProjectsDirectiveController($scope, current, sidebarUtil) {
+function sidebarProjectsDirectiveController($scope, current, sidebarUtil, mcapi, Projects) {
     $scope.showProjects = false;
 
     $scope.setProject = function(project) {
-        console.log("setProject called");
         $scope.project = project;
         current.setProject(project);
         $scope.showProjects = false;
         $scope.project.fileCount = sidebarUtil.projectFileCount($scope.project);
         $scope.project.projectSize = sidebarUtil.projectSize($scope.project);
+    };
+
+    $scope.createProject = function(){
+        if ($scope.model.name === "") {
+            return;
+        }
+        mcapi('/projects')
+            .success(function (data) {
+                console.dir(data);
+                Projects.getList(true).then(function(projects) {
+                    $scope.projects = projects;
+                });
+            }).post({'name': $scope.model.name});
     };
 }
