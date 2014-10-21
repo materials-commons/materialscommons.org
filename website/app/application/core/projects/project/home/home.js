988421d5b68b258fd7c38b093332aa2354fd5cbf
tammasr <tammasr@umich.edu>
2014-10-21 16:32:56 -0400

    Adding directives for Home-reviews-files and samples

--- website/app/application/core/projects/project/home/home.js
+++ website/app/application/core/projects/project/home/home.js
@@ -1,15 +1,22 @@
 Application.Controllers.controller('projectHome',
-                                   ["$scope", "project", "User","mcapi", projectHome]);
+    ["$scope", "project", "User", "mcapi", projectHome]);
 
 function projectHome($scope, project, User, mcapi) {
-    $scope.project = project;
-    $scope.mcuser = User.attr();
 
-    $scope.updateName = function(){
+
+    $scope.updateName = function () {
         mcapi('/users/%', $scope.mcuser.email)
             .success(function (u) {
                 $scope.editFullName = false;
                 User.save($scope.mcuser);
             }).put({fullname: $scope.mcuser.fullname});
     }
+
+    function init(){
+        $scope.project = project;
+        console.log($scope.project);
+        $scope.mcuser = User.attr();
+
+    }
+    init();
 }
---
+++ website/app/application/directives/home-files-directive.js
@@ -0,0 +1,26 @@
+Application.Controllers.controller("homeFilesController",
+    ["$scope",  function ($scope) {
+
+        function init() {
+            $scope.projectSize = bytesToSizeStr($scope.project.size);
+            var totalFiles = 0, key;
+            for (key in $scope.project.mediatypes) {
+                totalFiles += $scope.project.mediatypes[key];
+            }
+            $scope.fileCount = numberWithCommas(totalFiles);
+
+        }
+
+        init();
+    }]);
+Application.Directives.directive('homeFiles',
+    function () {
+        return {
+            restrict: "A",
+            controller: 'homeFilesController',
+            scope: {
+                project: '=project'
+            },
+            templateUrl: 'application/directives/home-files.html'
+        };
+    });
