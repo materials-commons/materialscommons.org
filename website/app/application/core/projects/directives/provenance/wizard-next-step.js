998a55a218ed729bff4f19ff4099dd742c6ccd05
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-21 12:29:37 -0400

    Change how we determine whether to show the checkboxes by using the project state and project files to explicitly set the view.

--- website/app/application/core/projects/directives/provenance/wizard-next-step.js
+++ website/app/application/core/projects/directives/provenance/wizard-next-step.js
@@ -10,16 +10,18 @@ function wizardNextStepDirective() {
 }
 
 Application.Controllers.controller('wizardNextStepDirectiveController',
-                                   ["$scope", "provStep", "pubsub", "projectState", "$stateParams",
+                                   ["$scope", "provStep", "pubsub", "projectState", "$stateParams", "projectFiles",
                                     "ui", wizardNextStepDirectiveController]);
-function wizardNextStepDirectiveController($scope, provStep, pubsub, projectState, $stateParams, ui) {
+function wizardNextStepDirectiveController($scope, provStep, pubsub, projectState, $stateParams, ui, projectFiles) {
     pubsub.waitOn($scope, "provenance.wizard.step", function() {
         $scope.wizardState = projectState.get($stateParams.id, $stateParams.sid);
         $scope.step = provStep.getCurrentStep($scope.wizardState.project.id);
         if ($scope.step.step === "files") {
             ui.setShowFiles($stateParams.id, true);
+            projectFiles.setActive($stateParams.id, true);
         } else {
             ui.setShowFiles($stateParams.id, false);
+            projectFiles.setActive($stateParams.id, false);
         }
     });
 
