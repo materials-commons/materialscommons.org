6fa0ae5c9cbeecbe66a1933f28a66b611352a339
tammasr <tammasr@umich.edu>
2014-10-22 15:04:55 -0400

    changes to convert.py script - added counts, sizes and generic mime type description to projects

--- website/app/application/directives/home-reviews-directive.js
+++ website/app/application/directives/home-reviews-directive.js
@@ -1,14 +1,23 @@
 Application.Controllers.controller("homeReviewsController",
-    ["$scope", "$filter", function ($scope, $filter) {
+    ["$scope", "$filter", "User", function ($scope, $filter, User) {
 
         $scope.reviewsCard = function () {
             $scope.open_reviews = $filter('reviewFilter')($scope.project.reviews, 'open');
             $scope.closed_reviews = $filter('reviewFilter')($scope.project.reviews, 'closed');
             //Recent reviews
-            $scope.recent_reviews = $scope.open_reviews.slice(0, 4);
+            var y = new Date($scope.user.last_login.epoch_time * 1000).toString();
+            $scope.recent_reviews = [];
+            $scope.project.reviews.forEach(function(item){
+                var x = new Date(item.modifiedtime.epoch_time * 1000).toString();
+                if(x > y) {
+                    $scope.recent_reviews.push(item);
+                }
+            })
+
         }
 
         function init() {
+            $scope.user = User.attr();
             $scope.reviewsCard();
         }
 
