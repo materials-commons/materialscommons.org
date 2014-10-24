24bdffab0162136f186af7fd99ede4d665666a87
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-24 15:29:03 -0400

    Fix New menu to work with state.

--- website/app/index/navbar.js
+++ website/app/index/navbar.js
@@ -11,10 +11,10 @@ function navbarDirective() {
 }
 
 Application.Controllers.controller("navbarDirectiveController",
-                                   ["$scope", "ui", "current", "$state",
+                                   ["$scope", "current", "$state", "projectState",
                                     navbarDirectiveController]);
 
-function navbarDirectiveController($scope, ui, current, $state) {
+function navbarDirectiveController($scope, current, $state, projectState) {
     // This is needed to toggle the menu closed when an item is selected.
     // This is a part of how ui-bootstrap interacts with the menus and
     // the menu item does an ng-click.
@@ -25,7 +25,9 @@ function navbarDirectiveController($scope, ui, current, $state) {
     $scope.create = function(action) {
         var projectID = current.projectID();
         var route = "projects.project." + action + ".create";
+        var state = null;
+        var stateID = projectState.add(projectID, state);
         $scope.status = false;
-        $state.go(route, {id: projectID});
+        $state.go(route, {id: projectID, sid: stateID});
     };
 }
