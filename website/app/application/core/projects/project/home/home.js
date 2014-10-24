906acfadbe623e7ae84d5cd380f1561b964c688a
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-24 09:33:15 -0400

    Remove init.

--- website/app/application/core/projects/project/home/home.js
+++ website/app/application/core/projects/project/home/home.js
@@ -1,21 +1,15 @@
 Application.Controllers.controller('projectHome',
-    ["$scope", "project", "User", "mcapi", projectHome]);
+                                   ["$scope", "project", "User", "mcapi", projectHome]);
 
 function projectHome($scope, project, User, mcapi) {
-
-
     $scope.updateName = function () {
         mcapi('/users/%', $scope.mcuser.email)
             .success(function (u) {
                 $scope.editFullName = false;
                 User.save($scope.mcuser);
             }).put({fullname: $scope.mcuser.fullname});
-    }
+    };
 
-    function init(){
-        $scope.project = project;
-        $scope.mcuser = User.attr();
-
-    }
-    init();
+    $scope.project = project;
+    $scope.mcuser = User.attr();
 }
