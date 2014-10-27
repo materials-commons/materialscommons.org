02bb9ed33ebacbe8e5f0f1790db8f8703573f267
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-27 16:10:53 -0400

    Styling and move movement of elements around. Tie functionality into the javascript code.

--- website/app/application/core/projects/project/provenance/overview.js
+++ website/app/application/core/projects/project/provenance/overview.js
@@ -1,18 +1,23 @@
 Application.Controllers.controller("projectProvenanceOverview",
-    ["$scope", "mcapi", "$stateParams", "model.projects", "ProcessList", "User", projectProvenanceOverview]);
+                                   ["$scope", "mcapi", "$stateParams", "model.projects", "ProcessList", "User", projectProvenanceOverview]);
 
 function projectProvenanceOverview($scope, mcapi, $stateParams, Projects, ProcessList, User) {
-
-    $scope.openProcess = function (p) {
+    var activeIndex = 0;
+    $scope.openProcess = function (index) {
         $scope.flag = false;
         $scope.sample = {};
         $scope.settings = [];
-        $scope.process = p;
+        $scope.process = $scope.processes[index];
+        activeIndex = index;
+    };
+
+    $scope.isActive = function(index) {
+        return activeIndex === index;
     };
 
     $scope.createName = function (name) {
-        if (name.length > 12) {
-            return name.substring(0, 25) + "...";
+        if (name.length > 25) {
+            return name.substring(0, 22) + "...";
         }
         return name;
     };
@@ -46,14 +51,15 @@ function projectProvenanceOverview($scope, mcapi, $stateParams, Projects, Proces
         }
     };
 
-    function init() {
-        $scope.apikey = User.apikey();
-        Projects.get($stateParams.id).then(function (project) {
-            $scope.project = project;
-            $scope.processes = ProcessList.getProcesses($stateParams.id);
-            $scope.showInputsOutputs = true;
+    $scope.apikey = User.apikey();
+    Projects.get($stateParams.id).then(function (project) {
+        $scope.project = project;
+        ProcessList.getProcesses($stateParams.id).then(function(processes) {
+            $scope.processes = processes;
+            if ($scope.processes.length !== 0) {
+                $scope.openProcess(0);
+            }
         });
-
-    }
-    init();
+        $scope.showInputsOutputs = true;
+    });
 }
