fc891f50463da58c1e660e163c038d20623f1098
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-10-26 21:25:52 -0400

    Minor changes for provenance layout and formatting code.

--- website/app/application/services/process-list-service.js
+++ website/app/application/services/process-list-service.js
@@ -1,23 +1,24 @@
 Application.Services.factory('ProcessList',
-    ["Restangular", "pubsub",
-        function (Restangular, pubsub) {
-            var service = {
-                processes: {}
-            }
-            return {
-                getProcesses: function(project_id){
-                    if (service.processes.hasOwnProperty(project_id)){
-                        return service.processes[project_id];
-                    }else{
-                        this.loadProcesses(project_id);
-                    }
-                },
+                             ["Restangular", "pubsub", processListService]);
+function processListService(Restangular, pubsub) {
+    var service = {
+        processes: {}
+    };
 
-                loadProcesses: function (project_id) {
-                    Restangular.one('processes').one('project', project_id).getList().then(function(data){
-                        service.processes[project_id] = data;
-                    });
-                }
+    return {
+        getProcesses: function(project_id){
+            if (service.processes.hasOwnProperty(project_id)){
+                return service.processes[project_id];
+            } else {
+                this.loadProcesses(project_id);
             }
+        },
 
-        }]);
+        loadProcesses: function (project_id) {
+            Restangular.one('processes').one('project', project_id).getList().then(function(data){
+                service.processes[project_id] = data;
+            });
+        }
+    };
+
+}
