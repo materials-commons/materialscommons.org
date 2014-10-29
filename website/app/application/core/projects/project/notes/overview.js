80473aca14f53ea10a7022b028815d7d4e68bfb8
tammasr <tammasr@umich.edu>
2014-10-29 15:38:31 -0400

    info box , added title to notes and edit feature from homepage

--- website/app/application/core/projects/project/notes/overview.js
+++ website/app/application/core/projects/project/notes/overview.js
@@ -1,7 +1,7 @@
 Application.Controllers.controller("projectNotesOverview",
-    ["$scope", "project", "User", projectNotesOverview]);
+    ["$scope", "project", "User",  "$stateParams", projectNotesOverview]);
 
-function projectNotesOverview($scope, project, User) {
+function projectNotesOverview($scope, project, User, $stateParams) {
     function saveNote() {
         project.put(User.keyparam()).then(function(){
             // nothing to do yet.
@@ -16,7 +16,14 @@ function projectNotesOverview($scope, project, User) {
     $scope.editNotes = function (index) {
         $scope.edit_index = index;
     };
-
     $scope.project = project;
     $scope.edit_index = -1;
+
+    function init(){
+        if ($stateParams.index == 0 || $stateParams.index ){
+            $scope.editNotes($stateParams.index);
+        }
+    }
+    init();
+    init();
 }
