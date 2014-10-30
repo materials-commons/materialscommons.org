debef5fa6b82eaf86560f060e5303da431e7ddba
V. Glenn Tarcea <gtarcea@umich.edu>
2014-10-30 13:19:55 -0400

    code reformatting. Update mcuser to add last_login and preferences to new users, and allow a new user to be setup as an admin.

--- backend/scripts/q.py
+++ backend/scripts/q.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import rethinkdb as r
 import json
+import sys
 
 if __name__ == "__main__":
     conn = r.connect("localhost", 30815, db="materialscommons")
-    pids = ["3b5d8aaa-67e0-4ea7-b4c7-ec0864029e60", "e65cb50d-6461-410e-9d27-56fb93486c2b", "286b4d3b-cdc8-4123-823f-d45377c77ebb"]
+    processes = r.table("processes")\
+                 .get_all("b1915a4a-543f-4fff-ae8e-66916b0cbfa7", index="project_id")\
+                 .run(conn, time_format="raw")
+    by_ids = {p['id']: p for p in processes}
     all = list(r.table("property_sets")
-               .get_all(*pids, index="item_id")
+               .get_all(*by_ids.keys(), index="item_id")
                .eq_join("id", r.table("properties"), index="item_id")
                .map(lambda row: row.merge({
-                   "right": {
+                   "left": {
                        "ps_id": row["left"]["id"],
                        "ps_name": row["left"]["name"],
                        "ps_item_id": row["left"]["item_id"],
@@ -25,4 +29,12 @@ if __name__ == "__main__":
                    }
                })
                .zip().run(conn, time_format="raw"))
-    print(json.dumps(all))
+    feeder_ids = [prop['value'] for prop in all
+                  if prop["ptype"] == "file" or prop["ptype"] == "sample"]
+    query = r.table("properties")\
+             .get_all(*feeder_ids, index="value")\
+             .eq_join("item_id", r.table("property_sets"))\
+             .zip()\
+             .eq_join("item_id", r.table("processes"))\
+             .zip()
+    print(json.dumps(list(query.run(conn, time_format="raw"))))
