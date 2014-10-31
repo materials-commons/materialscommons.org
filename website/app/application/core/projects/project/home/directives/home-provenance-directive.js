c8ecfbde743932c1d7f4409d9d821d897553d4f5
tammasr <tammasr@umich.edu>
2014-10-31 11:23:28 -0400

    provenance homepage

--- website/app/application/core/projects/project/home/directives/home-provenance-directive.js
+++ website/app/application/core/projects/project/home/directives/home-provenance-directive.js
@@ -14,6 +14,7 @@ Application.Controllers.controller("homeProvenanceDirectiveController",
                                    ["$scope", "projectState", "$state",
                                     homeProvenanceDirectiveController]);
 function homeProvenanceDirectiveController($scope, projectState, $state) {
+    console.log($scope.project);
     $scope.addProvenance = function() {
         var stateID = projectState.add($scope.project.id);
         $state.go("projects.project.provenance.create", {sid: stateID});
