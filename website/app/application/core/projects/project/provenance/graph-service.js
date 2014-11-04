ea1806509885f9af9e764200dfd321b97f832349
tammasr <tammasr@umich.edu>
2014-11-04 17:09:09 -0500

    grapgh service

---
+++ website/app/application/core/projects/project/provenance/graph-service.js
@@ -0,0 +1,187 @@
+Application.Services.factory("Graph", [graphService]);
+function graphService() {
+    var graph = {
+        nodes: [],
+        edges: [],
+        network_data: {},
+        network_options: {},
+        count: 1,
+        item_count: '',
+        times_linked_item_to_process: '',
+
+        constructGraph: function (process) {
+            graph.nodes = [];
+            graph.edges = [];
+            graph.count = 1;
+            graph.item_count = '';
+            graph.times_linked_item_to_process = '';
+            //Set up initial node (your main process)
+            graph.nodes.push({
+                id: 0,
+                label: String(process.name),
+                shape: 'dot',
+                color: {
+                    background: '#FF7F6E',
+                    border: "#666"
+                }
+            });
+            graph.nodes[0]['level'] = 2;
+            //build left side graph
+            angular.forEach(process.inputs, function (values, key) {
+                if (key === 'files') {
+                    if (values.length !== 0) {
+                        graph.buildFiles(values, process.output_processes, 'upstream');
+                    }
+                } else if (key === 'sample') {
+                    graph.buildSamples(values, process.input_processes, 'upstream');
+                } else {
+                    graph.buildSettings(key);
+                }
+            });
+
+            angular.forEach(process.outputs, function (values, key) {
+                if (key === 'files') {
+                    if (values.length !== 0) {
+                        graph.buildFiles(values, process.input_processes, 'downstream');
+                    }
+                } else if (key === 'sample') {
+                    graph.buildSamples(values, process.output_processes, 'downstream');
+                } else {
+                    graph.buildSettings(key);
+                }
+            });
+            graph.network_data = {
+                nodes: graph.nodes,
+                edges: graph.edges
+            };
+            graph.network_options = {
+                hierarchicalLayout: {
+                    direction: "LR"
+                },
+                edges: {style: "arrow"},
+                smoothCurves: false
+
+            };
+            return graph;
+        },
+
+        constructEdge: function (from, to) {
+            graph.edges.push({
+                from: from,
+                to: to
+            });
+        },
+        constructNode: function (name, type) {
+            if (type === 'process') {
+                graph.nodes.push({
+                    id: graph.count,
+                    label: String(name),
+                    title: name,
+                    shape: 'dot',
+                    color: {
+                        background: '#FF7F6E',
+                        border: "#666"
+                    }
+                });
+            } else {
+                graph.nodes.push({
+                    id: graph.count,
+                    label: String(name),
+                    title: name,
+                    shape: 'triangle',
+                    color: {
+                        background: '#109618',
+                        border: "#666"
+                    }
+                });
+            }
+
+        },
+
+        buildFiles: function (files, available_processes, stream) {
+            files.forEach(function (file) {
+                graph.constructNode(file.other.name, 'file');
+                if (stream === 'upstream') {
+                    graph.constructEdge(graph.count, 0);
+                    graph.nodes[graph.count]['level'] = 1;
+                } else {
+                    graph.constructEdge(0, graph.count);
+                    graph.nodes[graph.count]['level'] = 3;
+                }
+
+                graph.count++;
+                graph.connectProcesses(available_processes, file.other.name, stream);
+                //check for adjacent processes of this file
+
+            });
+        },
+        buildSamples: function (samples, available_processes, stream) {
+            samples.forEach(function (sample) {
+                graph.constructNode(sample.other.name, 'sample');
+                if (stream === 'upstream') {
+                    graph.constructEdge(graph.count, 0);
+                    graph.nodes[graph.count]['level'] = 1;
+                } else {
+                    graph.constructEdge(0, graph.count);
+                    graph.nodes[graph.count]['level'] = 3;
+                }
+                graph.count++;
+                console.log(sample.other.name)
+                graph.connectProcesses(available_processes, sample.other.name, stream);
+                //check for adjacent processes of this file
+
+            });
+        },
+        buildSettings: function (key) {
+            graph.constructNode(key, 'setting');
+            graph.constructEdge(graph.count, 0);
+            graph.nodes[graph.count]['level'] = 1;
+            graph.count++;
+        },
+
+        connectProcesses: function (available_processes, item_name, stream) {
+            graph.item_count = graph.count - 1;
+            graph.times_linked_item_to_process = 0;
+            angular.forEach(available_processes, function (process, key) {
+                if (process.related_files.indexOf(item_name) > -1) {
+                    console.log(process.related_files);
+                    graph.times_linked_item_to_process++;
+                    graph.nodes.push({
+                        id: graph.count,
+                        label: String(process.process_name),
+                        title: process.process_name,
+                        shape: 'dot',
+                        color: {
+                            background: '#FF7F6E',
+                            border: "#666"
+                        }
+
+                    });
+                    if (graph.times_linked_item_to_process > 0) {
+                        if (stream === 'upstream') {
+                            graph.constructEdge(graph.count, graph.item_count);
+                            graph.nodes[graph.count]['level'] = 0;
+
+                        } else {
+                            graph.constructEdge(graph.item_count, graph.count);
+                            graph.nodes[graph.count]['level'] = 4;
+
+                        }
+                    } else {
+                        if (stream === 'upstream') {
+                            graph.constructEdge(graph.count, graph.count - 1);
+                            graph.nodes[graph.count]['level'] = 0;
+
+                        } else {
+                            graph.constructEdge(graph.count - 1, graph.count);
+                            graph.nodes[graph.count]['level'] = 4;
+
+                        }
+                    }
+                    graph.count++;
+                }
+            });
+        }
+    };
+    return graph;
+};
--- website/app/application/core/projects/project/provenance/overview.js
+++ website/app/application/core/projects/project/provenance/overview.js
@@ -1,7 +1,7 @@
 Application.Controllers.controller("projectProvenanceOverview",
-    ["$scope", "mcapi", "project", "User", projectProvenanceOverview]);
+    ["$scope", "mcapi", "project", "User", "Graph", projectProvenanceOverview]);
 
-function projectProvenanceOverview($scope, mcapi, project, User) {
+function projectProvenanceOverview($scope, mcapi, project, User, Graph) {
 
 
     $scope.isActive = function (index) {
@@ -53,56 +53,252 @@ function projectProvenanceOverview($scope, mcapi, project, User) {
         $scope.process.network_data = {};
         $scope.process.network_options = {};
         activeIndex = index;
-        var nodes = [], count = 1, edges = [];
-        $scope.process.network_data = {},
-            $scope.process.network_options = {},
-            nodes.push({
-                id: 0,
-                label: String($scope.createName($scope.process.name)) ,
-                shape: 'dot'
-            });
-        nodes[0]['level'] = 1;
-        angular.forEach($scope.process.output_processes, function (values, key) {
-            nodes.push({
-                id: count,
-                label: String($scope.createName(values.process_name)),
-                shape: 'dot',
-                title: values.related_files
-            });
-            edges.push({
-                from: count,
-                to: 0
-            });
-            nodes[count]['level'] = 0;
-            count++;
-        });
-        angular.forEach($scope.process.input_processes, function (values, key) {
-            nodes.push({
-                id: count,
-                label: String($scope.createName(values.process_name)),
-                shape: 'dot',
-                title: values.related_files
+        console.log($scope.process);
+        $scope.graph = Graph.constructGraph($scope.process);
+        $scope.constructed_process = $scope.graph;
 
-            });
-            edges.push({
-                from: 0,
-                to: count
-            });
-            nodes[count]['level'] = 2;
-            count++;
-        });
-        $scope.process.network_data= {
-            nodes: nodes,
-            edges: edges
-        };
-        $scope.process.network_options = {
-            hierarchicalLayout: {
-                direction: "LR"
-            },
-            edges: {style: "arrow"},
-            smoothCurves: false
-        };
-        $scope.constructed_process = $scope.process;
+        //
+        //
+        //
+        //
+        //var nodes = [], count = 1, edges = [];
+        //console.log($scope.process);
+        //$scope.process.network_data = {};
+        //$scope.process.network_options = {};
+        //nodes.push({
+        //    id: 0,
+        //    label: String($scope.createName($scope.process.name)),
+        //    shape: 'dot',
+        //    color: {
+        //        background: '#FF7F6E',
+        //        border: "#666"
+        //    }
+        //});
+        //nodes[0]['level'] = 2;
+        //angular.forEach($scope.process.inputs, function (values, key) {
+        //    if (key === 'files') {
+        //        if (values.length !== 0) {
+        //            values.forEach(function (file) {
+        //                nodes.push({
+        //                    id: count,
+        //                    label: String($scope.createName(file.other.name)),
+        //                    title: file.other.name,
+        //                    shape: 'triangle',
+        //                    color: {
+        //                        background: '#109618',
+        //                        border: "#666"
+        //                    }
+        //
+        //                });
+        //                edges.push({
+        //                    from: count,
+        //                    to: 0
+        //                });
+        //                nodes[count]['level'] = 1;
+        //                count++;
+        //                //check for adjacent processes of this file
+        //                angular.forEach($scope.process.input_processes, function (process, key) {
+        //                    if (process.related_files.indexOf(file.other.name) > -1) {
+        //                        nodes.push({
+        //                            id: count,
+        //                            label: String($scope.createName(process.process_name)),
+        //                            title: process.process_name,
+        //                            shape: 'dot',
+        //                            color: {
+        //                                background: '#FF7F6E',
+        //                                border: "#666"
+        //                            }
+        //
+        //                        });
+        //                        edges.push({
+        //                            from: count,
+        //                            to: count - 1
+        //                        });
+        //                        nodes[count]['level'] = 0;
+        //                        count++;
+        //                    }
+        //                });
+        //
+        //            });
+        //        }
+        //    } else {
+        //        if (key === 'sample') {
+        //            var remember_sample_count = '';
+        //            nodes.push({
+        //                id: count,
+        //                label: String($scope.createName(values[0].other.name)),
+        //                title: values[0].other.name,
+        //                shape: 'triangle',
+        //                color: {
+        //                    background: '#109618',
+        //                    border: "#666"
+        //                }
+        //            });
+        //
+        //            edges.push({
+        //                from: count,
+        //                to: 0
+        //            });
+        //            nodes[count]['level'] = 1;
+        //            remember_sample_count = count;
+        //            var linked_sample_to_process = 0;
+        //            count++;
+        //            angular.forEach($scope.process.output_processes, function (process, key) {
+        //                if (process.related_files.indexOf(values[0].other.name) > -1) {
+        //                    linked_sample_to_process++;
+        //                    nodes.push({
+        //                        id: count,
+        //                        label: String($scope.createName(process.process_name)),
+        //                        title: process.process_name,
+        //                        shape: 'dot',
+        //                        color: {
+        //                            background: '#FF7F6E',
+        //                            border: "#666"
+        //                        }
+        //                    });
+        //                    if (linked_sample_to_process !== 0) {
+        //                        edges.push({
+        //                            from: remember_sample_count,
+        //                            to: count
+        //                        });
+        //                    } else {
+        //                        edges.push({
+        //                            from: count,
+        //                            to: count - 1
+        //                        });
+        //                    }
+        //                    nodes[count]['level'] = 0;
+        //                    count++;
+        //                }
+        //            });
+        //
+        //        } else {
+        //            nodes.push({
+        //                id: count,
+        //                label: String($scope.createName(key)),
+        //                title: key,
+        //                shape: 'triangle',
+        //                color: {
+        //                    background: '#109618',
+        //                    border: "#666"
+        //                }
+        //            });
+        //
+        //            edges.push({
+        //                from: count,
+        //                to: 0
+        //            });
+        //            nodes[count]['level'] = 1;
+        //            count++;
+        //        }
+        //
+        //    }
+        //});
+        //angular.forEach($scope.process.outputs, function (values, key) {
+        //    if (key === 'files' && values.length !== 0) {
+        //        var remember_file_count = '';
+        //        values.forEach(function (file) {
+        //            nodes.push({
+        //                id: count,
+        //                label: String($scope.createName(file.other.name)),
+        //                title: file.other.name,
+        //                shape: 'triangle',
+        //                color: {
+        //                    background: '#109618',
+        //                    border: "#666"
+        //                }
+        //            });
+        //            edges.push({
+        //                from: 0,
+        //                to: count
+        //            });
+        //            nodes[count]['level'] = 3;
+        //            remember_file_count = count;
+        //            var linked_file_to_process = 0;
+        //            count++;
+        //            angular.forEach($scope.process.input_processes, function (process, key) {
+        //                if (process.related_files.indexOf(file.other.name) > -1) {
+        //                    linked_file_to_process++;
+        //                    nodes.push({
+        //                        id: count,
+        //                        label: String($scope.createName(process.process_name)),
+        //                        title: process.process_name,
+        //                        shape: 'dot',
+        //                        color: {
+        //                            background: '#FF7F6E',
+        //                            border: "#666"
+        //                        }
+        //                    });
+        //                    if (linked_file_to_process !== 0) {
+        //                        edges.push({
+        //                            from: remember_file_count,
+        //                            to: count
+        //                        });
+        //                    } else {
+        //                        edges.push({
+        //                            from: count - 1,
+        //                            to: count
+        //                        });
+        //                    }
+        //                    nodes[count]['level'] = 4;
+        //                    count++;
+        //                }
+        //            });
+        //        });
+        //    } else {
+        //        if (key === 'sample') {
+        //            var remember_sample_count = '';
+        //            nodes.push({
+        //                id: count,
+        //                label: String($scope.createName(values[0].other.name)),
+        //                title: values[0].other.name,
+        //                shape: 'triangle',
+        //                color: {
+        //                    background: '#109618',
+        //                    border: "#666"
+        //                }
+        //            });
+        //            edges.push({
+        //                from: 0,
+        //                to: count
+        //            });
+        //            nodes[count]['level'] = 2;
+        //            count++;
+        //
+        //        } else {
+        //            nodes.push({
+        //                id: count,
+        //                label: String($scope.createName(key)),
+        //                title: key,
+        //                shape: 'triangle',
+        //                color: {
+        //                    background: '#109618',
+        //                    border: "#666"
+        //                }
+        //            });
+        //            edges.push({
+        //                from: 0,
+        //                to: count
+        //            });
+        //            nodes[count]['level'] = 2;
+        //            count++;
+        //        }
+        //
+        //    }
+        //
+        //});
+        //$scope.process.network_data = {
+        //    nodes: nodes,
+        //    edges: edges
+        //};
+        //$scope.process.network_options = {
+        //    hierarchicalLayout: {
+        //        direction: "LR"
+        //    },
+        //    edges: {style: "arrow"},
+        //    smoothCurves: false
+        //
+        //};
     };
 
     $scope.apikey = User.apikey();
@@ -111,7 +307,7 @@ function projectProvenanceOverview($scope, mcapi, project, User) {
     if ($scope.processes.length !== 0) {
 
         //Draw graph: with all connecting processes ( input and output )
-        $scope.processes.forEach(function(process){
+        $scope.processes.forEach(function (process) {
 
         });
         $scope.openProcess(0);
