7a495d195b4a0a02c96a3e76b27dcbf025e7751f
tammasr <tammasr@umich.edu>
2014-11-07 15:50:23 -0500

    provenance table

--- website/app/application/core/projects/project/provenance/custompop-over.js
+++ website/app/application/core/projects/project/provenance/custompop-over.js
@@ -1,11 +1,15 @@
 Application.Directives.directive("customPopOver", ["$compile",
     function ($compile) {
         var itemsTemplate = "<ul class='unstyled'><li ng-repeat='item in items'>{{item.item_type}}: {{item.item_name}}</li></ul>";
+        var processesTemplate = "<ul class='unstyled'><li ng-repeat='process in processes'>{{process.name}}</li></ul>";
         function getTemplate(contentType) {
             var template = '';
             switch (contentType) {
                 case 'items':
                     template = itemsTemplate;
+                    break;
+                case 'processes':
+                    template = processesTemplate;
                     break;
             }
             return template;
@@ -16,22 +20,33 @@ Application.Directives.directive("customPopOver", ["$compile",
             transclude: true,
             template: "<span ng-transclude></span>",
             link: function (scope, element, attrs) {
-                var popOverContent;
+                var popOverContent, html, options = {};
                 if (scope.items) {
-                    var html = getTemplate("items");
+                    html = getTemplate("items");
                     popOverContent = $compile(html)(scope);
+                    options = {
+                        content: popOverContent,
+                        placement: "left",
+                        html: true,
+                        title: scope.title
+                    };
+                }else{
+                    html = getTemplate("processes");
+                    popOverContent = $compile(html)(scope);
+                    options = {
+                        content: popOverContent,
+                        placement: "left",
+                        html: true,
+                        title: scope.title
+                    };
                 }
-                var options = {
-                    content: popOverContent,
-                    placement: "left",
-                    html: true,
-                    title: scope.title
-                };
+
                 $(element).popover(options);
             },
             scope: {
                 items: '=',
-                title: '='
+                title: '=' ,
+                processes: '='
             }
         };
     }]);
