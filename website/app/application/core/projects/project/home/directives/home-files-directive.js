ae63a89b9fed44514624c5bec7588262c9e32a14
V. Glenn Tarcea <gtarcea@umich.edu>
2014-12-04 16:31:48 -0500

    Back out changes for mediatype and move into a separate branch.

--- website/app/application/core/projects/project/home/directives/home-files-directive.js
+++ website/app/application/core/projects/project/home/directives/home-files-directive.js
@@ -11,23 +11,6 @@ function homeFilesDirective() {
 }
 
 Application.Controllers.controller("homeFilesDirectiveController",
-                                   ["$scope",  "projectFiles", homeFilesDirectiveController]);
-function homeFilesDirectiveController($scope, projectFiles) {
-    var id = $scope.project.id;
-
-    //console.log(projectFiles.model);
-    $scope.showFiles = function(mediatype) {
-        console.log("show files for: ", mediatype);
-        var files = [];
-        var treeModel = new TreeModel();
-        console.dir(projectFiles.model.projects[id].dir);
-        var root = treeModel.parse(projectFiles.model.projects[id].dir);
-        root.walk({strategy: 'pre'}, function(node) {
-            console.dir(node);
-            if (node.mediatype == mediatype) {
-                files.push(node);
-            }
-        });
-        console.dir(files);
-    };
+                                   ["$scope", homeFilesDirectiveController]);
+function homeFilesDirectiveController($scope) {
 }
