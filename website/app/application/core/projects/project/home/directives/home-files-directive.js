02df8a7ac03df527b0e5bd88eff75a8b0b920b09
V. Glenn Tarcea <gtarcea@umich.edu>
2014-12-04 16:04:52 -0500

    Get files matching mediatype.

--- website/app/application/core/projects/project/home/directives/home-files-directive.js
+++ website/app/application/core/projects/project/home/directives/home-files-directive.js
@@ -13,7 +13,21 @@ function homeFilesDirective() {
 Application.Controllers.controller("homeFilesDirectiveController",
                                    ["$scope",  "projectFiles", homeFilesDirectiveController]);
 function homeFilesDirectiveController($scope, projectFiles) {
-    //console.dir($scope.project);
     var id = $scope.project.id;
-    console.dir(projectFiles.model.projects[id]);
+
+    //console.log(projectFiles.model);
+    $scope.showFiles = function(mediatype) {
+        console.log("show files for: ", mediatype);
+        var files = [];
+        var treeModel = new TreeModel();
+        console.dir(projectFiles.model.projects[id].dir);
+        var root = treeModel.parse(projectFiles.model.projects[id].dir);
+        root.walk({strategy: 'pre'}, function(node) {
+            console.dir(node);
+            if (node.mediatype == mediatype) {
+                files.push(node);
+            }
+        });
+        console.dir(files);
+    };
 }
