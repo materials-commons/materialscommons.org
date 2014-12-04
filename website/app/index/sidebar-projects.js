bb64102969cb52bbd7dcdb847bb8a013c1b40f95
V. Glenn Tarcea <gtarcea@umich.edu>
2014-12-04 15:21:33 -0500

    Modal for project creation was showing up behind other elements when browser was safari or chrome.

--- website/app/index/sidebar-projects.js
+++ website/app/index/sidebar-projects.js
@@ -10,10 +10,10 @@ function sidebarProjectsDirective() {
 }
 
 Application.Controllers.controller("sidebarProjectsDirectiveController",
-                                   ["$scope", "current", "mcapi", "model.projects", "$state",
+                                   ["$scope", "current", "$state",
                                     sidebarProjectsDirectiveController]);
 
-function sidebarProjectsDirectiveController($scope, current, mcapi, Projects, $state) {
+function sidebarProjectsDirectiveController($scope, current, $state) {
     $scope.showProjects = false;
 
     $scope.setProject = function(project) {
@@ -21,22 +21,5 @@ function sidebarProjectsDirectiveController($scope, current, mcapi, Projects, $s
         current.setProject(project);
         $scope.showProjects = false;
         $state.go("projects.project.home", {id: project.id});
-    };
-
-    $scope.createProject = function(){
-        if ($scope.model.name === "") {
-            return;
-        }
-        mcapi('/projects')
-            .success(function (project) {
-                Projects.getList(true).then(function(projects) {
-                    $scope.projects = projects;
-                    $scope.model.name = "";
-                    var i = _.indexOf($scope.projects, function(p) {
-                        return p.id == project.project_id;
-                    });
-                    $scope.setProject(projects[i]);
-                });
-            }).post({'name': $scope.model.name});
     };
 }
