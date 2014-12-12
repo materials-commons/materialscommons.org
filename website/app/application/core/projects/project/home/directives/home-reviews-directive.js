f397d10f1d8aad008e70b442849b8febf1361664
tammasr <tammasr@umich.edu>
2014-12-12 17:22:06 -0500

    notes and reviews card updates

--- website/app/application/core/projects/project/home/directives/home-reviews-directive.js
+++ website/app/application/core/projects/project/home/directives/home-reviews-directive.js
@@ -14,4 +14,15 @@ Application.Controllers.controller("homeReviewsDirectiveController",
                                    ["$scope",
                                     homeReviewsDirectiveController]);
 function homeReviewsDirectiveController ($scope) {
+    var showReviewDetails = [];
+    for (var i = 0; i < $scope.project.notes.length; i++) {
+        showReviewDetails.push(false);
+    }
+    $scope.toggleDetails = function(index) {
+        showReviewDetails[index] = !showReviewDetails[index];
+    };
+
+    $scope.showDetails = function(index) {
+        return showReviewDetails[index];
+    };
 }
