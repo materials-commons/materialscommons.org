fae5d5115740f88860cb00c3c35d2725faa3514a
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-12-13 20:49:27 -0500

    Track expansion across projects. Start moving expansion code into a service so we can remember and also apply it to the home page.

--- website/app/application/services/ui.js
+++ website/app/application/services/ui.js
@@ -1,29 +1,77 @@
 Application.Services.factory('ui', uiService);
 
+// uiService tracks various ui component states by project.
 function uiService() {
     var self = this;
-    self.showByProject = {};
+    self.byProject = {};
 
-    self.initForProject = function(projectID) {
-        self.showByProject[projectID] = {
-            files: false
+    // initForProject initializes the ui state for a project.
+    function initForProject(projectID) {
+        self.byProject[projectID] = {
+            files: false,
+            expanded: {}
         };
-    };
+    }
+
+    // getForProject returns a ui state for a project. It
+    // creates the project entry if it doesn't exist.
+    function getForProject(projectID) {
+        if (!(projectID in self.byProject)) {
+            initForProject(projectID);
+        }
+        return self.byProject[projectID];
+    }
+
+    // getIsExpanded returns an expanded key state. If the
+    // key doesn't exist it creates it. If the project doesn't
+    // exist it creates the project.
+    function getIsExpanded(projectID, what) {
+        var proj = getForProject(projectID);
+        if (!(what in proj.expanded)) {
+            proj.expanded[what] = false;
+        }
+        return proj.expanded[what];
+    }
 
     return {
+        // showFiles returns the state of files.
         showFiles: function(projectID) {
-            if (!(projectID in self.showByProject)) {
-                self.initForProject(projectID);
-            }
-
-            return self.showByProject[projectID].files;
+            return getForProject(projectID).files;
         },
 
+        // setShowFiles sets the state of files attribute.
         setShowFiles: function(projectID, what) {
-            if (!(projectID in self.showByProject)) {
-                self.initForProject(projectID);
-            }
-            self.showByProject[projectID].files = what;
+            var filesState = getForProject(projectID);
+            filesState.files = what;
+        },
+
+        // isExpanded returns true the state for the given true.
+        isExpanded: function(projectID, what) {
+            return getIsExpanded(projectID, what);
+        },
+
+        // anyExpandedExcept checks if any expanded is set to true, except the
+        // key given.
+        anyExpandedExcept: function(projectID, what) {
+            var expanded = getForProject(projectID).expanded;
+            var anyTrue = false;
+            Object.keys(expanded).forEach(function(key) {
+                if (key !== what && expanded[key]) {
+                    anyTrue = true;
+                }
+            });
+            return anyTrue;
+        },
+
+        // setIsExpanded sets the the given expanded state.
+        setIsExpanded: function(projectID, what, to) {
+            var proj = getForProject(projectID);
+            proj.expanded[what] = to;
+        },
+
+        toggleIsExpanded: function(projectID, what) {
+            var proj = getForProject(projectID);
+            proj.expanded[what] = !getIsExpanded(projectID, what);
         }
     };
 }
