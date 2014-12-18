6cc02f3e61f9f374f3fbe017c7b314865640f9ff
tammasr <tammasr@umich.edu>
2014-12-18 12:25:48 -0500

    reviews rework

--- website/app/application/core/projects/project/reviews/edit.js
+++ website/app/application/core/projects/project/reviews/edit.js
@@ -1,82 +1,41 @@
 Application.Controllers.controller("projectReviewsEdit",
-    ["$scope", "project", "User", "$stateParams", "mcapi", "pubsub", "$state", "Review", projectReviewsEdit]);
+    ["$scope", "project", "$stateParams", "mcapi", "$state", "Review", projectReviewsEdit]);
 
-function projectReviewsEdit($scope, project, User, $stateParams, mcapi, pubsub, $state, Review) {
-    $scope.project = project;
+        function projectReviewsEdit($scope, project, $stateParams, mcapi, $state, Review) {
+            $scope.project = project;
 
-    $scope.editReview = function (index) {
-        $scope.edit_index = index;
-    };
+            $scope.editReview = function (index) {
+                $scope.edit_index = index;
+            };
 
-    $scope.saveReview = function (index) {
-        if ($scope.review.messages[index].message === "") {
-            return;
-        }
-        $scope.edit_index = -1;
-        mcapi('/reviews/%', $scope.review.id)
-            .success(function (data) {
-            }).put({'messages': $scope.review.messages});
-    };
+            $scope.saveReview = function (index) {
+                if ($scope.review.messages[index].message === "") {
+                    return;
+                }
+                $scope.edit_index = -1;
+                mcapi('/reviews/%', $scope.review.id)
+                    .success(function (data) {
+                    }).put({'messages': $scope.review.messages});
+            };
 
-    function findReview(reviewID, which) {
-        var i = _.indexOf(project[which], function (review) {
-            return review.id === reviewID;
-        });
-        return project[which][i];
-    }
+            $scope.addComment = function () {
+                Review.addComment($scope.model, $scope.review);
+            };
 
-    $scope.addComment = function () {
-        if ($scope.model.comment.length === 0) {
-            return;
-        }
-
-        var d = new Date();
-        $scope.review.messages.push({
-            'message': $scope.model.comment,
-            'who': User.u(),
-            'date': d.toDateString()
-        });
-        mcapi('/reviews/%', $scope.review.id)
-            .success(function (data) {
-            }).put({'messages': $scope.review.messages});
-    };
-    $scope.closeReview = function () {
-        var reviewID = $scope.review.id;
-        mcapi('/reviews/%', $scope.review.id)
-            .success(function () {
-                $scope.review = '';
-                var review = findReview(reviewID, "reviews");
-                review.status = "closed";
-                reviewCount();
+            $scope.closeReview = function () {
+                Review.closeReview($scope.review.id, project);
                 $state.go('projects.project.reviews.overview');
-            }).put({'status': 'closed'});
-    };
+            };
 
-    $scope.reOpenReview = function () {
-        var reviewID = $scope.review.id;
-        mcapi('/reviews/%', $scope.review.id)
-            .success(function () {
-                $scope.review = '';
-                var review = findReview(reviewID, "reviews");
-                review.status = "open";
-                reviewCount();
+            $scope.reOpenReview = function () {
+                Review.openReview($scope.review.id, project);
                 $state.go('projects.project.reviews.overview');
-            }).put({'status': 'open'});
-    };
+            };
 
-    function reviewCount() {
-        pubsub.send("reviews.change");
-    }
+            function init() {
+                var review_id = $stateParams.review_id;
+                $scope.review = Review.findReview(review_id, 'reviews', project);
+            }
 
-    function init() {
-        var review_id = $stateParams.review_id;
-        if ($stateParams.status === 'open') {
-            $scope.reviews = Review.getReviews('open');
-        } else {
-            $scope.reviews = Review.getReviews('closed');
+            init();
         }
-        $scope.review = Review.findReview(review_id, 'reviews', project);
-    }
-
-    init();
-}
--- website/app/application/services/review-service.js
+++ website/app/application/services/review-service.js
@@ -1,37 +1,52 @@
 Application.Services.factory('Review',
-    ["$filter", function ($filter) {
+    ["$filter", "mcapi",  "User", function ($filter, mcapi, User) {
         var service = {
 
-            findReview: function(reviewID, which, project) {
+            findReview: function (reviewID, which, project) {
                 var i = _.indexOf(project[which], function (review) {
                     return review.id === reviewID;
                 });
                 return project[which][i];
-            }
+            },
 
-            //closeReview: function () {
-            //    var reviewID = $scope.review.id;
-            //    mcapi('/reviews/%', reviewID)
-            //        .success(function () {
-            //            $scope.review = '';
-            //            var review = findReview(reviewID, "reviews");
-            //            review.status = "closed";
-            //            reviewCount();
-            //            $state.go('projects.project.reviews.overview');
-            //        }).put({'status': 'closed'});
-            //},
-            //
-            //openReview: function () {
-            //    var reviewID = $scope.review.id;
-            //    mcapi('/reviews/%', $scope.review.id)
-            //        .success(function () {
-            //            $scope.review = '';
-            //            var review = findReview(reviewID, "reviews");
-            //            review.status = "open";
-            //            reviewCount();
-            //            $state.go('projects.project.reviews.overview');
-            //        }).put({'status': 'open'});
-            //}
+            closeReview: function (review_id, project) {
+                var reviewID = review_id;
+                mcapi('/reviews/%', reviewID)
+                    .success(function () {
+                        var review = service.findReview(reviewID, "reviews", project);
+                        review.status = "closed";
+                        service.reviewCount();
+                    }).put({'status': 'closed'});
+            },
+
+            openReview: function (review_id, project) {
+                var reviewID = review_id;
+                mcapi('/reviews/%', reviewID)
+                    .success(function () {
+                        var review = service.findReview(reviewID, "reviews", project);
+                        review.status = "open";
+                        service.reviewCount();
+                    }).put({'status': 'open'});
+            },
+
+            addComment: function (bk, review) {
+                if (bk.comment.length === 0) {
+                    return;
+                }
+                var d = new Date();
+                review.messages.push({
+                    'message': bk.comment,
+                    'who': User.u(),
+                    'date': d.toDateString()
+                });
+                mcapi('/reviews/%', review.id)
+                    .success(function (data) {
+                    }).put({'messages': review.messages});
+            },
+
+            reviewCount: function () {
+                pubsub.send("reviews.change");
+            }
 
         };
         return service;
