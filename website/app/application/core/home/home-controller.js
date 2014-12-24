b27887e631d14b35e6df0364ed3930583f06e3f5
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-12-24 08:42:39 -0500

    Remove reference to old home-controller

--- website/app/application/core/home/home-controller.js
+++
@@ -1,6 +0,0 @@
-Application.Controllers.controller('home', ['$scope', 'mcapi', function ($scope, mcapi) {
-    mcapi('/news')
-        .success(function (data) {
-            $scope.news = data;
-        }).jsonp();
-}]);
