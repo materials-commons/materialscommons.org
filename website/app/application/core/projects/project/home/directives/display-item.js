ac86408e8d4d3b59b53b2927ca3ae060acfef1d6
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-12-23 20:26:27 -0500

    Add directives to determine the type of item to display.

---
+++ website/app/application/core/projects/project/home/directives/display-item.js
@@ -0,0 +1,12 @@
+Application.Directives.directive("displayItem", displayItemDirective);
+function displayItemDirective() {
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            item: "=item",
+            showSideboard: "=showSideboard"
+        },
+        templateUrl: "application/core/projects/project/home/directives/display-item.html"
+    };
+}
