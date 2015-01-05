da82fbde26d43c60315cd4f3992bd554ea17ee85
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-05 16:29:25 -0500

    Create a directive for adding files to upload to flow.

---
+++ website/app/application/directives/mc-flow-button.js
@@ -0,0 +1,106 @@
+Application.Directives.directive("mcFlowButton", ["mcFlow", "pubsub", mcFlowButtonDirective]);
+
+function mcFlowButtonDirective(mcFlow, pubsub) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: false,
+        template: "<span style='color: #389dc1; cursor: pointer' title='Upload to directory'><i class='fa fa-fw fa-upload'></i></span>",
+        link: function(scope, element, attrs) {
+            var flow = mcFlow.get();
+            var isDirectory = attrs.hasOwnProperty('flowDirectory');
+            var isSingleFile = attrs.hasOwnProperty('flowSingleFile');
+            var inputAttrs = attrs.hasOwnProperty('flowAttrs') && scope.$eval(attrs.flowAttrs);
+            function each(obj, callback, context) {
+                if (!obj) {
+                    return ;
+                }
+                var key;
+                // Is Array?
+                if (typeof(obj.length) !== 'undefined') {
+                    for (key = 0; key < obj.length; key++) {
+                        if (callback.call(context, obj[key], key) === false) {
+                            return ;
+                        }
+                    }
+                } else {
+                    for (key in obj) {
+                        if (obj.hasOwnProperty(key) && callback.call(context, obj[key], key) === false) {
+                            return ;
+                        }
+                    }
+                }
+            }
+
+            function extend(dst, src) {
+                each(arguments, function(obj) {
+                    if (obj !== dst) {
+                        each(obj, function(value, key){
+                            dst[key] = value;
+                        });
+                    }
+                });
+                return dst;
+            }
+
+            var assignBrowse = function(domNodes, isDirectory, isSingleFile, attributes) {
+                if (typeof domNodes.length === 'undefined') {
+                    domNodes = [domNodes];
+                }
+
+                each(domNodes, function (domNode) {
+                    var input;
+                    if (domNode.tagName === 'INPUT' && domNode.type === 'file') {
+                        input = domNode;
+                    } else {
+                        input = document.createElement('input');
+                        input.setAttribute('type', 'file');
+                        // display:none - not working in opera 12
+                        extend(input.style, {
+                            visibility: 'hidden',
+                            position: 'absolute'
+                        });
+                        // for opera 12 browser, input must be assigned to a document
+                        domNode.appendChild(input);
+                        // https://developer.mozilla.org/en/using_files_from_web_applications)
+                        // event listener is executed two times
+                        // first one - original mouse click event
+                        // second - input.click(), input is inside domNode
+                        domNode.addEventListener('click', function() {
+                            input.click();
+                        }, false);
+                    }
+                    if (!flow.opts.singleFile && !isSingleFile) {
+                        input.setAttribute('multiple', 'multiple');
+                    }
+                    if (isDirectory) {
+                        input.setAttribute('webkitdirectory', 'webkitdirectory');
+                    }
+                    each(attributes, function (value, key) {
+                        input.setAttribute(key, value);
+                    });
+                    // When new files are added, simply append them to the overall list
+                    //var $ = this;
+                    input.addEventListener('change', function (e) {
+                        var files = [];
+                        each(e.target.files, function(f) {
+                            var o = {
+                                id: "newid_" + f.name,
+                                file: f,
+                                attrs: {
+                                    hello: 'world'
+                                }
+                            };
+                            files.push(o);
+                        });
+                        flow.addFiles(files, e);
+                        pubsub.send("file-upload-added");
+                        e.target.value = '';
+                    }, false);
+                }, this);
+            };
+
+            assignBrowse(element, isDirectory, isSingleFile, inputAttrs);
+        }
+    };
+}
