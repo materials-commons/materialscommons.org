0162faedfe3b220267aa9d0800332c7bf0e8136d
tammasr <tammasr@umich.edu>
2015-01-05 13:16:10 -0500

    changes to sample note - bug fixed:

--- website/app/application/core/projects/project/home/directives/home-sample-details.js
+++ website/app/application/core/projects/project/home/directives/home-sample-details.js
@@ -3,7 +3,17 @@ function homeSampleDetailsDirective() {
     return {
         restrict: "AE",
         scope: true,
+        controller: 'homeSampleDetailsController',
         replace: true,
         templateUrl: "application/core/projects/project/home/directives/home-sample-details.html"
     };
+}
+
+Application.Controllers.controller("homeSampleDetailsController",
+    ["$scope", "current", "Project",
+        homeSampleDetailsController]);
+
+function homeSampleDetailsController($scope, current, Project) {
+    $scope.sample.notes = Project.getSpecificNotes(current.project(), $scope.sample.id, 'sample');
+    $scope.note = $scope.sample.notes[0];
 }
