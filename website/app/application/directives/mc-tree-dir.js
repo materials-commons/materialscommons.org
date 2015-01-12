4f217157263e2424f2c9e0e43a4b6c69285728dc
tammasr <tammasr@umich.edu>
2015-01-12 16:39:02 -0500

    add or drag items into review functionality along with service

--- website/app/application/directives/mc-tree-dir.js
+++ website/app/application/directives/mc-tree-dir.js
@@ -17,7 +17,35 @@ function mcTreeDirDirective(RecursionHelper) {
 }
 
 Application.Controllers.controller("mcTreeDirDirectiveController",
-                                   ["$scope", mcTreeDirDirectiveController]);
-function mcTreeDirDirectiveController($scope) {
+                                   ["$scope", "pubsub", "toggleDragButton", mcTreeDirDirectiveController]);
+function mcTreeDirDirectiveController($scope, pubsub, toggleDragButton) {
     $scope.items = $scope.item.children;
+    $scope.bk = {
+        addToReview: false,
+        addToProvenance: false,
+        addToSample: false ,
+        addToNote: false
+    };
+
+    pubsub.waitOn($scope, 'toggle-files.update', function () {
+        console.log('yes');
+        $scope.bk.addToReview = toggleDragButton.get('files', 'addToReview');
+    });
+
+    $scope.addItem = function (type, file) {
+        switch (type) {
+            case "review":
+                pubsub.send('addFileToReview', file);
+                break;
+            case "sample":
+                pubsub.send('addFileToSample', file);
+                break;
+            case "provenance":
+                pubsub.send('addFileToProvenance', file);
+                break;
+            case "file":
+                pubsub.send('addFileToNote', file);
+                break;
+        }
+    };
 }
