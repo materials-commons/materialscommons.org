e2cdc198709bf9742a7cc43261c3680050806f27
tammasr <tammasr@umich.edu>
2015-01-15 15:24:15 -0500

    drag drop working

--- website/app/application/core/projects/project/home/directives/home-sideboard.js
+++ website/app/application/core/projects/project/home/directives/home-sideboard.js
@@ -4,7 +4,7 @@ function homeSideboardDirective() {
         restrict: "A",
         controller: 'homeSideboardDirectiveController',
         scope: {
-            project: '=project' ,
+            project: '=project',
             empty: '=',
             splitIcon: '='
         },
@@ -13,16 +13,20 @@ function homeSideboardDirective() {
 }
 
 Application.Controllers.controller("homeSideboardDirectiveController",
-                                   ["$scope", "ui", "sideboard",
-                                    homeSideboardDirectiveController]);
+    ["$scope", "ui", "sideboard",
+        homeSideboardDirectiveController]);
 function homeSideboardDirectiveController($scope, ui, sideboard) {
-    if($scope.empty){
-        $scope.sideboard = [];
-    }else{
-        $scope.sideboard = sideboard.get($scope.project.id);
+    var result = [];
+    if ($scope.empty) {
+        result = sideboard.get($scope.project.id);
+        $scope.sideboard = result.emptySideBoard;
+    } else {
+        result = sideboard.get($scope.project.id);
+        $scope.sideboard = result.sideboard;
+
     }
 
-    $scope.splitScreen = function(what, col){
+    $scope.splitScreen = function (what, col) {
         ui.toggleColumns(what, col, $scope.project.id);
     };
 
@@ -30,4 +34,18 @@ function homeSideboardDirectiveController($scope, ui, sideboard) {
         return ui.getSplitStatus($scope.project.id);
     };
 
+    $scope.onDrop = function (ignore, item, event) {
+        if ($scope.empty) {
+            sideboard.handleFromEvent($scope.project.id, item, event, 'emptySideBoard');
+        } else {
+            var i = _.indexOf($scope.sideboard, function (f) {
+                return f.id === item.id;
+            });
+            // If item is already in list then don't add it again.
+            if (i === -1) {
+                sideboard.handleFromEvent($scope.project.id, item, event, 'sideboard');
+
+            }
+        }
+    };
 }
