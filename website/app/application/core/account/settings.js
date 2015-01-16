83bdfd6d830557b253b4962049f5d98d411a077b
tammasr <tammasr@umich.edu>
2015-01-16 14:12:49 -0500

    dropdown list

--- website/app/application/core/account/settings.js
+++ website/app/application/core/account/settings.js
@@ -1,19 +1,17 @@
 Application.Controllers.controller("accountSettings",
-                                   ["$scope", "mcapi", "User", "toastr",
-                                    accountSettingsController]);
+    ["$scope", "mcapi", "User", "toastr",
+        accountSettingsController]);
 function accountSettingsController($scope, mcapi, User, toastr) {
-    $scope.updateName = function() {
-        console.log("updateName");
+    $scope.updateName = function () {
         mcapi('/users/%', $scope.mcuser.email)
             .success(function (u) {
                 User.save($scope.mcuser);
                 toastr.success('User name updated', 'Success', {
                     closeButton: true
                 });
-            }).error(function() {
+            }).error(function () {
                 //console.log("update failed");
             }).put({fullname: $scope.mcuser.fullname});
     };
-
     $scope.mcuser = User.attr();
 }
