508285bd213575f2fccf3b3ad955f7f3cfa8fa4f
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-23 15:21:37 -0500

    Remove dead code and unused rest calls. Fix up the model to reflect what a project looks like.

--- backend/loader/model/project.py
+++ backend/loader/model/project.py
@@ -5,16 +5,9 @@ class Project(object):
     def __init__(self, name, datadir, owner):
         self.name = name
         self.description = ""
-        self.datadir = datadir
         self.owner = owner
         self.birthtime = r.now()
         self.mtime = self.birthtime
-        self.notes = []
-        self.tags = []
-        self.reviews = []
-        self.mytags = []
-        self.todos = []
         self.mediatypes = {}
         self.size = 0
-        self.uses = []
-        self.shares = []
+        self._type = "project"
