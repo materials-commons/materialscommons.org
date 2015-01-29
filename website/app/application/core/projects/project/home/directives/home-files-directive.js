08ec4b7ad3e787694cde4c245e36d033b3601b8a
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-29 15:10:32 -0500

    Fix showing the file is uploaded. Add Refresh to reload the directory tree.

--- website/app/application/core/projects/project/home/directives/home-files-directive.js
+++ website/app/application/core/projects/project/home/directives/home-files-directive.js
@@ -12,9 +12,9 @@ function homeFilesDirective() {
 
 Application.Controllers.controller("homeFilesDirectiveController",
                                    ["$scope", "ui", "projectFiles", "applySearch",
-                                    "$filter",  homeFilesDirectiveController]);
+                                    "$filter",  "mcapi", homeFilesDirectiveController]);
 function homeFilesDirectiveController($scope, ui, projectFiles, applySearch,
-                                      $filter) {
+                                      $filter, mcapi) {
     $scope.files = projectFiles.model.projects[$scope.project.id].dir.children;
     $scope.toggleExpanded = function() {
         ui.toggleIsExpanded($scope.project.id, "files");
@@ -30,6 +30,22 @@ function homeFilesDirectiveController($scope, ui, projectFiles, applySearch,
 
     $scope.isSplitExpanded = function () {
         return ui.getSplitStatus($scope.project.id);
+    };
+
+    $scope.isReloading = false;
+
+    $scope.reloadFiles = function() {
+        $scope.isReloading = !$scope.isReloading;
+
+        mcapi("/projects/%/tree2", $scope.project.id)
+            .success(function(files){
+                var obj = {};
+                obj.dir = files[0];
+                projectFiles.model.projects[$scope.project.id] = obj;
+                //projectFiles.loadByMediaType($scope.project);
+                $scope.files = projectFiles.model.projects[$scope.project.id].dir.children;
+                $scope.isReloading = false;
+            }).jsonp();
     };
 
     applySearch($scope, "searchInput", applyQuery);
