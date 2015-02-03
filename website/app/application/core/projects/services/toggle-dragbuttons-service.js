e4b8a21dbc5639ea770a118b9465d9f2257c8de2
V. Glenn Tarcea <gtarcea@umich.edu>
2015-02-03 13:05:21 -0500

    Add provenance type as item to reset.

--- website/app/application/core/projects/services/toggle-dragbuttons-service.js
+++ website/app/application/core/projects/services/toggle-dragbuttons-service.js
@@ -8,29 +8,36 @@ function toggleDragButton() {
             'files': false,
             'provenance': false
         },
+        addToProv: {
+            samples: false,
+            notes: false,
+            files: false,
+            provenance: false
+        },
+
         toggle: function (type, button) {
             switch (type) {
-                case "samples":
-                    service[button].samples = !service[button].samples;
-                    break;
-                case "notes":
-                    service[button].notes = !service[button].notes;
-                    break;
-                case "files":
-                    service[button].files = !service[button].files;
-                    break;
-                case "provenance":
-                    service[button].provenance = !service[button].provenance;
-                    break;
+            case "samples":
+                service[button].samples = !service[button].samples;
+                break;
+            case "notes":
+                service[button].notes = !service[button].notes;
+                break;
+            case "files":
+                service[button].files = !service[button].files;
+                break;
+            case "provenance":
+                service[button].provenance = !service[button].provenance;
+                break;
             }
         },
 
-        reset: function(){
-            service.addToReview=  {
+        reset: function(button) {
+            service[button] = {
                 'samples': false,
-                    'notes': false,
-                    'files': false,
-                    'provenance': false
+                'notes': false,
+                'files': false,
+                'provenance': false
             };
         },
 
