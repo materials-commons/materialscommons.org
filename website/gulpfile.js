8bc4f4d164c6eb5caa96698ddcbfce30743c3a30
V. Glenn Tarcea <gtarcea@umich.edu>
2015-02-11 09:29:25 -0500

    Update gulp file to build a clean version of prod. Sequence the tasks and update them to include needed assets.

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -5,21 +5,40 @@ var uglify = require("gulp-uglify");
 var minifyHtml = require("gulp-minify-html");
 var minifyCss = require("gulp-minify-css");
 var rev = require("gulp-rev");
+var clean = require("gulp-clean");
+var gulpSequence = require("gulp-sequence");
 
+// Brings over the angular html partials.
 gulp.task('partials', function() {
     var stream = gulp.src("app/**/*.html")
             .pipe(gulp.dest("prod/"));
     return stream;
 });
 
+// Brings over other assets such as fonts and images,
+// and the application style sheet.
 gulp.task('other', function() {
-    var stream = gulp.src(["app/assets/fonts/**/*", "app/assets/ico/**/*", "app/assets/img/**/*"], {
-        base: "app"
-    }).pipe(gulp.dest("prod/"));
+    var stream = gulp.src([
+        "app/assets/fonts/**/*",
+        "app/assets/ico/**/*",
+        "app/assets/css/select2/*",
+        "app/assets/css/images/*",
+        "app/assets/img/**/*",
+        "app/style-3.css"], {
+            base: "app"
+        }).pipe(gulp.dest("prod/"));
     return stream;
 });
 
-gulp.task('default', ['partials', 'other'], function() {
+// Cleans the destination directory.
+gulp.task('clean', function() {
+    var stream = gulp.src("prod/").pipe(clean({read: false, force: true}));
+    return stream;
+});
+
+// Builds the new index.html, concats and minimizes
+// the files.
+gulp.task('min', function() {
     return gulp.src("./app/index.html")
         .pipe(usemin({
             css: ['concat'],
@@ -28,3 +47,5 @@ gulp.task('default', ['partials', 'other'], function() {
         }))
         .pipe(gulp.dest("prod/"));
 });
+
+gulp.task('default', gulpSequence('clean', 'partials', 'other', 'min'));
