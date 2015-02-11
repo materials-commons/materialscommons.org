68074ff0a52eb24770a1d7f771b43e26057d5a84
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-02-10 21:17:02 -0500

    Partial working version of production website built using gulp.

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require("gulp");
 var usemin = require("gulp-usemin");
+var ngmin = require("gulp-ngmin");
 var uglify = require("gulp-uglify");
 var minifyHtml = require("gulp-minify-html");
 var minifyCss = require("gulp-minify-css");
 var rev = require("gulp-rev");
 
-gulp.task('default', function() {
+gulp.task('partials', function() {
+    var stream = gulp.src("app/**/*.html")
+            .pipe(gulp.dest("prod/"));
+    return stream;
+});
+
+gulp.task('default', ['partials'], function() {
     return gulp.src("./app/index.html")
         .pipe(usemin({
-            css: [minifyCss(), 'concat'],
-            html: [minifyHtml({empty: true})],
-            js: [uglify(), rev()]
+            css: ['concat'],
+            //html: [minifyHtml({empty: true})],
+            js: [ngmin(), uglify(), 'concat']
         }))
-        .pipe(gulp.dest("prod"));
+        .pipe(gulp.dest("prod/"));
 });
