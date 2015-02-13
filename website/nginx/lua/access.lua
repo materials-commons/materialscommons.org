1130c1414945fdceafebb29ccacbf3debad1e522
V. Glenn Tarcea <gtarcea@umich.edu>
2015-02-13 16:29:58 -0500

    Fill out a little more of the access checker.

--- website/nginx/lua/access.lua
+++ website/nginx/lua/access.lua
@@ -4,7 +4,15 @@ local users = ngx.shared.users
 local projects = ngx.shared.projects
 local project = ngx.req.project
 
-if project == nil then
-
-
+if (project == nil and (ngx.req.uri ~= '/settings' and ngx.req.uri ~= '/login')) then
+   ngx.say("invalid")
+   ngx.exit(406)
 end
+
+local val, err = jwt.decode(auth, key, true)
+
+if not val then
+   return ngx.say("Error:", err)
+end
+
+-- val.user
