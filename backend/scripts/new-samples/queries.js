e73ce9e1cfdf3790118e158aabce280a3ed3b53d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-02-18 14:39:01 -0500

    Add additional objects and queries to test multiple processes with transformed samples.

--- backend/scripts/new-samples/queries.js
+++ backend/scripts/new-samples/queries.js
@@ -90,3 +90,37 @@ r.db('samplesdb').table("samples")
                 .coerceTo("array")
         };
     });
+
+// Get the attributes for a process:
+r.db('samplesdb').table("processes").getAll("4239c4f4-0b11-4edf-9a6d-23bff250d62e")
+    .eqJoin("id", r.db("samplesdb").table("process2sample"), {index: "process_id"})
+    .zip()
+    .eqJoin("attribute_set_id", r.db("samplesdb").table("sample2attribute_set"), {index:"attribute_set_id"})
+    .zip()
+    .merge(function(aset) {
+        return {
+            attributes: r.db("samplesdb").table("attribute_set2attribute")
+                .getAll(aset("attribute_set_id"), {index: "attribute_set_id"})
+                .eqJoin("attribute_id", r.db("samplesdb").table("attributes"))
+                .zip()
+                .merge(function(attr) {
+                    return {
+                        best_measure: r.db("samplesdb").table("measurements").get(attr("best_measure_id")),
+                        history: r.db("samplesdb").table("best_measure_history")
+                            .getAll(attr("id"), {index: "attribute_id"})
+                            .merge(function(best) {
+                                return {
+                                    measurement: r.db("samplesdb").table("measurements").get(best("measurement_id"))
+                                };
+                            })
+                            .coerceTo("array"),
+                        measurements: r.db("samplesdb").table("attribute2measurement")
+                            .getAll(attr("id"), {index: "attribute_id"})
+                            .eqJoin("measurement_id", r.db("samplesdb").table("measurements"))
+                            .zip()
+                            .coerceTo("array")
+                    };
+                })
+                .coerceTo("array")
+        };
+    });
