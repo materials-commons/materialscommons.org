4f5146d720dfec373497c35069d4f871823221d7
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-02-19 11:53:46 -0500

    Add query capability.

--- backend/scripts/new-samples/search.py
+++ backend/scripts/new-samples/search.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
+# http://stackoverflow.com/questions/23046809/filtering-based-on-key-value-in-all-objects-in-array-in-rethinkdb
 
 import rethinkdb as r
 from optparse import OptionParser
-import sys
 import json
+import sys
 
 
-def main(conn):
+class Query(object):
+    def __init__(self, value, *path):
+        self.value = str(value)
+        self.path = path
+
+
+def is_property(best, *props):
+    in_path = best
+    for prop in props:
+        if prop in in_path:
+            in_path = in_path[prop]
+        else:
+            return False, ""
+    return True, in_path
+
+
+def match_query(samples, *queries):
+    matching_samples = []
+    for sample in samples:
+        matched_attrs = []
+        for attr in sample["attributes"]:
+            for query in queries:
+                matched, value = is_property(attr, *query.path)
+                if matched and str(value) == query.value:
+                    matched_attrs.append(True)
+        if len(matched_attrs) == len(queries):
+            matching_samples.append(sample)
+    return matching_samples
+
+
+def main(conn, queries):
     value = list(r.table("samples")
                  .eq_join("id", r.table("sample2attribute_set"),
                           index="sample_id")
@@ -19,34 +50,37 @@ def main(conn):
                      .zip()
                      .merge(lambda attr: {
                          "best_measure": r.table("measurements")
-                         .get(attr["best_measure_id"]),
-                         "history": r.table("best_measure_history")
-                         .get_all(attr["id"], index="attribute_id")
-                         .merge(lambda best: {
-                             "measurement": r.table("measurements")
-                             .get(best["measurement_id"]).default("")
-                         })
-                         .coerce_to("array"),
-                         "measurements": r.table("attribute2measurement")
-                         .get_all(attr["id"], index="attribute_id")
-                         .eq_join("measurement_id", r.table("measurements"))
-                         .zip()
-                         .coerce_to("array")
+                         .get(attr["best_measure_id"])
                      })
                      .coerce_to("array")
                  })
-                 .filter(lambda entry: entry["attributes"].contains(
-                     lambda element: element["best_measure"]["properties"]
-                     ["grain_size"]["value"].eq(1.09)))
                  .run(conn, time_format="raw"))
-    print json.dumps(value)
+    ms = match_query(value, *queries)
+    print json.dumps(ms)
 
-# http://stackoverflow.com/questions/23046809/filtering-based-on-key-value-in-all-objects-in-array-in-rethinkdb
+
+def construct_queries(query_list):
+    queries = []
+    for query in query_list:
+        pieces = query.split(":")
+        # 0 is path portion
+        # 1 is value
+        path = pieces[0].split(",")
+        q = Query(pieces[1], *path)
+        queries.append(q)
+    return queries
+
 
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-P", "--port", dest="port", type="int",
                       help="rethinkdb port", default=30815)
+    parser.add_option("-q", "--query", dest="queries", action="append",
+                      help="queries to run (path:value) where path is comma separated")
     (options, args) = parser.parse_args()
+    if not options.queries:
+        print "You must specify a query"
+        sys.exit(1)
+    queries = construct_queries(options.queries)
     conn = r.connect("localhost", options.port, db="samplesdb")
-    main(conn)
+    main(conn, queries)
