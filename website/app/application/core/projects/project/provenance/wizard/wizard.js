572de193be8ac891af4e635af10d191dfcf38ab1
V. Glenn Tarcea <gtarcea@umich.edu>
2015-02-25 17:05:42 -0500

    Debug directed depth first search.

--- website/app/application/core/projects/project/provenance/wizard/wizard.js
+++ website/app/application/core/projects/project/provenance/wizard/wizard.js
@@ -27,6 +27,8 @@ function provWizardController($scope, $timeout) {
         {id: 6, label: "SEM Analysis", level: 2, group: "process"}
     ];
 
+    var savedNodes = nodes;
+
     var lastNodeID = 6;
     var lastEdgeID = 6;
 
@@ -40,14 +42,11 @@ function provWizardController($scope, $timeout) {
     ];
 
     var height = window.innerHeight || document.body.clientHeight;
-    console.log("height = ", height);
-
     var heightToSet = height -180;
 
     $scope.options = {
         stabilize: true,
         height: heightToSet+"px",
-        //width: '650px',
         dragNetwork: false,
         navigation: true,
         zoomable: true,
@@ -155,4 +154,157 @@ function provWizardController($scope, $timeout) {
             });
         });
     };
+
+    $scope.focusOnNode = function() {
+        if (!$scope.selectedNode) {
+            return;
+        }
+
+        console.dir($scope.selectedNode);
+
+        var nodeID = $scope.selectedNode.id;
+        var nodeLevel = $scope.selectedNode.level;
+
+
+        //console.log("==========");
+        // for (v in al) {
+        //     if (al.hasOwnProperty(v) && !visited[v]) {
+        //         groups.push(bfs(v, al, visited));
+        //     }
+        // }
+
+        //var newNodes = [];
+
+        // Get rid of all nodes that are parents this node,
+        // or that are at the same level as this node.
+        // $scope.data.nodes.forEach(function(node) {
+        //     if (node.id == nodeID) {
+        //         newNodes.push(node);
+        //     } else if (node.level > nodeLevel) {
+        //         if (isConnected(node, $scope.selectedNode)) {
+        //             newNodes.push(node);
+        //         }
+        //     }
+        // });
+
+        var al = convertToAdjacencyList($scope.data.edges);
+
+        console.dir(al);
+
+        console.dir(dfs($scope.selectedNode.id, al, $scope.selectedNode.level));
+
+        if (true) {
+            return;
+        }
+
+        $timeout(function() {
+            savedNodes = $scope.data.nodes;
+            $scope.data.nodes = newNodes;
+        });
+    };
+
+    function isConnected(nodeToCheck, baseNode) {
+        if (nodeToCheck.level == baseNode.level + 1) {
+            // if no edge directly connects then return false
+            var connected = false;
+            $scope.data.edges.forEach(function(edge) {
+                if (nodeToCheck.id == edge.to && edge.from == baseNode.id) {
+                    connected = true;
+                }
+            });
+            return connected;
+        }
+        return true;
+    }
+
+    $scope.removeFocusOnNode = function() {
+        $timeout(function() {
+            $scope.data.nodes = savedNodes;
+        });
+    };
+
+    function convertToAdjacencyList(edgeList) {
+        var adjList = {},
+            i, len, pari, to, from;
+        for (i = 0, len = edgeList.length; i < len; i++) {
+            to = edgeList[i].to;
+            from = edgeList[i].from;
+            if (adjList[to]) {
+                adjList[to].push(from);
+            } else {
+                adjList[to] = [from];
+            }
+            if (adjList[from]) {
+                adjList[from].push(to);
+            } else {
+                adjList[from] = [to];
+            }
+        }
+        return adjList;
+    }
+
+    function bfs(vertex, adjList, visited) {
+        var q = [],
+            current_group = [],
+            i, len, adjV, nextVertex;
+        q.push(vertex);
+        visited[vertex] = true;
+        while (q.length > 0) {
+            vertex = q.shift();
+            current_group.push(vertex);
+            // Go through the adjacency list of vertex and push
+            // any unvisited vertex onto the queue.
+            adjV = adjList[vertex];
+            for (i = 0, len = adjV.length; i < len; i++) {
+                nextVertex = adjV[i];
+                if (!visited[nextVertex]) {
+                    q.push(nextVertex);
+                    visited[nextVertex] = true;
+                }
+            }
+        }
+        return current_group;
+    }
+
+    function dfs(vertex, adjList, level) {
+        var stack = [];
+        var iterations = 0;
+        var current_group = [];
+        var visited = {};
+        stack.push(vertex);
+        while (stack.length > 0) {
+            iterations++;
+            vertex = stack.shift();
+            current_group.push(vertex);
+            //console.log("vertex", vertex);
+            //console.log("stack %O", stack);
+            if (iterations > 10) {
+                break;
+            }
+            if (!visited[vertex]) {
+                visited[vertex] = true;
+                var adjacent = adjList[vertex];
+                adjacent.forEach(function(vertexID) {
+                    if (!visited[vertexID]) {
+                        if (!isHigher(vertexID, level)) {
+                            stack.push(vertexID);
+                        }
+                    }
+                });
+            }
+        }
+        return current_group;
+    }
+
+    function isHigher(nodeID, level) {
+        var isHigherLevel = false;
+        $scope.data.nodes.forEach(function(node) {
+            if (node.id == nodeID) {
+                if (node.level <= level) {
+                    isHigherLevel = true;
+                }
+            }
+        });
+        return isHigherLevel;
+    }
 }
