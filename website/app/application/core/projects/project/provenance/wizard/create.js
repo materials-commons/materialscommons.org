bbb423cab46abb8a617ab83fbb43d7f11cbf2a08
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-17 14:21:52 -0400

    Rework how we display the properties and how people search for properties to enter.   - Added a sidebar with the sections, categories and attributes for a   template.   - Click on an entry in the sidebar to edit that attribute or section.   - Allow graph to be hidden to increase the space for attributes. This   is important for the charts.
    Charts don't resize themselves when I adjust the column sizes. It might
    be better to have adding properties be a separate page that doesn't show
    the chart. This would also open more space up for adding measurements.

--- website/app/application/core/projects/project/provenance/wizard/create.js
+++ website/app/application/core/projects/project/provenance/wizard/create.js
@@ -1,6 +1,18 @@
 Application.Controllers.controller("createProvenanceFromTemplate",
-                                   ["$scope", "template", createProvenanceFromTemplate]);
+                                   ["$scope", "$state", "template",
+                                    createProvenanceFromTemplate]);
 
-function createProvenanceFromTemplate($scope, template) {
-    $scope.template = angular.copy(template);
+function createProvenanceFromTemplate($scope, $state, template) {
+    $scope.search = {
+        name: ""
+    };
+    $scope.template = template;
+    // Activate the first category in the first section
+    var section = template.sections[0];
+    var category = section.categories[0];
+    $state.go(".edit", {
+        section: section.name,
+        category: category.category,
+        attribute: ''
+    });
 }
