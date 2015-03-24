10a15f7461c702a8facaa4a8fd4c1bf97f6dca61
tammasr <tammasr@umich.edu>
2015-03-24 17:11:04 -0400

    add model note2item

---
+++ backend/loader/model/note2item.py
@@ -0,0 +1,9 @@
+import rethinkdb as r
+
+
+class Note2Item(object):
+    def __init__(self, item_id, item_type, note_id):
+        # self.item_id = item_id
+        # self.item_type = item_type
+        self.note_id = note_id
+        pass
