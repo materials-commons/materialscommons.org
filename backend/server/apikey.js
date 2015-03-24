a6e0c94b2787d53d0c967591c7d717e899a4f0f9
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-24 17:53:45 -0400

    Refactor handling of users apikey into separate testable modules.

--- backend/server/apikey.js
+++ backend/server/apikey.js
@@ -1,31 +1,19 @@
+// apikey module validates the apikey argument used in the call. It
+// caches the list of users to make lookups faster.
 var http = require('http');
-module.exports = function(rethink) {
+module.exports = function(apikeyCache) {
     'use strict';
-    let r = rethink;
-    let users = [];
-    return function*(next) {
-        users = yield getUsers();
+    // validateAPIKey Looks up the apikey. If none is specified, or a
+    // bad key is passed then abort the calls and send back an 401.
+    return function* validateAPIKey(next) {
         let apikey = this.query.apikey || this.throw(401, 'Invalid apikey');
-        let user = users[apikey] || this.throw(401, 'Invalid apikey');
+        let user = yield apikeyCache.find(apikey);
+        if (! user) {
+            this.throw(401, 'Invalid apikey');
+        }
         this.mcapp = {
-            user: {
-                id: user.id,
-                apikey: apikey,
-                isadmin: !!user.admin
-            }
+            user: user
         };
         yield next;
     };
-
-    function* getUsers() {
-        if (users.length !== 0) {
-            return users;
-        }
-        let allUsers = yield r.table('users').pluck('id', 'apikey', 'admin').run();
-        let usersMap = {};
-        allUsers.forEach(function(user) {
-            usersMap[user.apikey] = user;
-        });
-        return usersMap;
-    }
 };
