a8d754916d6d7cd6c83cca7da8ef6747fc5c6cf7
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-25 17:05:39 -0400

    Test good apikey.

--- backend/server/specs/apikey.spec.js
+++ backend/server/specs/apikey.spec.js
@@ -1,3 +1,5 @@
+/* jshint expr: true */
+
 var app = require('../app');
 var status = require('http-status');
 var request = require('supertest').agent(app.listen());
@@ -9,5 +11,17 @@ describe('apikey', function() {
 
     it('should give a 401 when a bad apikey is specified', function(done) {
         request.get('/projects?apikey=123').expect(status.UNAUTHORIZED, done);
+    });
+
+    it('should return data with a good apikey', function(done) {
+        var isProjects = function(res) {
+            res.should.be.json;
+            res.body.should.be.length(2);
+        };
+        request.get('/projects?apikey=user1key')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .expect(isProjects)
+            .end(done);
     });
 });
