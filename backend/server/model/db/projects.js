40463e43ea35538c90c0f5f4d0ac1f52121a1ba5
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-25 11:59:17 -0400

    Return a promise rather than a generator since you can always pass a promise to a generator. Rely on module name (golang style) for method names. The module makes the method name more understandable. General cleanup of the interface.

--- backend/server/model/db/projects.js
+++ backend/server/model/db/projects.js
@@ -1,25 +1,54 @@
 module.exports = function(r) {
     'use strict';
+    let run = require('./run');
     return {
-        getProjectsForUser: getProjectsForUser
+        forUser: forUser
     };
 
     ///////////////
 
-    function *getProjectsForUser(user, isAdmin) {
+    function forUser(user) {
         let rql;
-        if (isAdmin) {
+        if (user.isAdmin) {
             rql = r.table('projects')
-                .filter(r.row('owner').ne('delete@materialscommons.org'))
-                .pluck('name').orderBy('name');
+                .filter(r.row('owner').ne('delete@materialscommons.org'));
         } else {
-            rql = r.table('access').getAll(user, {index: 'user_id'})
+            rql = r.table('access').getAll(user.id, {index: 'user_id'})
                 .eqJoin('project_id', r.table('projects'))
-                .zip()
-                .pluck('name')
-                .orderBy('name');
+                .zip();
         }
 
-        return yield rql.run();
+        rql = addComputed(rql);
+
+        return run(rql);
+    }
+
+    // addComputed adds additional attributes to the rql that
+    // that are computed from other tables.
+    function addComputed(rql) {
+        rql = rql.merge(function(project) {
+            return {
+                users: r.table('access')
+                    .getAll(project('id'), {index: 'project_id'})
+                    .map(function(access) {
+                        return access.merge({
+                            'user': access('user_id')
+                        });
+                    })
+                    .pluck('user', 'permissions')
+                    .coerceTo('array'),
+                reviews: r.table('reviews')
+                    .getAll(project('id'), {index: 'project_id'})
+                    .coerceTo('array'),
+                notes: r.table('notes')
+                    .getAll(project('id'), {index: 'project_id'})
+                    .coerceTo('array'),
+                events: r.table('events')
+                    .getAll(project('id'), {index: 'project_id'})
+                    .coerceTo('array')
+            };
+        });
+
+        return rql;
     }
 };
