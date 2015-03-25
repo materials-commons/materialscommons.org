743b797284ecffbee78663db1ca1f7d4f81823b9
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-25 13:01:56 -0400

    Clean up the code.

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,39 +1,31 @@
 var mount = require('koa-mount');
 var router = require('koa-router')();
 var koa = require('koa');
-var Bus = require('busmq');
 var app = koa();
 require('koa-qs')(app);
+
 var ropts = {
     db: 'materialscommons',
     port: 30815
 };
 var r = require('rethinkdbdash')(ropts);
-var projectsModel = require('./model/db/projects')(r);
-var projects = require('./resources/projects-routes')(projectsModel);
 
+var model = require('./db/model')(r);
+var projects = require('./resources/projects-routes')(model.projects);
+var keycache = require('./apikey-cache')(model.users);
+var apikey = require('./apikey')(keycache);
+
+app.use(apikey);
+app.use(mount('/', projects.routes())).use(projects.allowedMethods());
+
+app.listen(3000);
+
+//////////////////////
+
+var Bus = require('busmq');
 var bus = Bus.create({redis: ['redis://localhost:6379']});
-
 var q;
-
 bus.on('online', function() {
     q = bus.queue('samples');
     q.attach();
 });
-
-function* samples(next) {
-    yield next;
-    this.body = {samples:['a', 'b']};
-}
-
-var router2 = require('koa-router')();
-router2.get('/samples', samples);
-
-var usersModel = require('./model/db/users')(r);
-var keycache = require('./apikey-cache')(usersModel);
-var apikey = require('./apikey');
-
-app.use(apikey(keycache));
-app.use(mount('/', projects.routes())).use(projects.allowedMethods());
-app.use(mount('/', router2.routes()));
-app.listen(3000);
