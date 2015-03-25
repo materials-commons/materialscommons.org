545288c80b0004101e9d9d905515eec55c825efd
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-25 16:14:36 -0400

    Remove hard coding the 401 and instead give it a readable name.

--- backend/server/apikey.js
+++ backend/server/apikey.js
@@ -1,15 +1,17 @@
 // apikey module validates the apikey argument used in the call. It
 // caches the list of users to make lookups faster.
 var http = require('http');
+var httpStatus = require('http-status');
 module.exports = function(apikeyCache) {
     'use strict';
     // validateAPIKey Looks up the apikey. If none is specified, or a
     // bad key is passed then abort the calls and send back an 401.
     return function* validateAPIKey(next) {
-        let apikey = this.query.apikey || this.throw(401, 'Invalid apikey');
+        let UNAUTHORIZED = httpStatus.UNAUTHORIZED;
+        let apikey = this.query.apikey || this.throw(UNAUTHORIZED, 'Invalid apikey');
         let user = yield apikeyCache.find(apikey);
         if (! user) {
-            this.throw(401, 'Invalid apikey');
+            this.throw(UNAUTHORIZED, 'Invalid apikey');
         }
         this.mcapp = {
             user: user
