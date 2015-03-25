d4c2d08a797fc9fdf7913702e1c4220fea81fcf9
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-25 16:15:03 -0400

    Allow app to be started outside of main for testing purposes.

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,7 +1,7 @@
 var mount = require('koa-mount');
 var router = require('koa-router')();
 var koa = require('koa');
-var app = koa();
+var app = module.exports = koa();
 require('koa-qs')(app);
 
 var ropts = {
@@ -18,14 +18,17 @@ var apikey = require('./apikey')(keycache);
 app.use(apikey);
 app.use(mount('/', projects.routes())).use(projects.allowedMethods());
 
-app.listen(3000);
+if (!module.parent) {
+    app.listen(3000);
+}
+
 
 //////////////////////
 
-var Bus = require('busmq');
-var bus = Bus.create({redis: ['redis://localhost:6379']});
-var q;
-bus.on('online', function() {
-    q = bus.queue('samples');
-    q.attach();
-});
+// var Bus = require('busmq');
+// var bus = Bus.create({redis: ['redis://localhost:6379']});
+// var q;
+// bus.on('online', function() {
+//     q = bus.queue('samples');
+//     q.attach();
+// });
