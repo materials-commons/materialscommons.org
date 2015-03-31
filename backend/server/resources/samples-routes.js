6b0b86c30b878e6ca6be922cc5423b137ecc5e4d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-31 16:58:53 -0400

    Add update to routes.

--- backend/server/resources/samples-routes.js
+++ backend/server/resources/samples-routes.js
@@ -3,5 +3,6 @@ module.exports = function(model, schema) {
     let router = require('koa-router')();
     let samples = require('./samples')(model, schema);
     router.post('/samples', samples.create);
+    router.put('/samples/:id', samples.update);
     return router;
 };
