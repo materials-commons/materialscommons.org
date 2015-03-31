04eebc1c92c8d1d5f031ff2b1681ebe7de1fec11
V. Glenn Tarcea <gtarcea@umich.edu>
2015-03-31 15:52:58 -0400

    Add rules for validating property schema _type and units.

--- backend/server/schema/schema-rules.js
+++ backend/server/schema/schema-rules.js
@@ -1,11 +1,31 @@
+var _ = require('lodash');
 module.exports = function(model) {
+    'use strict';
+
+    let propertyTypes = [
+        'number',
+        'string',
+        'histogram',
+        'composition'
+    ];
+
+    let propertyUnits = [
+        'mm',
+        'm',
+        'c',
+        'f'
+    ];
+
     return {
         mustExist: mustExist,
-        mustNotExist: mustNotExist
+        mustNotExist: mustNotExist,
+        isValidPropertyType: isValidPropertyType,
+        isValidUnit: isValidUnit
     };
 
+    // mustExist looks up an entry in the named table by id. If
+    // the entry doesn't exist it returns an error.
     function mustExist(what, modelName, done) {
-        'use strict';
         model[modelName].get(what).then(function(value) {
             let error = null;
             if (!value) {
@@ -19,9 +39,9 @@ module.exports = function(model) {
         });
     }
 
+    // mustNotExist looks up an entry in the named table by the named
+    // index. If the entry exists it returns an error.
     function mustNotExist(what, spec, done) {
-        'use strict';
-
         let pieces = spec.split(':'),
             modelName = pieces[0],
             modelIndex = pieces[1];
@@ -37,5 +57,24 @@ module.exports = function(model) {
             }
             done(error);
         });
+    }
+
+
+    function isValidPropertyType(what, _ignore) {
+        let invalid = {
+            rule: 'isValidPropertyType',
+            actual: what,
+            expected: `type to be one of ${propertyTypes}`
+        };
+        return _.indexOf(propertyTypes, what) === -1 ? invalid : null;
+    }
+
+    function isValidUnit(what, _ignore) {
+        let invalid = {
+            rule: 'isValidUnit',
+            actual: what,
+            expected: `units to be one of ${propertyUnits}`
+        };
+        return _.indexOf(propertyUnits, what) === -1 ? invalid : null;
     }
 };
