45400de1e8bd4a4f7da643e5bfff7812a5b4de4c
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-02 15:21:47 -0400

    Move samples routes under projects. Get rid of unused file.

--- backend/server/resources/projects-routes.js
+++ backend/server/resources/projects-routes.js
@@ -1,11 +1,15 @@
-module.exports = function(model, schema, validateAccess) {
+module.exports = function(model) {
     'use strict';
 
+    let validateProjectAccess = require('./project-access')(model.access);
+    let schema = require('.././schema')(model);
     let router = require('koa-router')();
     let projects = require('./projects')(model.projects);
     let samples = require('./samples')(model.samples, schema);
+
     router.get('/projects', projects.all);
-    router.post('/projects/:project_id/samples', validateAccess, samples.create);
-    router.put('/projects/:project_id/samples/:sample_id', validateAccess, samples.update);
+    router.post('/projects/:project_id/samples', validateProjectAccess, samples.create);
+    router.put('/projects/:project_id/samples/:sample_id', validateProjectAccess, samples.update);
+
     return router;
 };
--- backend/server/resources/samples-routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(model, schema) {
-    'use strict';
-    let router = require('koa-router')();
-    let samples = require('./samples')(model, schema);
-    router.post('/samples', samples.create);
-    router.put('/samples/:id', samples.update);
-    return router;
-};
