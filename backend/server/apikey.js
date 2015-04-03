5611af2a9e5f0d0a9fa33a990f91e571ec7cf566
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-03 12:35:29 -0400

    Add white list of paths that don't require a apikey.

--- backend/server/apikey.js
+++ backend/server/apikey.js
@@ -1,15 +1,19 @@
 // apikey module validates the apikey argument used in the call. It
 // caches the list of users to make lookups faster.
-var httpStatus = require('http-status');
 module.exports = function(users) {
     'use strict';
     let apikeyCache = require('./apikey-cache')(users);
+    let httpStatus = require('http-status');
+
+    // whiteList contains paths that don't require
+    // a valid apikey.
+    let whiteList = {
+        "/login": true
+    };
     // validateAPIKey Looks up the apikey. If none is specified, or a
     // bad key is passed then abort the calls and send back an 401.
     return function *validateAPIKey(next) {
-        if (this.unprotected) {
-            yield next;
-        } else {
+        if (!(this.path in whiteList)) {
             let UNAUTHORIZED = httpStatus.UNAUTHORIZED;
             let apikey = this.query.apikey || this.throw(UNAUTHORIZED, 'Not authorized');
             let user = yield apikeyCache.find(apikey);
@@ -19,7 +23,7 @@ module.exports = function(users) {
             this.reqctx = {
                 user: user
             };
-            yield next;
         }
+        yield next;
     };
 };
