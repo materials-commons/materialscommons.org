58c8070577065a00907854eaedc8b4ecc90283ce
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-06 16:50:38 -0400

    Add and cleanup schema definitions.

--- backend/server/schema/index.js
+++ backend/server/schema/index.js
@@ -18,6 +18,67 @@ module.exports = function(model) {
 
     /////////////// Define Schemas ///////////////
 
+    function defineProcessSchema() {
+        let process = schema.defineSchema('Process', {
+            owner: {
+                type: 'string',
+                nullable: false
+            },
+            name: {
+                type: 'string',
+                nullable: false
+            },
+            template_id: {
+                type: 'string',
+                nullable: false,
+                mustExist: 'templates'
+            },
+            description: {
+                type: 'string',
+                nullable: true
+            },
+            how: {
+                type: 'string',
+                nullable: true
+            },
+            settings: {
+                type: 'array',
+                nullable: false
+            },
+            files_created: {
+                type: 'array',
+                nullable: true
+            },
+            files_used: {
+                type: 'array',
+                nullable: true
+            },
+            samples_created: {
+                type: 'array',
+                nullable: true
+            },
+            samples_used: {
+                type: 'array',
+                nullable: true
+            },
+            measurements: {
+                type: 'array',
+                nullable: true
+            }
+        });
+        process.setDefaults({
+            description: '',
+            how: '',
+            files_created: [],
+            files_used: [],
+            samples_created: [],
+            samples_used: [],
+            measurements: []
+        });
+        process.validateAsync = promise.promisify(process.validate);
+        return process;
+    }
+
     function defineSamplesSchema() {
         let samples = schema.defineSchema('Samples', {
             name:{
@@ -40,6 +101,11 @@ module.exports = function(model) {
             properties: {
                 type: 'array',
                 nullable: true
+            },
+            sample_id: {
+                type: 'string',
+                nullable: true,
+                mustExistInProject: 'samples'
             }
         });
         samples.setDefaults({
@@ -79,7 +145,91 @@ module.exports = function(model) {
         return properties;
     }
 
+    function defineMeasurementsSchema() {
+        let measurements = schema.defineSchema('Measurement', {
+            name: {
+                type: 'string',
+                nullable: false
+            },
+
+            _type: {
+                type: 'string',
+                nullable: false,
+                isValidPropertyType: true
+            },
+
+            value: {
+                nullable: false
+            },
+
+            units: {
+                type: 'string',
+                nullable: false,
+                isValidUnit: true
+            },
+
+            nvalue: {
+                nullable: true
+            },
+
+            nunits: {
+                nullable: true
+            },
+
+            file: {
+                nullable: true,
+
+                file_id: {
+                    type: 'string',
+                    nullable: false,
+                    mustExistInProject: 'datafiles'
+                },
+
+                grid: {
+                    nullable: true,
+
+                    row_start: {
+                        type: 'number',
+                        nullable: false
+                    },
+
+                    row_end: {
+                        type: 'number',
+                        nullable: false
+                    },
+
+                    column_start: {
+                        type: 'number',
+                        nullable: false
+                    },
+
+                    column_end: {
+                        type: 'number',
+                        nullable: false
+                    }
+                },
+
+                offset: {
+                    nullable: true,
+
+                    start: {
+                        type: 'number',
+                        nullable: false
+                    },
+
+                    end: {
+                        type: 'number',
+                        nullable: false
+                    }
+                }
+            }
+        });
+        measurements.validateAsync = promise.promisify(measurements.validate);
+        return measurements;
+    }
+
     /////////////// Define Types ///////////////
+
     function defineTypes() {
         schema.defineDataType('Measurement', dataTypes.measurement);
     }
@@ -91,6 +241,8 @@ module.exports = function(model) {
         schema.defineRule('mustNotExist', schemaRules.mustNotExist, true);
         schema.defineRule('mustNotExistInProject',
                           schemaRules.mustNotExistInProject, true);
+        schema.defineRule('mustExistInProject',
+                          schemaRules.mustExistInProject, true);
         schema.defineRule('isValidPropertyType', schemaRules.isValidPropertyType);
         schema.defineRule('isValidUnit', schemaRules.isValidUnit);
     }
