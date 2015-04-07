7b9557808cfaa6a28281923dbefd7f911f0b9824
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-07 15:48:36 -0400

    Updates to reflect work in process schema.

--- backend/server/db/model/processes.js
+++ backend/server/db/model/processes.js
@@ -1,6 +1,7 @@
 module.exports = function(r) {
     'use strict';
 
+    let util = require('util');
     let _ = require('lodash');
     let model = require('./model')(r);
     let db = require('./db')(r);
@@ -13,10 +14,14 @@ module.exports = function(r) {
 
     function *create(process) {
         let p = new model.Process(process.name, process.owner,
-                            process.template_id, process.description);
+                                  process.template_id, process.what, process.how);
         let proc = yield addProcess(process.project_id, p);
-        yield addSettings(proc.id, process.settings);
-        yield addMeasurements(proc.id, process.measurements);
+        let settings = yield addSettings(proc.id, process.settings);
+        let measurements = yield addMeasurements(proc.id, process.measurements_created);
+        proc.settings = settings;
+        proc.measurements = measurements;
+        console.log(util.inspect(proc, {showHidden: false, depth: null}));
+        return proc;
     }
 
     //////////////////////////////////////
@@ -28,34 +33,51 @@ module.exports = function(r) {
         return p;
     }
 
+    // Optimize this method like we did addSettings
     function *addSettings(processID, settings) {
+        let created = [];
         for (let i = 0; i < settings.length; i++) {
             let current = settings[i];
-            let s = new model.Settings(current.name);
-            for (let j = 0; j < current.properties.length; j++) {
-                let p = current.properties[j];
-                s.addProperty(p.name, p._type, p.value, p.units);
-            }
+            let s = new model.Settings(current.name, current.attribute);
             let setting = yield db.insert('settings', s);
+            setting.properties = [];
             let p2s = new model.Process2Setting(processID, setting.id);
             yield db.insert('process2setting', p2s);
+
+            for (let j = 0; j < current.properties.length; j++) {
+                let p = current.properties[j];
+                let prop = new model.SettingProperty(setting.id, p.name, p.attribute,
+                                                     p._type, p.value, p.units);
+                let sprop = yield db.insert('setting_properties', prop);
+                setting.properties.push(sprop);
+            }
+            created.push(setting);
         }
+        return created;
     }
 
     function *addMeasurements(processID, measurements) {
+        let toCreate = [];
         for (let i = 0; i < measurements.length; i++) {
             let current = measurements[i];
-            let m = new model.Measurement(current.name, current.sample_id);
-            m.addValue(current.value, current.units, current.nvalue, current.nunits);
-            let rv = yield db.insert('measurements', m);
-            let mid = rv.id;
-            let p2m = new model.Process2Measurement(processID, mid);
-            yield db.insert('process2measurement', p2m);
-            if (_.has(current, 'file')) {
-                let location = current.file.grid ? current.file.grid : current.file.offset;
-                let m2f = new model.Measurement2DataFile(mid, current.file.file_id, location);
-                yield db.insert('measurement2datafile', m2f);
+            let m = new model.Measurement(current.name, current.attribute, current.sample_id);
+            m.setValue(current.value, current.units, current._type,
+                       current.nvalue, current.nunits);
+
+            if (_.has(current, 'from_file')) {
+                m.setFile(current.from_file);
+            } else if (_.has(current, 'from_measurements')) {
+                m.setMeasurements(current.from_measurements);
             }
+            toCreate.push(m);
         }
+        let created = yield db.insert('measurements', toCreate, {toArray: true});
+        let p2ms = [];
+        created.forEach(function(m) {
+            let p2m = new model.Process2Measurement(processID, m.id);
+            p2ms.push(p2m);
+        });
+        yield db.insert('process2measurement', p2ms);
+        return created;
     }
 };
