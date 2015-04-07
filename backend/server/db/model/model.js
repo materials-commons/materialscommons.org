032546b07837058c44763b24caa459d82ed7a10b
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-07 15:48:03 -0400

    Add additional models. Move return of function to bottom so functions set up as prototypes get defined.

--- backend/server/db/model/model.js
+++ backend/server/db/model/model.js
@@ -2,28 +2,6 @@ module.exports = function(r) {
     'use strict';
 
     let db = require('./db')(r);
-    let _ = require('lodash');
-
-    return {
-        Sample: Sample,
-        Process: Process,
-        Settings: Settings,
-        Property: Property,
-        Measurement: Measurement,
-        Attribute2Measurement: Attribute2Measurement,
-        Project2Process: Project2Process,
-        Project2Sample: Project2Sample,
-        AttributeSet2Attribute: AttributeSet2Attribute,
-        BestMeasureHistory: BestMeasureHistory,
-        addMeasurement: addMeasurement,
-        addProcess: addProcess,
-        addAttributeSet: addAttributeSet,
-        addAttributeSetID: addAttributeSetID,
-        addAttribute: addAttribute,
-        addAttributeID: addAttributeID,
-        addBestMeasure: addBestMeasure,
-        addSettings: addSettings
-    };
 
     ///////////////////////////////////////
 
@@ -36,10 +14,11 @@ module.exports = function(r) {
         this._type = "sample";
     }
 
-    function Process(name, owner, templateID, description) {
+    function Process(name, owner, templateID, what, how) {
         this.name = name;
         this.owner = owner;
-        this.description = description;
+        this.what = what;
+        this.how = how;
         this.template_id = templateID;
         this.birthtime = r.now();
         this.mtime = this.birthtime;
@@ -56,17 +35,28 @@ module.exports = function(r) {
         this.process_id = processID;
     }
 
-    function Settings(name) {
-        this.name = name;
-        this.birthtime = r.now();
-        this._type = "settings";
-        this.properties = {};
+    function Process2Sample(sampleID, processID, asetID, _type) {
+        this.sample_id = sampleID;
+        this.process_id = processID;
+        this.attribute_set_id = asetID;
+        this._type = _type;
     }
 
-    Settings.prototype.addProperty = function(name, _type, value, units, nvalue, nunits) {
-        let p = new Property(_type, value, units ? units : "", nvalue ? nvalue : value, nunits ? nunits : units);
-        this.properties[name] = p;
-    };
+    function Settings(name, attribute) {
+        this.name = name;
+        this.attribute = attribute;
+        this.birthtime = r.now();
+        this._type = "settings";
+    }
+
+    function SettingProperty(settingID, name, attribute, _type, value, units) {
+        this.setting_id = settingID;
+        this.name = name;
+        this.attribute = attribute;
+        this._type = _type;
+        this.value = value;
+        this.units = units;
+    }
 
     function Property(_type, value, units, nvalue, nunits) {
         this._type = _type;
@@ -80,19 +70,45 @@ module.exports = function(r) {
         this.properties[name] = val;
     }
 
-    function Measurement(name, sampleID) {
+    function Measurement(name, attribute, sampleID) {
         this.name = name;
+        this.attribute = attribute;
         this.sample_id = sampleID;
         this._type = "measurement";
-        this.properties = {};
+        this.file = {};
+        this.measurements = [];
     }
 
-    Measurement.prototype.addProperty = addProperty;
+    Measurement.prototype.setValue = function(value, units, _type, nvalue, nunits) {
+        this.value = value;
+        this._type = _type;
+        this.units = units;
+        this.nvalue = nvalue ? nvalue : value;
+        this.nunits = nunits ? nunits : units;
+    };
 
-    function Measurement2DataFile(mID, fileID, location) {
-        this.measurement_id = mID;
-        this.datafile_id = fileID;
-        this.location = location;
+    Measurement.prototype.setFile = function(f) {
+        this.file = f;
+    };
+
+    Measurement.prototype.setMeasurements = function(measurements) {
+        this.measurements = measurements;
+    };
+
+    function Attribute(name, attribute) {
+        this.parent_id = '';
+        this.birthtime = r.now();
+        this._type = 'attribute';
+        this.name = name;
+        this.attribute = attribute;
+        this.best_measure_id = '';
+    }
+
+    function AttributeSet(name, attribute, current, parent_id) {
+        this.name = name;
+        this.attribute = attribute;
+        this.current = current;
+        this.parent_id = parent_id;
     }
 
     function Attribute2Measurement(attrID, measurementID) {
@@ -117,13 +133,6 @@ module.exports = function(r) {
         this.current = current;
     }
 
-    function Process2Sample(sampleID, processID, asetID, _type) {
-        this.sample_id = sampleID;
-        this.process_id = processID;
-        this.attribute_set_id = asetID;
-        this._type = _type;
-    }
-
     function AttributeSet2Attribute(asetID, attrID) {
         this.attribute_set_id = asetID;
         this.attribute_id = attrID;
@@ -137,6 +146,7 @@ module.exports = function(r) {
     }
 
     function *addMeasurement(name, prop, attrID, processID, sampleID) {
+        'use strict';
         let m = new Measurement(name, processID, sampleID);
         let rv = yield db.insert('measurements', m);
         let mid = rv.id;
@@ -145,9 +155,8 @@ module.exports = function(r) {
         return mid;
     }
 
-
-
     function *addAttributeSet(processID, sampleID, attrSet, direction) {
+        'use strict';
         let aset = db.insert('attributesets', attrSet);
         let s2as = new Sample2AttributeSet(aset.id, sampleID, 0, true);
         yield db.insert('sample2attributeset', s2as);
@@ -157,11 +166,13 @@ module.exports = function(r) {
     }
 
     function *addAttributeSetID(processID, sampleID, asetID, direction) {
+        'use strict';
         let p2s = new Process2Sample(processID, sampleID, asetID, direction);
         yield db.insert('process2sample', p2s);
     }
 
     function *addAttribute(asetID, attr) {
+        'use strict';
         let newAttr = yield db.insert('attributes', attr);
         let as2a = new AttributeSet2Attribute(asetID, newAttr.id);
         yield db.insert('attributeset2attribute', as2a);
@@ -169,11 +180,13 @@ module.exports = function(r) {
     }
 
     function *addAttributeID(asetID, attrID) {
+        'use strict';
         let as2a = new AttributeSet2Attribute(asetID, attrID);
         yield db.insert('attributeset2attribute', as2a);
     }
 
     function *addBestMeasure(attrID, mID) {
+        'use strict';
         yield db.update('attributes', attr_id, {best_measure_id: mID});
         let bmHistory = new BestMeasureHistory(attrID, mID);
         yield db.insert('best_measure_history', bmHistory);
@@ -182,6 +195,7 @@ module.exports = function(r) {
     //////////////////////////////// moved into specific model //////////////
 
     function *addProcess(projectID, process) {
+        'use strict';
         let proc = yield db.insert('processes', process);
         let p2proc = new Project2Process(projectID, proc.id);
         yield db.insert('project2process', p2proc);
@@ -189,8 +203,29 @@ module.exports = function(r) {
     }
 
     function *addSettings(processID, settings, direction) {
+        'use strict';
         let setting = yield db.insert('settings', settings);
         let p2s = new Process2Setting(processID, setting.id, direction);
         yield db.insert('process2setting', p2s);
     }
+
+    return {
+        Sample: Sample,
+        Process: Process,
+        Process2Setting: Process2Setting,
+        Process2Measurement: Process2Measurement,
+        Process2Sample: Process2Sample,
+        Settings: Settings,
+        SettingProperty: SettingProperty,
+        Property: Property,
+        Measurement: Measurement,
+        Attribute: Attribute,
+        Attribute2Measurement: Attribute2Measurement,
+        AttributeSet: AttributeSet,
+        AttributeSet2Attribute: AttributeSet2Attribute,
+        Project2Process: Project2Process,
+        Project2Sample: Project2Sample,
+        Sample2AttributeSet: Sample2AttributeSet,
+        BestMeasureHistory: BestMeasureHistory
+    };
 };
