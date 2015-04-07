6110ccd7fa62f73e9475cd9fde87940967af17d3
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-07 15:47:12 -0400

    Formatting of output.

--- backend/server/db/model/createdb.js
+++ backend/server/db/model/createdb.js
@@ -18,26 +18,26 @@ promise.coroutine(function *() {
         console.log(err);
     }
 
-    console.log("Creating test database.");
+    console.log("Creating mctest database.");
     console.log("=======================");
     yield r.dbCreate('mctest');
     yield * createTable('processes', 'template_id');
     yield * createTable('project2process', 'project_id', 'process_id');
     yield * createTable('settings');
-    yield * createTable('process2settings', 'process_id', 'setting_id');
+    yield * createTable('process2setting', 'process_id', 'setting_id');
     yield * createTable('measurements', 'process_id');
     yield * createTable('process2measurement', 'process_id', 'measurement_id');
-    yield * createTable('measurement2datafile', 'measurement_id', 'datafile_id');
+    yield * createTable('setting_properties', 'setting_id');
     yield r.getPool().drain();
 
     /////////////////////////////
 
     function *createTable(table) {
-        console.log('create table', table);
+        console.log('  create table...', table);
         yield r.tableCreate(table);
         if (arguments.length > 1) {
             for (let i = 1; i < arguments.length; i++) {
-                console.log('   create index', arguments[i]);
+                console.log('     create index', arguments[i]);
                 yield r.table(table).indexCreate(arguments[i]);
             }
         }
