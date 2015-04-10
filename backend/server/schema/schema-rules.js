85d9b1f4d4268304105b375af3f4f919b13d670e
V. Glenn Tarcea <gtarcea@umich.edu>
2015-04-10 15:34:35 -0400

    Additional rules for schema validation.

--- backend/server/schema/schema-rules.js
+++ backend/server/schema/schema-rules.js
@@ -23,7 +23,9 @@ module.exports = function(model) {
         mustExistInProject: mustExistInProject,
         isValidPropertyType: isValidPropertyType,
         isValidUnit: isValidUnit,
-        oneOf: oneOf
+        oneOf: oneOf,
+        mustBeForSample: mustBeForSample,
+        mustBeValidMeasurements: mustBeValidMeasurements
     };
 
     ////////////////////////////////////////
@@ -116,6 +118,10 @@ module.exports = function(model) {
     }
 
     /**
+     *
+     * @param {} what
+     * @param {} spec
+     * @returns {}
      */
     function oneOf(what, spec) {
         let attrs = spec.split(':');
@@ -130,5 +136,126 @@ module.exports = function(model) {
             actual: this,
             expected: `one of the following attributes ${attrs}`
         };
+    }
+
+    /**
+     * Checks that a given attribute_id or attribute_set_id belongs
+     * to the sample_id in this.
+     * @param {String} id - The id to look up.
+     * @param {String} idType - Either 'attribute' or 'attributset'
+     * @param {Function} done - Function to call with validation status.
+     */
+    function mustBeForSample(id, idType, done) {
+        if (!id) {
+            // Rule can be called because we couldn't specify
+            // the id as optionally nullable. So even if the
+            // attribute doesn't exist this rule is called.
+            // This check just returns no error when that occurs.
+            done(null);
+            return;
+        }
+
+        let sample_id = this.sample_id;
+        if (idType === 'attribute') {
+            model.samples.validateAttribute(sample_id, id).then(checkMatch);
+        } else if (idType === 'attributeset') {
+            model.samples.validateAttributeSet(sample_id, id).then(checkMatch);
+        } else {
+            done({
+                rule: 'mustBeForSample',
+                actual: idType,
+                expected: `${idType} to be either 'attribute' or 'attributeset'`
+            });
+        }
+
+        return;
+
+        /////////////////////////////////////
+
+        /**
+         * Checks if there were any matches. If not generates an error.
+         * @param {Array} matches - A list of matching items.
+         */
+        function checkMatch(matches) {
+            if (matches.length !== 0) {
+                done(null);
+            } else {
+                let error = {
+                    rule: 'mustBeForSample',
+                    actual: id,
+                    expected: `${id} to belong to sample ${sample_id}`
+                };
+                done(error);
+            }
+        }
+    }
+
+    /**
+     * Ensures that the measurement ids are valid. It does this by making sure
+     * all the measurements came from this sample, and that the measurements
+     * all have the same type.
+     * @param {Array} measurements - List of measurement ids.
+     * @param {Bool} _ignore - Ignored (parameter in schema definition)
+     * @param {Function} done - Callback to signal success or failure
+     */
+    function mustBeValidMeasurements(measurements, _ignore, done) {
+        let measurementType = this._type;
+        let sampleID = this.sample_id;
+        if (measurements.length === 0) {
+            done(null);
+        } else {
+            model.sample.getMeasurements(sampleID, measurements).then(checkMeasures);
+        }
+
+        return;
+
+        //////////////////
+
+        function checkMeasures(measures) {
+            let error = {
+                rule: 'mustBeValidMeasurements',
+                actual: measures,
+                expected: null
+            };
+            if (measures.length !== measurements.length) {
+                // One or more measurements were not associated with the given
+                // sample id.
+                error.expected = `measurements to all be for sample ${sampleID}`;
+            } else {
+                let index = _.indexOf(measures, function(m) {
+                    return m._type !== measurementType;
+                });
+                if (index !== -1) {
+                    // found a measurement with the wrong type.
+                    error.expected = `all measurement to have type ${measurementType}`;
+                }
+            }
+            // if error.expected was set then return an error, otherwise
+            // signal success by calling done with null.
+            done(error.expected ? error : null);
+        }
+    }
+
+    /**
+     *
+     * @param {} attributes
+     * @param {} _ignore
+     * @param {} done
+     */
+    function mustBeForAttributeSet(attributes, _ignore, done) {
+        if (!attributes) {
+            done(null);
+            return;
+        }
+        let asetID = this.attribute_set_id;
+        let error = {
+            rule: 'mustBeForAttributeSet',
+            actual: attributes,
+            expected: `all attributes to belong to attribute set ${asetID}`
+        };
+        model.sample.getAttributesFromAS(asetID, attributes)
+            .then(function(attrs) {
+                done(attrs.length !== attributes.length ? error : null);
+            });
     }
 };
