139ef3ffac06b8b2ceb0708a0bbc1b3116b92659
tammasr <tammasr@umich.edu>
2015-04-13 17:21:29 -0400

    review page details updated - 2

---
+++ website/app/application/core/projects/project/files/file-edit-controller.js
@@ -0,0 +1,7 @@
+Application.Controllers.controller("FilesEditController",
+    ["$scope", "projectFiles", "applySearch",
+        "$filter", "pubsub", "mcfile", "tags", "User", FilesEditController]);
+function FilesEditController($scope, projectFiles, applySearch,
+                         $filter, pubsub, mcfile, tags, User) {
+
+}
