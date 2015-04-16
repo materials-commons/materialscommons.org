941a286db034c1e0a41cc062a517f15f9b47c454
tammasr <tammasr@umich.edu>
2015-04-16 15:29:36 -0400

    file details page

--- backend/mcapi/user/build_notes.py
+++ backend/mcapi/user/build_notes.py
@@ -9,14 +9,22 @@ def construct_notes(j):
     title = dmutil.get_required('title', j)
     text = dmutil.get_required('note', j)
     project_id = dmutil.get_required('project_id', j)
-    n = note.Note(owner, note, title, project_id)
+    #n = note.Note(owner, text, title, project_id)
+    n = {}
+    n['owner'] = owner
+    n['title'] = title
+    n['note'] = text
+    n['project_id'] = project_id
     return n
 
 
 def update_join(n, n2item):
     result = r.table('notes').get(n2item[0]['note_id']).update(
-            {'note': n['text'], 'title': n['title'], 'mtime': r.now()
-            },return_changes=True).run(g.conn)
+            {
+                'note': n['note'],
+                'title': n['title'],
+                'mtime': r.now()
+            },return_changes=True).run(g.conn, time_format='raw')
     updated_note = result['changes'][0]['new_val']
     return resp.to_json(updated_note)
 
