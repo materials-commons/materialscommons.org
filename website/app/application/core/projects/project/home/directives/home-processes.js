02a6e37eb5092648071df6d34336ac31c1b1436a
tammasr <tammasr@umich.edu>
2015-05-01 13:29:22 -0400

    updates to home page - changing to angulargrid

--- website/app/application/core/projects/project/home/directives/home-processes.js
+++ website/app/application/core/projects/project/home/directives/home-processes.js
@@ -11,88 +11,60 @@ function homeProcessesDirective() {
 }
 
 Application.Controllers.controller("homeProcessesDirectiveController",
-                                   ["$scope", "ui", "projectState", "$state", "pubsub",
-                                    homeProcessesDirectiveController]);
+    ["$scope", "ui", "projectState", "$state", "pubsub",
+        homeProcessesDirectiveController]);
 function homeProcessesDirectiveController($scope, ui, projectState, $state, pubsub) {
     function segmentProcesses() {
-        $scope.bk = {
-            all: []
-        };
-
-        $scope.project.drafts.forEach(function(draft) {
-            if (!('showDetails' in draft)) {
-                draft.showDetails = false;
-            }
+        $scope.bk = {all: []};
+        $scope.project.drafts.forEach(function (draft) {
             $scope.bk.all.push(draft);
         });
-        $scope.project.processes.forEach(function(process) {
-            if (!('showDetails' in process)) {
-                process.showDetails = false;
-            }
+        $scope.project.processes.forEach(function (process) {
             $scope.bk.all.push(process);
         });
 
         var columnDefs = [
-            {displayName: "", field: "name", width: 300, template: '<span style="font-weight: bold;" ng-bind="data.name"></span>' +
-            '<p style="padding-top:12px;"><small  class="text-muted">April 30, 2015</small></p>' , cellStyle:{border: 0}},
-            {displayName: "", field: "owner", width: 300 ,  template: '<span style="padding-top:100px;" ng-bind="data.owner"></span>', cellStyle:{border: 0}},
-            {displayName: "", field: "", width: 300,  cellStyle:{border: 0}}
+            {
+                displayName: "",
+                field: "name",
+                width: 300,
+                template: '<span ng-bind="data.name"></span>' +
+                '<p><small><small  class="text-muted">April 30, 2015</small></small></p>',
+                cellStyle: {border: 0}
+            },
+            {
+                displayName: "",
+                field: "owner",
+                width: 300,
+                template: '<span ng-bind="data.owner"></span>',
+                cellStyle: {border: 0}
+            },
+            {displayName: "", field: "", width: 300, cellStyle: {border: 0}}
         ];
-        var rowData = []
-        $scope.project.processes.forEach(function(process) {
+        var rowData = [];
+        $scope.project.processes.forEach(function (process) {
             rowData.push({name: process.name, owner: process.owner});
         });
+
         $scope.gridOptions = {
             columnDefs: columnDefs,
             rowData: rowData,
             enableColResize: true,
             headerHeight: 0,
-            rowHeight:60,
-            //colWidth: 500,
-            angularCompileRows:true
+            rowHeight: 60,
+            angularCompileRows: true
         };
     }
 
-    pubsub.waitOn($scope, "processes.change", function() {
+    pubsub.waitOn($scope, "processes.change", function () {
         segmentProcesses();
     });
-
-    $scope.toggleExpanded = function() {
-        ui.toggleIsExpanded($scope.project.id, "processes");
-    };
-
-    $scope.isExpanded = function() {
-        return ui.isExpanded($scope.project.id, "processes");
-    };
-
-    $scope.minimize = function() {
-        ui.togglePanelState($scope.project.id, 'processes');
-    };
-
-    $scope.createProcess = function() {
+    $scope.createProcess = function () {
         var state = null;
         var stateID = projectState.add($scope.project.id, state);
         $state.go("projects.project.home.provenance", {sid: stateID});
     };
-
-    $scope.splitScreen = function(what, col){
-        ui.toggleColumns($scope.project.id, what, col);
-    };
-
-    $scope.isSplitExpanded = function () {
-        return ui.getSplitStatus($scope.project.id);
-    };
-
     segmentProcesses();
-
-
-
-
-
-
-
-
-
 
 
 }
--- website/app/application/core/projects/project/home/directives/home-reviews-directive.js
+++ website/app/application/core/projects/project/home/directives/home-reviews-directive.js
@@ -1,5 +1,5 @@
 Application.Directives.directive('homeReviews', homeReviewsDirective);
-function homeReviewsDirective () {
+function homeReviewsDirective() {
     return {
         restrict: "EA",
         controller: 'homeReviewsDirectiveController',
@@ -11,41 +11,55 @@ function homeReviewsDirective () {
 }
 
 Application.Controllers.controller("homeReviewsDirectiveController",
-                                   ["$scope",  "ui",
-                                    homeReviewsDirectiveController]);
-function homeReviewsDirectiveController ($scope, ui) {
-    $scope.project.reviews.forEach(function(review) {
-        if (!('showDetails' in review)) {
-            review.showDetails = false;
-        }
+    ["$scope",
+        homeReviewsDirectiveController]);
+function homeReviewsDirectiveController($scope) {
+    var rowData = [];
+    $scope.project.reviews.forEach(function (review) {
+        rowData.push({
+            title: review.title,
+            messages: review.messages,
+            who: review.messages[(review.messages.length - 1)].who,
+            mtime: review.messages[(review.messages.length - 1)].date
+        });
     });
-
-    $scope.minimize = function() {
-        ui.togglePanelState($scope.project.id, 'reviews');
+    var columnDefs = [
+        {
+            displayName: "",
+            field: "title",
+            width: 300,
+            template: '<span ng-bind="data.title"></span>' +
+            '<p><small class="text-muted"><i class="fa fa-fw fa-user"></i>' +
+            '<span ng-bind="data.who"></span>' +
+            '<small  style="padding-left: 60px;" ng-bind="data.mtime"></small></small></p>',
+            cellStyle: {border: 0}
+        },
+        {
+            displayName: "",
+            field: "messages",
+            width: 530,
+            template: '<span class="pull-right">' +
+            '<a ng-if="data.messages.length !== 0" class="fa-stack fa-2x">' +
+            '<i style="color: #FDAC6B;" class="fa fa-comment fa-stack-1x"></i>' +
+            '<i style="font-size: 10px;" class="fa fa-stack-1x fa-inverse">{{data.messages.length}}</i>' +
+            '</span>',
+            cellStyle: {border: 0}
+        },
+    ];
+    $scope.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: rowData,
+        enableColResize: true,
+        headerHeight: 0,
+        rowHeight: 65,
+        rowStyle: {'border-bottom': 'dotted #d3d3d3'},
+        angularCompileRows: true
     };
 
-    $scope.toggleExpanded = function() {
-        ui.toggleIsExpanded($scope.project.id, "reviews");
-    };
-
-    $scope.isExpanded = function() {
-        return ui.isExpanded($scope.project.id, "reviews");
-    };
-
-    $scope.createReview = function(){
+    $scope.createReview = function () {
         $scope.bk.createReview = true;
     };
-
     $scope.bk = {
         createReview: false
     };
-
-    $scope.splitScreen = function(what, col){
-        ui.toggleColumns($scope.project.id, what, col);
-    };
-
-    $scope.isSplitExpanded = function () {
-        return ui.getSplitStatus($scope.project.id);
-    };
-
 }
