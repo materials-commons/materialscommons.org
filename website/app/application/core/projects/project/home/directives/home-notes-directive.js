28fe8b7433835ad545257687b4b6a705355a1180
tammasr <tammasr@umich.edu>
2015-05-01 13:29:22 -0400

    updates to home page - changing to angulargrid

--- website/app/application/core/projects/project/home/directives/home-notes-directive.js
+++ website/app/application/core/projects/project/home/directives/home-notes-directive.js
@@ -15,12 +15,37 @@ Application.Controllers.controller("homeNotesDirectiveController",
         homeNotesDirectiveController]);
 
 function homeNotesDirectiveController($scope, ui) {
+    var rowData = [];
     $scope.project.notes.forEach(function (note) {
-        if (!('showDetails' in note)) {
-            note.showDetails = false;
-        }
+        rowData.push({
+            title: note.title,
+            note: note.note,
+            owner: note.creator,
+            mtime: note.mtime
+        });
     });
-
+    var columnDefs = [
+        {
+            displayName: "",
+            field: "title",
+            width: 830,
+            template: '<span ng-bind="data.title"></span>' +
+            '<p><small class="text-muted"><i class="fa fa-fw fa-user"></i>' +
+            '<span ng-bind="data.owner"></span>' +
+            '<small  style="padding-left: 60px;" ng-bind="data.mtime"></small></small></p>' +
+            '<p ta-bind="text" ng-model="data.note" ta-readonly="disabled"></p>',
+            cellStyle: {border: 0}
+        }
+    ];
+    $scope.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: rowData,
+        enableColResize: true,
+        headerHeight: 0,
+        rowHeight: 65,
+        rowStyle: {'border-bottom': 'dotted #d3d3d3'},
+        angularCompileRows: true
+    };
     $scope.minimize = function () {
         ui.togglePanelState($scope.project.id, 'notes');
     };
