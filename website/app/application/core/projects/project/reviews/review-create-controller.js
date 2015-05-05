926188fd27039147e2dd0dc899ac5f7b2e9d2697
tammasr <tammasr@umich.edu>
2015-05-05 16:40:30 -0400

    update checkbox selection - using grid:

--- website/app/application/core/projects/project/reviews/review-create-controller.js
+++ website/app/application/core/projects/project/reviews/review-create-controller.js
@@ -1,13 +1,13 @@
 Application.Controllers.controller('projectCreatetReview',
-    ["$scope", "project", "User", "pubsub", projectCreatetReview]);
+    ["$scope", "project", "User", "pubsub", "$modal", "$log" ,projectCreatetReview]);
 
-function projectCreatetReview($scope, project, User, pubsub) {
+function projectCreatetReview($scope, project, User, pubsub,$modal, $log) {
 
     pubsub.waitOn($scope, 'addSampleToReview', function (sample) {
         addAttachment({'id': sample.id, 'name': sample.name, 'type': 'sample'});
     });
-    pubsub.waitOn($scope, 'addProcessToReview', function (provenance) {
-        addAttachment({'id': provenance.id, 'name': provenance.name, 'type': 'process'});
+    pubsub.waitOn($scope, 'addProcessToReview', function (process) {
+        addAttachment({'id': process.id, 'name': process.name, 'type': 'process'});
     });
     pubsub.waitOn($scope, 'addFileToReview', function (file) {
         addAttachment({'id': file.id, 'name': file.name, 'type': 'file', 'path': file.fullname});
@@ -37,8 +37,22 @@ function projectCreatetReview($scope, project, User, pubsub) {
         if (i === -1) {
             $scope.model.attachments.push(item);
         } else{
-            $scope.model.attachments.splice(i, 1);
+
+            //$scope.model.attachments.splice(i, 1);
         }
     }
-}
 
+    $scope.modal = {
+        instance: null,
+        items: ['item1', 'item2', 'item3']
+    };
+
+    $scope.open = function (size) {
+        $scope.modal.instance = $modal.open({
+            template: '<div modal-instance modal="modal" project="project">',
+            scope: $scope,
+            size: size
+        });
+
+    };
+}
