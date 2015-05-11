7607c77705a4fc963022a59d002cc5d47d56d876
tammasr <tammasr@umich.edu>
2015-05-11 16:49:51 -0400

    attachments updates

--- website/app/application/core/projects/project/reviews/modal-instance-directive.js
+++ website/app/application/core/projects/project/reviews/modal-instance-directive.js
@@ -1,27 +1,29 @@
-Application.Directives.directive('modalInstance', modalInstanceDirective);
-
-function modalInstanceDirective() {
-    return {
-        scope: {
-            project: '=project',
-            modal: "="
-        },
-        controller: 'ModalInstanceCtrl',
-        restrict: "AE",
-        templateUrl: "application/core/projects/project/reviews/myModalContent.html"
-    };
-}
+//Application.Directives.directive('modalInstance', modalInstanceDirective);
+//
+//function modalInstanceDirective() {
+//    return {
+//        scope: {
+//            project: '=project',
+//            modal: "="
+//        },
+//        controller: 'ModalInstanceCtrl',
+//        restrict: "AE",
+//        templateUrl: "application/core/projects/project/reviews/myModalContent.html"
+//    };
+//}
 
 Application.Controllers.controller('ModalInstanceCtrl',
-    ["$scope", "$log",ModalInstanceCtrl]);
+    ["$scope", "$log", "modal", "project",ModalInstanceCtrl]);
 
-function ModalInstanceCtrl($scope, $log) {
+function ModalInstanceCtrl($scope, $log, modal, project) {
+    $scope.modal = modal;
+    $scope.project = project;
     $scope.selected = {
         item: $scope.modal.items[0]
     };
 
     $scope.ok = function () {
-        $scope.modal.instance.close($scope.selected);
+        $scope.modal.instance.close($scope.selected.item);
     };
 
     $scope.cancel = function () {
