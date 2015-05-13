88b6cbc225af4fc66e00bcbf917cb30f434734d1
tammasr <tammasr@umich.edu>
2015-05-13 16:10:06 -0400

    file tree- using angularGrid

--- website/app/application/core/projects/project/home/directives/home-files-directive.js
+++ website/app/application/core/projects/project/home/directives/home-files-directive.js
@@ -21,9 +21,7 @@ function homeFilesDirectiveController($scope, ui, projectFiles, applySearch,
     f.showDetails = true;
     $scope.files = [f];
     $scope.files.showDetails = true;
-
     var columnDefs = [
-
         {
             displayName: "",
             field: "name",
@@ -31,80 +29,23 @@ function homeFilesDirectiveController($scope, ui, projectFiles, applySearch,
             template: '<i style="color: #BFBFBF;" class="fa fa-fw fa-file"></i><span ng-bind="data.name"></span>'
         },
         {displayName: "", field: "size", width: 250},
-        {displayName: "", field: "dateModified", width: 250}
+        {displayName: "", field: "birthtime", width: 250}
     ];
-    var rowData = [
-
-        {
-            group: true,
-            data: {name: 'Windows', size: '20kb', type: 'datadir', dateModified: '27/02/2014 04:12'},
-            children: [
-                {
-                    group: true,
-                    data: {
-                        name: 'bfsve.exe',
-                        size: '56 kb',
-                        type: 'datadir',
-                        dateModified: '13/03/2014 10:14'
-                    },
-                    children: [
-                        {
-                            group: false,
-                            data: {
-                                name: '1.txt',
-                                size: '56 kb',
-                                type: 'datafile',
-                                dateModified: '13/03/2014 10:14'
-                            }
-                        },
-
-                        {
-                            group: false,
-                            data: {
-                                name: '2.txt',
-                                size: '1 kb',
-                                type: 'datafile',
-                                dateModified: '27/11/2012 04:12'
-                            }
-                        },
-
-                        {
-                            group: false,
-                            data: {
-                                name: '3.xml',
-                                size: '21 kb',
-                                type: 'datafile',
-                                dateModified: '18/03/2014 00:56'
-                            }
-                        }]
-
-                },
-
-                {
-                    group: false,
-                    data: {
-                        name: 'csup.txt',
-                        size: '1 kb',
-                        type: 'datafile',
-                        dateModified: '27/11/2012 04:12'
-                    }
-                },
-
-                {
-                    group: false,
-                    data: {
-                        name: 'diagwrn.xml',
-                        size: '21 kb',
-                        type: 'datafile',
-                        dateModified: '18/03/2014 00:56'
-                    }
-                }]
-
+    var treeModel = new TreeModel(),
+        root = treeModel.parse(f);
+    root.walk({strategy: 'pre'}, function(node){
+        if (node.model.type === 'datadir'){
+            node.model.group = true;
+            node.model.data = {name: node.model.name, size: node.model.size, birthtime: $filter('toDateString')(node.model.birthtime)};
+        }else{
+            node.model.group = false;
+            node.model.data = {name: node.model.name, size: node.model.size, birthtime: $filter('toDateString')(node.model.birthtime)};
         }
-    ];
+    });
+
     $scope.gridOptions = {
         columnDefs: columnDefs,
-        rowData: rowData,
+        rowData: $scope.files,
         rowSelection: 'multiple',
         rowsAlreadyGrouped: true,
         enableColResize: true,
@@ -117,23 +58,10 @@ function homeFilesDirectiveController($scope, ui, projectFiles, applySearch,
 
         },
         groupInnerCellRenderer: groupInnerCellRenderer
-        //rowClicked: rowClicked
     };
-
-    //function rowClicked(params) {
-    //    var node = params.node;
-    //    var path = node.data.name;
-    //    while (node.parent) {
-    //        node = node.parent;
-    //        path = node.data.name  + '\\' + path;
-    //    }
-    //    $scope.selectedFile = path;
-    //}
-
-
     function groupInnerCellRenderer(params) {
-        if (params.data.type === 'datadir') {
-            return '' + params.data.name;
+        if (params.node.type === 'datadir') {
+            return '' + params.node.name;
         }
         //var image = params.node.level === 0 ? 'disk' : 'folder';
         //var imageFullUrl = '/example-file-browser/' + image + '.png';
