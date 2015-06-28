4de70a804492b32729db61cfb7e3da5c07a4fd1e
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-28 14:56:20 -0400

    Support website on port other than 80.

--- website/app/index/config.js
+++ website/app/index/config.js
@@ -2,6 +2,6 @@ function doConfig() {
     if (window.location.hostname === 'localhost') {
         mcglobals.apihost = window.location.protocol + '//localhost:5002';
     } else {
-        mcglobals.apihost = window.location.protocol + '//' + window.location.hostname + '/api';
+        mcglobals.apihost = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api';
     }
 }
