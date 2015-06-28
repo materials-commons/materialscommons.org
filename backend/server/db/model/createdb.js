dafa53c11ea9bd66f427847e170c5e93d8a49d88
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-28 14:56:47 -0400

    Create tables.

--- backend/server/db/model/createdb.js
+++ backend/server/db/model/createdb.js
@@ -25,24 +25,19 @@ promise.coroutine(function *() {
     yield * createTable('project2process', 'project_id', 'process_id');
     yield * createTable('setups');
     yield * createTable('process2setup', 'process_id', 'setup_id');
-    yield * createTable('measurements', 'process_id');
-    yield * createTable('measure2datafile', 'measurement_id', 'datafile_id');
-    yield * createTable('process2measurement', 'process_id', 'measurement_id');
     yield * createTable('setupproperties', 'setting_id');
-    yield * createTable('attributes', 'parent_id');
-    yield * createTable('attribute2measurement', 'attribute_id', 'measurement_id');
-    yield * createTable('attributeset2attribute', 'attribute_set_id', 'attribute_id');
-    yield * createTable('attribute2process', 'attribute_id', 'process_id');
-    yield * createTable('samples');
-    yield * createTable('sample2file', 'sample_id', 'datafile_id');
-    yield * createTable('attributeset', 'parent_id');
-    yield * createTable('sample2attributeset', 'sample_id', 'attribute_set_id');
-    yield * createTable('process2sample', 'process_id', 'sample_id', 'attribute_set_id');
-    yield * createTable('project2sample', 'project_id', 'sample_id');
-    yield * createTable('best_measure_history', 'process_id', 'attribute_id', 'measurement_id');
     yield * createTable('process2setupfile', 'process_id', 'datafile_id');
-    yield * createTable('process2output', 'process_id', 'datafile_id');
-    yield * createTable('process2input', 'process_id', 'datafile_id');
+    yield * createTable('properties', 'parent_id');
+    yield * createTable('propertyset2property', 'attribute_set_id', 'attribute_id'); // TODO: Review indices
+    yield * createTable('measurements', 'process_id');
+    yield * createTable('property2measurement', 'attribute_id', 'measurement_id'); // TODO: Review indices
+    yield * createTable('process2measurement', 'process_id', 'measurement_id');
+    yield * createTable('samples');
+    yield * createTable('sample2propertyset', 'sample_id', 'attribute_set_id'); // TODO: Review indices
+    yield * createTable('process2sample', 'process_id', 'sample_id', 'attribute_set_id'); // TODO: Review indices
+    yield * createTable('project2sample', 'project_id', 'sample_id');
+    yield * createTable('best_measure_history', 'process_id', 'attribute_id', 'measurement_id'); // TODO: Review indices
+    yield * createTable('process2file'); // TODO: Review indices
     yield r.getPool().drain();
 
     /////////////////////////////
