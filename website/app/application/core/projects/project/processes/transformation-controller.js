e97a43bd2d8c5e97b7c5ab762a6930f7d4c5df97
tammasr <tammasr@umich.edu>
2015-07-14 12:35:50 -0400

    updates to the transformed sample interface

--- website/app/application/core/projects/project/processes/transformation-controller.js
+++ website/app/application/core/projects/project/processes/transformation-controller.js
@@ -4,21 +4,21 @@ Application.Controllers.controller('TransformationController',
 function TransformationController($scope, $log, modal) {
 
     $scope.modal = modal;
-    console.dir(modal.sample);
-    $scope.selected = {
-        item: {}
-    };
+    setPropertiesToUnknown(); //is a default option
 
     $scope.ok = function () {
-        console.log($scope.selected.item );
-        $scope.transformed_sample = {sample_id: $scope.modal.sample.id,
-            'property_set_id': $scope.modal.sample.property_set_id,
-            action: $scope.selected.item }
-        $scope.modal.instance.close($scope.selected.item);
+        $scope.modal.sample.transformed_properties = $scope.modal.sample.properties;
+        $scope.modal.instance.close($scope.modal);
     };
 
     $scope.cancel = function () {
     };
+
+    function setPropertiesToUnknown(){
+        $scope.modal.sample.properties.forEach(function(property){
+             property.action = 'unknown';
+        });
+    }
 
     $scope.modal.instance.result.then(function (selectedItem) {
         $scope.selected = selectedItem;
