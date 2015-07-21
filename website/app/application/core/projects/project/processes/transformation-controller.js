deeb3265844e856b4c1f47982f8cc459fc0607e4
tammasr <tammasr@umich.edu>
2015-07-21 14:53:16 -0400

    updates to the transormed sample

--- website/app/application/core/projects/project/processes/transformation-controller.js
+++ website/app/application/core/projects/project/processes/transformation-controller.js
@@ -5,23 +5,30 @@ function TransformationController($scope, $log, modal, pubsub) {
 
     $scope.modal = modal;
     setPropertiesToUnknown(); //is a default option
-
+    $scope.actions = ["unknown", "copy","share", "delete"];
     $scope.ok = function () {
         var transformed_sample = {
             sample_id: $scope.modal.sample.id,
             name: $scope.modal.sample.name,
             shares: [],
             uses: [],
+            deletes: [],
+            unknowns: [],
             property_set_id: $scope.modal.sample.property_set_id
         };
-
         $scope.modal.sample.properties.forEach(function(property){
-            if (property.action === 'is_not_transformed'){
+            if (property.action === 'share'){
                 transformed_sample.shares.push({id: property.id, name: property.name});
-            }else if(property.action === 'is_transformed'){
+            }else if(property.action === 'copy'){
                 transformed_sample.uses.push({id: property.id, name: property.name});
+            } else if(property.action === 'unknown'){
+                transformed_sample.unknowns.push({id: property.id, name: property.name});
+            }else if(property.action === 'delete'){
+                transformed_sample.deletes.push({id: property.id, name: property.name});
             }
         });
+
+        console.dir(transformed_sample);
         pubsub.send('updateTransformedSample', transformed_sample);
         $scope.modal.instance.close($scope.modal);
     };
