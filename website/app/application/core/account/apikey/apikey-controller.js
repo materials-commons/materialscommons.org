39677f3898f234c32d8974480d9904b033a5d61d
tammasr <tammasr@umich.edu>
2015-07-22 13:53:27 -0400

    removed services n cleanup dependencies

--- website/app/application/core/account/apikey/apikey-controller.js
+++ website/app/application/core/account/apikey/apikey-controller.js
@@ -1,5 +1,5 @@
 Application.Controllers.controller('accountApikey',
-    ["$scope", "mcapi", "User", "Nav", function ($scope, mcapi, User, Nav) {
+    ["$scope", "mcapi", "User", function ($scope, mcapi, User) {
 
         $scope.showApiKey = function () {
             $scope.showKey = !$scope.showKey;
@@ -18,7 +18,6 @@ Application.Controllers.controller('accountApikey',
         };
 
         function init() {
-            Nav.setActiveNav('apikey');
             $scope.showKey = false;
             $scope.showHideButton = "Show API Key";
             $scope.apikey = User.apikey();
