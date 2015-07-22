72b17aae16d261b037893127659f14923b8f9412
tammasr <tammasr@umich.edu>
2015-07-22 14:09:21 -0400

    changes to navigation logout navbar controllers

--- website/app/application/core/logout/logout-controller.js
+++ website/app/application/core/logout/logout-controller.js
@@ -1,11 +1,10 @@
 Application.Controllers.controller('logout',
-    ["$rootScope", "$state", "$window", "User", "projectFiles", "ProvDrafts", "model.projects",
-        function ($rootScope, $state, $window, User, projectFiles, ProvDrafts, projects) {
+    ["$rootScope", "$state", "$window", "User", "projectFiles", "model.projects",
+        function ($rootScope, $state, $window, User, projectFiles, projects) {
             $rootScope.email_address = '';
             $window.sessionStorage.removeItem('mcuser');
             User.setAuthenticated(false, '', '');
             projectFiles.clear();
-            //ProvDrafts.clear();
             projects.clear();
             $state.transitionTo('home');
         }]);
