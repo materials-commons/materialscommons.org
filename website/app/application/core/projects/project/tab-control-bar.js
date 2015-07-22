767e9b64dd1c8eb07d450f9a24738bf25855f063
tammasr <tammasr@umich.edu>
2015-07-22 10:39:36 -0400

    removed tab control bar

--- website/app/application/core/projects/project/tab-control-bar.js
+++
@@ -1,9 +0,0 @@
-Application.Directives.directive("tabControlBar", tabControlBarDirective);
-
-function tabControlBarDirective() {
-    return {
-        transclude: true,
-        restrict: "AE",
-        templateUrl: "application/core/projects/project/tab-control-bar.html"
-    };
-}
