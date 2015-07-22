5ef6b773bc9c4945484019c602ca69d573bac715
tammasr <tammasr@umich.edu>
2015-07-22 13:40:56 -0400

    removed project file count and mfilter , projectsize filters

--- website/app/application/core/projects/project/project-size-filter.js
+++
@@ -1,8 +0,0 @@
-Application.Filters.filter('projectSize', function() {
-    return function(project) {
-        if (!project) {
-            return 0;
-        }
-        return bytesToSizeStr(project.size);
-    };
-});
