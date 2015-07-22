537558c66b79eaa9612038fef5b44ae9deea308b
tammasr <tammasr@umich.edu>
2015-07-22 14:35:13 -0400

    cleanup

--- website/app/application/default-stack-drop.js
+++
@@ -1,8 +0,0 @@
-Application.Controllers.controller('defaultStackDrop', ["$scope", defaultStackDrop]);
-
-function defaultStackDrop($scope) {
-    $scope.onDrop = function() {
-        console.log("onDrop");
-        return true;
-    };
-}
