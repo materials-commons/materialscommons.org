53336161ca72c6e975b689384ad63da04a82ee17
tammasr <tammasr@umich.edu>
2015-07-24 17:13:26 -0400

    updates to the measurement types

--- website/app/application/core/components/properties/partials/display-measurement-directive.js
+++ website/app/application/core/components/properties/partials/display-measurement-directive.js
@@ -2,10 +2,39 @@ Application.Directives.directive('displayMeasurement', displayMeasurementDirecti
 function displayMeasurementDirective() {
     return {
         restrict: "EA",
+        controller: 'displayMeasurementDirectiveController',
         scope: {
             measure: '=measure',
             currentProcess: '='
         },
         templateUrl: 'application/core/components/properties/partials/display-measurement.html'
+    };
+}
+
+Application.Controllers.controller("displayMeasurementDirectiveController",
+    ["$scope",  "$modal", displayMeasurementDirectiveController]);
+function displayMeasurementDirectiveController($scope, $modal) {
+
+    $scope.showLineChart = function(measurement){
+
+        console.dir(measurement);
+        $scope.modal = {
+            instance: null,
+            measurement: measurement
+        };
+
+        $scope.modal.instance = $modal.open({
+            size: 'lg',
+            templateUrl: 'application/core/projects/project/processes/sidebar/line-graph.html',
+            controller: '',
+            resolve: {
+                modal: function () {
+                    return $scope.modal;
+                },
+                project: function () {
+                    return $scope.project;
+                }
+            }
+        });
     };
 }
