9757c7c4200cb50d867eb2c2c17580917f3556ee
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-24 14:31:19 -0400

    Rework how the default route functions for the files tab now that there are additional routes and tabs.

--- website/app/application/core/projects/project/files/files-controller.js
+++ website/app/application/core/projects/project/files/files-controller.js
@@ -1,90 +1,13 @@
-Application.Controllers.controller("FilesController",
-    ["$scope", "projectFiles", "applySearch",
-        "pubsub", "mcfile", "$state", "pubsub", FilesController]);
-function FilesController($scope, projectFiles, applySearch,
-                         $filter, mcfile, $state, pubsub) {
-    var f = projectFiles.model.projects[$scope.project.id].dir;
+Application.Controllers.controller("FilesController", ["$state", FilesController]);
+function FilesController($state) {
+    //console.log("FileController going to projects.project.files.all");
+    //$state.go('projects.project.files.all');
+    var ctrl = this;
 
-    // Root is name of project. Have it opened by default.
-    $scope.files = [f];
-    applySearch($scope, "searchInput", applyQuery);
+    ctrl.showSearchResults = showSearchResults;
 
-    pubsub.waitOn($scope, 'files.refresh', function() {
-        $scope.gridOptions.api.refreshView();
-    });
-
-    function applyQuery() {
-        var search = {
-            name: ""
-        };
-
-        if ($scope.searchInput === "") {
-            $scope.files = [projectFiles.model.projects[$scope.project.id].dir];
-        } else {
-            var filesToSearch = projectFiles.model.projects[$scope.project.id].byMediaType.all;
-            search.name = $scope.searchInput;
-            $scope.files = $filter('filter')(filesToSearch, search);
-        }
+    //////////////////
+    function showSearchResults() {
+        $state.go('projects.project.files.search');
     }
-
-    $scope.fileSrc = function (file) {
-        return mcfile.src(file.id);
-    };
-
-    var columnDefs = [
-        {
-            displayName: "",
-            field: "name",
-            width: 350,
-            cellClicked: cellClicked,
-            cellRenderer: function (params) {
-                return '<i style="color: #BFBFBF;"  class="fa fa-fw fa-file"></i><span>' +
-                    '<a data-toggle="tooltip" data-placement="top" title="{{params.node.name}}">' +
-                    params.node.name + '</a></span>';
-            }
-        }];
-
-    $scope.gridOptions = {
-        columnDefs: columnDefs,
-        rowData: $scope.files,
-        rowSelection: 'multiple',
-        rowsAlreadyGrouped: true,
-        enableColResize: true,
-        enableSorting: true,
-        rowHeight: 30,
-        angularCompileRows: true,
-        icons: {
-            groupExpanded: '<i style="color: #D2C4D5 " class="fa fa-folder-open"/>',
-            groupContracted: '<i style="color: #D2C4D5 " class="fa fa-folder"/>'
-        },
-        groupInnerCellRenderer: groupInnerCellRenderer
-    };
-
-    //groupInnerCellRenderer does not have ability to add ng click. So as
-    // an alternative i used event listener to display the data directory
-    function groupInnerCellRenderer(params) {
-        var eCell = document.createElement('span');
-        eCell.innerHTML = params.node.displayname;
-        eCell.addEventListener('click', function () {
-            projectFiles.setActiveDirectory(params.node);
-            if ($state.current.name === 'projects.project.files') {
-                $state.go('projects.project.files.edit', {'file_id': ''}, {reload: true});
-            } else {
-                pubsub.send('display-directory');
-            }
-
-        });
-        return eCell;
-    }
-
-    function cellClicked(params) {
-        projectFiles.setActiveFile(params.node);
-        $state.go('projects.project.files.edit', {'file_id': params.node.df_id}, {reload: true});
-    }
-
-    function init() {
-        projectFiles.setActiveDirectory($scope.files[0]);
-    }
-
-    init();
 }
