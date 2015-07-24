ed5ea3468a3bb81726f4913dd8dd43db11538b10
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-24 14:31:19 -0400

    Rework how the default route functions for the files tab now that there are additional routes and tabs.

---
+++ website/app/application/core/projects/project/files/files-all-controller.js
@@ -0,0 +1,71 @@
+Application.Controllers.controller("FilesAllController",
+    ["$scope", "projectFiles", "mcfile", "$state", "pubsub", FilesAllController]);
+function FilesAllController($scope, projectFiles, mcfile, $state, pubsub) {
+    console.log("FilesAllController");
+    var f = projectFiles.model.projects[$scope.project.id].dir;
+
+    // Root is name of project. Have it opened by default.
+    $scope.files = [f];
+
+    pubsub.waitOn($scope, 'files.refresh', function() {
+        $scope.gridOptions.api.refreshView();
+    });
+
+    $scope.fileSrc = function (file) {
+        return mcfile.src(file.id);
+    };
+
+    var columnDefs = [
+        {
+            displayName: "",
+            field: "name",
+            width: 350,
+            cellRenderer: function (params) {
+                return '<i style="color: #BFBFBF;"  class="fa fa-fw fa-file"></i><span>' +
+                    '<a data-toggle="tooltip" data-placement="top" title="{{params.node.name}}">' +
+                    params.node.name + '</a></span>';
+            }
+        }];
+
+    $scope.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: $scope.files,
+        rowSelection: 'multiple',
+        rowsAlreadyGrouped: true,
+        rowClicked: rowClicked,
+        enableColResize: true,
+        enableSorting: true,
+        rowHeight: 30,
+        angularCompileRows: true,
+        icons: {
+            groupExpanded: '<i style="color: #D2C4D5 " class="fa fa-folder-open"/>',
+            groupContracted: '<i style="color: #D2C4D5 " class="fa fa-folder"/>'
+        },
+        groupInnerCellRenderer: groupInnerCellRenderer
+    };
+
+    function groupInnerCellRenderer(params) {
+        var eCell = document.createElement('span');
+        eCell.innerHTML = params.node.displayname;
+        return eCell;
+    }
+
+    function rowClicked(params) {
+        console.log("rowClicks %O", params);
+        if (params.node.type == 'datadir') {
+            projectFiles.setActiveDirectory(params.node)
+        } else {
+            projectFiles.setActiveFile(params.node);
+        }
+        $state.go('projects.project.files.all.edit', {file_id: params.node.df_id, file_type: params.node.type});
+    }
+
+    function init() {
+        console.log("FilesAllController init");
+        projectFiles.setActiveDirectory($scope.files[0]);
+        $state.go('projects.project.files.all.edit', {file_id: $scope.files[0].df_id, file_type: 'datadir'});
+    }
+
+    init();
+}
+
