927f134f72634ad149dfe7165eb228b1c7214beb
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-24 15:24:25 -0400

    Remove console statement. Move initialization logic for controller into init() method.

--- website/app/application/core/projects/project/files/files-controller.js
+++ website/app/application/core/projects/project/files/files-controller.js
@@ -1,13 +1,19 @@
 Application.Controllers.controller("FilesController", ["$state", FilesController]);
 function FilesController($state) {
-    //console.log("FileController going to projects.project.files.all");
-    //$state.go('projects.project.files.all');
     var ctrl = this;
 
     ctrl.showSearchResults = showSearchResults;
 
+    init();
+
     //////////////////
     function showSearchResults() {
         $state.go('projects.project.files.search');
+    }
+
+    function init() {
+        if ($state.current.name == "projects.project.files") {
+            $state.go('projects.project.files.all');
+        }
     }
 }
