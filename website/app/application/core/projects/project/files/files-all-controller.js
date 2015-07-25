5354e5a65d37163921812e18cba97408f7caef92
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-07-25 15:47:08 -0400

    Save open state of tree so it displays in the proper state. Show different icons for different file types.

--- website/app/application/core/projects/project/files/files-all-controller.js
+++ website/app/application/core/projects/project/files/files-all-controller.js
@@ -1,5 +1,5 @@
 Application.Controllers.controller("FilesAllController",
-    ["$scope", "projectFiles", "mcfile", "$state", "pubsub","$filter",  FilesAllController]);
+    ["$scope", "projectFiles", "mcfile", "$state", "pubsub", "$filter", FilesAllController]);
 function FilesAllController($scope, projectFiles, mcfile, $state, pubsub, $filter) {
     var f = projectFiles.model.projects[$scope.project.id].dir;
 
@@ -7,7 +7,7 @@ function FilesAllController($scope, projectFiles, mcfile, $state, pubsub, $filte
     $scope.files = [f];
     $scope.files[0].children = $filter('orderBy')($scope.files[0].children, 'displayname');
 
-    pubsub.waitOn($scope, 'files.refresh', function() {
+    pubsub.waitOn($scope, 'files.refresh', function () {
         $scope.gridOptions.api.refreshView();
     });
 
@@ -21,8 +21,37 @@ function FilesAllController($scope, projectFiles, mcfile, $state, pubsub, $filte
             field: "name",
             width: 350,
             cellRenderer: function (params) {
-                return '<i style="color: #BFBFBF;"  class="fa fa-fw fa-file"></i><span>' +
-                    '<a data-toggle="tooltip" data-placement="top" title="{{params.node.name}}">' +
+                var icon = "fa-file";
+                switch (params.node.mediatype) {
+                case "application/pdf":
+                    icon = "fa-file-pdf-o";
+                    break;
+                case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                    icon = "fa-file-excel-o";
+                    break;
+                case "application/vnd.ms-excel":
+                    icon = "fa-file-excel-o";
+                    break;
+                case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+                    icon = "fa-file-word-o";
+                    break;
+                case "application/ms-word":
+                    icon = "fa-file-word-o";
+                    break;
+                case "application/vnd.ms-powerpoint.presentation.macroEnabled.12":
+                    icon = "fa-file-powerpoint-o";
+                    break;
+                case "application/vnd.ms-powerpoint":
+                    icon = "fa-file-powerpoint-o";
+                    break;
+                default:
+                    if (isImage(params.node.mediatype)) {
+                        icon = "fa-file-image-o";
+                    }
+                    break;
+                }
+                return '<i style="color: #D2C4D5;" class="fa fa-fw ' + icon + '"></i><span title="' + params.node.name + '">' +
+                    '<a data-toggle="tooltip" data-placement="top">' +
                     params.node.name + '</a></span>';
             }
         }];
@@ -30,13 +59,13 @@ function FilesAllController($scope, projectFiles, mcfile, $state, pubsub, $filte
     $scope.gridOptions = {
         columnDefs: columnDefs,
         rowData: $scope.files,
-        rowSelection: 'multiple',
+        //rowSelection: 'multiple',
         rowsAlreadyGrouped: true,
         rowClicked: rowClicked,
-        enableColResize: true,
+        enableColResize: false,
         enableSorting: false,
         rowHeight: 30,
-        angularCompileRows: true,
+        angularCompileRows: false,
         icons: {
             groupExpanded: '<i style="color: #D2C4D5 " class="fa fa-folder-open"/>',
             groupContracted: '<i style="color: #D2C4D5 " class="fa fa-folder"/>'
@@ -52,9 +81,10 @@ function FilesAllController($scope, projectFiles, mcfile, $state, pubsub, $filte
 
     function rowClicked(params) {
         if (params.node.type == 'datadir') {
-            projectFiles.setActiveDirectory(params.node)
+            projectFiles.setActiveDirectory(params.node);
+            var file = projectFiles.findFileByID($scope.project.id, params.node.df_id);
+            file.expanded = params.node.expanded;
             if (!params.node.sorted) {
-                var file = projectFiles.findFileByID($scope.project.id, params.node.df_id);
                 file.children = $filter('orderBy')(file.children, 'displayname');
                 file.sorted = true;
                 $scope.gridOptions.api.onNewRows();
