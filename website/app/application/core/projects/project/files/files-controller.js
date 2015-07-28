7c4744e0063abc955c211f1ae73f2e311b4dd980
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-28 13:44:37 -0400

    Show details on files. Allow user to click on file to see the details.

--- website/app/application/core/projects/project/files/files-controller.js
+++ website/app/application/core/projects/project/files/files-controller.js
@@ -4,16 +4,8 @@ function FilesController($state) {
 
     ctrl.showSearchResults = showSearchResults;
 
-    init();
-
     //////////////////
     function showSearchResults() {
         $state.go('projects.project.files.search');
-    }
-
-    function init() {
-        if ($state.current.name == "projects.project.files") {
-            $state.go('projects.project.files.all');
-        }
     }
 }
