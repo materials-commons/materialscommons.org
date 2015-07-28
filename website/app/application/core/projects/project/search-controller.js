55591c189c2f5fc2835ec71ac4f61ad4b073de0d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-28 13:44:37 -0400

    Show details on files. Allow user to click on file to see the details.

--- website/app/application/core/projects/project/search-controller.js
+++ website/app/application/core/projects/project/search-controller.js
@@ -1,12 +1,31 @@
 Application.Controllers.controller('searchController',
-    ["mcapi", "project", "$stateParams", searchController]);
+    ["mcapi", "project", "$stateParams", "mcfile", "$state", "projectFiles", searchController]);
 
-function searchController(mcapi, project, $stateParams) {
+function searchController(mcapi, project, $stateParams, mcfile, $state, projectFiles) {
     var ctrl = this;
+    ctrl.isImage = isImage;
+    ctrl.fileSrc = fileSrc;
+    ctrl.openFile = openFile;
 
-    mcapi("/search/project/%/files", project.id)
-        .success(function (results) {
-            ctrl.results = results;
-        })
-        .post({query_string: $stateParams.query});
+    init();
+
+    //////////////////////////////
+
+    function init() {
+        mcapi("/search/project/%/files", project.id)
+            .success(function (results) {
+                ctrl.results = results;
+            })
+            .post({query_string: $stateParams.query});
+    }
+
+    function fileSrc(id) {
+        return mcfile.src(id);
+    }
+
+    function openFile(f) {
+        var file = projectFiles.findFileByID(project.id, f.id);
+        projectFiles.setActiveFile(file);
+        $state.go('projects.project.files.edit', {file_id: f.id, file_type: f._type});
+    }
 }
