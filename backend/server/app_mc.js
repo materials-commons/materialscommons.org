d139eff5f2de707f157bfb05ec86a0aa815c41a1
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-29 14:43:17 -0400

    Start debugging caching.

--- backend/server/app_mc.js
+++ backend/server/app_mc.js
@@ -12,10 +12,22 @@ var projects = require('./resources/projects-routes')(model);
 app.use(apikey);
 app.use(mount('/', projects.routes())).use(projects.allowedMethods());
 
-//model.r.table('access').changes().toStream().on('data', function(data) {
-//    console.log(data);
-//});
+// Look for changes on the access and projects tables. If a change is detected
+// then invalidate the project access cache so that it will be reloaded.
+const projectAccessCache = require('./resources/project-access-cache')(model.access);
+model.r.table('access').changes().toStream().on('data', function() {
+    projectAccessCache.clear();
+});
+model.r.table('projects').changes().toStream().on('data', function() {
+    projectAccessCache.clear();
+});
 
+// Look for changes on the users table. If a change it detected then invalidate
+// the apikey cache so it will be reloaded.
+const apikeyCache = require('./apikey-cache')(model.users);
+model.r.table('users').changes().toStream().on('data', function() {
+    apikeyCache.clear()
+});
 
 if (!module.parent) {
     var cli = cliArgs([
--- backend/server/resources/project-access-cache.js
+++ backend/server/resources/project-access-cache.js
@@ -1,9 +1,12 @@
 module.exports = function(access) {
     'use strict';
 
+    let self = {
+        projectAccessCache: {},
+        adminUsersCache: {}
+    };
+
     let _ = require('lodash');
-    let projectAccessCache = {};
-    let adminUsersCache = {};
     return {
         find: find,
         clear: clear,
@@ -14,26 +17,30 @@ module.exports = function(access) {
     // If the projectAccessCache hasn't been loaded it loads the
     // cache.
     function *find(project_id) {
-        if (_.isEmpty(adminUsersCache)) {
+        if (_.isEmpty(self.adminUsersCache)) {
+            console.log('loading admin users cache');
             let adminUsers = yield access.adminUsers();
             adminUsers.forEach(function(user) {
-                adminUsersCache[user.id] = user;
+                self.adminUsersCache[user.id] = user;
             });
         }
 
-        if (! _.isEmpty(projectAccessCache)) {
-            return projectAccessCache[project_id];
+        if (!_.isEmpty(self.projectAccessCache)) {
+            return self.projectAccessCache[project_id];
         }
 
-        projectAccessCache = yield access.allByProject();
-        return projectAccessCache[project_id];
+        console.log('loading project access cache');
+        self.projectAccessCache = yield access.allByProject();
+        return self.projectAccessCache[project_id];
     }
 
     // clear will clear the current project cache. This is useful
     // when project permissions have been updated or a new project
     // has been created.
     function clear() {
-        projectAccessCache = {};
+        console.log('clearing project access cache', self.adminUsersCache);
+        self.projectAccessCache = {};
+        self.adminUsersCache = {};
     }
 
     // validateAccess checks if the user has access to the
@@ -41,18 +48,18 @@ module.exports = function(access) {
     // first so that the projectAccessCache was preloaded. If the
     // projectAccessCache is empty then it returns false (no access).
     function validateAccess(project_id, user) {
-        if (user.id in adminUsersCache) {
+        if (user.id in self.adminUsersCache) {
             return true;
         }
 
-        if (_.isEmpty(projectAccessCache)) {
+        if (_.isEmpty(self.projectAccessCache)) {
             return false;
         }
 
-        if (!(project_id in projectAccessCache)) {
+        if (!(project_id in self.projectAccessCache)) {
             return false;
         }
-        let index = _.indexOf(projectAccessCache[project_id], function(a) {
+        let index = _.indexOf(self.projectAccessCache[project_id], function(a) {
             return a.user_id == user.id;
         });
 
