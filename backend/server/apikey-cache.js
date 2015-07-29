72888be680a98b17de54f1d0c6d0996d8fb8dc24
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-29 14:43:17 -0400

    Start debugging caching.

--- backend/server/apikey-cache.js
+++ backend/server/apikey-cache.js
@@ -3,9 +3,10 @@
 var _ = require('lodash');
 module.exports = function(model) {
     'use strict';
-    let keycache = {};
+    let apikeyCache = {};
     return {
-        find: find
+        find: find,
+        clear: clear
     };
 
     ////////////////////
@@ -13,18 +14,27 @@ module.exports = function(model) {
     // find looks up an apikey in the cache. If the cache
     // is empty is loads it.
     function* find(apikey) {
-        if (! _.isEmpty(keycache)) {
-            return keycache[apikey];
+        if (! _.isEmpty(apikeyCache)) {
+            console.log('keycache not empty');
+            return apikeyCache[apikey];
         }
 
+        console.log('loading apikey cache');
         let users = yield model.getUsers();
-        keycache = users2map(users);
-        return keycache[apikey];
+        apikeyCache = users2map(users);
+        return apikeyCache[apikey];
+    }
+
+    function clear() {
+        console.log('clearing apikey cache');
+        apikeyCache = {};
+        console.log('after clear: ', apikeyCache);
     }
 
     // users2map converts the array of users into a map
     // indexed by apikey.
     function users2map(users) {
+        console.log('users2map');
         let map = {};
         users.forEach(function(user) {
             map[user.apikey] = {
