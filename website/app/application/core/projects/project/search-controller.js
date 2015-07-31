c68e653e6da44d61ecb6ab02cce2a51692689835
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-31 13:30:37 -0400

    For samples get a list of image files.

--- website/app/application/core/projects/project/search-controller.js
+++ website/app/application/core/projects/project/search-controller.js
@@ -6,6 +6,7 @@ function searchController(mcapi, project, $stateParams, mcfile, $state, projectF
     ctrl.isImage = isImage;
     ctrl.fileSrc = fileSrc;
     ctrl.openFile = openFile;
+    ctrl.images = images;
 
     init();
 
@@ -26,5 +27,16 @@ function searchController(mcapi, project, $stateParams, mcfile, $state, projectF
     function openFile(f) {
         projectFiles.setActiveFile(f);
         $state.go('projects.project.files.edit', {file_id: f.id, file_type: f._type});
+    }
+
+    // images goes through the list of files and returns the list of images.
+    function images(files) {
+        var images = [];
+        files.forEach(function(f) {
+            if (isImage(f.mediatype.mime)) {
+                images.push(f);
+            }
+        });
+        return images;
     }
 }
