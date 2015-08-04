70458cfcaf2222f3665c846b1e30db8be6237a18
V. Glenn Tarcea <gtarcea@umich.edu>
2015-08-04 13:46:37 -0400

    Make sure files are displayed sorted. Fix warning.

--- website/app/application/core/projects/project/home/directives/home-files-directive.js
+++ website/app/application/core/projects/project/home/directives/home-files-directive.js
@@ -11,42 +11,25 @@ function homeFilesDirective() {
 }
 
 Application.Controllers.controller("homeFilesDirectiveController",
-    ["$scope", "projectFiles",
-        "$filter",  homeFilesDirectiveController]);
-function homeFilesDirectiveController($scope, projectFiles,
-                                      $filter) {
+    ["$scope", "projectFiles", "$filter",  homeFilesDirectiveController]);
+function homeFilesDirectiveController($scope, projectFiles, $filter) {
     var f = projectFiles.model.projects[$scope.project.id].dir;
 
     // Root is name of project. Have it opened by default.
     f.showDetails = true;
     $scope.files = [f];
+    $scope.files[0].expanded = true;
+    $scope.files[0].children = $filter('orderBy')($scope.files[0].children, 'displayname');
     $scope.files.showDetails = true;
     var columnDefs = [
         {
             displayName: "",
             field: "name",
             width: 350,
-            //template: '<i style="color: #BFBFBF;" class="fa fa-fw fa-file"></i><span>' +
-            //'<a data-toggle="tooltip" data-placement="top" title="{{displayname}}">ABC</a></span>'
             cellRenderer: function (params) {
                 return '<i style="color: #BFBFBF;" class="fa fa-fw fa-file"></i><span>' +
                     '<a data-toggle="tooltip" data-placement="top" title="{{params.node.name}}">' +
                     params.node.name + '</a></span>';
-            }
-        },
-        {
-            displayName: "", field: "size", width: 250, cellRenderer: function (params) {
-            if (params.node.size === 0) {
-                return '';
-            } else {
-                return parseInt(params.node.size / 1024) + ' kb';
-            }
-        }
-        },
-        {
-            displayName: "", field: "birthtime", width: 250,
-            cellRenderer: function (params) {
-                return $filter('toDateString')(params.node.birthtime);
             }
         }
     ];
@@ -54,7 +37,7 @@ function homeFilesDirectiveController($scope, projectFiles,
     $scope.gridOptions = {
         columnDefs: columnDefs,
         rowData: $scope.files,
-        rowSelection: 'multiple',
+        rowClicked: rowClicked,
         rowsAlreadyGrouped: true,
         enableColResize: true,
         enableSorting: true,
@@ -67,8 +50,20 @@ function homeFilesDirectiveController($scope, projectFiles,
         },
         groupInnerCellRenderer: groupInnerCellRenderer
     };
+
     function groupInnerCellRenderer(params) {
-        var template = params.node.type === 'datadir' ? params.node.displayname : 'File';
-        return template;
+        return params.node.type === 'datadir' ? params.node.displayname : 'File';
+    }
+
+    function rowClicked(params) {
+        if (params.node.type == 'datadir') {
+            var file = projectFiles.findFileByID($scope.project.id, params.node.df_id);
+            file.expanded = params.node.expanded;
+            if (!params.node.sorted) {
+                file.children = $filter('orderBy')(file.children, 'displayname');
+                file.sorted = true;
+                $scope.gridOptions.api.onNewRows();
+            }
+        }
     }
 }
