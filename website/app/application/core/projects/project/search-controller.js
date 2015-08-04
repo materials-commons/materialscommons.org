2c00fbc046ef5c56bfbdcf4cc50601dd750b6a50
V. Glenn Tarcea <gtarcea@umich.edu>
2015-08-04 15:06:28 -0400

    Popup modal if user clicks on an image. If they click on the name go to the full page.

--- website/app/application/core/projects/project/search-controller.js
+++ website/app/application/core/projects/project/search-controller.js
@@ -1,11 +1,12 @@
 Application.Controllers.controller('searchController',
-    ["mcapi", "project", "$stateParams", "mcfile", "$state", "projectFiles", searchController]);
+    ["mcapi", "project", "$stateParams", "mcfile", "$state", "projectFiles", "modalInstance", searchController]);
 
-function searchController(mcapi, project, $stateParams, mcfile, $state, projectFiles) {
+function searchController(mcapi, project, $stateParams, mcfile, $state, projectFiles, modalInstance) {
     var ctrl = this;
     ctrl.isImage = isImage;
     ctrl.fileSrc = fileSrc;
-    ctrl.openFile = openFile;
+    ctrl.gotoFilesPage = gotoFilesPage;
+    ctrl.openFilePopup = openFilePopup;
     ctrl.images = images;
 
     init();
@@ -24,12 +25,25 @@ function searchController(mcapi, project, $stateParams, mcfile, $state, projectF
         return mcfile.src(id);
     }
 
-    function openFile(f) {
+    function gotoFilesPage(f) {
         projectFiles.setActiveFile(f);
         $state.go('projects.project.files.edit', {file_id: f.id, file_type: f._type});
     }
 
-    // images goes through the list of files and returns the list of images.
+    function openFilePopup(file) {
+        var f = file;
+        if ('datafile_id' in file) {
+            // We don't have a full file object, so find it in projectFiles
+            f = projectFiles.findFileByID(project.id, file.datafile_id);
+            var mime = f.mediatype;
+            f.mediatype = {
+                mime: mime
+            };
+        }
+        modalInstance.openModal(f, 'datafile', project);
+    }
+
+    // images goes through the list of files and returns all the images.
     function images(files) {
         var images = [];
         files.forEach(function(f) {
