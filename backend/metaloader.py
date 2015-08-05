6355014c18c32928074b216e0a96d29ceb8ac37b
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-08-05 18:53:13 -0400

    Formatting.

--- backend/metaloader.py
+++ backend/metaloader.py
@@ -11,13 +11,16 @@ from celery import chain
 
 r.connect('localhost', 28015, db='materialscommons').repl()
 
+
 def load_user_data(user, usergroup, dirpaths):
     setup_user(user, usergroup)
     chain(load_data_dirs.si(user, dirpaths) | import_data_dirs_to_repo.si(dirpaths))()
 
+
 def setup_user(user, usergroup):
     create_user_if_needed(user)
     associate_user_with_usergroup(user, usergroup)
+
 
 def create_user_if_needed(user):
     if r.table('users').get(user).run() is None:
@@ -26,12 +29,14 @@ def create_user_if_needed(user):
         u = User(user, user, hash)
         r.table('users').insert(u.__dict__).run()
 
+
 def associate_user_with_usergroup(user, usergroup):
     dbusergroup = create_usergroup_if_needed(user, usergroup)
     users = dbusergroup['users']
     if user not in users:
         users.append(user)
-        r.table('usergroups').get(usergroup).update({'users':users}).run()
+        r.table('usergroups').get(usergroup).update({'users': users}).run()
+
 
 def create_usergroup_if_needed(user, usergroup):
     dbusergroup = r.table('usergroups').get(usergroup).run()
@@ -41,15 +46,18 @@ def create_usergroup_if_needed(user, usergroup):
         dbusergroup = rv['new_val']
     return dbusergroup
 
+
 def usage():
     print("metaloader user usergroup directories")
     sys.exit(1)
+
 
 def main():
     if len(sys.argv) < 3:
         usage()
     else:
         load_user_data(user=sys.argv[1], usergroup=sys.argv[2], dirpaths=sys.argv[3:])
+
 
 if __name__ == "__main__":
     main()
