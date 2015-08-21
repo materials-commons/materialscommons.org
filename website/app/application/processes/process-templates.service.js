28b94e4b88c47098edc955319fef29ed4804a81d
tammasr <tammasr@umich.edu>
2015-08-21 15:24:26 -0400

    added templates for - jake , Ellen

--- website/app/application/processes/process-templates.service.js
+++ website/app/application/processes/process-templates.service.js
@@ -104,12 +104,36 @@ function processTemplates() {
             name: "Low Cycle Fatigue",
             description: "Low Cycle Fatigue",
             fn: LowCycleFatigue,
-            does_transform: false
+            does_transform: true
         },
         {
             name: "Annealing",
             description: "Annealing",
             fn: Annealing,
+            does_transform: true
+        },
+        {
+            name: "Ultrasonic Fatigue",
+            description: "Ultrasonic Fatigue",
+            fn: UltrasonicFatigue,
+            does_transform: true
+        },
+        {
+            name: "TEM",
+            description: "TEM",
+            fn: TEM,
+            does_transform: false
+        },
+        {
+            name: "Heat Treatment",
+            description: "Heat Treatment",
+            fn: HeatTreatment,
+            does_transform: true
+        },
+        {
+            name: "Hardness",
+            description: "Hardness",
+            fn: Hardness,
             does_transform: false
         }
     ];
