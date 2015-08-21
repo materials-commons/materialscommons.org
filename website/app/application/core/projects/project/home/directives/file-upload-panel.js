ca3ca8d4ab7d7c3a781558f64684c4d1958c7952
V. Glenn Tarcea <gtarcea@umich.edu>
2015-08-21 15:14:43 -0400

    Change to IIFE. Capitalized controller name. Remove $scope. Update template references to view.

--- website/app/application/core/projects/project/home/directives/file-upload-panel.js
+++ website/app/application/core/projects/project/home/directives/file-upload-panel.js
@@ -1,41 +1,52 @@
-Application.Directives.directive('fileUploadPanel', fileUploadPanelDirective);
-function fileUploadPanelDirective() {
-    return {
-        restrict: "E",
-        replace: true,
-        controller: 'fileUploadPanelDirectiveController',
-        scope: true,
-        templateUrl: 'application/core/projects/project/home/directives/file-upload-panel.html'
-    };
-}
-
-Application.Controllers.controller("fileUploadPanelDirectiveController",
-                                   ["$scope", "mcFlow", "$timeout",
-                                    fileUploadPanelDirectiveController]);
-function fileUploadPanelDirectiveController($scope, mcFlow, $timeout) {
-    $scope.filesByDir = {};
-    $scope.dirCount = 0;
-
-    function loadFilesByDir() {
-        var files = $scope.flow.files;
-        $scope.filesByDir = {}; // reset the list
-
-        // Load files indexed by the directory
-        files.forEach(function(file) {
-            if (!(file.attrs.directory_name in $scope.filesByDir)) {
-                $scope.filesByDir[file.attrs.directory_name] = [];
-            }
-            $scope.filesByDir[file.attrs.directory_name].push(file);
-        });
+(function (module) {
+    module.directive('fileUploadPanel', fileUploadPanelDirective);
+    function fileUploadPanelDirective() {
+        return {
+            restrict: "E",
+            replace: true,
+            controller: 'FileUploadPanelDirectiveController',
+            scope: true,
+            controllerAs: 'uploader',
+            bindToController: true,
+            templateUrl: 'application/core/projects/project/home/directives/file-upload-panel.html'
+        };
     }
 
-    $scope.flow = mcFlow.get();
-    $scope.flow.on('catchAll', function(eventName) {
-        // Force a dirty check of the changed flow state.
-        $timeout(function() {
-            if (eventName === "filesAdded" || eventName === "fileRemoved") {
-                loadFilesByDir();
-            }
+    ///////////////////////////////////////////////////
+
+    module.controller("FileUploadPanelDirectiveController", FileUploadPanelDirectiveController);
+    FileUploadPanelDirectiveController.$inject = ["mcFlow", "$timeout"];
+
+    /* @ngInject */
+    function FileUploadPanelDirectiveController(mcFlow, $timeout) {
+        var ctrl = this;
+
+        ctrl.filesByDir = {};
+        ctrl.dirCount = 0;
+        ctrl.flow = mcFlow.get();
+        ctrl.flow.on('catchAll', function (eventName) {
+            // Force a dirty check of the changed flow state.
+            $timeout(function () {
+                if (eventName === "filesAdded" || eventName === "fileRemoved") {
+                    loadFilesByDir();
+                }
+            });
         });
-    });
-}
+
+        /////////////////////////
+
+        function loadFilesByDir() {
+            var files = ctrl.flow.files;
+            ctrl.filesByDir = {}; // reset the list
+
+            // Load files indexed by the directory
+            files.forEach(function (file) {
+                if (!(file.attrs.directory_name in ctrl.filesByDir)) {
+                    ctrl.filesByDir[file.attrs.directory_name] = [];
+                }
+                ctrl.filesByDir[file.attrs.directory_name].push(file);
+            });
+        }
+    }
+
+}(angular.module('materialscommons')));
