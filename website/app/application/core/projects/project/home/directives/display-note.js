c16f0983d64d72227fa6c8c029c23611ef543142
V. Glenn Tarcea <gtarcea@umich.edu>
2015-08-21 14:35:21 -0400

    Change to IIFE. Remove $scope. Update references in templates. Remove unused methods.

--- website/app/application/core/projects/project/home/directives/display-note.js
+++ website/app/application/core/projects/project/home/directives/display-note.js
@@ -1,51 +1,26 @@
-Application.Directives.directive("displayNote", displayNoteDirective);
-function displayNoteDirective() {
-    return {
-        restrict: "E",
-        replace: true,
-        scope: {
-            note: "=note",
-            showSideboard: "=showSideboard"
-        },
-        controller: "displayNoteDirectiveController",
-        templateUrl: "application/core/projects/project/home/directives/display-note.html"
-    };
-}
-Application.Controllers.controller("displayNoteDirectiveController",
-                                   ["$scope", "sideboard", "current","pubsub", "toggleDragButton",
-                                    displayNoteDirectiveController]);
+(function (module) {
+    module.directive("displayNote", displayNoteDirective);
+    function displayNoteDirective() {
+        return {
+            restrict: "E",
+            replace: true,
+            scope: {
+                note: "=note",
+                showSideboard: "=showSideboard"
+            },
+            controller: "DisplayNoteDirectiveController",
+            controllerAs: 'note',
+            bindToController: true,
+            templateUrl: "application/core/projects/project/home/directives/display-note.html"
+        };
+    }
 
-function displayNoteDirectiveController($scope, sideboard, current, pubsub, toggleDragButton) {
-    $scope.addToSideboard = function(note, event) {
-        sideboard.handleFromEvent(current.projectID(), note, event, 'sideboard');
-    };
+    module.controller("DisplayNoteDirectiveController", DisplayNoteDirectiveController);
+    DisplayNoteDirectiveController.$inject = [];
 
-    $scope.remove = function (note, event) {
-        sideboard.handleFromEvent(current.projectID(), note, event, 'sideboard');
-    };
+    /* @ngInject */
+    function DisplayNoteDirectiveController() {
+        var ctrl = this;
+    }
 
-    $scope.bk = {
-        addToReview: false
-    };
-
-    $scope.isActive = function(type, button){
-        return toggleDragButton.get(type, button);
-    };
-
-    $scope.addItem = function (type) {
-        switch (type) {
-            case "review":
-                pubsub.send('addNoteToReview', $scope.note);
-                break;
-            case "sample":
-                pubsub.send('addNoteToSample', $scope.note);
-                break;
-            case "provenance":
-                pubsub.send('addNoteToProvenance', $scope.note);
-                break;
-            case "file":
-                pubsub.send('addNoteToFile', $scope.note);
-                break;
-        }
-    };
-}
+}(angular.module('materialscommons')));
