6459475dfe71d3392e16faffff53cce7af5bca68
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-09 12:35:43 -0400

    Switch to IIFE

--- website/app/application/core/projects/project/processes/for-later/wizard/show-template-details.js
+++ website/app/application/core/projects/project/processes/for-later/wizard/show-template-details.js
@@ -1,24 +1,27 @@
-Application.Directives.directive("showTemplateDetails",
-                                 ["RecursionHelper", showTemplateDetailsDirective]);
-function showTemplateDetailsDirective(RecursionHelper) {
-    return {
-        restrict: "E",
-        replace: true,
-        scope: {
-            template: "=template",
-        },
-        controller: "showTemplateDetailsDirectiveController",
-        templateUrl: "/show-template-details.html",
-        compile: function(element) {
-            return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn) {
-            });
-        }
-    };
-}
-Application.Controllers.controller("showTemplateDetailsDirectiveController",
-                                   ["$scope", "templateConstructer",
-                                    showTemplateDetailsDirectiveController]);
+(function (module) {
+    module.directive("showTemplateDetails",
+        ["RecursionHelper", showTemplateDetailsDirective]);
+    function showTemplateDetailsDirective(RecursionHelper) {
+        return {
+            restrict: "E",
+            replace: true,
+            scope: {
+                template: "=template",
+            },
+            controller: "showTemplateDetailsDirectiveController",
+            templateUrl: "/show-template-details.html",
+            compile: function (element) {
+                return RecursionHelper.compile(element, function (scope, iElement, iAttrs, controller, transcludeFn) {
+                });
+            }
+        };
+    }
 
-function showTemplateDetailsDirectiveController($scope, templateConstructer) {
-    $scope.template = templateConstructer.constructTemplate($scope.template);
-}
+    module.controller("showTemplateDetailsDirectiveController", showTemplateDetailsDirectiveController)
+    showTemplateDetailsDirectiveController.$inject = ["$scope", "templateConstructer"];
+
+    function showTemplateDetailsDirectiveController($scope, templateConstructer) {
+        $scope.template = templateConstructer.constructTemplate($scope.template);
+    }
+
+}(angular.module('materialscommons')));
