0c64433dfbcc7fb52d5d35e8cf7628ac0f98595e
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-09 10:32:55 -0400

    Switch to IIFE

--- website/app/application/directives/on-repeat-finished.js
+++ website/app/application/directives/on-repeat-finished.js
@@ -1,15 +1,17 @@
-Application.Directives.directive("onRepeatFinished", ["$timeout", onRepeatFinishedDirective]);
-function onRepeatFinishedDirective($timeout) {
-    return {
-        restrict: "A",
-        link: function(scope, element, attr) {
-            if (scope.$last === true) {
-                element.ready(function() {
-                    $timeout(function() {
-                        scope.$emit(attr.onRepeatFinished);
+(function (module) {
+    module.directive("onRepeatFinished", ["$timeout", onRepeatFinishedDirective]);
+    function onRepeatFinishedDirective($timeout) {
+        return {
+            restrict: "A",
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    element.ready(function () {
+                        $timeout(function () {
+                            scope.$emit(attr.onRepeatFinished);
+                        });
                     });
-                });
+                }
             }
-        }
-    };
-}
+        };
+    }
+}(angular.module('materialscommons')));
