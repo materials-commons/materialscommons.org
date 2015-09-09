5b4c233fb16f99210bcba2bd19759dd4c7bd70e8
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-09 10:32:55 -0400

    Switch to IIFE

--- website/app/application/directives/paged-directive.js
+++ website/app/application/directives/paged-directive.js
@@ -1,8 +1,9 @@
-Application.Directives.directive('paged',
-    function () {
-        return {
+(function (module) {
+    module.directive('paged',
+        function () {
+            return {
 
-            template: '<div>' +
+                template: '<div>' +
                 '<div ng-transclude=""></div>' +
                 '<div ng-show="data.length > pageSize">' +
                 '<button ng-disabled="!hasPreviousPage()" ng-click="previousPage()"> Previous </button>' +
@@ -10,66 +11,67 @@ Application.Directives.directive('paged',
                 '<button ng-disabled="!hasNextPage()" ng-click="nextPage()"> Next </button>' +
                 '</div>' +
                 '</div>',
-            restrict: 'E',
-            transclude: true,
-            scope: {
-                'currentPage': '=',
-                'pageSize': '=',
-                'data': '='
-            },
-            link: function ($scope, element, attrs) {
-                $scope.size = function () {
-                    return $scope.data.length;
-                };
+                restrict: 'E',
+                transclude: true,
+                scope: {
+                    'currentPage': '=',
+                    'pageSize': '=',
+                    'data': '='
+                },
+                link: function ($scope, element, attrs) {
+                    $scope.size = function () {
+                        return $scope.data.length;
+                    };
 
-                $scope.end = function () {
-                    var endItem = $scope.start() + $scope.pageSize;
-                    return endItem > $scope.size() ? $scope.data.length : endItem;
-                };
+                    $scope.end = function () {
+                        var endItem = $scope.start() + $scope.pageSize;
+                        return endItem > $scope.size() ? $scope.data.length : endItem;
+                    };
 
-                $scope.start = function () {
-                    return $scope.currentPage * $scope.pageSize;
-                };
+                    $scope.start = function () {
+                        return $scope.currentPage * $scope.pageSize;
+                    };
 
-                $scope.showStart = function () {
-                    var s = $scope.start();
-                    return s + 1;
-                };
+                    $scope.showStart = function () {
+                        var s = $scope.start();
+                        return s + 1;
+                    };
 
-                $scope.page = function () {
-                    return $scope.size() ? ($scope.currentPage + 1) : 0;
-                };
+                    $scope.page = function () {
+                        return $scope.size() ? ($scope.currentPage + 1) : 0;
+                    };
 
-                $scope.hasNextPage = function () {
-                    return $scope.page() < ($scope.size() / $scope.pageSize);
-                };
+                    $scope.hasNextPage = function () {
+                        return $scope.page() < ($scope.size() / $scope.pageSize);
+                    };
 
-                $scope.nextPage = function () {
-                    $scope.currentPage = parseInt($scope.currentPage) + 1;
-                };
+                    $scope.nextPage = function () {
+                        $scope.currentPage = parseInt($scope.currentPage) + 1;
+                    };
 
-                $scope.hasPreviousPage = function () {
-                    return $scope.currentPage !== 0;
-                };
+                    $scope.hasPreviousPage = function () {
+                        return $scope.currentPage !== 0;
+                    };
 
-                $scope.previousPage = function () {
-                    $scope.currentPage = $scope.currentPage - 1;
-                };
+                    $scope.previousPage = function () {
+                        $scope.currentPage = $scope.currentPage - 1;
+                    };
 
-                try {
-                    if (!angular.isDefined($scope.data)) {
-                        $scope.data = [];
+                    try {
+                        if (!angular.isDefined($scope.data)) {
+                            $scope.data = [];
+                        }
+
+                        if (!angular.isDefined($scope.currentPage)) {
+                            $scope.currentPage = 0;
+                        }
+
+                        if (!angular.isDefined($scope.pageSize)) {
+                            $scope.pageSize = 10;
+                        }
+                    } catch (e) {
                     }
-
-                    if (!angular.isDefined($scope.currentPage)) {
-                        $scope.currentPage = 0;
-                    }
-
-                    if (!angular.isDefined($scope.pageSize)) {
-                        $scope.pageSize = 10;
-                    }
-                } catch (e) {
                 }
-            }
-        };
-    });
+            };
+        });
+}(angular.module('materialscommons')));
