42e7c81188d811b15c5ccf06553bd90a9f4e3f40
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-09 09:59:49 -0400

    Switch to IIFE

--- website/app/application/core/projects/project/samples/samples-all-controller.js
+++ website/app/application/core/projects/project/samples/samples-all-controller.js
@@ -1,14 +1,16 @@
-Application.Controllers.controller("SamplesAllController",
-    ["$scope", "$state", "project", "$filter", SamplesAllController]);
-function SamplesAllController($scope, $state, project, $filter) {
-    if(project.samples.length !== 0) {
-        var sortedSamples = $filter('orderBy')(project.samples, 'name');
-        $scope.current = sortedSamples[0];
-        $state.go('projects.project.samples.all.edit', {sample_id : $scope.current.id});
-    }
+(function (module) {
+    module.controller("SamplesAllController", SamplesAllController);
+    SamplesAllController.$inject = ["$scope", "$state", "project", "$filter"];
+    function SamplesAllController($scope, $state, project, $filter) {
+        if (project.samples.length !== 0) {
+            var sortedSamples = $filter('orderBy')(project.samples, 'name');
+            $scope.current = sortedSamples[0];
+            $state.go('projects.project.samples.all.edit', {sample_id: $scope.current.id});
+        }
 
-    $scope.viewSample = function (sample) {
-        $scope.current = sample;
-        $state.go('projects.project.samples.all.edit', {sample_id : $scope.current.id});
-    };
-}
+        $scope.viewSample = function (sample) {
+            $scope.current = sample;
+            $state.go('projects.project.samples.all.edit', {sample_id: $scope.current.id});
+        };
+    }
+}(angular.module('materialscommons')));
