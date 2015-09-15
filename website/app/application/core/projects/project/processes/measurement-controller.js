c7de6a169b693cc779c1e21faa37867aa7510e91
tammasr <tammasr@umich.edu>
2015-09-15 14:24:18 -0400

    update meaurements functionality

--- website/app/application/core/projects/project/processes/measurement-controller.js
+++ website/app/application/core/projects/project/processes/measurement-controller.js
@@ -6,17 +6,15 @@
 
         $scope.modal = modal;
         //Initializing the sample
-        $scope.copySample = angular.copy($scope.modal.sample);
-        $scope.touchedProperties = [];
-
+        measurements.copySample($scope.modal.sample);
 
         $scope.switchProperty = function (template) {
             //Before you switch the property, verify existing property is Valid and Save
             if ($scope.chosenProperty) {
-                var verified = verifyAndSave($scope.chosenProperty);
-                if (verified){
+                var verified = measurements.verifyAndSave($scope.chosenProperty);
+                if (verified) {
                     newProperty(template);
-                }else{
+                } else {
                     $scope.message = "Please enter valid numbers.";
                 }
             } else {
@@ -24,7 +22,8 @@
             }
         };
 
-        function newProperty(template){
+        function newProperty(template) {
+            $scope.touchedProperties = measurements.getTouchedProperties();
             var i = _.indexOf($scope.touchedProperties, function (prop) {
                 return prop.name === template.name;
             });
@@ -35,128 +34,6 @@
             }
         }
 
-        function verifyAndSave(chosenProperty) {
-            if (('value' in chosenProperty) && chosenProperty.value !== null) {
-                if (isExistingPropertyValid(chosenProperty)) {
-                    $scope.touchedProperties.push(chosenProperty);
-                    processMeasurments(chosenProperty);
-                    return true;
-                }
-                else { return false; }
-            } else{ return true; }
-        }
-
-        function isExistingPropertyValid(property) {
-            var type = property._type;
-            var values = [];
-            switch (type) {
-                case 'number':
-                    values = property.value.split("\n");
-                    return isNumberValid(values);
-                    break;
-                case 'fraction':
-                    values = property.value.split("\n");
-                    return isFractionValid(values);
-                    break;
-                case 'histogram':
-                    var values = [];
-                    values = property.value.values.split("\n");
-                    return isNumberValid(values);
-                    break;
-                case 'line':
-                    var values = [];
-                    values = property.value.values.split("\n");
-                    return isNumberValid(values);
-                    break;
-                case 'selection':
-                    return true;
-                    break;
-                case 'composition':
-                    var values = [];
-                    values = property.value.values.split("\n");
-                    return isNumberValid(values);
-                    break;
-            }
-        }
-
-        function processMeasurments(property) {
-            var type = property._type;
-            switch (type) {
-                case 'number':
-                    values = property.value.split("\n");
-                    property.measurements = [];
-                    values.forEach(function (v) {
-                        property.measurements.push({
-                            value: v,
-                            _type: type,
-                            unit: property.unit,
-                            attribute: property.attribute
-                        });
-                    });
-                    saveToSample(property);
-                    break;
-                case 'fraction':
-                    values = property.value.split("\n");
-                    property.measurements = [];
-                    values.forEach(function (v) {
-                        property.measurements.push({
-                            value: v,
-                            _type: type,
-                            unit: property.unit,
-                            attribute: property.attribute
-                        });
-                    });
-                    saveToSample(property);
-                    break;
-                case 'histogram':
-                    saveToSample(property);
-                    break;
-                case 'line':
-                    saveToSample(property);
-                    break;
-                case 'selection':
-                    saveToSample(property);
-                    break;
-                case 'composition':
-                    var elements = property.value.elements.split("\n");
-                    values = property.value.values.split("\n");
-                    property.measurements = [];
-                    for(var i = 0; i < elements.length; i ++){
-                        property.measurements.push({
-                            value: values[i],
-                            element: elements[i],
-                            _type: property._type,
-                            unit: property.unit,
-                            attribute: property.attribute
-                        });
-                    }
-                    saveToSample(property);
-                    break;
-            }
-        }
-
-        function isNumberValid(values) {
-            var isNumeric = true;
-            values.forEach(function (v) {
-                if (isNaN(v)) {
-                    isNumeric = false;
-                }
-            });
-            return isNumeric;
-        }
-
-        function isFractionValid(values) {
-            var isNumeric = true;
-            var fraction = [];
-            values.forEach(function (v) {
-                fraction = v.split("/");
-                if (isNaN(fraction[0]) || isNaN(fraction[1])) {
-                    isNumeric = false;
-                }
-            });
-            return isNumeric;
-        }
-
         $scope.modal.instance.result.then(function (selectedItem) {
             $scope.selected = selectedItem;
 
@@ -164,55 +41,14 @@
             $log.info('Modal dismissed at: ' + new Date());
         });
 
-        function saveToSample(property) {
-            var i, j, k;
-            i = _.indexOf($scope.copySample.properties, function (entry) {
-                return property.name === entry.name;
-            });
-            if (i < 0) {
-                j = _.indexOf($scope.modal.sample.new_properties, function (prop) {
-                    return property.name === prop.name;
-                });
-                if (j < 0) {
-                    $scope.modal.sample.new_properties.push(property);
-                } else {
-                    $scope.modal.sample.new_properties[j] = property;
-                }
-            } else {
-                var old_prop = $scope.copySample.properties[i];
-                property.property_set_id = old_prop.property_set_id;
-                property.property_id = old_prop.property_id;
-                k = _.indexOf($scope.modal.sample.old_properties, function (entry) {
-                    return property.name === entry.name;
-                });
-                if (k > 0){
-                    $scope.modal.sample.old_properties[k] = property;
-
-                }   else{
-                    $scope.modal.sample.old_properties.push(property);
-                }
-            }
-        }
-
-        $scope.ok = function (isValid) {
-            verifyAndSave($scope.chosenProperty);
-
+        $scope.ok = function () {
+            measurements.verifyAndSave($scope.chosenProperty);
             $scope.modal.instance.close($scope.chosenProperty);
         };
 
         $scope.cancel = function () {
             $scope.modal.instance.dismiss('cancel');
         };
-
-        //$scope.addNewChoice = function () {
-        //    $scope.message = "";
-        //    $scope.editMeasurement();
-        //};
-        //
-        //$scope.removeChoice = function (index) {
-        //    $scope.message = "";
-        //    $scope.chosenProperty.measures.splice(index, 1);
-        //};
 
         function init() {
             $scope.templates = measurements.templatesCopy();
