86dfeac50517884f73ed6a950cc0d3e207a82fdd
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-17 17:01:47 -0400

    Add REST call for getting a datafile.

--- backend/server/db/model/files.js
+++ backend/server/db/model/files.js
@@ -6,14 +6,38 @@
  */
 
 module.exports = function(r) {
-    'use strict';
+    const runQuery = require('./run');
+
     return {
-        countInProject: countInProject
+        countInProject: countInProject,
+        get: get
     };
 
-    function *countInProject(ids, projectID) {
+    function* countInProject(ids, projectID) {
         let rql = r.table('datafiles').getAll(r.args(ids));
         let count = yield rql.filter({project_id: projectID}).count();
         return count;
+    }
+
+    // get details on a file
+    function* get(file_id) {
+        let rql = r.table('datafiles').get(file_id).
+            merge(function() {
+                return {
+                    tags: r.table('tag2item').getAll(file_id, {index: 'item_id'}).
+                        pluck('tag').coerceTo('array'),
+                    notes: r.table('note2item').getAll(file_id, {index: 'item_id'}).
+                        eqJoin('note_id', r.table('notes')).zip().coerceTo('array'),
+                    samples: r.table('sample2datafile').getAll(file_id, {index: 'datafile_id'}).
+                        eqJoin('sample_id', r.table('samples')).zip().
+                        pluck('name', 'id').distinct().
+                        coerceTo('array'),
+                    processes: r.table('process2file').getAll(file_id, {index: 'datafile_id'}).
+                        eqJoin('process_id', r.table('processes')).zip().
+                        pluck('name', 'id').distinct().
+                        coerceTo('array')
+                }
+            });
+        return runQuery(rql);
     }
 };
