eeaf63f5cc0cf7383673684197d4cccb192fe34d
tammasr <tammasr@umich.edu>
2015-09-17 17:30:31 -0400

    update nav tab uisref issue

--- website/app/application/core/projects/project/processes/process-view-controller.js
+++ website/app/application/core/projects/project/processes/process-view-controller.js
@@ -8,8 +8,8 @@
         viewCtrl.openSample = openSample;
         viewCtrl.openFile = openFile;
         viewCtrl.editProvenance = editProvenance;
-        viewCtrl.setTab = setTab;
-        viewCtrl.isSet = isSet;
+        //viewCtrl.setTab = setTab;
+        //viewCtrl.isSet = isSet;
 
         viewCtrl.project = project;
         viewCtrl.tab = 'setup';
@@ -26,13 +26,13 @@
             $state.go('projects.project.processes.list.edit', {process_id: viewCtrl.current.id});
         }
 
-        function setTab(tabId) {
-            viewCtrl.tab = tabId;
-        }
-
-        function isSet(tabId) {
-            return viewCtrl.tab === tabId;
-        }
+        //function setTab(tabId) {
+        //    viewCtrl.tab = tabId;
+        //}
+        //
+        //function isSet(tabId) {
+        //    return viewCtrl.tab === tabId;
+        //}
 
         function getSampleDetails(process) {
             if (process.samples.length !== 0) {
