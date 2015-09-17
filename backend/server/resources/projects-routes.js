ac1ce2e19868df6c0d96a626aeee83b70bf503dd
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-17 17:03:13 -0400

    Add REST call for files.

--- backend/server/resources/projects-routes.js
+++ backend/server/resources/projects-routes.js
@@ -6,13 +6,18 @@ module.exports = function(model) {
     let router = require('koa-router')();
     let projects = require('./projects')(model.projects);
     let samples = require('./samples')(model.samples, schema);
+    let files = require('./files')(model.files);
     let processes = require('./processes')(model.processes, schema);
 
     router.get('/projects2', projects.all);
+    router.get('/projects2/:project_id/dir/:directory_id', validateProjectAccess, projects.dirTree);
+
     router.post('/projects2/:project_id/processes', validateProjectAccess, processes.create);
+
     router.post('/projects2/:project_id/samples', validateProjectAccess, samples.create);
     router.put('/projects2/:project_id/samples/:sample_id', validateProjectAccess, samples.update);
-    router.get('/projects2/:project_id/dir/:directory_id', validateProjectAccess, projects.dirTree);
+
+    router.get('/projects2/:project_id/files/:file_id', validateProjectAccess, files.get);
 
     return router;
 };
