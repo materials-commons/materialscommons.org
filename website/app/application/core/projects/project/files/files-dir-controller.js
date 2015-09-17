ae6708f3dee67f2f7e9a3fef5ab86a864ebc5d4f
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-17 16:58:24 -0400

    Controller and view for displaying directories.

---
+++ website/app/application/core/projects/project/files/files-dir-controller.js
@@ -0,0 +1,17 @@
+(function(module) {
+    module.controller('DirController', DirController);
+    DirController.$inject = [];
+
+    /* @ngInject */
+    function DirController() {
+        var ctrl = this;
+
+        init();
+
+        /////////////////
+
+        function init() {
+
+        }
+    }
+}(angular.module('materialscommons')));
