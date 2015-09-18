f9eb6a2fa552f113a2bf63d045c0ef11c83811a2
tammasr <tammasr@umich.edu>
2015-09-18 14:33:34 -0400

    update the files tab

--- website/app/application/core/projects/project/processes/process-view-controller.js
+++ website/app/application/core/projects/project/processes/process-view-controller.js
@@ -8,8 +8,6 @@
         viewCtrl.openSample = openSample;
         viewCtrl.openFile = openFile;
         viewCtrl.editProvenance = editProvenance;
-        //viewCtrl.setTab = setTab;
-        //viewCtrl.isSet = isSet;
 
         viewCtrl.project = project;
         viewCtrl.tab = 'setup';
@@ -26,19 +24,10 @@
             $state.go('projects.project.processes.list.edit', {process_id: viewCtrl.current.id});
         }
 
-        //function setTab(tabId) {
-        //    viewCtrl.tab = tabId;
-        //}
-        //
-        //function isSet(tabId) {
-        //    return viewCtrl.tab === tabId;
-        //}
-
         function getSampleDetails(process) {
             if (process.samples.length !== 0) {
                 mcapi('/samples')
                     .success(function (samples) {
-                        console.dir(samples);
                         viewCtrl.current.samples = samples;
                     })
                     .error(function (err) {
@@ -46,7 +35,6 @@
                     })
                     .post({samples: process.samples});
             }
-
         }
 
         function init() {
