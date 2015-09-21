5cdd0dbdecb40b186e96685eb6eab2a827592af3
tammasr <tammasr@umich.edu>
2015-09-21 17:22:00 -0400

    directive for tab layout - updated

--- website/app/application/core/projects/project/processes/samples-tab-directive.js
+++ website/app/application/core/projects/project/processes/samples-tab-directive.js
@@ -3,31 +3,31 @@
 
     function samplesTabDirective() {
         return {
-            scope: {
-                current: "="
-            },
+            scope: true,
             restrict: "E",
-            controller: "samplesTabDirectiveController",
+            //controller: "samplesTabDirectiveController",
+            //controllerAs: 'samplesTab',
+            //bindToController: true,
             templateUrl: "application/core/projects/project/processes/samples.html"
         };
     }
 
-    module.controller("samplesTabDirectiveController", samplesTabDirectiveController);
-    samplesTabDirectiveController.$inject = [];
-
-    function samplesTabDirectiveController() {
-        var samplesTabCtrl = this;
-
-        samplesTabCtrl.images = images;
-
-        function images(files) {
-            var images = [];
-            files.forEach(function (f) {
-                if (isImage(f.mediatype.mime)) {
-                    images.push(f);
-                }
-            });
-            return images;
-        }
-    }
+    //module.controller("samplesTabDirectiveController", samplesTabDirectiveController);
+    //samplesTabDirectiveController.$inject = [];
+    //
+    //function samplesTabDirectiveController() {
+    //    var samplesTabCtrl = this;
+    //
+    //    samplesTabCtrl.images = images;
+    //
+    //    function images(files) {
+    //        var images = [];
+    //        files.forEach(function (f) {
+    //            if (isImage(f.mediatype.mime)) {
+    //                images.push(f);
+    //            }
+    //        });
+    //        return images;
+    //    }
+    //}
 }(angular.module('materialscommons')));
