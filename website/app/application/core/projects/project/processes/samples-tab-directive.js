e3272653053433950579e7ab484957be24e79fc1
tammasr <tammasr@umich.edu>
2015-09-22 15:53:47 -0400

    update: directives

--- website/app/application/core/projects/project/processes/samples-tab-directive.js
+++ website/app/application/core/projects/project/processes/samples-tab-directive.js
@@ -5,29 +5,7 @@
         return {
             scope: true,
             restrict: "E",
-            //controller: "samplesTabDirectiveController",
-            //controllerAs: 'samplesTab',
-            //bindToController: true,
             templateUrl: "application/core/projects/project/processes/samples.html"
         };
     }
-
-    //module.controller("samplesTabDirectiveController", samplesTabDirectiveController);
-    //samplesTabDirectiveController.$inject = [];
-    //
-    //function samplesTabDirectiveController() {
-    //    var samplesTabCtrl = this;
-    //
-    //    samplesTabCtrl.images = images;
-    //
-    //    function images(files) {
-    //        var images = [];
-    //        files.forEach(function (f) {
-    //            if (isImage(f.mediatype.mime)) {
-    //                images.push(f);
-    //            }
-    //        });
-    //        return images;
-    //    }
-    //}
 }(angular.module('materialscommons')));
