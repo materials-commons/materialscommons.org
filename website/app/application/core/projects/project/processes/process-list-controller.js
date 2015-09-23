2cecc559dd9218e37b0917d703fdef4c2c12964b
tammasr <tammasr@umich.edu>
2015-09-23 12:37:58 -0400

    resolve in app.js

--- website/app/application/core/projects/project/processes/process-list-controller.js
+++ website/app/application/core/projects/project/processes/process-list-controller.js
@@ -1,21 +1,27 @@
 (function (module) {
     module.controller('projectListProcess', projectListProcess);
-    projectListProcess.$inject = ["$scope", "project", "$state", "modalInstance"];
+    projectListProcess.$inject = ["processes", "project", "$state", "modalInstance"];
 
-    function projectListProcess($scope, project, $state, modalInstance) {
-        $scope.project = project;
-        if (project.processes.length !== 0) {
-            $scope.current = project.processes[0];
-            //$state.go('projects.project.processes.list.view.setup', {process_id: $scope.current.id});
+    function projectListProcess(processes, project, $state, modalInstance) {
+        var processListCtrl = this;
+
+        processListCtrl.chooseTemplate = chooseTemplate;
+        processListCtrl.viewProcess = viewProcess;
+
+        processListCtrl.processes = processes;
+        processListCtrl.project = project;
+        processListCtrl.current = {};
+        if (processListCtrl.processes.length !== 0) {
+            processListCtrl.current = processListCtrl.processes[0];
         }
 
-        $scope.viewProcess = function (process) {
-            $scope.current = process;
-            $state.go('projects.project.processes.list.view.setup', {process_id: $scope.current.id});
-        };
+         function viewProcess(process) {
+            processListCtrl.current = process;
+            $state.go('projects.project.processes.list.view.setup', {process_id: processListCtrl.current.id});
+        }
 
-        $scope.chooseTemplate = function () {
-            modalInstance.chooseTemplate($scope.project);
-        };
+        function chooseTemplate() {
+            modalInstance.chooseTemplate(processListCtrl.project);
+        }
     }
 }(angular.module('materialscommons')));
