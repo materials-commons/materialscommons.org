510c5d3cc5a61a611f44552115d9e4e4ba5d8bf1
V. Glenn Tarcea <gtarcea@umich.edu>
2015-09-29 13:59:08 -0400

    Add routes and support for updating a process, and a skeleton implementation for getting a process.

--- backend/server/resources/projects-routes.js
+++ backend/server/resources/projects-routes.js
@@ -13,7 +13,8 @@ module.exports = function(model) {
     router.get('/projects/:project_id/dir/:directory_id', validateProjectAccess, projects.dirTree);
 
     router.post('/projects/:project_id/processes', validateProjectAccess, processes.create);
-    router.put('/projects/:project_id/processes', validateProjectAccess, processes.update);
+    router.put('/projects/:project_id/processes/:process_id', validateProjectAccess, processes.update);
+    router.get('/projects/:project_id/processes/:process_id', validateProjectAccess, processes.get);
 
     router.post('/projects/:project_id/samples', validateProjectAccess, samples.create);
     router.put('/projects/:project_id/samples/:sample_id', validateProjectAccess, samples.update);
