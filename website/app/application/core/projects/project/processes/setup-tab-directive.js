7b3f97c9ecf43f6f354951894631adb83238a51a
tammasr <tammasr@umich.edu>
2015-10-15 14:10:31 -0400

    updated layout

--- website/app/application/core/projects/project/processes/setup-tab-directive.js
+++ website/app/application/core/projects/project/processes/setup-tab-directive.js
@@ -3,9 +3,23 @@
 
     function setupTabDirective() {
         return {
-            scope: true,
+            scope: {
+                setup: '='
+            },
+            controller: "setupTabDirectiveController",
+            controllerAs: 'ctrl',
+            bindToController: true,
             restrict: "E",
             templateUrl: "application/core/projects/project/processes/setup.html"
         };
+    }
+
+    module.controller("setupTabDirectiveController", setupTabDirectiveController);
+    setupTabDirectiveController.$inject = [];
+
+    function setupTabDirectiveController() {
+        var ctrl = this;
+        console.log("here is ");
+        console.dir(ctrl.setup);
     }
 }(angular.module('materialscommons')));
--- website/app/application/core/projects/project/samples/samples-edit-controller.js
+++ website/app/application/core/projects/project/samples/samples-edit-controller.js
@@ -1,7 +1,7 @@
 (function (module) {
     module.controller('SamplesEditController', SamplesEditController);
     SamplesEditController.$inject = ["$scope", "sample", "project",
-       "pubsub", "mcfile", "Restangular"];
+        "pubsub", "mcfile", "Restangular"];
 
     function SamplesEditController($scope, sample, project, pubsub, mcfile, Restangular) {
         var ctrl = this;
@@ -9,8 +9,6 @@
         ctrl.isSet = isSet;
         ctrl.setTab = setTab;
         ctrl.fileSrc = fileSrc;
-        //ctrl.getMeasurements = getMeasurements;
-        //ctrl.setOthersToFalse = setOthersToFalse;
 
         ctrl.tab = "measurements";
         ctrl.sample = sample[0];
@@ -28,24 +26,12 @@
             return mcfile.src(id);
         }
 
-        //$scope.fileSrc = mcfile.src;
-        //$scope.isImage = isImage;
-
         pubsub.waitOn($scope, 'updateBestMeasurement', function () {
-            Restangular.one('sample').one('details', ctrl.sample.id).get().then(function(updated_sample){
+            Restangular.one('sample').one('details', ctrl.sample.id).get().then(function (updated_sample) {
                 ctrl.sample = updated_sample[0];
             });
         });
 
-        //function processColumns() {
-        //    $scope.properties.forEach(function (property) {
-        //        if (property.best_measure.length > 0 && (property.best_measure[0]._type === 'line' ||
-        //            property.best_measure[0]._type === 'histogram')) {
-        //            property.best_measure[0].categories = property.best_measure[0].value.categories.split("\n");
-        //            property.best_measure[0].values = property.best_measure[0].value.values.split("\n");
-        //        }
-        //    });
-        //}
 
     }
 }(angular.module('materialscommons')));
