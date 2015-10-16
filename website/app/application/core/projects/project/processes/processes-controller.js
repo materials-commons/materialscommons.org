9975aca5aef83bfe4518b0e2ac59344a866f7726
tammasr <tammasr@umich.edu>
2015-10-16 14:08:46 -0400

    process page card

--- website/app/application/core/projects/project/processes/processes-controller.js
+++ website/app/application/core/projects/project/processes/processes-controller.js
@@ -1,9 +1,25 @@
 (function (module) {
     module.controller('projectProcesses', projectProcesses);
-    projectProcesses.$inject = ["project"];
+    projectProcesses.$inject = ["project", "processes", "$state", "$filter"];
 
-    function projectProcesses(project) {
-        this.all = project.processes;
+    function projectProcesses(project, processes, $state, $filter) {
+        var ctrl = this;
+
+        ctrl.viewProcess = viewProcess;
+
+        ctrl.processes = processes;
+        ctrl.project = project;
+
+        if (ctrl.processes.length !== 0) {
+            var sortedProcesses= $filter('orderBy')(ctrl.processes, 'name');
+            ctrl.current = sortedProcesses[0];
+            $state.go('projects.project.processes.edit', {process_id: ctrl.current.id});
+        }
+
+        function viewProcess(process) {
+            ctrl.current = process;
+            $state.go('projects.project.processes.edit', {process_id: ctrl.current.id});
+        }
     }
 
 }(angular.module('materialscommons')));
