2ba687fff8f6532832a6e2e1e7047ebb27f821f0
tammasr <tammasr@umich.edu>
2015-10-19 15:15:49 -0400

    update on process page

--- website/app/application/core/projects/project/processes/processes-controller.js
+++ website/app/application/core/projects/project/processes/processes-controller.js
@@ -1,25 +1,28 @@
 (function (module) {
     module.controller('projectProcesses', projectProcesses);
-    projectProcesses.$inject = ["project", "processes", "$state", "$filter"];
+    projectProcesses.$inject = [];
 
-    function projectProcesses(project, processes, $state, $filter) {
+    function projectProcesses() {
         var ctrl = this;
 
-        ctrl.viewProcess = viewProcess;
-
-        ctrl.processes = processes;
-        ctrl.project = project;
-
-        if (ctrl.processes.length !== 0) {
-            var sortedProcesses= $filter('orderBy')(ctrl.processes, 'name');
-            ctrl.current = sortedProcesses[0];
-            $state.go('projects.project.processes.edit', {process_id: ctrl.current.id});
-        }
-
-        function viewProcess(process) {
-            ctrl.current = process;
-            $state.go('projects.project.processes.edit', {process_id: ctrl.current.id});
-        }
+        //ctrl.viewProcess = viewProcess;
+        //ctrl.chooseTemplate = chooseTemplate;
+        //
+        //ctrl.processes = processes;
+        //ctrl.project = project;
+        //if (ctrl.processes.length !== 0) {
+        //    var sortedProcesses= $filter('orderBy')(ctrl.processes, 'name');
+        //    ctrl.current = sortedProcesses[0];
+        //    $state.go('projects.project.processes.view', {process_id: ctrl.current.id});
+        //}
+        //
+        //function viewProcess(process) {
+        //    ctrl.current = process;
+        //    $state.go('projects.project.processes.view', {process_id: ctrl.current.id});
+        //}
+        //
+        //function chooseTemplate() {
+        //    modalInstance.chooseTemplate(ctrl.project);
+        //}
     }
-
 }(angular.module('materialscommons')));
