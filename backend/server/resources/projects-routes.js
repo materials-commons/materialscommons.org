a7bc664449cc1e15506bf0034b56f94953d134da
V. Glenn Tarcea <gtarcea@umich.edu>
2015-10-19 13:31:04 -0400

    Move route with process id so it is before route with process id. Otherwise the route isn't found.

--- backend/server/resources/projects-routes.js
+++ backend/server/resources/projects-routes.js
@@ -15,8 +15,8 @@ module.exports = function(model) {
 
     router.post('/projects/:project_id/processes', validateProjectAccess, processes.create);
     router.put('/projects/:project_id/processes/:process_id', validateProjectAccess, processes.update);
-    router.get('/projects/:project_id/processes/:process_id', validateProjectAccess, processes.get);
     router.get('/projects/:project_id/processes', validateProjectAccess, processes.getList);
+    router.get('/projects/:project_id/processes/:process_id', validateProjectAccess, processes.get);
 
     router.post('/projects/:project_id/samples', validateProjectAccess, samples.create);
     router.put('/projects/:project_id/samples/:sample_id', validateProjectAccess, samples.update);
