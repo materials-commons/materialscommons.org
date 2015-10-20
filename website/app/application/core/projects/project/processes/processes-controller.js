2b66ea60be185ad2854021c4215554fd0c8c46b5
tammasr <tammasr@umich.edu>
2015-10-20 12:13:59 -0400

    update

--- website/app/application/core/projects/project/processes/processes-controller.js
+++ website/app/application/core/projects/project/processes/processes-controller.js
@@ -3,26 +3,7 @@
     projectProcesses.$inject = [];
 
     function projectProcesses() {
-        var ctrl = this;
 
-        //ctrl.viewProcess = viewProcess;
-        //ctrl.chooseTemplate = chooseTemplate;
-        //
-        //ctrl.processes = processes;
-        //ctrl.project = project;
-        //if (ctrl.processes.length !== 0) {
-        //    var sortedProcesses= $filter('orderBy')(ctrl.processes, 'name');
-        //    ctrl.current = sortedProcesses[0];
-        //    $state.go('projects.project.processes.view', {process_id: ctrl.current.id});
-        //}
-        //
-        //function viewProcess(process) {
-        //    ctrl.current = process;
-        //    $state.go('projects.project.processes.view', {process_id: ctrl.current.id});
-        //}
-        //
-        //function chooseTemplate() {
-        //    modalInstance.chooseTemplate(ctrl.project);
-        //}
     }
+
 }(angular.module('materialscommons')));
