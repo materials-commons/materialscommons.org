bc63991352ebc0b9a2c9dd637f6b9ad982c2397c
V. Glenn Tarcea <gtarcea@umich.edu>
2015-10-21 12:48:22 -0400

    Move processes and files over to the new detail-tabs directive.

--- website/app/application/core/projects/project/samples/samples-edit-controller.js
+++ website/app/application/core/projects/project/samples/samples-edit-controller.js
@@ -1,26 +1,15 @@
 (function (module) {
     module.controller('SamplesEditController', SamplesEditController);
-    SamplesEditController.$inject = ["$scope", "sample", "project",
-        "pubsub", "mcfile", "Restangular"];
+    SamplesEditController.$inject = ["$scope", "sample", "project", "pubsub", "mcfile", "Restangular"];
 
     function SamplesEditController($scope, sample, project, pubsub, mcfile, Restangular) {
         var ctrl = this;
 
-        ctrl.isSet = isSet;
-        ctrl.setTab = setTab;
         ctrl.fileSrc = fileSrc;
 
         ctrl.tab = "measurements";
         ctrl.sample = sample[0];
         ctrl.project = project;
-
-        function setTab(tabId) {
-            ctrl.tab = tabId;
-        }
-
-        function isSet(tabId) {
-            return ctrl.tab === tabId;
-        }
 
         function fileSrc(id) {
             return mcfile.src(id);
@@ -31,8 +20,6 @@
                 ctrl.sample = updated_sample[0];
             });
         });
-
-
     }
 }(angular.module('materialscommons')));
 
