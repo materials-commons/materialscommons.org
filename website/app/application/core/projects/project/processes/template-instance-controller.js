0c29be0756be631a32dd5eeaf94b9937409e47b1
tammasr <tammasr@umich.edu>
2015-10-28 17:09:42 -0400

    update

--- website/app/application/core/projects/project/processes/template-instance-controller.js
+++ website/app/application/core/projects/project/processes/template-instance-controller.js
@@ -7,9 +7,7 @@
                                         modal, processTemplates, Review, modalInstance) {
         $scope.modal = modal;
         this.all = project.processes;
-        $scope.selected = {
-            item: {}
-        };
+
         $scope.showDetails = function (template) {
             $scope.selected.item = template;
             $scope.template_details = processTemplates.newInstance(template);
@@ -18,6 +16,13 @@
         $scope.viewSetUp = function (template) {
             $scope.template_details = processTemplates.newInstance(template);
             modalInstance.viewSetUp($scope.template_details.setup.settings[0].properties);
+        };
+
+        $scope.preFill = function (template) {
+            $scope.showDetails(template);
+            $scope.modal.instance.close();
+            processTemplates.setActiveTemplate(template);
+            $state.go('projects.project.processes.prefill');
         };
 
         $scope.ok = function () {
@@ -31,15 +36,21 @@
             $scope.modal.instance.dismiss('cancel');
         };
 
-        $scope.setActive = function(tab) {
+        $scope.setActive = function (tab) {
             $scope.activeTab = tab;
         };
 
         $scope.addToFavourite = function (template) {
             $scope.favourites.push(template);
+            var index = _.indexOf($scope.templates, function (item) {
+                return item.name === template.name;
+            });
+            if (index > -1) {
+                $scope.templates[index].isFavorite = true;
+            }
         };
 
-        $scope.isActive = function(tab) {
+        $scope.isActive = function (tab) {
             return $scope.activeTab === tab;
         };
 
@@ -53,12 +64,11 @@
 
         function init() {
             $scope.templates = processTemplates.templates();
-            $scope.setActive('favourite');
-            $scope.favourites  = [];
-            //var index = $scope.favorites.indexOf(r.id);
-            //if(index > -1) {
-            //    $scope.favorites[index].isFavorite = true;
-            //}
+            $scope.setActive('all');
+            $scope.favourites = [];
+            $scope.selected = {
+                item: {}
+            };
         }
 
         init();
