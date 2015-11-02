cd2d8871b3c4ecfb172afbd4dd4e4b9ea9dbfc29
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-02 17:03:11 -0500

    Change REST signature for /users to /users/:project_id for adding items related to projects for a user. Update code to reflect changes.

--- backend/server/db/model/users.js
+++ backend/server/db/model/users.js
@@ -1,11 +1,11 @@
-module.exports = function(r) {
+module.exports = function (r) {
     const _ = require('lodash');
     const getSingle = require('./get-single');
 
     return {
         getUsers: getUsers,
         getUser: getUser,
-        get: function(id, index) {
+        get: function (id, index) {
             return getSingle(r, 'users', id, index);
         },
         update: update
@@ -31,27 +31,29 @@ module.exports = function(r) {
         return yield r.table('users').get(id).run();
     }
 
-    function* update(userID, attrs) {
+    function* update(userID, projectID, attrs) {
         if (attrs.favorites) {
             // TODO: validate the projectID exists
             let user = yield r.table('users').get(userID);
             if (!user.favorites) {
                 user.favorites = {};
             }
-            Object.keys(attrs.favorites).forEach(proj => {
-                let toAdd = attrs.favorites[proj].processes.
-                    filter(p => p.command === 'add').map(p => p.name);
-                let toDelete = attrs.favorites[proj].processes.
-                    filter(p => p.command === 'delete').map(p => p.name);
-                if (!(proj in user.favorites)) {
-                    user.favorites[proj] = {processes:[]};
-                }
-                let projProcesses = user.favorites[proj].processes;
-                // remove deleted process favorites
-                projProcesses = projProcesses.filter(p => _.indexOf(toDelete, name => name == p, null) === -1);
-                let toAddFavs = _.difference(toAdd, projProcesses);
-                user.favorites[proj].processes = projProcesses.concat(toAddFavs);
-            });
+
+            if (!(projectID in user.favorites)) {
+                user.favorites[projectID] = {
+                    processes: []
+                };
+            }
+
+            let toAdd = attrs.favorites.processes.
+                filter(p => p.command === 'add').map(p => p.name);
+            let toDelete = attrs.favorites.processes.
+                filter(p => p.command === 'delete').map(p => p.name);
+            let projProcesses = user.favorites[projectID].processes;
+            // remove deleted process favorites
+            projProcesses = projProcesses.filter(p => _.indexOf(toDelete, name => name == p, null) === -1);
+            let toAddFavs = _.difference(toAdd, projProcesses);
+            user.favorites[projectID].processes = projProcesses.concat(toAddFavs);
             yield r.table('users').get(userID).update({favorites: user.favorites});
         }
         return yield r.table('users').get(userID).without('admin', 'apikey', 'password');
