6e16f2c4b801ddd16a5b910a6a7de722882a3392
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-03 12:45:02 -0500

    Only add an empty process_templates if one doesn't exist.

--- backend/mcapi/user/projects.py
+++ backend/mcapi/user/projects.py
@@ -40,7 +40,8 @@ def add_computed_attributes(projects, user):
         p['notes'] = []
         p['events'] = []
         p['file_count'] = 0
-        p['process_templates'] = []
+        if 'process_templates' not in p:
+            p['process_templates'] = []
     project_ids = [p['id'] for p in projects]
     projects_by_id = {p['id']: p for p in projects}
 
