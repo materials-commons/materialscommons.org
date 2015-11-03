d879e5926f9cc3b3c185ab9244adcb769c727bf5
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-03 10:38:42 -0500

    Show the correct counts for processes, samples, files and reviews. Don't have counts show as links.

--- website/app/application/core/projects/project/home/home.js
+++ website/app/application/core/projects/project/home/home.js
@@ -1,45 +1,14 @@
 (function (module) {
     module.controller('ProjectHomeController', ProjectHomeController);
-    ProjectHomeController.$inject = ["project", "$filter", "modalInstance"];
+    ProjectHomeController.$inject = ["project", "modalInstance"];
 
-    function ProjectHomeController(project, $filter, modalInstance) {
-
+    function ProjectHomeController(project, modalInstance) {
         var ctrl = this;
-        ctrl.project = project;
 
+        ctrl.project = project;
         ctrl.chooseTemplate = chooseTemplate;
 
-        var columnDefs = [
-            {headerName: "Name", field: "name"},
-            {
-                headerName: "Composition", field: "composition",
-                cellRenderer: function(params){
-                    var measure = '';
-                    params.data.properties.forEach(function(property){
-                        if (property.attribute === 'composition'){
-                            measure =  property.best_measure[0];
-                        }
-                    });
-                    if (measure !== ''){
-                        return  measure.element + ': ' + measure.value+ ' '+measure.unit;
-                    } else{
-                        return '-';
-                    }
-
-                }
-            },
-            {headerName: "Owner", field: "owner"},
-            {headerName: "Description", field: "description"},
-            {headerName: "Files", field: "files"}
-        ];
-
-        var samples = $filter('orderBy')(ctrl.project.samples, 'name');
-
-        ctrl.gridOptions = {
-            columnDefs: columnDefs,
-            rowData: samples,
-            rowHeight: 45
-        };
+        /////////////////////////
 
         function chooseTemplate() {
             modalInstance.chooseTemplate(ctrl.project);
