09a05cdad0d4e6941b49723f5082c2a5a451c1af
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-03 15:22:47 -0500

    Hae the prefill controller save new and updated templates. This moves the save/update to the save button for the popup.

--- website/app/application/core/projects/project/processes/prefill-controller.js
+++ website/app/application/core/projects/project/processes/prefill-controller.js
@@ -1,21 +1,49 @@
 (function (module) {
     module.controller('projectPreFillProcess', projectPreFillProcessController);
-    projectPreFillProcessController.$inject = ["modal", "$modalInstance"];
+    projectPreFillProcessController.$inject = ["template", "$modalInstance", "Restangular", "project"];
 
-    function projectPreFillProcessController(modal, $modalInstance) {
+    function projectPreFillProcessController(template, $modalInstance, Restangular, project) {
         var ctrl = this;
 
-        ctrl.template = modal.template;
-        ctrl.modal = modal;
+        ctrl.template = template;
         ctrl.ok = ok;
         ctrl.cancel = cancel;
 
+        /////////////////////////
+
         function ok() {
-            $modalInstance.close({template: ctrl.template});
+            var command = templateExists(ctrl.template.name) ? 'update' : 'add';
+            Restangular.one('v2').one('projects', project.id)
+                .customPUT({
+                    process_templates: [
+                        {
+                            command: command,
+                            template: {
+                                name: ctrl.template.name,
+                                setup: ctrl.template.setup.settings[0],
+                                process_name: ctrl.template.process_name
+                            }
+                        }
+                    ]
+                }).then(
+                function success() {
+                    $modalInstance.close(ctrl.template);
+                },
+                function failure() {
+
+                });
         }
 
         function cancel() {
             $modalInstance.dismiss('cancel');
+        }
+
+        function templateExists(templateName) {
+            var i = _.indexOf(project.process_templates, function(t) {
+                return t.name === templateName;
+            });
+
+            return i !== -1;
         }
     }
 }(angular.module('materialscommons')));
