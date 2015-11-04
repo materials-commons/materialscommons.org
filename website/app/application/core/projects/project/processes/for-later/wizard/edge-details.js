17cc866f6450dddedbe5408d4c089b7cc4243786
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-04 14:28:29 -0500

    Code cleanup by removing unused files.

--- website/app/application/core/projects/project/processes/for-later/wizard/edge-details.js
+++
@@ -1,7 +0,0 @@
-(function (module) {
-    module.controller("edgeDetailsController", edgeDetailsController);
-    edgeDetailsController.$inject = ["$scope", "$timeout"];
-
-    function edgeDetailsController($scope, $timeout) {
-    }
-}(angular.module('materialscommons')));
