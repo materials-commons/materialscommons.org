d377dedf1312a68cb65f7e8d4912f03c60d0a9d9
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-04 15:07:59 -0500

    Code cleanup by removing unused files.

--- website/app/application/filters/page-filter.js
+++
@@ -1,8 +0,0 @@
-(function (module) {
-    module.filter('page', function () {
-        return function (input, currentPage, pageSize) {
-            var from = currentPage * pageSize, to = from + pageSize;
-            return input ? input.slice(from, to) : [];
-        };
-    });
-}(angular.module('materialscommons')));
