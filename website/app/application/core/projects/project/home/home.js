47fb0a7693e190e07f412bfc9a31ed1c4166c3f0
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-05 10:56:36 -0500

    Add simple sample create by launching the "As Received" process.

--- website/app/application/core/projects/project/home/home.js
+++ website/app/application/core/projects/project/home/home.js
@@ -1,17 +1,24 @@
 (function (module) {
     module.controller('ProjectHomeController', ProjectHomeController);
-    ProjectHomeController.$inject = ["project", "modalInstance"];
+    ProjectHomeController.$inject = ["project", "modalInstance", "processTemplates", "$state"];
 
-    function ProjectHomeController(project, modalInstance) {
+    function ProjectHomeController(project, modalInstance, processTemplates, $state) {
         var ctrl = this;
 
         ctrl.project = project;
         ctrl.chooseTemplate = chooseTemplate;
+        ctrl.createSample = createSample;
 
         /////////////////////////
 
         function chooseTemplate() {
             modalInstance.chooseTemplate(ctrl.project);
+        }
+
+        function createSample() {
+            var template = processTemplates.getTemplateByName('As Received');
+            processTemplates.setActiveTemplate(template);
+            $state.go('projects.project.processes.create');
         }
     }
 }(angular.module('materialscommons')));
