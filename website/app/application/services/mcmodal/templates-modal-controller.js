46d9c151656f12c4cbeeccecbca42d4c1082064e
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-10 16:20:13 -0500

    Rework templates to return a create function that will return the template instance. Update code to use create. Change how edit and setup work for new templates. Modify how favorites.

---
+++ website/app/application/services/mcmodal/templates-modal-controller.js
@@ -0,0 +1,118 @@
+(function (module) {
+    module.controller('TemplatesModalController', TemplatesModalController);
+    TemplatesModalController.$inject = ["project", "templates", "mcmodal", "$modalInstance",
+        "Restangular", "User", "processTemplates"];
+
+    function TemplatesModalController(project, templates, mcmodal, $modalInstance, Restangular, User, processTemplates) {
+        var ctrl = this;
+        ctrl.templates = templates;
+        ctrl.viewSetup = viewSetup;
+
+        ctrl.editPrefill = editPrefill;
+        ctrl.addPrefill = addPrefill;
+
+        ctrl.toggleFavorite = toggleFavorite;
+        ctrl.selectTemplate = selectTemplate;
+        ctrl.dismiss = dismiss;
+        ctrl.activeTab = 'all';
+        ctrl.isActive = isActive;
+        ctrl.setActive = setActive;
+
+        /////////////////////
+
+        function viewSetup(template) {
+            mcmodal.viewSetup(template);
+        }
+
+        function editPrefill(template) {
+            console.log('editPrefill');
+            var templateDetails = template.create();
+            mcmodal.preFill(templateDetails).then(function(t) {
+                console.log('editedTemplate', t);
+                Restangular.one('v2').one('projects', project.id)
+                    .customPUT({
+                        process_templates: [
+                            {
+                                command: 'update',
+                                template: {
+                                    name: t.name,
+                                    setup: t.setup.settings[0],
+                                    process_name: t.process_name
+                                }
+                            }
+                        ]
+                    }).then(function() {
+                        processTemplates.replace(ctrl.templates, t);
+                    });
+            });
+        }
+
+        function addPrefill(template) {
+            var templateDetails = template.create();
+            var existingTemplateNames = templates.map(function(t) {
+                return t.name;
+            });
+            mcmodal.preFill(templateDetails, existingTemplateNames).then(function(t) {
+                var existing = _.find(ctrl.templates, {name: t.name});
+                if (!existing) {
+                    Restangular.one('v2').one('projects', project.id)
+                        .customPUT({
+                            process_templates: [
+                                {
+                                    command: 'add',
+                                    template: {
+                                        name: t.name,
+                                        setup: t.setup.settings[0],
+                                        process_name: t.process_name
+                                    }
+                                }
+                            ]
+                        }).then(function () {
+                            processTemplates.add(ctrl.templates, t);
+                        });
+                } else {
+                    console.log('template already exists');
+                }
+            });
+        }
+
+        function toggleFavorite(template) {
+            var command = template.favorite ? 'delete' : 'add';
+            template.favorite = !template.favorite;
+
+            Restangular.one('v2').one('users', project.id)
+                .customPUT({
+                    favorites: {
+                        processes: [
+                            {
+                                command: command,
+                                name: template.name
+                            }
+                        ]
+                    }
+                }).then(function() {
+                    if (command == 'add') {
+                        User.addToFavorites(project.id, template.name);
+                    } else {
+                        User.removeFromFavorites(project.id, template.name);
+                    }
+                });
+        }
+
+        function selectTemplate(template) {
+            $modalInstance.close(template.name);
+        }
+
+        function dismiss() {
+            $modalInstance.dismiss('cancel');
+        }
+
+        function isActive(tab) {
+            return ctrl.activeTab === tab;
+        }
+
+        function setActive(tab) {
+            ctrl.activeTab = tab;
+        }
+    }
+}(angular.module('materialscommons')));
