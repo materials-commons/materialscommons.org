9667108be2f0d22082a4b7269ec3001f753486c6
tammasr <tammasr@umich.edu>
2015-11-10 17:01:45 -0500

    move functions ino the service

--- website/app/application/core/projects/project/processes/edit/edit-process-controller.js
+++ website/app/application/core/projects/project/processes/edit/edit-process-controller.js
@@ -1,8 +1,8 @@
 (function (module) {
     module.controller('EditProcessController', EditProcessController);
-    EditProcessController.$inject = ["Restangular", "$stateParams", "selectItems", "$state", "process", "processTemplates"];
+    EditProcessController.$inject = ["Restangular", "processEdit", "selectItems", "$state", "process", "processTemplates"];
 
-    function EditProcessController(Restangular, $stateParams, selectItems, $state, process, processTemplates) {
+    function EditProcessController(Restangular, processEdit, selectItems, $state, process, processTemplates) {
         var ctrl = this;
 
         ctrl.process = process[0];
@@ -16,60 +16,13 @@
         ctrl.chooseSamples = chooseSamples;
         ctrl.chooseInputFiles = chooseInputFiles;
         ctrl.chooseOutputFiles = chooseOutputFiles;
-        ctrl.fillSetUp = fillSetUp;
-        ctrl.samples = samples;
-        ctrl.files = files;
+
         ctrl.submit = submit;
         ctrl.cancel = cancel;
         ctrl.remove = remove;
 
-        ctrl.fillSetUp();
-        ctrl.samples();
-        ctrl.files();
-        /**
-         * fillSetUp: will read all the setup values from process
-         * and place inside template.
-         *
-         */
-        function fillSetUp() {
-            ctrl.settings = ctrl.template.setup.settings[0].properties;
-            ctrl.process.setup[0].properties.forEach(function (property) {
-                var i = _.indexOf(ctrl.settings, function (setting) {
-                    return setting.property.attribute === property.attribute
-                });
-                if (i > -1) {
-                    ctrl.settings[i].property.value = property.value;
-                    ctrl.settings[i].property.unit = property.unit;
-                    ctrl.settings[i].property.id = property.setup_id;
-                    ctrl.settings[i].property.property_id = property.id;
-                    ctrl.settings[i].property._type = property._type;
-                    ctrl.settings[i].property.attribute = property.attribute;
-                }
-            });
-            ctrl.process.setup = ctrl.template.setup;
-        }
+        processEdit.fillProcess(ctrl.template, ctrl.process);
 
-        function samples() {
-            ctrl.process.input_samples = ctrl.process.samples.map(function (sample) {
-                return {
-                    id: sample.id,
-                    name: sample.name,
-                    old_properties: [],
-                    new_properties: [],
-                    property_set_id: sample.property_set_id,
-                    files: []
-                }
-            });
-        }
-
-        function files() {
-            ctrl.process['input_files'] = ctrl.process.files_used.map(function (file) {
-                return {id: file.id, name: file.name}
-            });
-            ctrl.process['output_files'] = ctrl.process.files_produced.map(function (file) {
-                return {id: file.id, name: file.name}
-            });
-        }
 
         function submit() {
             var updated_process = {
