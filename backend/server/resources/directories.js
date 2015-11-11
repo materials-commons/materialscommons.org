ce6e8cbc8476759102f65f66640b74677095804c
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-11 14:53:29 -0500

    Add support for creating directory paths.

--- backend/server/resources/directories.js
+++ backend/server/resources/directories.js
@@ -1,21 +1,35 @@
 module.exports = function(directories) {
     const parse = require('co-body');
+    const httpStatus = require('http-status');
 
     return {
-        get: get,
-        create: create
+        get,
+        create
     };
 
     ////////////////
     function* get(next) {
         let dirID = this.params.directory_id || 'top';
-        this.body = yield get.tree(this.params.project_id, dirID);
+        this.body = yield directories.get(this.params.project_id, dirID);
         yield next;
     }
 
     function* create(next) {
         let dirArgs = yield parse(this);
-        this.body = yield directories.create(this.params.project_id, dirArgs);
+        if (!dirArgs.from_dir) {
+            this.throw(httpStatus.BAD_REQUEST, 'no from_dir specified');
+        }
+
+        if (!dirArgs.path) {
+            this.throw(httpStatus.BAD_REQUEST, 'no path specified');
+        }
+
+        let rv = yield directories.create(this.params.project_id, this.reqctx.project.name, dirArgs);
+        if (rv.error) {
+            this.throw(httpStatus.BAD_REQUEST, rv.error);
+        }
+
+        this.body = rv.val;
         yield next;
     }
 };
