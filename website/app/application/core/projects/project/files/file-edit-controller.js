2711e01348a10e94b4a5836a71ef5efb4fc7f4ae
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-17 15:52:32 -0500

    Checkpoint changes for debug and cleanup.

--- website/app/application/core/projects/project/files/file-edit-controller.js
+++ website/app/application/core/projects/project/files/file-edit-controller.js
@@ -1,11 +1,16 @@
 (function (module) {
     module.controller("FileEditController", FileEditController);
 
-    FileEditController.$inject = ['file'];
+    FileEditController.$inject = ['file', '$scope'];
 
     /* @ngInject */
-    function FileEditController(file) {
+    function FileEditController(file, $scope) {
+        console.log('FileEditController');
         var ctrl = this;
         ctrl.file = file;
+
+        $scope.$on('$viewContentLoaded', function(event) {
+            console.log('$viewContentLoaded', event);
+        });
     }
 }(angular.module('materialscommons')));
