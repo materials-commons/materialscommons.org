d370ccf421f775468e356c889a9c01ee07395212
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-17 13:22:37 -0500

    Bug fixes for transforming process to template format:   - Set the id and setup_id   - Set the name from the process

--- website/app/application/core/projects/project/processes/edit/process-edit-service.js
+++ website/app/application/core/projects/project/processes/edit/process-edit-service.js
@@ -17,9 +17,11 @@
                 });
                 if (i > -1) {
                     template.setup.settings[0].properties[i].property.value = property.value;
+                    template.setup.settings[0].properties[i].property.name = property.name;
+                    template.setup.settings[0].properties[i].property.description = property.description;
                     template.setup.settings[0].properties[i].property.unit = property.unit;
-                    template.setup.settings[0].properties[i].property.id = property.setup_id;
-                    template.setup.settings[0].properties[i].property.property_id = property.id;
+                    template.setup.settings[0].properties[i].property.id = property.id;
+                    template.setup.settings[0].properties[i].property.setup_id = property.setup_id;
                     template.setup.settings[0].properties[i].property._type = property._type;
                     template.setup.settings[0].properties[i].property.attribute = property.attribute;
                 }
@@ -33,8 +35,6 @@
                 return {
                     id: sample.id,
                     name: sample.name,
-                    old_properties: [],
-                    new_properties: [],
                     property_set_id: sample.property_set_id,
                     files: sample.linked_files
                 }
@@ -52,15 +52,8 @@
             return process;
         }
 
-        function basicDetails(template, process) {
-            process.name = template.name;
-            return process;
-        }
-
-
         return {
             fillProcess: function (template, process) {
-                process = basicDetails(template, process);
                 process = setUp(template, process);
                 process = samples(process);
                 process = files(process);
