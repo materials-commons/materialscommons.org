d6d323c63831a091eeb21d759629856a25604161
V. Glenn Tarcea <gtarcea@umich.edu>
2015-11-30 10:21:50 -0500

    Remove editProvenance and replace with ui-sref.

--- website/app/application/core/projects/project/processes/process-view-controller.js
+++ website/app/application/core/projects/project/processes/process-view-controller.js
@@ -1,14 +1,9 @@
 (function (module) {
     module.controller('projectViewProcess', projectViewProcess);
-    projectViewProcess.$inject = ["$state", "process"];
+    projectViewProcess.$inject = ["process"];
 
-    function projectViewProcess($state, process) {
+    function projectViewProcess(process) {
         var ctrl = this;
-        ctrl.editProvenance = editProvenance;
         ctrl.process = process;
-
-        function editProvenance() {
-            $state.go('projects.project.processes.edit', {process_id: ctrl.process.id});
-        }
     }
 }(angular.module('materialscommons')));
