6eae80717a63267daa3e6cc6a962cfdaead7cb88
tammasr <tammasr@umich.edu>
2015-12-01 16:26:28 -0500

    file page updates

--- website/app/application/directives/mc-flow-button.js
+++ website/app/application/directives/mc-flow-button.js
@@ -9,7 +9,7 @@
             scope: {
                 dir: "=dir"
             },
-            template: "<span style='color: #64b5f6 ;cursor: pointer' title='Upload to directory' class='heading-label text-uppercase'>upload files</span>",
+            template: "<a><span title='Upload to directory' class='heading-label text-uppercase'>upload files</span></a>",
             link: function (scope, element, attrs) {
                 var flow = mcFlow.get();
                 var isDirectory = attrs.hasOwnProperty('flowDirectory');
