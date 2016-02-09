bfd27186406a2bdb299c502612183643a8f1b935
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-09 10:40:59 -0500

    Add date type.

--- website/app/application/core/projects/project/processes/create/processing-settings.js
+++ website/app/application/core/projects/project/processes/create/processing-settings.js
@@ -18,12 +18,19 @@
 
     function ProcessSettingsDirectiveController() {
         var ctrl = this;
+        ctrl.datePickerOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+        ctrl.openDatePicker = openDatePicker;
 
         // Set default value on selections to first choice.
         ctrl.settings.forEach(function(setting) {
             // selection type
             if (setting.property._type === 'selection') {
                 setting.property.value = setting.property.choices[0];
+            } else if (setting.property._type === 'date') {
+                setting.property.opened = false;
             }
 
             // unit selections
@@ -31,5 +38,13 @@
                 setting.property.unit = setting.property.units[0];
             }
         });
+
+        ///////////////////////////////////////
+
+        function openDatePicker($event, prop) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            prop.opened = true;
+        }
     }
 }(angular.module('materialscommons')));
