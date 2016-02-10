896d1a525f69cae5a6ff12becf2197720412b692
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-10 09:59:51 -0500

    Add checkbox to samples list and samples selection list to to show all samples not just top level samples.

--- website/app/application/directives/select-items-samples.js
+++ website/app/application/directives/select-items-samples.js
@@ -6,7 +6,35 @@
             scope: {
                 samples: '='
             },
+            controller: 'SelectItemsSamplesDirectiveController',
+            controllerAs: 'ctrl',
+            bindToController: true,
             templateUrl: 'application/directives/partials/select-items-samples.html'
+        }
+    }
+
+    module.controller('SelectItemsSamplesDirectiveController', SelectItemsSamplesDirectiveController);
+    SelectItemsSamplesDirectiveController.$inject = [];
+
+    function SelectItemsSamplesDirectiveController() {
+        var ctrl = this;
+
+        ctrl.showSamplesInGroups = false;
+        ctrl.showGroupsChanged = showGroupsChanged;
+        ctrl.showGroupsFilter = {
+            is_grouped: false
+        };
+
+        /////////////////////////
+
+        function showGroupsChanged() {
+            if (!ctrl.showSamplesInGroups) {
+                ctrl.showGroupsFilter = {
+                    is_grouped: false
+                }
+            } else {
+                ctrl.showGroupsFilter = {};
+            }
         }
     }
 }(angular.module('materialscommons')));
