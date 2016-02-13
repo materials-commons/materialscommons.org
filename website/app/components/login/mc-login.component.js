f7d6b2bf074dfa9a4ac935d75ea57a2943e9d599
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-02-13 15:57:25 -0500

    Start restructuring app to clean up structure and switch over to angular materials.

---
+++ website/app/components/login/mc-login.component.js
@@ -0,0 +1,42 @@
+(function(module) {
+    module.component('mcLogin', {
+        templateUrl: 'components/login/mc-login.html',
+        controller: 'MCLoginController'
+    });
+
+    module.controller('MCLoginController', MCLoginController);
+    MCLoginController.$inject = ["$state", "User", "toastr", "mcapi", "Restangular"];
+    function MCLoginController($state, User, toastr, mcapi, Restangular) {
+        console.log('MCLoginController');
+        var ctrl = this;
+
+        ctrl.message = "MCLoginController";
+        ctrl.userLogin = "";
+        ctrl.password = "";
+        ctrl.cancel = cancel;
+        ctrl.login = login;
+
+        ////////////////////
+
+        function login() {
+            console.log('login');
+            mcapi('/user/%/apikey', ctrl.userLogin, ctrl.password)
+                .success(function (u) {
+                    User.setAuthenticated(true, u);
+                    Restangular.setDefaultRequestParams({apikey: User.apikey()});
+                    $state.go('projects');
+                })
+                .error(function (reason) {
+                    ctrl.message = "Incorrect Password/Username!";
+                    toastr.error(reason.error, 'Error', {
+                        closeButton: true
+                    });
+                }).put({password: ctrl.password});
+        }
+
+        function cancel() {
+            ctrl.userLogin = "";
+            ctrl.password = "";
+        }
+    }
+}(angular.module('materialscommons')));
