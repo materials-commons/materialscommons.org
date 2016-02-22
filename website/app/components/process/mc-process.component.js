50ec7146af930e7ba3070dc42cb2a89753e7a8e7
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-22 11:28:15 -0500

    Add support for processes.

---
+++ website/app/components/process/mc-process.component.js
@@ -0,0 +1,17 @@
+(function (module) {
+    module.component('mcProcess', {
+        templateUrl: 'components/process/mc-process.html',
+        controller: 'MCProcessComponentController'
+    });
+
+    module.controller('MCProcessComponentController', MCProcessComponentController);
+    MCProcessComponentController.$inject = ["$stateParams", "projectsService"];
+    function MCProcessComponentController($stateParams, projectsService) {
+        var ctrl = this;
+        projectsService.getProjectProcess($stateParams.project_id, $stateParams.process_id)
+            .then(function(process) {
+                console.dir(process);
+                ctrl.process = process;
+            });
+    }
+}(angular.module('materialscommons')));
