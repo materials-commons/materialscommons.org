e37854a9da4bbe05f8d19645eff53c86f7645d34
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-22 15:17:15 -0500

    Change colors of sidebar. Put project name in project navbar. Move projects out of navbar and put Change in project navbar.

--- website/app/components/project/mc-project-navbar.js
+++ website/app/components/project/mc-project-navbar.js
@@ -1,6 +1,15 @@
 (function (module) {
     module.component("mcProjectNavbar", {
-        templateUrl: 'components/project/mc-project-navbar.html'
+        templateUrl: 'components/project/mc-project-navbar.html',
+        controller: 'MCProjectNavbarComponentController'
     });
+
+    module.controller('MCProjectNavbarComponentController', MCProjectNavbarComponentController);
+    MCProjectNavbarComponentController.$inject = ['project'];
+    function MCProjectNavbarComponentController(project) {
+        var ctrl = this;
+
+        ctrl.project = project.get();
+    }
 
 }(angular.module('materialscommons')));
