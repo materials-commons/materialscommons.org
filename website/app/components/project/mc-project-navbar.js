07a8a304edf3e38ac677bf6292b214e97b18a5fe
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-23 13:23:35 -0500

    On page refresh set the active project tab to the correct tab setting.

--- website/app/components/project/mc-project-navbar.js
+++ website/app/components/project/mc-project-navbar.js
@@ -5,11 +5,30 @@
     });
 
     module.controller('MCProjectNavbarComponentController', MCProjectNavbarComponentController);
-    MCProjectNavbarComponentController.$inject = ['project'];
-    function MCProjectNavbarComponentController(project) {
+    MCProjectNavbarComponentController.$inject = ['project', '$state'];
+    function MCProjectNavbarComponentController(project, $state) {
         var ctrl = this;
 
+        ctrl.currentTab = getCurrentTabIndex();
         ctrl.project = project.get();
+
+        /////////////////////
+
+        function getCurrentTabIndex() {
+            if ($state.includes('project.home')) {
+                return 0;
+            } else if ($state.includes('project.processes')) {
+                return 1;
+            } else if ($state.includes('project.samples')) {
+                return 2;
+            } else if ($state.includes('project.files')) {
+                return 3;
+            } else if ($state.includes('project.settings')) {
+                return 4;
+            }
+
+            return 0;
+        }
     }
 
 }(angular.module('materialscommons')));
