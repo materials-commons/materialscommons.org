dca31a84fd3ff04258907f5c8474c5576b224d9f
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-23 14:16:27 -0500

    Sort processes and if there is no process id go to the first one.

--- website/app/components/project/processes/mc-project-processes.component.js
+++ website/app/components/project/processes/mc-project-processes.component.js
@@ -5,11 +5,17 @@
     });
 
     module.controller('MCProjectProcessesComponentController', MCProjectProcessesComponentController);
-    MCProjectProcessesComponentController.$inject = ["project", "$stateParams"];
-    function MCProjectProcessesComponentController(project, $stateParams) {
+    MCProjectProcessesComponentController.$inject = ['project', '$stateParams', '$state', '$filter'];
+    function MCProjectProcessesComponentController(project, $stateParams, $state, $filter) {
         var ctrl = this;
 
         ctrl.processes = project.get().processes;
+
+        if (!$stateParams.process_id && ctrl.processes.length) {
+            ctrl.processes = $filter('orderBy')(ctrl.processes, 'name');
+            var firstProcess = ctrl.processes[0];
+            $state.go('project.processes.process', {process_id: firstProcess.id});
+        }
 
         ///////////////////////////
     }
