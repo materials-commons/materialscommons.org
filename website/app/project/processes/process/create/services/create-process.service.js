fe8e6b84000101be5dfeaa4a637a4871bcfb2e52
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-02-25 21:58:23 -0500

    Add missing or factored out services to keep the create controller for samples simple.

--- website/app/project/processes/process/create/services/create-process.service.js
+++ website/app/project/processes/process/create/services/create-process.service.js
@@ -1,12 +1,10 @@
 (function(module) {
     module.factory('createProcess', createProcessService);
-    createProcessService.$inject = ['projectsAPI'];
-    function createProcessService(projectsAPI) {
+    createProcessService.$inject = ['projectsAPI', 'onChangeService'];
+    function createProcessService(projectsAPI, onChangeService) {
         return function(projectID, process) {
             return projectsAPI.api(projectID).one('processes').customPOST(process).then(function (p) {
-                if (projectsAPI.onChangeFn) {
-                    projectsAPI.onChangeFn(p);
-                }
+                onChangeService.execif(p);
                 return p;
             });
         }
