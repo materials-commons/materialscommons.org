2b4211c88dac9662ad3941e24a10dd376c83ecc0
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-29 14:10:59 -0500

    Add selections for updating existing samples or files.

---
+++ website/app/project/processes/process/services/process-selections.service.js
@@ -0,0 +1,71 @@
+(function(module) {
+    module.factory('processSelections', processSelectionsService);
+    processSelectionsService.$inject = ['selectItems'];
+    function processSelectionsService(selectItems) {
+        return {
+            selectSamples: function(processSamples) {
+                selectItems.open('samples').then(function (item) {
+                    var uniqueSamples = differenceById(item.samples, processSamples);
+                    uniqueSamples.forEach(function (sample) {
+                        processSamples.push({
+                            id: sample.id,
+                            name: sample.name,
+                            old_properties: [],
+                            new_properties: [],
+                            property_set_id: sample.property_set_id,
+                            files: []
+                        });
+                    });
+                });
+            },
+
+            selectUpdatedSamples: function(processSamples, updatedSamples) {
+                selectItems.open('samples').then(function (item) {
+                    var uniqueSamples = differenceById(item.samples, processSamples);
+                    uniqueSamples.forEach(function (sample) {
+                        processSamples.push({
+                            id: sample.id,
+                            name: sample.name,
+                            old_properties: [],
+                            new_properties: [],
+                            property_set_id: sample.property_set_id,
+                            files: []
+                        });
+                        updatedSamples.push({
+                            id: sample.id,
+                            command: 'add',
+                            property_set_id: sample.property_set_id
+                        });
+                    });
+                });
+            },
+
+            selectFiles: function(processFiles) {
+                selectItems.open('files').then(function (item) {
+                    var uniqueFiles = differenceById(item.files, processFiles);
+                    uniqueFiles.forEach(function (file) {
+                        processFiles.push({
+                            id: file.id,
+                            name: file.name,
+                            mediatype: file.mediatype
+                        });
+                    });
+                });
+            },
+
+            selectUpdatedFiles: function(processFiles, updatedFiles) {
+                selectItems.open('files').then(function (item) {
+                    var uniqueFiles = differenceById(item.files, processFiles);
+                    uniqueFiles.forEach(function (file) {
+                        processFiles.push({
+                            id: file.id,
+                            name: file.name,
+                            mediatype: file.mediatype
+                        });
+                        updatedFiles.push({id: file.id, command: 'add'});
+                    });
+                });
+            }
+        }
+    }
+}(angular.module('materialscommons')));
