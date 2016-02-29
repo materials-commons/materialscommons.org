ba929616a8c0a52b846f2b174944b21d30d1e857
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-29 15:49:53 -0500

    Move menu of items for creating a process into a component. On the home page the menu changes the size of the process box. Add an adjustment to make all boxes the same size.

--- website/app/project/home/mc-project-home.component.js
+++ website/app/project/home/mc-project-home.component.js
@@ -5,32 +5,11 @@
     });
 
     module.controller('MCProjectHomeComponentController', MCProjectHomeComponentController);
-    MCProjectHomeComponentController.$inject = ['$state', 'project', 'templates', 'mcmodal', 'projectsService'];
+    MCProjectHomeComponentController.$inject = ['project'];
 
-    function MCProjectHomeComponentController($state, project, templates, mcmodal, projectsService) {
+    function MCProjectHomeComponentController(project) {
         var ctrl = this;
         ctrl.project = project.get();
         ctrl.projectLoaded = true;
-        ctrl.templates = templates.get();
-        ctrl.chooseTemplate = chooseTemplate;
-        ctrl.chooseExistingProcess = chooseExistingProcess;
-        ctrl.hasFavorites = _.partial(_.any, ctrl.templates, _.matchesProperty('favorite', true));
-
-        /////////////////////////
-
-        function chooseTemplate() {
-            mcmodal.chooseTemplate(ctrl.project, ctrl.templates).then(function (processTemplateName) {
-                $state.go('project.processes.create', {template_id: processTemplateName, process_id: ''});
-            });
-        }
-
-        function chooseExistingProcess() {
-            projectsService.getProjectProcesses(ctrl.project.id).then(function (processes) {
-                mcmodal.chooseExistingProcess(processes).then(function (existingProcess) {
-                    $state.go('project.processes.create',
-                        {template_id: existingProcess.process_name, process_id: existingProcess.id});
-                });
-            });
-        }
     }
 }(angular.module('materialscommons')));
