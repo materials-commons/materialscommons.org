2a8be101ac3f3b8a8ccaeb4478572fe723a30741
V. Glenn Tarcea <gtarcea@umich.edu>
2016-02-29 11:16:49 -0500

    Add back menu for choosing how to create a process.

--- website/app/global.services/templates.service.js
+++ website/app/global.services/templates.service.js
@@ -1,13 +1,24 @@
 (function(module) {
     module.factory("templates", templatesService);
-    templatesService.$inject = ['$filter'];
+    templatesService.$inject = ['$filter', 'processEdit'];
 
-    function templatesService($filter) {
+    function templatesService($filter, processEdit) {
         var self = this;
         self.templates = {};
 
         function createName(templateName) {
             return templateName + ' - ' + $filter('date')(new Date(), 'MM/dd/yyyy @ h:mma');
+        }
+
+        function getTemplate(name) {
+            var t = _.find(self.templates, {name: name});
+            if (!t) {
+                console.log('did not find template');
+                return null;
+            }
+            var template = t.create();
+            template.name = createName(template.name);
+            return template;
         }
 
         return {
@@ -19,17 +30,17 @@
                 return self.templates;
             },
 
-            getTemplate: function(name) {
-                console.log('getTemplate:', name);
-                var t = _.find(self.templates, {name: name});
-                if (!t) {
-                    console.log('did not find template');
-                    return null;
-                }
-                var template = t.create();
-                template.name = createName(template.name);
-                return template;
-            }
+            loadTemplateFromProcess(templateName, process) {
+                var t = getTemplate(templateName);
+                process.name = t.name;
+                var p = processEdit.fillProcess(t, process);
+                p.input_samples = [];
+                p.input_files = [];
+                p.output_files = [];
+                return p;
+            },
+
+            getTemplate: getTemplate
         };
     }
 }(angular.module('materialscommons')));
