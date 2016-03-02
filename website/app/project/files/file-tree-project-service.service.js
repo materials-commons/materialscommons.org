fb203574458e104261697b8c8f0610e3d5cef6e7
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-03-02 13:39:41 -0500

    Move some apis into move service.

--- website/app/project/files/file-tree-project-service.service.js
+++ website/app/project/files/file-tree-project-service.service.js
@@ -3,25 +3,6 @@
     fileTreeProjectService.$inject = ['projectsAPI', 'gridFiles'];
     function fileTreeProjectService(projectsAPI, gridFiles) {
         return {
-            moveFile: function(projectID, fileID, oldDirID, newDirID) {
-                var moveArgs = {
-                    move: {
-                        old_directory_id: oldDirID,
-                        new_directory_id: newDirID
-                    }
-                };
-                return projectsAPI(projectID).one('files', fileID).customPUT(moveArgs);
-            },
-
-            moveDir: function(projectID, dirID, newDirID) {
-                var moveArgs = {
-                    move: {
-                        new_directory_id: newDirID
-                    }
-                };
-                return projectsAPI(projectID).one('directories', dirID).customPUT(moveArgs);
-            },
-
             getDirectory: function(projectID, directoryID) {
                 return projectsAPI(projectID).one('directories', directoryID).get()
                     .then(function(files) {
@@ -35,6 +16,6 @@
                         return gridFiles.toGrid(files);
                     });
             }
-        }
+        };
     }
 }(angular.module('materialscommons')));
