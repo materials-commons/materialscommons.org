c967fd75b008418ff81c72e3467acf6f14a34e3b
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-03-02 13:39:41 -0500

    Move some apis into move service.

--- website/app/project/files/file-tree-move-service.service.js
+++ website/app/project/files/file-tree-move-service.service.js
@@ -1,1 +1,34 @@
+(function(module) {
+    module.factory('fileTreeMoveService', fileTreeMoveService);
+    fileTreeMoveService.$inject = ['projectsAPI', 'project'];
+    function fileTreeMoveService(projectsAPI, project) {
+        function moveFileOnServer(projectID, fileID, oldDirID, newDirID) {
+            var moveArgs = {
+                move: {
+                    old_directory_id: oldDirID,
+                    new_directory_id: newDirID
+                }
+            };
+            return projectsAPI(projectID).one('files', fileID).customPUT(moveArgs);
+        }
 
+        function moveDirOnServer(projectID, dirID, newDirID) {
+            var moveArgs = {
+                move: {
+                    new_directory_id: newDirID
+                }
+            };
+            return projectsAPI(projectID).one('directories', dirID).customPUT(moveArgs);
+        }
+
+        return {
+            moveFile: function(fileID, oldDirID, newDirID) {
+                
+            },
+
+            moveDir: function(dirID, newDirID) {
+
+            }
+        };
+    }
+}(angular.module('materialscommons')));
