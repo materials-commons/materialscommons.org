f8d82f8b8d23673ff36f2d5c8ae71983c6d331a3
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-10 14:00:23 -0500

    Add support for picking files for inputs and outputs.

--- website/app/global.services/select-items/select-items.service.js
+++ website/app/global.services/select-items/select-items.service.js
@@ -1,10 +1,10 @@
-(function (module) {
+(function(module) {
     module.factory('selectItems', selectItemsService);
 
     selectItemsService.$inject = ["$modal"];
     function selectItemsService($modal) {
         return {
-            open: function () {
+            open: function() {
                 var tabs = {
                     processes: false,
                     files: false,
@@ -26,19 +26,19 @@
                     controller: 'SelectItemsServiceModalController',
                     controllerAs: 'ctrl',
                     resolve: {
-                        showProcesses: function () {
+                        showProcesses: function() {
                             return tabs.processes;
                         },
 
-                        showFiles: function () {
+                        showFiles: function() {
                             return tabs.files;
                         },
 
-                        showSamples: function () {
+                        showSamples: function() {
                             return tabs.samples;
                         },
 
-                        showReviews: function () {
+                        showReviews: function() {
                             return tabs.reviews;
                         }
                     }
@@ -50,30 +50,21 @@
 
     module.controller('SelectItemsServiceModalController', SelectItemsServiceModalController);
     SelectItemsServiceModalController.$inject = ['$modalInstance', 'showProcesses',
-        'showFiles', 'showSamples', 'showReviews', 'projectsService', '$stateParams', 'project', 'gridFiles'];
+        'showFiles', 'showSamples', 'showReviews', 'projectsService', '$stateParams', 'project'];
 
     function SelectItemsServiceModalController($modalInstance, showProcesses, showFiles, showSamples,
-                                               showReviews, projectsService, $stateParams, project, gridFiles) {
+                                               showReviews, projectsService, $stateParams, project) {
         var ctrl = this;
 
-
-        var proj = project.get();
-
-        projectsService.getProjectDirectory(proj.id).then(function (files) {
-            proj.files = gridFiles.toGrid(files);
-            ctrl.files = proj.files;
-            ctrl.files[0].expanded = true;
-            ctrl.gridShowingFlag = true;
-
-            ctrl.tabs = loadTabs();
-            ctrl.activeTab = ctrl.tabs[0].name;
-            ctrl.setActive = setActive;
-            ctrl.isActive = isActive;
-            ctrl.ok = ok;
-            ctrl.cancel = cancel;
-            ctrl.processes = [];
-            ctrl.samples = [];
-        });
+        ctrl.project = project.get();
+        ctrl.tabs = loadTabs();
+        ctrl.activeTab = ctrl.tabs[0].name;
+        ctrl.setActive = setActive;
+        ctrl.isActive = isActive;
+        ctrl.ok = ok;
+        ctrl.cancel = cancel;
+        ctrl.processes = [];
+        ctrl.samples = [];
 
         /////////////////////////
 
@@ -86,11 +77,11 @@
         }
 
         function ok() {
-            var selectedProcesses = ctrl.processes.filter(function (p) {
+            var selectedProcesses = ctrl.processes.filter(function(p) {
                 return p.input || p.output;
             });
 
-            var selectedSamples = ctrl.samples.filter(function (s) {
+            var selectedSamples = ctrl.samples.filter(function(s) {
                 return s.selected;
             });
 
@@ -111,7 +102,7 @@
             // list of files. Also reset the selected flag so the next time
             // the popup for files is used it doesn't show previously selected
             // items.
-            root.walk({strategy: 'pre'}, function (node) {
+            root.walk({strategy: 'pre'}, function(node) {
                 if (node.model.data.selected) {
                     node.model.data.selected = false;
                     if (node.model.data._type === 'file') {
@@ -130,14 +121,14 @@
             var tabs = [];
             if (showProcesses) {
                 tabs.push(newTab('processes', 'fa-code-fork'));
-                projectsService.getProjectProcesses($stateParams.project_id).then(function (processes) {
+                projectsService.getProjectProcesses($stateParams.project_id).then(function(processes) {
                     ctrl.processes = processes;
                 });
             }
 
             if (showSamples) {
                 tabs.push(newTab('samples', 'fa-cubes'));
-                projectsService.getProjectSamples($stateParams.project_id).then(function (samples) {
+                projectsService.getProjectSamples($stateParams.project_id).then(function(samples) {
                     ctrl.samples = samples;
                 });
             }
---
+++ website/app/project/components/select-items/mc-files-select.component.js
@@ -0,0 +1,104 @@
+(function(module) {
+    module.component('mcFilesSelect', {
+        templateUrl: 'project/components/select-items/mc-files-select.html',
+        controller: 'MCFilesSelectComponentController',
+        bindings: {
+            project: '='
+        }
+    });
+
+    module.controller('MCFilesSelectComponentController', MCFilesSelectComponentController);
+    MCFilesSelectComponentController.$inject = ['fileTreeProjectService'];
+    function MCFilesSelectComponentController(fileTreeProjectService) {
+        var ctrl = this;
+
+        fileTreeProjectService.getProjectRoot(ctrl.project.id).then(function(files) {
+            ctrl.project.files = files;
+            ctrl.files = ctrl.project.files;
+            ctrl.files[0].data.childrenLoaded = true;
+            ctrl.files[0].expand = true;
+        });
+
+    }
+
+    module.directive('mcFilesSelectDir', MCFilesSelectDirDirective);
+    MCFilesSelectDirDirective.$inject = ['RecursionHelper'];
+    function MCFilesSelectDirDirective(RecursionHelper) {
+        return {
+            restrict: 'E',
+            scope: {
+                file: '=',
+                project: '='
+            },
+            controller: 'MCFilesSelectDirDirectiveController',
+            replace: true,
+            controllerAs: 'ctrl',
+            bindToController: true,
+            templateUrl: 'project/components/select-items/mc-files-select-dir.html',
+            compile: function(element) {
+                return RecursionHelper.compile(element, function() {});
+            }
+        }
+    }
+
+    module.controller('MCFilesSelectDirDirectiveController', MCFilesSelectDirDirectiveController);
+    MCFilesSelectDirDirectiveController.$inject = ['fileTreeProjectService'];
+    function MCFilesSelectDirDirectiveController(fileTreeProjectService) {
+        var ctrl = this;
+        ctrl.files = ctrl.file.children;
+        ctrl.setActive = setActive;
+        ctrl.dirToggle = dirToggle;
+
+        ////////////////////
+
+        function setActive(file) {
+            clearActiveStateInAllNodes();
+
+            if (file.data._type === 'file') {
+                file.active = true;
+            } else {
+                if (!file.data.childrenLoaded) {
+                    fileTreeProjectService.getDirectory(ctrl.project.id, file.data.id).then(function(files) {
+                        file.children = files;
+                        file.active = true;
+                        file.data.childrenLoaded = true;
+                        file.expand = !file.expand;
+                    });
+                } else {
+                    file.active = true;
+                    file.expand = !file.expand;
+                }
+            }
+        }
+
+        function clearActiveStateInAllNodes() {
+            var treeModel = new TreeModel(),
+                root = treeModel.parse(ctrl.project.files[0]);
+            root.walk(function(treeNode) {
+                treeNode.model.active = false;
+            });
+        }
+
+        function dirToggle(dir) {
+            if (!dir.data.childrenLoaded) {
+                fileTreeProjectService.getDirectory(ctrl.project.id, dir.data.id).then(function(files) {
+                    dir.children = files;
+                    dir.active = true;
+                    dir.data.childrenLoaded = true;
+                    dir.children.forEach(function(f) {
+                        if (f.data._type === 'file') {
+                            f.data.selected = dir.data.selected;
+                        }
+                    });
+                });
+            } else {
+                dir.children.forEach(function(f) {
+                    if (f.data._type === 'file') {
+                        f.data.selected = dir.data.selected;
+                    }
+                });
+            }
+        }
+    }
+
+}(angular.module('materialscommons')));
