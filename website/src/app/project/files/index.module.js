00932e59074dd989cf2381735408803112f792ab
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-15 15:44:26 -0400

    Convert to gulp-angular/es6.

---
+++ website/src/app/project/files/index.module.js
@@ -0,0 +1,11 @@
+import './file-tree-move-service.service';
+import './file-tree-project-service.service';
+import './mc-file-tree.component';
+import './components/mc-flow.service';
+import './components/mc-flow-button.directive';
+import './components/dir/mc-dir-controls.component';
+import './components/dir/mc-dir-overview.component';
+import './components/dir/mc-dir.component';
+import './components/file/mc-file-description.component';
+import './components/file/mc-file-edit-controls.component';
+import './components/file/mc-file.component';
