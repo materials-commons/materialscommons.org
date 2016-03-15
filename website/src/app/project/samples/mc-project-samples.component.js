46e4d18a24a652c0666f6bd4a5452e527cdcf132
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-15 16:32:24 -0400

    Switch to gulp-angular/es6.

--- website/src/app/project/samples/mc-project-samples.component.js
+++ website/src/app/project/samples/mc-project-samples.component.js
@@ -1,32 +1,30 @@
-(function (module) {
-    module.component('mcProjectSamples', {
-        templateUrl: 'app/project/samples/mc-project-samples.html',
-        controller: 'MCProjectSamplesComponentController'
-    });
+angular.module('materialscommons').component('mcProjectSamples', {
+    templateUrl: 'app/project/samples/mc-project-samples.html',
+    controller: MCProjectSamplesComponentController
+});
 
-    module.controller('MCProjectSamplesComponentController', MCProjectSamplesComponentController);
-    MCProjectSamplesComponentController.$inject = ["project"];
-    function MCProjectSamplesComponentController(project) {
-        var ctrl = this;
-        ctrl.samples = project.get().samples;
-        ctrl.searchText = '';
-        ctrl.showSamplesInGroups = false;
+function MCProjectSamplesComponentController(project) {
+    'ngInject';
 
-        ctrl.showGroupsChangede = showGroupsChanged;
-        ctrl.showGroupsFilter = {
-            is_grouped: false
-        };
+    var ctrl = this;
+    ctrl.samples = project.get().samples;
+    ctrl.searchText = '';
+    ctrl.showSamplesInGroups = false;
 
-        ///////////////////////////
+    ctrl.showGroupsChangede = showGroupsChanged;
+    ctrl.showGroupsFilter = {
+        is_grouped: false
+    };
 
-        function showGroupsChanged() {
-            if (!ctrl.showSamplesInGroups) {
-                ctrl.showGroupsFilter = {
-                    is_grouped: false
-                }
-            } else {
-                ctrl.showGroupsFilter = {};
+    ///////////////////////////
+
+    function showGroupsChanged() {
+        if (!ctrl.showSamplesInGroups) {
+            ctrl.showGroupsFilter = {
+                is_grouped: false
             }
+        } else {
+            ctrl.showGroupsFilter = {};
         }
     }
-}(angular.module('materialscommons')));
+}
