a5481cd7cf316e68caebd3b87b3dc8e92a3ea853
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-15 13:25:57 -0400

    Convert model to ES6/Gulp Angular.

--- website/src/app/project/components/display-file-contents-directive.js
+++ website/src/app/project/components/display-file-contents-directive.js
@@ -1,55 +1,51 @@
-(function (module) {
-    module.directive("displayFileContents", displayFileContentsDirective);
-    function displayFileContentsDirective() {
-        return {
-            restrict: "E",
-            controller: 'DisplayFileContentsDirectiveController',
-            controllerAs: 'view',
-            bindToController: true,
-            scope: {
-                file: '=file'
-            },
-            templateUrl: 'app/project/components/display-file-contents.html'
-        };
-    }
+angular.module('mc.project').directive("displayFileContents", displayFileContentsDirective);
+function displayFileContentsDirective() {
+    return {
+        restrict: "E",
+        controller: DisplayFileContentsDirectiveController,
+        controllerAs: 'view',
+        bindToController: true,
+        scope: {
+            file: '=file'
+        },
+        templateUrl: 'app/project/components/display-file-contents.html'
+    };
+}
 
-    module.controller("DisplayFileContentsDirectiveController", DisplayFileContentsDirectiveController);
-    DisplayFileContentsDirectiveController.$inject = ["mcfile", "mcmodal"];
+function DisplayFileContentsDirectiveController(mcfile, mcmodal) {
+    'ngInject';
 
-    /* @ngInject */
-    function DisplayFileContentsDirectiveController(mcfile, mcmodal) {
-        var ctrl = this;
-        ctrl.fileType = determineFileType(ctrl.file.mediatype);
-        ctrl.fileSrc = mcfile.src(ctrl.file.id);
-        ctrl.showImage = showImage;
-        ctrl.downloadSrc = downloadSrc;
+    var ctrl = this;
+    ctrl.fileType = determineFileType(ctrl.file.mediatype);
+    ctrl.fileSrc = mcfile.src(ctrl.file.id);
+    ctrl.showImage = showImage;
+    ctrl.downloadSrc = downloadSrc;
 
-        //////////////
+    //////////////
 
-        function determineFileType(mediatype) {
-            if (isImage(mediatype.mime)) {
-                return "image";
-            } else {
-                switch (mediatype.mime) {
-                case "application/pdf":
-                    return "pdf";
-                case "application/vnd.ms-excel":
-                    return "xls";
-                case "application/octet-stream":
-                    return "binary";
-                default:
-                    return mediatype.mime;
-                }
+    function determineFileType(mediatype) {
+        if (isImage(mediatype.mime)) {
+            return "image";
+        } else {
+            switch (mediatype.mime) {
+            case "application/pdf":
+                return "pdf";
+            case "application/vnd.ms-excel":
+                return "xls";
+            case "application/octet-stream":
+                return "binary";
+            default:
+                return mediatype.mime;
             }
         }
-
-        function showImage(file) {
-            mcmodal.viewImage(file);
-        }
-
-        function downloadSrc() {
-            return mcfile.downloadSrc(ctrl.file.id);
-        }
-
     }
-}(angular.module('materialscommons')));
+
+    function showImage(file) {
+        mcmodal.viewImage(file);
+    }
+
+    function downloadSrc() {
+        return mcfile.downloadSrc(ctrl.file.id);
+    }
+
+}
