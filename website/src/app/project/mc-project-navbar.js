511a8acc697ef6f7fe37a6aef4f86d28de1ddb38
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-23 11:15:19 -0400

    Commit due to fire alarm.

--- website/src/app/project/mc-project-navbar.js
+++ website/src/app/project/mc-project-navbar.js
@@ -22,14 +22,16 @@ function MCProjectNavbarComponentController(project, $state, $rootScope, $scope)
     function getCurrentTabIndex() {
         if ($state.includes('project.home')) {
             return 0;
-        } else if ($state.includes('project.processes')) {
+        } else if ($state.includes('project.experiments')) {
             return 1;
-        } else if ($state.includes('project.samples')) {
+        } else if ($state.includes('project.processes')) {
             return 2;
-        } else if ($state.includes('project.files')) {
+        } else if ($state.includes('project.samples')) {
             return 3;
-        } else if ($state.includes('project.settings')) {
+        } else if ($state.includes('project.files')) {
             return 4;
+        } else if ($state.includes('project.settings')) {
+            return 5;
         }
 
         return 0;
