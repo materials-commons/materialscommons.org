25272845591fc981c8310520e9514449f1b2dd9d
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-25 14:20:27 -0400

    Change directive so that its layout matches the top level.

--- website/src/app/global.components/check-md-content.component.js
+++ website/src/app/global.components/check-md-content.component.js
@@ -1,26 +1,21 @@
 angular.module('materialscommons').component('checkMdContent', {
     template: `
-    <div ng-if="$ctrl.show">
-        Hello there
-        <md-content>
+    <div layout="column" flex>
+        <div>Hello there from directive<div>
+        <md-content flex>
             <div ng-repeat="item in $ctrl.items">{{item}}</div>
         </md-content>
     </div>
     `,
-    controller: CheckMDContentComponentController,
-    bindings: {
-        show: '<'
-    }
+    controller: CheckMDContentComponentController
 });
 
 /*@ngInject*/
 function CheckMDContentComponentController() {
     let ctrl = this;
     ctrl.items = [];
-    if (ctrl.show) {
-        console.log('showing numbers');
-        for (let i = 0; i < 1000; i++) {
-            ctrl.items.push(i);
-        }
+    console.log('showing numbers');
+    for (let i = 0; i < 1000; i++) {
+        ctrl.items.push(i);
     }
 }
