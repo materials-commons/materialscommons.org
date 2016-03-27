31de383f2c9cd10a9362afd80d379a64ecb2c1fd
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-03-27 17:11:35 -0400

    Add directive to detect different key codes and perform an action when they are detected.

---
+++ website/src/app/global.components/on-key-code.directive.js
@@ -0,0 +1,17 @@
+angular.module("materialscommons").directive('onKeyCode', onKeyCodeDirective);
+function onKeyCodeDirective() {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attrs) {
+            $element.bind("keypress", function(event) {
+                var keyCode = event.which || event.keyCode;
+                if (keyCode == $attrs.code) {
+                    $scope.$apply(function() {
+                        $scope.$eval($attrs.onKeyCode, {$event: event});
+                    });
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+}
