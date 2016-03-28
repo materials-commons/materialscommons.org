d0158a6e9791cf4af41e60af319d30584726cffc
V. Glenn Tarcea <gtarcea@umich.edu>
2016-03-28 15:58:10 -0400

    Add toggle and select to steps. Allow steps to be selected by clicking on more than just the name.

--- website/src/app/project/experiments/experiment/mc-experiment-outline-simple-view.component.js
+++ website/src/app/project/experiments/experiment/mc-experiment-outline-simple-view.component.js
@@ -2,11 +2,29 @@ angular.module('materialscommons').component('mcExperimentOutlineSimpleView', {
     templateUrl: 'app/project/experiments/experiment/mc-experiment-outline-simple-view.html',
     controller: MCExperimentOutlineSimpleViewComponentController,
     bindings: {
-        experiment: '='
+        experiment: '=',
+        currentStep: '=',
+        selectedStep: '='
     }
 });
 
 /*@ngInject*/
 function MCExperimentOutlineSimpleViewComponentController() {
+    let ctrl = this;
+    ctrl.toggleSelected = toggleSelected;
 
+    function toggleSelected(step) {
+        ctrl.currentStep = step;
+        if (ctrl.selectedStep && ctrl.selectedStep === step) {
+            step.selected = false;
+            ctrl.selectedStep = null;
+            return;
+        }
+
+        if (ctrl.selectedStep && ctrl.selectedStep !== step) {
+            ctrl.selectedStep.selected = false;
+        }
+        step.selected = true;
+        ctrl.selectedStep = step;
+    }
 }
