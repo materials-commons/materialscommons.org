d188a8d14e22403893f905411fae8e5ca4daf73b
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-03-27 22:03:54 -0400

    Add directive for handling different key codes.

--- website/src/app/global.components/on-key-code.directive.js
+++ website/src/app/global.components/on-key-code.directive.js
@@ -1,14 +1,22 @@
 angular.module("materialscommons").directive('onKeyCode', onKeyCodeDirective);
+
+/* This directive accepts a list of key codes. To get shift key code add a capital 'S' to your key code */
 function onKeyCodeDirective() {
     return {
         restrict: 'A',
         link: function($scope, $element, $attrs) {
             $element.bind("keypress", function(event) {
+                console.log($attrs.keyCodes);
+                let keyCodes = $attrs.keyCodes ? $attrs.keyCodes.split(',') : [];
                 var keyCode = event.which || event.keyCode;
-                if (keyCode == $attrs.code) {
-                    $scope.$apply(function() {
-                        $scope.$eval($attrs.onKeyCode, {$event: event});
-                    });
+                if (event.shiftKey) {
+                    keyCode = "S" + keyCode;
+                }
+                if ($attrs.keyCode && keyCode == $attrs.keyCode) {
+                    $scope.$apply(() => $scope.$eval($attrs.onKeyCode, {$event: event}));
+                    event.preventDefault();
+                } else if ($attrs.keyCodes && _.findIndex(keyCodes, keyCode) !== -1) {
+                    $scope.$apply(() => $scope.$eval($attrs.onKeyCode, {$event: event}));
                     event.preventDefault();
                 }
             });
