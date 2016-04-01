1c9d6231d3a0779e1dcf76301a037088a2591272
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-01 10:23:56 -0400

    Set editor size and add experiment name and description (example) to the toolbar.

--- website/src/app/project/experiments/experiment/components/mc-experiment-step-details.component.js
+++ website/src/app/project/experiments/experiment/components/mc-experiment-step-details.component.js
@@ -1,11 +1,15 @@
 angular.module('materialscommons').component('mcExperimentStepDetails', {
     templateUrl: 'app/project/experiments/experiment/components/mc-experiment-step-details.html',
+    controller: MCExperimentStepDetailsComponentController,
     bindings: {
         step: '=',
         currentStep: '='
     }
 });
 
-//function MCExperimentStepDetailsComponentController() {
-//
-//}
+function MCExperimentStepDetailsComponentController($scope) {
+    $scope.editorOptions = {
+        height: '55vh',
+        width: '93vw'
+    };
+}
