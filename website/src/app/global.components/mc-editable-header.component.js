42534252d2cdeaf27673b74450a4c89dba322fcc
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-04-01 21:29:37 -0400

    Make experiment name and description editable. Create a component for this type of activity.

---
+++ website/src/app/global.components/mc-editable-header.component.js
@@ -0,0 +1,23 @@
+angular.module('materialscommons').component('mcEditableHeader', {
+    template: `
+    <div layout="column" layout-align="start stretch">
+        <span ng-if="!$ctrl.editField"
+              ng-click="$ctrl.editField = true"
+              ng-class="$ctrl.headerClasses"
+              style="cursor: pointer">
+            {{$ctrl.header}}
+        </span>
+        <input ng-if="$ctrl.editField"
+               type="text" class="mc-input-as-line"
+               on-enter="$ctrl.editField = false"
+               ng-model="$ctrl.header"
+               ng-class="$ctrl.inputClasses"
+               ng-mouseleave="$ctrl.editField = false">
+    </div>
+    `,
+    bindings: {
+        header: '=',
+        headerClasses: '@',
+        inputClasses: '@'
+    }
+});
