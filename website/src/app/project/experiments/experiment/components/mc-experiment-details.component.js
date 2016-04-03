39a284388376bf681f0ae1eb3205d5f6c40f7311
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-04-03 11:33:17 -0400

    Implement maximize/minimize for a step.

--- website/src/app/project/experiments/experiment/components/mc-experiment-details.component.js
+++ website/src/app/project/experiments/experiment/components/mc-experiment-details.component.js
@@ -12,4 +12,8 @@ function MCExperimentDetailsComponentController($scope, currentStep) {
     let ctrl = this;
     ctrl.currentStep = currentStep.get();
     $scope.editorOptions = ctrl.editorOptions;
+    ctrl.maximize = () => {
+        ctrl.currentStep = ctrl.step;
+        ctrl.currentStep.displayState.maximize = true;
+    }
 }
