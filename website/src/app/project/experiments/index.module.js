e7aee2e492f324eec881588b3c5852aa1650f84e
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-04-02 21:56:46 -0400

    Update list of modules with additions/deletions.

--- website/src/app/project/experiments/index.module.js
+++ website/src/app/project/experiments/index.module.js
@@ -2,6 +2,7 @@ import './mc-project-experiments.component';
 import './mc-project-experiments-table.component';
 import './experiment/move-step.service';
 import './experiment/mc-experiment.component';
+import './experiment/components/mc-experiment-details.component';
 import './experiment/components/mc-experiment-outline.component';
-import './experiment/components/mc-experiment-sidebar-details.component';
 import './experiment/components/mc-experiment-step-details.component';
+import './experiment/current-step.service';
