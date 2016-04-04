eee8592019fa7343c50d141a3b194b7aef42a1be
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-04 13:27:20 -0400

    Add creation of experiments dialog that uses a REST call to create the experiment.

--- website/src/app/project/experiments/mc-project-experiments.component.js
+++ website/src/app/project/experiments/mc-project-experiments.component.js
@@ -1,3 +1,5 @@
+import {Experiment} from './experiment/experiment.model';
+
 angular.module('materialscommons').component('mcProjectExperiments', {
     templateUrl: 'app/project/experiments/mc-project-experiments.html',
     controller: MCProjectExperimentsComponentController,
@@ -6,10 +8,52 @@ angular.module('materialscommons').component('mcProjectExperiments', {
     }
 });
 
+class CreateNewExperimentDialogController {
+    /*@ngInject*/
+    constructor($mdDialog, experimentsService, $stateParams) {
+        this.$mdDialog = $mdDialog;
+        this.name = '';
+        this.description = '';
+        this.projectID = $stateParams.project_id;
+        this.experimentsService = experimentsService;
+    }
+
+    submit() {
+        let e = new Experiment(this.name);
+        e.description = this.description;
+        this.experimentsService.createForProject(this.projectID, e).then(
+            (createdExperiment) => {
+                console.log('createdExperiment', createdExperiment);
+                this.$mdDialog.hide(createdExperiment)
+            },
+            (error) => {
+                console.log('create experiment failed', error);
+            });
+    }
+
+    cancel() {
+        this.$mdDialog.cancel();
+    }
+
+}
+
 /*@ngInject*/
-function MCProjectExperimentsComponentController() {
+function MCProjectExperimentsComponentController($mdDialog, $state) {
     let ctrl = this;
     ctrl.showTableView = true;
+    ctrl.startNewExperiment = () => {
+        $mdDialog.show({
+            templateUrl: 'app/project/experiments/create-experiment-dialog.html',
+            controller: CreateNewExperimentDialogController,
+            controllerAs: 'ctrl',
+            bindToController: true
+        }).then(
+            (e) => {
+                $state.go('project.experiment', {experiment_id: e.id});
+            }
+        );
+    };
 }
+
 
 
