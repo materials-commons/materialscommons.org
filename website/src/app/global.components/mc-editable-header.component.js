61fe5543a0d137f6c675bfa16bb9f36f5868b1fe
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-05 16:26:12 -0400

    When calling onChange call with the changed field.

--- website/src/app/global.components/mc-editable-header.component.js
+++ website/src/app/global.components/mc-editable-header.component.js
@@ -12,7 +12,7 @@ angular.module('materialscommons').component('mcEditableHeader', {
                on-enter="$ctrl.editField = false"
                ng-model="$ctrl.header"
                ng-model-options="{debounce: 250}"
-               ng-change="$ctrl.onChange && $ctrl.onChange()"
+               ng-change="$ctrl.onChange && $ctrl.onChange({what: $ctrl.header})"
                ng-class="$ctrl.inputClasses"
                ng-mouseleave="$ctrl.editField = false">
     </div>
