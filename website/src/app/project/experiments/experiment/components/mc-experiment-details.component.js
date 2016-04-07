fca12e321add8825c680b9c0ed0ec8705f658c68
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-07 15:02:35 -0400

    Update notes when they change. Set debounce to 3 seconds as people are likely to work in a note for a while.

--- website/src/app/project/experiments/experiment/components/mc-experiment-details.component.js
+++ website/src/app/project/experiments/experiment/components/mc-experiment-details.component.js
@@ -8,12 +8,23 @@ angular.module('materialscommons').component('mcExperimentDetails', {
     }
 });
 
-function MCExperimentDetailsComponentController($scope, currentStep) {
+/*@ngInject*/
+function MCExperimentDetailsComponentController($scope, currentStep, $stateParams, experimentsService, toast) {
     let ctrl = this;
     ctrl.currentStep = currentStep.get();
     $scope.editorOptions = ctrl.editorOptions;
+
     ctrl.maximize = () => {
         ctrl.currentStep = ctrl.step;
         ctrl.currentStep.displayState.maximize = true;
-    }
+    };
+
+    ctrl.updateNotes = () => {
+        experimentsService
+            .updateStep($stateParams.project_id, $stateParams.experiment_id, ctrl.step.id, {notes: ctrl.step.notes})
+            .then(
+                (s) => s,
+                (error) => toast.error('Failed to update step')
+            );
+    };
 }
