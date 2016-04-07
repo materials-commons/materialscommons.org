71b403d327547028a997e1c947106861a09564c9
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-07 12:29:15 -0400

    Have on-enter pass along the event.

--- website/src/app/global.components/on-enter.js
+++ website/src/app/global.components/on-enter.js
@@ -5,7 +5,7 @@ function onEnterDirective() {
         element.bind("keydown keypress", function(event) {
             if (event.which === 13) {
                 scope.$apply(function() {
-                    scope.$eval(attrs.onEnter);
+                    scope.$eval(attrs.onEnter, {$event: event});
                 });
                 event.preventDefault();
             }
