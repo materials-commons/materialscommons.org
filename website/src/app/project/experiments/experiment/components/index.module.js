d9df3e5652a5c1ce0a164eb8f07816577465a0e5
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-08 15:17:49 -0400

    Consolidate and rename experiment tasks and services under the tasks directory. Update all depdendencies. Rename mc-experiment to mc-experiment-tasks to reflect what it does.

--- website/src/app/project/experiments/experiment/components/index.module.js
+++ website/src/app/project/experiments/experiment/components/index.module.js
@@ -1,2 +1,5 @@
 import './tasks/mc-experiment-tasks-list.component';
 import './tasks/mc-experiment-task-details.component';
+import './tasks/move-step.service';
+import './tasks/mc-experiment-tasks.component';
+import './tasks/current-step.service';
