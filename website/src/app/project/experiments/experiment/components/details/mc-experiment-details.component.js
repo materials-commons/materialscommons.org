9fadd50365d8266f69594f6ce1afb165fd19da7f
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-04-10 21:34:24 -0400

    Add today to experiment sidebar as well as components for sidebar. Move sidebar into the project.experiment route so it doesn't flash when clicked on. Only route not converted is tasks. That will take a little more work.

--- website/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
+++ website/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
@@ -1,6 +1,15 @@
 angular.module('materialscommons').component('mcExperimentDetails', {
     templateUrl: 'app/project/experiments/experiment/components/details/mc-experiment-details.html',
+    controller: MCExperimentDetailsComponentController,
     bindings: {
         experiment: '='
     }
 });
+
+/*@ngInject*/
+function MCExperimentDetailsComponentController($scope) {
+    $scope.editorOptions = {
+        height: '30vh',
+        width: '84vw'
+    };
+}
