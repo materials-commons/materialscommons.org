de7ab81f471d572133e835e7ed29a517ec357574
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-11 15:44:15 -0400

    Reenable creating tasks and updating backend on changes.

--- website/src/app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list.component.js
+++ website/src/app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list.component.js
@@ -6,7 +6,7 @@
  */
 
 angular.module('materialscommons').component('mcExperimentTasksList', {
-    templateUrl: 'app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list2.html',
+    templateUrl: 'app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list.html',
     controller: MCExperimentTasksListComponentController,
     bindings: {
         experiment: '=',
@@ -15,116 +15,22 @@ angular.module('materialscommons').component('mcExperimentTasksList', {
 });
 
 /*@ngInject*/
-function MCExperimentTasksListComponentController(focus, $stateParams, experimentsService, toast,
-                                                  currentStep, toUIStep) {
-    //let lastID = 1;
+function MCExperimentTasksListComponentController() {
     let ctrl = this;
-
-    ctrl.editorOptions = {
-        height: '55vh',
-        width: '85vw'
+    ctrl.$onInit = () => {
+        var treeModel = new TreeModel({childrenPropertyName: 'steps'}),
+            root = treeModel.parse(ctrl.experiment);
+        root.walk((node) => {
+            if (node.model.displayState) {
+                node.model.displayState.selectedClass = '';
+            }
+        });
+        ctrl.experiment.steps[0].displayState.selectedClass = 'step-selected';
     };
-
-    ctrl.onNameChange = (step) => {
-        experimentsService
-            .updateStep($stateParams.project_id, $stateParams.experiment_id, step.id, {name: step.name})
-            .then(
-                () => null,
-                () => toast.error('Failed to update step')
-            );
-    };
-
-    ctrl.updateDoneStatus = (step) => {
-        experimentsService
-            .updateStep($stateParams.project_id, $stateParams.experiment_id, step.id, {flags: step.flags})
-            .then(
-                () => null,
-                () => toast.error('Failed to update step')
-            );
-    };
-
-    ctrl.toggleOpenDetails = (step, event) => {
-        step.displayState.open = !step.displayState.open;
-        step.displayState.details.loadEditor = false;
-        if (step.displayState.open) {
-            $(event.target).removeClass('fa-angle-double-right');
-            $(event.target).addClass('fa-angle-double-down');
-        } else {
-            $(event.target).removeClass('fa-angle-double-down');
-            $(event.target).addClass('fa-angle-double-right');
-        }
-    };
-
-    ctrl.toggleFlag = (step, whichFlag, event) => {
-        // toggle flag and then get its value so we know
-        // what classes to add/remove.
-        step.flags[whichFlag] = !step.flags[whichFlag];
-        let flagColorClass = 'mc-' + whichFlag + '-color';
-        let flag = step.flags[whichFlag];
-        if (flag) {
-            // Toggled to on, remove dark grey and add in class for specific flag
-            $(event.target).removeClass('mc-dark-grey-color');
-            $(event.target).addClass(flagColorClass);
-        } else {
-            $(event.target).removeClass(flagColorClass);
-            $(event.target).addClass('mc-dark-grey-color');
-        }
-
-        experimentsService
-            .updateStep($stateParams.project_id, $stateParams.experiment_id, step.id, {flags: step.flags})
-            .then(
-                () => null,
-                () => toast.error('Failed to update step')
-            );
-    };
-
-    ctrl.currentStep = currentStep.get();
-    ctrl.setCurrent = setCurrent;
-    ctrl.addBlankStep = addBlankStep;
-    ctrl.remove = remove;
-
-    function setCurrent(step, node, event) {
-        $('.mc-experiment-outline-step').removeClass('step-selected');
-        $(event.currentTarget).addClass('step-selected');
-        ctrl.currentStep = step;
-        currentStep.set(step);
-        ctrl.currentNode = node;
-    }
-
-    function addBlankStep(node) {
-        let newStep = {
-            name: '',
-            description: '',
-            parent_id: ''
-        };
-        experimentsService.createStep($stateParams.project_id, $stateParams.experiment_id, newStep)
-            .then(
-                (step) => {
-                    $('.mc-experiment-outline-step').removeClass('step-selected');
-                    toUIStep(step);
-                    step.displayState.selectedClass = 'step-selected';
-                    node.$parentNodesScope.$modelValue.push(step);
-                    ctrl.currentStep = step;
-                    currentStep.set(step);
-                    focus(step.id);
-                },
-                () => toast.error('Unable to create new step')
-            );
-    }
-
-    function remove(node) {
-        if (node.depth() === 1 && ctrl.experiment.steps.length === 1) {
-            toast.error('Cannot remove last step');
-            return;
-        }
-        if (node.$modelValue == ctrl.currentStep) {
-            ctrl.currentStep = null;
-        }
-        node.remove();
-    }
 }
 
 angular.module('materialscommons').directive('mcExperimentTasksListDir', MCExperimentTasksListDirDirective);
+
 /*@ngInject*/
 function MCExperimentTasksListDirDirective(RecursionHelper) {
     return {
@@ -144,15 +50,103 @@ function MCExperimentTasksListDirDirective(RecursionHelper) {
     }
 }
 
-function MCExperimentTasksListDirDirectiveController() {
+/*@ngInject*/
+function MCExperimentTasksListDirDirectiveController($stateParams, experimentsService, toast,
+                                                     currentStep, toUIStep, focus) {
     let ctrl = this;
     ctrl.setCurrent = setCurrent;
 
-    function setCurrent(step, node, event) {
+    function setCurrent(node, event) {
         $('.mc-experiment-outline-step').removeClass('step-selected');
         $(event.currentTarget).addClass('step-selected');
         //ctrl.currentStep = step;
-        //currentStep.set(step);
-        ctrl.currentNode = node;
+        currentStep.set(ctrl.step);
+        //ctrl.currentNode = node;
+    }
+
+    ctrl.toggleFlag = (whichFlag, event) => {
+        // toggle flag and then get its value so we know
+        // what classes to add/remove.
+        ctrl.step.flags[whichFlag] = !ctrl.step.flags[whichFlag];
+        let flagColorClass = 'mc-' + whichFlag + '-color';
+        let flag = ctrl.step.flags[whichFlag];
+        if (flag) {
+            // Toggled to on, remove dark grey and add in class for specific flag
+            $(event.target).removeClass('mc-dark-grey-color');
+            $(event.target).addClass(flagColorClass);
+        } else {
+            $(event.target).removeClass(flagColorClass);
+            $(event.target).addClass('mc-dark-grey-color');
+        }
+
+        experimentsService
+            .updateStep($stateParams.project_id, $stateParams.experiment_id, ctrl.step.id, {flags: ctrl.step.flags})
+            .then(
+                () => null,
+                () => toast.error('Failed to update step')
+            );
+    };
+
+    ctrl.onNameChange = () => {
+        experimentsService
+            .updateStep($stateParams.project_id, $stateParams.experiment_id, ctrl.step.id, {name: ctrl.step.name})
+            .then(
+                () => null,
+                () => toast.error('Failed to update step')
+            );
+    };
+
+    ctrl.updateDoneStatus = () => {
+        experimentsService
+            .updateStep($stateParams.project_id, $stateParams.experiment_id, ctrl.step.id, {flags: ctrl.step.flags})
+            .then(
+                () => null,
+                () => toast.error('Failed to update step')
+            );
+    };
+
+    ctrl.toggleOpenDetails = (event) => {
+        ctrl.step.displayState.open = !ctrl.step.displayState.open;
+        ctrl.step.displayState.details.loadEditor = false;
+        if (ctrl.step.displayState.open) {
+            $(event.target).removeClass('fa-angle-double-right');
+            $(event.target).addClass('fa-angle-double-down');
+        } else {
+            $(event.target).removeClass('fa-angle-double-down');
+            $(event.target).addClass('fa-angle-double-right');
+        }
+    };
+
+    ctrl.addBlankStep = (node) => {
+        let newStep = {
+            name: '',
+            description: '',
+            parent_id: ''
+        };
+        experimentsService.createStep($stateParams.project_id, $stateParams.experiment_id, newStep)
+            .then(
+                (step) => {
+                    $('.mc-experiment-outline-step').removeClass('step-selected');
+                    ctrl.step.displayState.selectedClass = '';
+                    toUIStep(step);
+                    step.displayState.selectedClass = 'step-selected';
+                    node.$nodeScope.$parentNodesScope.$modelValue.push(step);
+                    //ctrl.currentStep = step;
+                    currentStep.set(step);
+                    focus(step.id);
+                },
+                () => toast.error('Unable to create new step')
+            );
+    }
+
+    ctrl.remove = (node) => {
+        if (node.depth() === 1 && ctrl.experiment.steps.length === 1) {
+            toast.error('Cannot remove last step');
+            return;
+        }
+        if (node.$modelValue == ctrl.currentStep) {
+            ctrl.currentStep = null;
+        }
+        node.remove();
     }
 }
