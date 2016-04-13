0c06ef141927b015c325bf4182830825ebf52967
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-13 15:36:59 -0400

    Auto update notes.

--- website/src/app/project/experiments/experiment/mc-experiment.component.js
+++ website/src/app/project/experiments/experiment/mc-experiment.component.js
@@ -7,11 +7,31 @@ angular.module('materialscommons').component('mcExperiment', {
 });
 
 /*@ngInject*/
-function MCExperimentComponentController($scope) {
+function MCExperimentComponentController($scope, $stateParams, experimentsService, toast) {
+    let ctrl = this;
+    let lastNotes = ctrl.experiment.notes;
+
     $scope.editorOptions = {
         height: '68vh',
         width: '38vw',
         uiColor: '#f4f5f7'
+    };
+
+    ctrl.updateNotes = () => {
+        if (!ctrl.experiment.notes) {
+            ctrl.experiment.notes = '';
+        }
+
+        if (lastNotes === ctrl.experiment.notes) {
+            return;
+        }
+
+        experimentsService
+            .updateForProject($stateParams.project_id, $stateParams.experiment_id, {notes: ctrl.experiment.notes})
+            .then(
+                () => null,
+                () => toast.error('Failed to update notes')
+            );
     };
 }
 
