3f5d0e4bd6f8abfc62d965bfbe0e37fa90bf4ff3
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-13 15:35:22 -0400

    Auto save experiment name and description.

--- website/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
+++ website/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
@@ -7,5 +7,26 @@ angular.module('materialscommons').component('mcExperimentDetails', {
 });
 
 /*@ngInject*/
-function MCExperimentDetailsComponentController() {
+function MCExperimentDetailsComponentController($stateParams, experimentsService, toast) {
+    let ctrl = this;
+
+    ctrl.updateName = () => {
+        experimentsService
+            .updateForProject($stateParams.project_id, $stateParams.experiment_id,
+                              {name: ctrl.experiment.name})
+            .then(
+                () => null,
+                () => toast.error('Failed to update experiment name')
+            );
+    };
+
+    ctrl.updateDescription = () => {
+        experimentsService
+            .updateForProject($stateParams.project_id, $stateParams.experiment_id,
+                              {description: ctrl.experiment.description})
+            .then(
+                () => null,
+                () => toast.error('Failed to update experiment description')
+            );
+    };
 }
