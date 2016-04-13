f3e53b11cd29d87206891998c7a737b4d8934b9f
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-13 15:38:13 -0400

    Start changing the project home page to include experiments and other project attributes.

--- website/src/app/project/home/mc-project-home.component.js
+++ website/src/app/project/home/mc-project-home.component.js
@@ -3,10 +3,18 @@ angular.module('materialscommons').component('mcProjectHome', {
     controller: MCProjectHomeComponentController
 });
 
-function MCProjectHomeComponentController(project) {
-    'ngInject';
-
+/*@ngInject*/
+function MCProjectHomeComponentController(project, experimentsService, toast, $stateParams) {
     var ctrl = this;
     ctrl.project = project.get();
     ctrl.projectLoaded = true;
+    ctrl.whichExperiments = 'active';
+
+    ctrl.experiments = [];
+    experimentsService.getAllForProject($stateParams.project_id).then(
+        (experiments) => {
+            ctrl.experiments = experiments;
+        },
+        () => toast.error('Unable to retrieve experiments for project')
+    );
 }
