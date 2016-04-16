2dd9cbe3dd033b3bd1f66679b170c9216abcf2bd
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-04-15 22:21:04 -0400

    Layout changes for filters and tasks sidebar. Begin addressing feedback from Jake.

---
+++ website/src/app/project/experiments/experiment/components/notes/mc-experiment-notes.component.js
@@ -0,0 +1,6 @@
+angular.module('materialscommons').component('mcExperimentNotes', {
+    templateUrl: 'app/project/experiments/experiment/components/notes/mc-experiment-notes.html',
+    bindings: {
+        experiment: '='
+    }
+});
--- website/src/app/project/experiments/experiment/components/today/mc-experiment-today.component.js
+++
@@ -1,6 +0,0 @@
-angular.module('materialscommons').component('mcExperimentToday', {
-    templateUrl: 'app/project/experiments/experiment/components/today/mc-experiment-today.html',
-    bindings: {
-        experiment: '='
-    }
-});
