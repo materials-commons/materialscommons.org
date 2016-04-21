2db0ace0ed6dd9c869ace773dc2b0aaf03a39a49
V. Glenn Tarcea <gtarcea@umich.edu>
2016-04-21 16:24:13 -0400

    Add controller (to be filled out).

--- website/src/app/project/experiments/experiment/components/notes/mc-experiment-notes.component.js
+++ website/src/app/project/experiments/experiment/components/notes/mc-experiment-notes.component.js
@@ -1,6 +1,18 @@
 angular.module('materialscommons').component('mcExperimentNotes', {
     templateUrl: 'app/project/experiments/experiment/components/notes/mc-experiment-notes.html',
+    controller: MCExperimentNotesComponentController,
     bindings: {
         experiment: '='
     }
 });
+
+class MCExperimentNotesComponentController {
+    /*@ngInject*/
+    constructor($scope) {
+        $scope.editorOptions = {};
+    }
+
+    addNote() {
+
+    }
+}
