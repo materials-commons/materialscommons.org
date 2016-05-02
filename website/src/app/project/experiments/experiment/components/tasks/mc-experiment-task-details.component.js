71ceddfa55ae9530e1c7b27a726d997799f9b512
V. Glenn Tarcea <gtarcea@umich.edu>
2016-05-02 15:01:18 -0400

    Remove old view layout to rework for new task details.

--- website/src/app/project/experiments/experiment/components/tasks/mc-experiment-task-details.component.js
+++ website/src/app/project/experiments/experiment/components/tasks/mc-experiment-task-details.component.js
@@ -2,44 +2,12 @@ angular.module('materialscommons').component('mcExperimentTaskDetails', {
     templateUrl: 'app/project/experiments/experiment/components/tasks/mc-experiment-task-details.html',
     controller: MCExperimentTaskDetailsComponentController,
     bindings: {
-        task: '=',
-        editorOptions: '<',
-        compactSize: '<'
+        task: '='
     }
 });
 
 /*@ngInject*/
-function MCExperimentTaskDetailsComponentController($scope, currentTask, $stateParams, experimentsService, toast) {
+function MCExperimentTaskDetailsComponentController(currentTask) {
     let ctrl = this;
     ctrl.currentTask = currentTask.get();
-    ctrl.noNotes = 'No notes';
-    $scope.editorOptions = ctrl.editorOptions;
-    let experimentID = $stateParams.experiment_id;
-    let projectID = $stateParams.project_id;
-
-    ctrl.maximize = () => {
-        ctrl.currentTask = ctrl.task;
-        ctrl.currentTask.displayState.maximize = true;
-    };
-
-    ctrl.updateNotes = () => {
-        // Handle case where the editor is activating even though its in an ng-if.
-        if (!ctrl.task.displayState.open) {
-            return;
-        } else if (!experimentID || !projectID) {
-            console.log('experimentID or projectID not defined');
-            return;
-        }
-
-        if (!ctrl.task.notes) {
-            ctrl.task.notes = '';
-        }
-
-        experimentsService
-            .updateTask(projectID, experimentID, ctrl.task.id, {notes: ctrl.task.notes})
-            .then(
-                () => null,
-                () => toast.error('Failed to update notes')
-            );
-    };
 }
