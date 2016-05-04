c477aecf9de7db6fff138bbc21500fca44c5df34
V. Glenn Tarcea <gtarcea@umich.edu>
2016-05-04 11:51:38 -0400

    Add placeholder files for creating task templates.

---
+++ website/src/app/project/experiments/experiment/components/tasks/components/mc-task-template.component.js
@@ -0,0 +1,8 @@
+class MCTaskTemplateComponentController {
+
+}
+
+angular.module('materialscommons').component('mcTaskTemplate', {
+    template: 'app/project/experiments/experiment/components/tasks/components/mc-task-template.html',
+    controller: MCTaskTemplateComponentController
+});
