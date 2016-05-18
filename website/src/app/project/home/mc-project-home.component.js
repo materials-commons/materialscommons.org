c1c2e2e618bdef4853d16005c33ba1924ecf4ec9
V. Glenn Tarcea <gtarcea@umich.edu>
2016-05-18 16:17:04 -0400

    Add "+ START NEW EXPERIMENT" to home page.

--- website/src/app/project/home/mc-project-home.component.js
+++ website/src/app/project/home/mc-project-home.component.js
@@ -1,11 +1,13 @@
+import {Experiment} from '../experiments/experiment/components/tasks/experiment.model';
+
 angular.module('materialscommons').component('mcProjectHome', {
     templateUrl: 'app/project/home/mc-project-home.html',
     controller: MCProjectHomeComponentController
 });
 
 /*@ngInject*/
-function MCProjectHomeComponentController($scope, project, experimentsService, toast,
-                                          $stateParams, projectsService, editorOpts) {
+function MCProjectHomeComponentController($scope, project, experimentsService, toast, $state,
+                                          $stateParams, projectsService, editorOpts, $mdDialog) {
     var ctrl = this;
     ctrl.project = project.get();
     ctrl.projectLoaded = true;
@@ -35,4 +37,49 @@ function MCProjectHomeComponentController($scope, project, experimentsService, t
                 () => toast.error('Unable to update project description')
             );
     };
+
+    ctrl.startNewExperiment = () => {
+        $mdDialog.show({
+            templateUrl: 'app/project/experiments/create-experiment-dialog.html',
+            controller: CreateNewExperimentDialogController,
+            controllerAs: 'ctrl',
+            bindToController: true
+        }).then(
+            (e) => {
+                $state.go('project.experiment.details', {experiment_id: e.id});
+            }
+        );
+    };
+}
+
+class CreateNewExperimentDialogController {
+    /*@ngInject*/
+    constructor($mdDialog, experimentsService, $stateParams, toast) {
+        this.$mdDialog = $mdDialog;
+        this.name = '';
+        this.description = '';
+        this.projectID = $stateParams.project_id;
+        this.experimentsService = experimentsService;
+        this.toast = toast;
+    }
+
+    submit() {
+        if (this.name === '') {
+            this.toast.error('You must supply an experiment name');
+            return;
+        }
+        let e = new Experiment(this.name);
+        e.description = this.description;
+        this.experimentsService.createForProject(this.projectID, e).then(
+            (createdExperiment) => {
+                this.$mdDialog.hide(createdExperiment)
+            },
+            (error) => {
+                console.log('create experiment failed', error);
+            });
+    }
+
+    cancel() {
+        this.$mdDialog.cancel();
+    }
 }
