56173a0fa4fc1ff9245aa727009b69709cba55f0
V. Glenn Tarcea <gtarcea@umich.edu>
2016-06-13 11:07:51 -0400

    Demonstartion of how adding samples would work (no samples are actually added).

--- website/src/app/project/experiments/experiment/components/tasks/components/mc-task-template-create-samples.component.js
+++ website/src/app/project/experiments/experiment/components/tasks/components/mc-task-template-create-samples.component.js
@@ -1,3 +1,52 @@
+class MCTaskTemplateCreateSamplesComponentController {
+    /*@ngInject*/
+    constructor(prepareCreatedSample, focus, $mdDialog) {
+        this.lastId = 0;
+        this.prepareCreatedSample = prepareCreatedSample;
+        this.focus = focus;
+        this.$mdDialog = $mdDialog;
+
+        if (!this.task.template.samples) {
+            let id = 'sample_' + this.lastId++;
+            this.task.template.samples = [];
+        }
+    }
+
+    addSample() {
+        let id = 'sample_' + this.lastId++;
+
+        this.task.template.samples.push({
+            name: '',
+            id: id
+        });
+        this.focus(id);
+    }
+
+    addMultipleSamples() {
+        this.$mdDialog.show({
+            templateUrl: 'app/project/experiments/experiment/components/tasks/components/add-multiple-samples-dialog.html',
+            controller: AddMultipleSamplesDialogController,
+            controllerAs: '$ctrl',
+            bindToController: true,
+            locals: {
+                lastId: this.lastId
+            }
+        }).then(
+            (addedSamples, nextId) => {
+                this.task.template.samples = this.task.template.samples.concat(addedSamples);
+                this.lastId = nextId;
+            }
+        )
+    }
+
+    prepareSample() {
+        this.prepareCreatedSample.filloutComposition(this.sample, this.composition);
+        this.prepareCreatedSample.setupSampleGroup(this.sample, this.sampleGroup, this.sampleGroupSizing,
+            this.sampleGroupSize);
+        this.prepareCreatedSample.addSampleInputFiles(this.sample, this.process.input_files);
+    }
+}
+
 angular.module('materialscommons').component('mcTaskTemplateCreateSamples', {
     templateUrl: 'app/project/experiments/experiment/components/tasks/components/mc-task-template-create-samples.html',
     controller: MCTaskTemplateCreateSamplesComponentController,
@@ -6,22 +55,29 @@ angular.module('materialscommons').component('mcTaskTemplateCreateSamples', {
     }
 });
 
-/*@ngInject*/
-function MCTaskTemplateCreateSamplesComponentController(prepareCreatedSample) {
-    let ctrl = this,
-        lastId = 0;
+class AddMultipleSamplesDialogController {
+    /*@ngInject*/
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+        this.nameTemplate = "";
+        this.count = 2;
+    }
 
-    ctrl.addSample = () => {
-        ctrl.task.template.samples.push({
-            name: '',
-            id: 'sample_' + lastId++
-        });
-    };
+    done() {
+        let addedSamples = [];
+        for (let i = 0; i < this.count; i++) {
+            let id = "sample_" + i,
+                entry = {
+                    name: 'sample_' + (i + 1),
+                    id: id
+                };
+            addedSamples.push(entry);
+        }
+        console.log(addedSamples.length);
+        this.$mdDialog.hide(addedSamples);
+    }
 
-    function prepareSample() {
-        prepareCreatedSample.filloutComposition(ctrl.sample, ctrl.composition);
-        prepareCreatedSample.setupSampleGroup(ctrl.sample, ctrl.sampleGroup, ctrl.sampleGroupSizing,
-            ctrl.sampleGroupSize);
-        prepareCreatedSample.addSampleInputFiles(ctrl.sample, ctrl.process.input_files);
+    cancel() {
+        this.$mdDialog.cancel();
     }
 }
