b4c61f5e352efc9ab28a8b685134929ce6a890af
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-06-18 11:43:26 -0400

    Clean up debug and unused controller.

--- website/src/app/project/experiments/experiment/components/tasks/components/mc-task-template.component.js
+++ website/src/app/project/experiments/experiment/components/tasks/components/mc-task-template.component.js
@@ -1,14 +1,6 @@
 angular.module('materialscommons').component('mcTaskTemplate', {
     templateUrl: 'app/project/experiments/experiment/components/tasks/components/mc-task-template.html',
-    controller: MCTaskTemplateComponentController,
     bindings: {
         task: '<'
     }
 });
-
-/*@ngInject*/
-function MCTaskTemplateComponentController() {
-    var ctrl = this;
-    //console.dir(ctrl.task);
-    //ctrl.template = template.get();
-}
