13fce699370899229acd097e9f5750d424ad83f8
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-06-21 16:59:36 -0400

    Show existing composition. It's a hack and something that will need to be reworked.

--- website/src/app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list.component.js
+++ website/src/app/project/experiments/experiment/components/tasks/mc-experiment-tasks-list.component.js
@@ -90,7 +90,7 @@ function MCExperimentTasksListDirDirectiveController($stateParams, toast, curren
         currentTask.set(ctrl.task);
         currentNode.set(node);
         loadTaskTemplate();
-        console.log(ctrl.task);
+        //console.log(ctrl.task);
     }
 
     function loadTaskTemplate() {
