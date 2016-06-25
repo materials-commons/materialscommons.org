72b7a5b7e682199aa919864d295c6b4fd8489d39
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-06-25 14:26:50 -0400

    For experiments retrieve samples, files and processes that can be displayed in home.

--- backend/server/db/model/experiments.js
+++ backend/server/db/model/experiments.js
@@ -50,6 +50,18 @@ module.exports = function(r) {
                         .getAll(experiment('id'), {index: 'experiment_id'})
                         .eqJoin('experiment_note_id', r.table('experimentnotes')).zip()
                         .orderBy('name')
+                        .coerceTo('array'),
+                    samples: r.table('experiment2sample').getAll(experiment('id'), {index: 'experiment_id'})
+                        .eqJoin('sample_id', r.table('samples')).zip()
+                        .orderBy('name')
+                        .coerceTo('array'),
+                    files: r.table('experiment2datafile').getAll(experiment('id'), {index: 'experiment_id'})
+                        .eqJoin('datafile_id', r.table('datafiles')).zip()
+                        .orderBy('name')
+                        .coerceTo('array'),
+                    processes: r.table('experiment2process').getAll(experiment('id'), {index: 'experiment_id'})
+                        .eqJoin('process_id', r.table('processes')).zip()
+                        .orderBy('name')
                         .coerceTo('array')
                 }
             });
