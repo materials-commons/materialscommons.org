66d6e8d8b89358f304b0a1b2b09f096b9ecdab49
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-06-28 12:24:02 -0400

    Dropdown to pick default project.

--- website/src/app/user/components/mc-user-settings-projects.component.js
+++ website/src/app/user/components/mc-user-settings-projects.component.js
@@ -1,3 +1,26 @@
+class MCUserSettingsProjectsComponentController {
+    /*@ngInject*/
+    constructor(User, projectsService, toast) {
+        this.User = User;
+        this.projectsService = projectsService;
+        this.projects = [];
+        this.defaultProject = "";
+        this.toast = toast;
+    }
+
+    $onInit() {
+        this.projectsService.getAllProjects().then(
+            (projects) => this.projects = projects,
+            () => this.toast.error('Unable to retrieve users projects')
+        );
+    }
+
+    updateDefaultProject() {
+        console.log('defaultProject set to', this.defaultProject.plain());
+    }
+}
+
 angular.module('materialscommons').component('mcUserSettingsProjects', {
-    templateUrl: 'app/user/components/mc-user-settings-projects.html'
+    templateUrl: 'app/user/components/mc-user-settings-projects.html',
+    controller: MCUserSettingsProjectsComponentController
 });
