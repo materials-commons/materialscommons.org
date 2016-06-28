d8ddf9056508e804844ca40048120f1d00d0d99b
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-06-28 15:40:50 -0400

    Layout and backend to set default project/experiment.

--- backend/server/resources/users.js
+++ backend/server/resources/users.js
@@ -1,14 +1,65 @@
-module.exports = function(users) {
+module.exports = function(users, experiments) {
     const parse = require('co-body');
+    const status = require('http-status');
+    const _ = require('lodash');
 
     return {
-        update: update
+        updateProjectFavorites: updateProjectFavorites,
+        updateUserSettings: updateUserSettings
     };
 
-    function* update(next) {
+    function* updateProjectFavorites(next) {
         let attrs = yield parse(this);
-        this.body = yield users.update(this.reqctx.user.id, this.params.project_id, attrs);
+        this.body = yield users.updateProjectFavorites(this.reqctx.user.id, this.params.project_id, attrs);
         this.status = 200;
         yield next;
+    }
+
+    function* updateUserSettings(next) {
+        let args = yield parse(this);
+        let errors = yield validateUserSettingsArgs(args, this.reqctx.user.id);
+        if (errors != null) {
+            this.status = status.BAD_REQUEST;
+            this.body = errors;
+        } else {
+            let settingArgs = {
+                default_project: args.default_project,
+                default_experiment: args.default_experiment
+            };
+            let rv = yield users.updateUserSettings(this.reqctx.user.id, settingArgs);
+            if (rv.error) {
+                this.status = status.BAD_REQUEST;
+                this.body = rv;
+            } else {
+                this.body = rv.val;
+            }
+        }
+        yield next;
+    }
+
+    function* validateUserSettingsArgs(args, userId) {
+        if (!args.default_project || !_.isString(args.default_project)) {
+            return {error: `Bad argument for default_project`};
+        }
+
+        let isValidProject = yield users.userHasProjectAccess(userId, args.default_project);
+        if (!isValidProject) {
+            return {error: `No such project: ${args.default_project}`};
+        }
+
+        if (args.default_experiment && !_.isString(args.default_experiment)) {
+            return {error: `Bad argument for default_experiment`};
+        }
+
+        if (args.default_experiment) {
+            let experimentInProject = yield experiments.experimentExistsInProject(args.default_project, args.default_experiment);
+            if (!experimentInProject) {
+                return {error: `No such experiment ${args.default_experiment} in project ${args.default_project}`};
+            }
+        } else {
+            args.default_experiment = '';
+        }
+
+        return null;
     }
 };
