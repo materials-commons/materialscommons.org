f4fa568b6e3c089ed4c4aac38e07d61fd4862555
V. Glenn Tarcea <gtarcea@umich.edu>
2016-07-01 14:36:40 -0400

    Create unvalidated accounts in a separate table. Format success message to the user.

--- website/src/app/routes.js
+++ website/src/app/routes.js
@@ -5,10 +5,10 @@ export function setupRoutes($stateProvider, $urlRouterProvider) {
             url: '/login',
             template: '<mc-login></mc-login>'
         })
-        //.state('join', {
-        //    url: '/join',
-        //    template: '<mc-join></mc-join>'
-        //})
+        .state('join', {
+            url: '/join',
+            template: '<mc-join></mc-join>'
+        })
         .state('projects', {
             url: '/projects',
             abstract: true,
