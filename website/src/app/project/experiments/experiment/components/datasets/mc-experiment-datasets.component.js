1a99cc22a69c70b2612ca5f882dba35550595dc0
V. Glenn Tarcea <gtarcea@umich.edu>
2016-07-06 12:38:32 -0400

    Add dialog to create new datasets.

--- website/src/app/project/experiments/experiment/components/datasets/mc-experiment-datasets.component.js
+++ website/src/app/project/experiments/experiment/components/datasets/mc-experiment-datasets.component.js
@@ -1,11 +1,12 @@
 class MCExperimentDatasetsComponentController {
     /*@ngInject*/
-    constructor($stateParams, datasetService, toast, $state) {
+    constructor($stateParams, datasetService, toast, $state, $mdDialog) {
         this.projectId = $stateParams.project_id;
         this.experimentId = $stateParams.experiment_id;
         this.datasetService = datasetService;
         this.toast = toast;
         this.$state = $state;
+        this.$mdDialog = $mdDialog;
         this.datasets = [];
     }
 
@@ -18,17 +19,50 @@ class MCExperimentDatasetsComponentController {
     }
 
     createNewDataset() {
-        this.datasetService.createDatasetForExperiment(this.projectId, this.experimentId, "new dataset", "my new dataset")
-            .then(
-                (dataset) => {
-                    console.log('dataset', dataset.plain());
-                },
-                () => this.toast.error('Unable to create new dataset')
-            );
+        this.$mdDialog.show({
+            templateUrl: 'app/project/experiments/experiment/components/datasets/new-dataset-dialog.html',
+            controller: NewExperimentDatasetDialogController,
+            controllerAs: '$ctrl',
+            bindingToController: true
+        }).then(
+            () => {
+                this.datasetService.getDatasetsForExperiment(this.projectId, this.experimentId)
+                    .then(
+                        (datasets) => this.datasets = datasets,
+                        () => this.toast.error('Unable to retrieve datasets for experiment')
+                    )
+            }
+        );
     }
 
     openDataset(datasetId) {
         this.$state.go("project.experiment.dataset", {dataset_id: datasetId});
+    }
+}
+
+class NewExperimentDatasetDialogController {
+    /*@ngInject*/
+    constructor($mdDialog, datasetService, $stateParams) {
+        this.$mdDialog = $mdDialog;
+        this.datasetService = datasetService;
+        this.projectId = $stateParams.project_id;
+        this.experimentId = $stateParams.experiment_id;
+        this.title = "";
+        this.description = "";
+    }
+
+    create() {
+        if (this.name !== '') {
+            this.datasetService.createDatasetForExperiment(this.projectId, this.experimentId, this.title, this.description)
+                .then(
+                    () => this.$mdDialog.hide(),
+                    () => this.toast.error('Unable to create new dataset')
+                );
+        }
+    }
+
+    cancel() {
+        this.$mdDialog.cancel();
     }
 }
 
