8675b0109db732a436c7b97816c2beeb3efe1c55
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-07-08 16:30:14 -0400

    Start working through loading a single sample.

--- website/src/app/global.components/mc-show-sample.component.js
+++ website/src/app/global.components/mc-show-sample.component.js
@@ -1,7 +1,17 @@
 class MCShowSampleComponentController {
     /*@ngInject*/
-    constructor() {
+    constructor($stateParams, samplesService, toast) {
+        this.projectId = $stateParams.project_id;
+        this.samplesService = samplesService;
+        this.toast = toast;
+    }
 
+    $onInit() {
+        this.samplesService.getProjectSample(this.projectId, this.sample.id)
+            .then(
+                (sample) => console.log('retrieved sample', sample.plain()),
+                () => this.toast.error('Unable to retrieve sample')
+            )
     }
 }
 
