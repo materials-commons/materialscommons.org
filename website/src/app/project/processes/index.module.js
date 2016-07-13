8826b7a933bb4661f03c41f95619c999aedf2f78
V. Glenn Tarcea <gtarcea@umich.edu>
2016-07-13 09:37:17 -0400

    Move and rename mc-project-processes and mc-project-processes-table to mc-experiment-processes and mc-experiment-processes-table. Fix up table show it uses the same set of popups as the other tables are using.

--- website/src/app/project/processes/index.module.js
+++ website/src/app/project/processes/index.module.js
@@ -1,5 +1,3 @@
-import './mc-project-processes.component';
-import './mc-project-processes-table.component';
 import './process/mc-process.component';
 import './process/create/components/periodic-table.directive';
 import './process/create/components/processing-settings.directive';
