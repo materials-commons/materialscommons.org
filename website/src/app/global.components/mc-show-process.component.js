6530cee5ef84cf9060df89ffd44810864a76f4b7
V. Glenn Tarcea <gtarcea@umich.edu>
2016-07-13 13:05:27 -0400

    Start adding in process settings. In prepartion for adding processes directly to a sample.

--- website/src/app/global.components/mc-show-process.component.js
+++ website/src/app/global.components/mc-show-process.component.js
@@ -1,9 +1,11 @@
 class MCShowProcessComponentController {
     /*@ngInject*/
-    constructor($stateParams, projectsService, toast) {
+    constructor($stateParams, projectsService, toast, templates, processEdit) {
         this.projectId = $stateParams.project_id;
         this.projectsService = projectsService;
         this.toast = toast;
+        this.templates = templates;
+        this.processEdit = processEdit;
     }
 
     $onInit() {
@@ -11,6 +13,9 @@ class MCShowProcessComponentController {
             .then(
                 (process) => {
                     this.process = process;
+                    let templateName = process.process_name ? process.process_name : process.template_id.substring(7);
+                    var t = this.templates.getTemplate(templateName);
+                    this.process = this.processEdit.fillProcess(t, process);
                     this.files = process.input_files.concat(process.output_files);
                 },
                 () => this.toast.error('Unable to retrieve process details')
