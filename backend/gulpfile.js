9ee0dca7ae5cad055fc997b0d1485fdcc250ef1c
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-07-21 11:40:33 -0400

    In preparation for adding the mcpub server to the backend, the nodejs server code was moved around to separate out the servers into separate directories and builds.

--- backend/gulpfile.js
+++ backend/gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var clean = require('gulp-clean');
 
-gulp.task('default', ['build-server']);
+var mcapiServerSrc = 'servers/mcapi/**/*.js';
+var mcpubServerSrc = 'servers/mcpub/**/*.js';
+var libSrc = 'servers/lib/**/*.js';
+
+gulp.task('default', ['build-servers']);
 
 gulp.task('clean-sbin', function() {
     return gulp
@@ -10,14 +14,37 @@ gulp.task('clean-sbin', function() {
         .pipe(clean({read: false, force: true}));
 });
 
-var serverSrc = 'server/**/*.js';
-gulp.task('build-server', ['clean-sbin'], function() {
-    return gulp
-        .src(serverSrc)
-        .pipe(babel({presets: ['es2015'], plugins:['transform-runtime']}))
-        .pipe(gulp.dest('sbin'));
+gulp.task('clean-lib', function() {
+    return gulp.src('sbin/lib/')
+        .pipe(clean({read: false, force: true}));
 });
 
-gulp.task('server-watch', ['build-server'], function() {
-    gulp.watch([serverSrc], ['build-server']);
+gulp.task('build-lib', ['clean-lib'], function() {
+    return gulp.src([libSrc])
+        .pipe(babel({presets: ['es2015'], plugins: ['transform-runtime']}))
+        .pipe(gulp.dest('sbin/lib'));
 });
+
+gulp.task('clean-mcapi', function() {
+    return gulp.src('sbin/mcapi/')
+        .pipe(clean({read: false, force: true}));
+});
+
+gulp.task('clean-mcpub', function() {
+    return gulp.src('sbin/mcpub/')
+        .pipe(clean({read: false, force: true}));
+});
+
+gulp.task('build-mcapi-server', ['clean-mcapi', 'build-lib'], function() {
+    return gulp.src([mcapiServerSrc])
+        .pipe(babel({presets: ['es2015'], plugins:['transform-runtime']}))
+        .pipe(gulp.dest('sbin/mcapi'));
+});
+
+gulp.task('build-mcpub-server', ['clean-mcpub', 'clean-lib'], function() {
+    return gulp.src([mcpubServerSrc])
+        .pipe(babel({presets: ['es2015'], plugins: ['transform-runtime']}))
+        .pipe(gulp.dest('sbin/mcpub'));
+});
+
+gulp.task('build-servers', ['build-lib', 'build-mcapi-server', 'build-mcpub-server']);
