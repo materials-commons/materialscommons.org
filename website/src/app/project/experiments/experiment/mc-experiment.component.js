cd5b5f495a44c9c525b9d19a800c9034cd56a2df
V. Glenn Tarcea <gtarcea@umich.edu>
2016-07-22 14:28:48 -0400

    Updates to quickbar - moved so it can be seen in project. Moved link to activate into navbar. Added services and components to separate out the functionality. Changed md-sidenav entries to div entries that didn't need to be md-sidenav entries and were interfering with the quickbar sidenav.

--- website/src/app/project/experiments/experiment/mc-experiment.component.js
+++ website/src/app/project/experiments/experiment/mc-experiment.component.js
@@ -1,46 +1,8 @@
 class MCExperimentComponentController {
     /*@ngInject*/
-    constructor(currentExperiment, $stateParams, projectsService, experimentsService, datasetService, toast, showSampleService) {
+    constructor(currentExperiment) {
         this.currentExperiment = currentExperiment;
         this.experiment = currentExperiment.get();
-        this.showSamplesSidenav = false;
-        this.$stateParams = $stateParams;
-        this.projectId = $stateParams.project_id;
-        this.experimentId = $stateParams.experiment_id;
-        this.projectsService = projectsService;
-        this.experimentsService = experimentsService;
-        this.datasetService = datasetService;
-        this.projectSamples = [];
-        this.experimentSamples = [];
-        this.datasetExamples = [];
-        this.toast = toast;
-        this.showSampleService = showSampleService;
-    }
-
-    openSamplesSidenav() {
-        this.datasetId = this.$stateParams.dataset_id;
-        this.showSamplesSidenav = true;
-        this.projectsService.getProjectSamples(this.projectId)
-            .then(
-                (samples) => this.projectSamples = samples,
-                () => this.toast.error('Unable to retrieve project samples')
-            );
-        this.experimentsService.getSamplesForExperiment(this.projectId, this.experimentId)
-            .then(
-                (samples) => this.experimentSamples = samples,
-                () => this.toast.error('Unable to retrieve experiment samples')
-            );
-        if (this.datasetId) {
-            this.datasetService.getSamplesForDataset(this.projectId, this.experimentId, this.datasetId)
-                .then(
-                    (samples) => this.datasetSamples = samples,
-                    () => this.toast.error('Unable to retrieve dataset samples')
-                )
-        }
-    }
-
-    showSample(sample) {
-        this.showSampleService.showSample(sample);
     }
 }
 
