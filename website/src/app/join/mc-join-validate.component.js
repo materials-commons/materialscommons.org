b894e113339e9aff404b37206423bf0c2971d01b
Terry Weymouth <weymouth@umich.edu>
2016-07-26 10:31:58 +0100

    I think that this is correct as a front end

--- website/src/app/join/mc-join-validate.component.js
+++ website/src/app/join/mc-join-validate.component.js
@@ -1,29 +1,63 @@
 class MCJoinValidateComponentController {
     /*@ngInject*/
-    constructor(accountsService, toast, $stateParams, $state) {
+    constructor(accountsService, $stateParams, $state) {
         var ctrl = this;
         ctrl.uuid = $stateParams.validation_uuid;
         ctrl.accountsService = accountsService;
-        ctrl.toast = toast;
         ctrl.$stateParams = $stateParams;
         ctrl.$state = $state;
         ctrl.message = null;
 
-        ctrl.$onInit = () => {
-            accountsService.getUserDataForVerifyFromUuid(ctrl.uuid).then(
-                (userData) => {
-                    console.log('MCJoinValidateComponentController.$onInit - data' + userData.id);
-                    ctrl.userData = userData;
-                },
-                (error) => {
-                    console.log('MCJoinValidateComponentController.$onInit - error: ' + error.data.error);
-                    this.message = 'getUserDataForVerifyFromUuid - failed'
-                }
-            )
-        };
+//        ctrl.getData = () => {
+//            console.log('MCJoinValidateComponentController.$onInit - call: ' + ctrl.uuid);
+//            accountsService.getUserDataForVerifyFromUuid(ctrl.uuid).then(
+//                (userData) => {
+//                    console.log('MCJoinValidateComponentController.$onInit - data' + userData.id);
+//                    ctrl.userData = userData;
+//                },
+//                (error) => {
+//                    console.log('MCJoinValidateComponentController.$onInit - error: ' + error.data.error);
+//                    this.message = 'getUserDataForVerifyFromUuid - failed'
+//                }
+//            )
+//        };
 
         ctrl.setPassword = (setPasswordForm) => {
             console.log('MCJoinValidateComponentController.setPassword');
+            ctrl.password = this.password1;
+
+            if (setPasswordForm.$invalid) {
+                return;
+            }
+
+            if (!this.allFieldsValid()) {
+                return;
+            }
+
+            this.accountsService.setUserFromRegistrationData(ctrl.uuid,ctrl.password)
+                .then(
+                    () => this.showSuccessMsg = true,
+                    (e) => this.message = `${e.data.error}`
+                );
+        };
+
+        ctrl.allFieldsValid = () => {
+            console.log('MCJoinValidateComponentController.allFieldsValid');
+            console.log('this.password1: ' + this.password1 + ', this.password2:' + this.password2);
+            if (this.password1 === '' || this.password2 === '') {
+                this.message = 'Both fields are required';
+                return false;
+            }
+            if (!(this.password1 === this.password2)) {
+                this.message = "Password entries do not match";
+                return false;
+            }
+            if (this.password.length < 6) {
+                this.message = "Password must be at least 6 character in length";
+                return false;
+            }
+            this.message = null;
+            return true;
         };
     }
 
