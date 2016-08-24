56866d0a1664baf75b7e963ddcd44662565907a1
Terry Weymouth <weymouth@umich.edu>
2016-08-23 21:30:06 -0400

    Well it's a mess, but at least there is something there that runs; see the TESTS=READMS.md file in the directory materialcommons.org/backend/servers/mcapi

---
+++ backend/servers/mcapi/db/model/specs/general2-spec.js
@@ -0,0 +1,12 @@
+// this file contains a few general specs for global testing of testing
+
+var assert = require('assert');
+
+describe('General specs - in servers/mcapi/db/model/specs - ', function(){
+    describe('general spec', function() {
+       it('should suceed', function() {
+           assert.equal(0,0, "zero is zero");
+       });
+    });
+
+});
