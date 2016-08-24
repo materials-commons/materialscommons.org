b1e07d3d1242224784a6eee1becccca9e3becd33
Terry Weymouth <weymouth@umich.edu>
2016-08-23 22:55:12 -0400

    Worse the pulling teeth, but I got another test to work

--- backend/servers/mcapi/db/model/run.js
+++ backend/servers/mcapi/db/model/run.js
@@ -1,4 +1,5 @@
 module.exports = function(rql) {
+    'use strict';
     let runopts = {
         timeFormat: 'raw'
     };
--- backend/servers/mcapi/db/model/specs/projects-spec.js
+++ backend/servers/mcapi/db/model/specs/projects-spec.js
@@ -2,11 +2,34 @@
 
 'use strict';
 
-let assert = require('assert');
+let assert = require('chai').assert;
+let connection = require('rethinkdbdash')({
+    db: process.env.MCDB || 'materialscommons',
+    port: process.env.MCDB_PORT || 30815
+});
+
+let projects = require('../projects')(connection);
 
 describe('Test of db/model projects - ', function() {
-    describe('return all projects:', function() {
-        it ("should not be null" , function() {
+    describe('return all projects: ', function() {
+        it("should not be null", function () {
+            let projectList = projects.all();
+            assert.isNotNull(projectList, "Project List should contain projects");
+        });
+    });
+    describe('find project - ', function() {
+        it ("project named 'Test' should exist" , function() {
+            let projectList = projects.all();
+            projectList.then(function(theList){
+                let name = "not found";
+                assert.isNotNull(theList, "Project list should exist");
+                for (let project of theList) {
+                    if (project.name == 'Test') {
+                        name = project.name;
+                    }
+                }
+                assert.equal(name,"Test","missing 'Test' project");
+            });
         });
     });
 });
