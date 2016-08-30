da2ac5c4fe43c780f99c5182b597556cba1d3ba1
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-08-30 10:37:16 -0400

    Revert "Cleanup tests in backend/resources"
    This reverts commit 650a1f7b3d81f3ef54f4229e934d6d5a9b1e9d1e.

--- backend/servers/mcapi/resources/specs/projects-spec.js
+++ backend/servers/mcapi/resources/specs/projects-spec.js
@@ -2,21 +2,29 @@
 
 'use strict';
 
-let assert = require('chai').assert;
-let connection = require('rethinkdbdash')({
+require('co-mocha');
+
+var assert = require('assert');
+
+var ropts = {
     db: process.env.MCDB || 'materialscommons',
     port: process.env.MCDB_PORT || 30815
-});
+};
+
+//var r = require('rethinkdbdash')(ropts);
+//var projectsModel = require('../../db/model/projects')(r);
 
 var projectsModel = require('../../db/model/projects')(connection);
 var projects = require('../projects')(projectsModel);
+// var projects = require('../resources/projects')(projectsModel);
 
 describe('Testing in backend/servers/mcapi/resources/project.js - ', function (){ // does this also need to be function*
     describe('all projects', function () { // does this also need to be function*
         it('for user', function* () {
-            var user='admin@mc.org';
-            var projectList = yield projects._allProjectsForUser(user);
-            assert.isNotNull(projectList, 'is null');
+//            var user='admin@mc.org';
+//            var projectList = yield projects._allProjectsForUser(user);
+//            assert.isNotNull(projectList, 'is null');
+            assert.equal(1,1);
         });
     });
 
