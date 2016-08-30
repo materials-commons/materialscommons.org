ee68cf1318a4914b48a960f05a28a7a6d0f62a39
Terry Weymouth <weymouth@umich.edu>
2016-08-30 09:10:03 -0400

    Cleanup tests in backend/resources

--- backend/servers/mcapi/resources/specs/projects-spec.js
+++ backend/servers/mcapi/resources/specs/projects-spec.js
@@ -2,27 +2,21 @@
 
 'use strict';
 
-require('co-mocha');
-
-var assert = require('assert');
-
-var ropts = {
+let assert = require('chai').assert;
+let connection = require('rethinkdbdash')({
     db: process.env.MCDB || 'materialscommons',
     port: process.env.MCDB_PORT || 30815
-};
+});
 
-//var r = require('rethinkdbdash')(ropts);
-//var projectsModel = require('../../db/model/projects')(r);
-
-// var projects = require('../resources/projects')(projectsModel);
+var projectsModel = require('../../db/model/projects')(r);
+var projects = require('../resources/projects')(projectsModel);
 
 describe.skip('Testing project.js - ', function (){ // does this also need to be function*
     describe('all projects', function () { // does this also need to be function*
         it('for user', function* () {
-//            var user='admin@mc.org';
-//            var projectList = yield projects._allProjectsForUser(user);
-//            assert.isNotNull(projectList, 'is null');
-            assert.equal(1,1);
+            var user='admin@mc.org';
+            var projectList = yield projects._allProjectsForUser(user);
+            assert.isNotNull(projectList, 'is null');
         });
     });
 
