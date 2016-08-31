4fc48e4726b7b4fe59888ea5daefc4fa219c2688
Terry Weymouth <weymouth@umich.edu>
2016-08-31 11:04:31 -0400

    Added simple test of asyncronous function - but not working correctly

--- backend/servers/mcapi/specs/general-spec.js
+++ backend/servers/mcapi/specs/general-spec.js
@@ -1,13 +1,25 @@
 // this file contains a few general specs for global testing of testing
 'use strict';
 
+require('mocha');
+//require('mocha-co');
+let chai = require('chai');
 let assert = require('chai').assert;
 
-describe('General specs - top level', function(){
-    describe('First general spec', function() {
-       it('should suceed', function() {
-           assert.equal(0,0, "zero is zero");
-       });
-    });
+let promiseify = require('promise.ify');
 
+describe('General specs - top level', function () {
+    describe('Testing general spec', function () {
+        it('should succeed', function () {
+            assert.equal(0, 0, "zero is zero");
+        });
+        it.skip('should fail', function () {
+            assert.equal(0, 1, "zero is zero");
+        });
+    });
+    describe('Testing spec of acynchronous function',function(){
+        it('should succeed', function* (){
+
+        });
+    });
 });
