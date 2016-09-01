653a536ab0328a7ce3ddc9d64be7d3c2928fb281
Terry Weymouth <weymouth@umich.edu>
2016-09-01 13:04:24 -0400

    Using return promise form of test, accepted by mocha - this works

--- backend/servers/mcapi/specs/testing-spec.js
+++ backend/servers/mcapi/specs/testing-spec.js
@@ -7,43 +7,35 @@ let chai = require('chai');
 let assert = require('chai').assert;
 
 describe('Place holder for testing of testing - top level', function () {
-    describe('Testing spec of acynchronous function: ', function () {
-        it('should fail', function() {
+    describe('Testing spec of acynchronous function (using promise): ', function () {
+        it('random test of sucess/fail on promise', function() {
             let worker = function () {
-                //return Math.floor(Math.random() * 2) + 1;
-                return 2;
+                return Math.floor(Math.random() * 2) + 1;
             };
 
-            console.log('1');
+            console.log('start');
             var promise = new Promise(function (fulfill, reject) {
+                
                 var n = worker();
                 if (n === 1) {
                     fulfill(n);
                 } else {
                     reject(n);
                 }
-                console.log('2');
+                console.log('run promise');
             });
 
-            assert.equal(1,1);
+            console.log('promise set');
 
-            promise.then(function (x) {
-                console.log('one');
+            return promise.then(function (x) {
+                console.log('success branch');
                 assert.equal(x,1);
                 console.log('after assert one');
             }, function (x) {
-                assert.equal(2,2);
-                console.log('two');
+                console.log('fail branch');
                 assert.equal(x,2);
                 console.log('after assert two');
-                assert.fail('this failed');
-                console.log('after assert fail');
             });
-
-            assert.equal(3,3);
-            console.log('3');
-            assert.fail("fail at end");
-
         });
     });
 });
