fb65dda05a05b928256e70756505f0039d30b8d0
Terry Weymouth <weymouth@umich.edu>
2016-09-06 08:18:05 -0400

    cleaning up generic test examples

--- backend/servers/mcapi/specs/general-spec.js
+++ backend/servers/mcapi/specs/general-spec.js
@@ -2,24 +2,13 @@
 'use strict';
 
 require('mocha');
-//require('co-mocha');
 let chai = require('chai');
 let assert = require('chai').assert;
-
-let promiseify = require('promise.ify');
 
 describe('General specs - top level', function () {
     describe('Testing general spec', function () {
         it('should succeed', function () {
             assert.equal(0, 0, "zero is zero");
-        });
-        it.skip('should fail', function () {
-            assert.equal(0, 1, "zero is zero");
-        });
-    });
-    describe('Testing spec of acynchronous function',function(){
-        it.skip('should succeed', function* (){
-
         });
     });
 });
