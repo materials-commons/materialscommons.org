3e01dee9e2f9af67d288a7805f77c1de8b3a088f
Terry Weymouth <weymouth@umich.edu>
2016-09-08 16:51:08 -0400

    Another pass at server side for building zip files

--- backend/servers/lib/zipFileUtils.js
+++ backend/servers/lib/zipFileUtils.js
@@ -1,15 +1,21 @@
 const sanitize = require("sanitize-filename");
 
-module.exports.baseDir = function (dataset) {
+module.exports.zipDirPath = function (dataset) {
     var base = getBase();
     var zipDir = base + "/zipfiles/" + dataset.id + "/";
     return zipDir;
 };
 
+module.exports.fullPathAndFilename = function(dataset) {
+    var zipFilename = module.exports.zipFilename(dataset);
+    var zipDir = module.exports.zipDirPath(dataset);
+    return zipDir + zipFilename;
+}
+
 module.exports.zipFilename = function(dataset) {
     var title = dataset.title;
     var filename = sanitize(title);
-    return filename;
+    return filename + ".zip";
 }
 
 module.exports.zipEntry = function (datafile) {   // sets fileName and sourcePath
