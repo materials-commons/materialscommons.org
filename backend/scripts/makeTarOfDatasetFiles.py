5a1b1e1ce0bfb7f4443a7c58f0faff05db040a22
Terry Weymouth <weymouth@umich.edu>
2016-09-16 07:49:03 -0400

    Script for generating tar of files for dataset, now generating all paths of data files from database information

--- backend/scripts/makeTarOfDatasetFiles.py
+++ backend/scripts/makeTarOfDatasetFiles.py
@@ -6,52 +6,60 @@ import sys
 import os
 
 def usesidRemap(x):
-    r = e['datafile_id']
-    if (e['usesid']):
-        r = e['usesid']
-    print r
+    r = x['datafile_id']
+    if (x['usesid']):
+        r = x['usesid']
     return r
 
-if __name__ == "__main__":
+def makePath(x):
+    s = x.split('-')[1]
+    return s[:2] + "/" + s[2:] + "/" + x
+
+def getOptions():
     parser = OptionParser()
     parser.add_option("-i", "--id", dest="id",
                       help="dataset id", type="string")
-    parser.add_option("-P", "--port", dest="port", type="int",
+    parser.add_option("-P", "--port",dest="port", type="int",
                       help="rethinkdb port", default=30815)
+    parser.add_option("-B", "--base",dest="base", type='string');
 
     (options, args) = parser.parse_args()
 
-    (options, args) = parser.parse_args()
     if options.id is None:
         print "You must specify a dataset id"
         sys.exit(1)
 
-    conn = r.connect('localhost', options.port, db='materialscommons')
+    if options.base is None:
+        print "You must specify a base directory"
+        sys.exit(1)
 
-    dataset = r.table('datasets').get(options.id).run(conn)
+    return options
+
+
+if __name__ == "__main__":
+
+    options = getOptions()
+    dataset_id = options.id
+    base_dir = options.base
+    db_port = options.port
+
+
+    conn = r.connect('localhost', db_port, db='materialscommons')
+
+    dataset = r.table('datasets').get(dataset_id).run(conn)
 
     if dataset is None:
-        print "No such dataset: " + options.id
+        print "No such dataset: " + dataset_id
         sys.exit(1)
 
     print "getting all files for the dataset with the name '" + dataset["title"] + "'"
 
-    entries = r.table('dataset2datafile')\
-        .eq_join('datafile_id',r.table("datafiles")).zip()\
-        .filter({"dataset_id":options.id})\
-        .pluck("usesid","datafile_id")\
-        .run(conn)
+    entries = list(r.table('dataset2datafile')\
+        .get_all(dataset_id, index='dataset_id')\
+        .eq_join('datafile_id',r.table('datafiles'))
+        .zip().pluck('usesid', 'datafile_id').run(conn))
 
-    for e in entries:
-        print e['datafile_id']
-        if (e['usesid']):
-            print "  ==>>" + e['usesid']
-
-    print "-----------------------"
-
-    mapping = map(None,entries)
-
-    print "-----------------------"
+    mapping = map(makePath,map(usesidRemap,entries))
 
     for x in mapping:
-        print x
+        print base_dir + "/" + x
