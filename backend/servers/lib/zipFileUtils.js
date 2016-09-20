7f9c3bdf1506b85e4f651bddb95daca4d3b5ee73
Terry Weymouth <weymouth@umich.edu>
2016-09-20 09:50:39 -0400

    Corrected Zipfile Builder to use usesid. There was bug: when non-empty, usesid was not being used to determine the file path for source files in building the zip file for the download of all files. This bug has been corrected.

--- backend/servers/lib/zipFileUtils.js
+++ backend/servers/lib/zipFileUtils.js
@@ -22,7 +22,10 @@ module.exports.zipEntry = function(datafile) {   // sets fileName and sourcePath
     var base = getBase();
     var writeName = sanitize(datafile.name);
     var readName = datafile.id;
-    var parts = datafile.id.split("-");
+    if (datafile.usesid) {
+        readName = datafile.usesid;
+    }
+    var parts = readName.split("-");
     var part = parts[1];
     var partA = part.substring(0, 2);
     var partB = part.substring(2);
@@ -33,7 +36,7 @@ module.exports.zipEntry = function(datafile) {   // sets fileName and sourcePath
 let getBase = function() {
     var base = process.env.MCDIR;
     if (!base) {
-        throw new Error({message: "Can not create zipfile for dataset: please relay to admin - 'MCDIR is not set in environment' "});
+        throw new Error({message: "Can not create zipfile for dataset: please show this message to a site adminstrator - 'MCDIR is not set in environment' "});
     }
     return base;
 };
