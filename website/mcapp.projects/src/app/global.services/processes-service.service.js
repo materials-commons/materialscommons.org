6e838576d0f831dff28285b91d9ead771598de9b
Terry Weymouth <weymouth@umich.edu>
2016-09-29 16:51:33 -0400

    An initial pass at deleteProcess; does no actual deleting, but gets all the 'pieces' involved

--- website/mcapp.projects/src/app/global.services/processes-service.service.js
+++ website/mcapp.projects/src/app/global.services/processes-service.service.js
@@ -6,6 +6,13 @@ class ProcessesService {
     updateProcess(projectId, processId, updateArgs) {
         return this.projectsAPI(projectId).one('processes', processId).customPUT(updateArgs);
     }
+
+    deleteProcess(projectId,processId) {
+        console.log("Deleting process: " + projectId,processId);
+        var ret =  this.projectsAPI(projectId).one('processes', processId).remove();
+        console.log(ret);
+        return ret;
+    }
 }
 
 angular.module('materialscommons').service('processesService', ProcessesService);
