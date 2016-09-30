47126773cf6c66d1188f57c97c6290efd31d6738
Terry Weymouth <weymouth@umich.edu>
2016-09-30 11:48:49 -0400

    Expaned control flow to include dialog for user to confirm delete; only outline of control at this point - not checking for samples and measurements yet

--- backend/servers/mcapi/resources/processes.js
+++ backend/servers/mcapi/resources/processes.js
@@ -84,9 +84,34 @@ module.exports = function(processes, samples, experiments, schema) {
 
     function* deleteProcess(next) {
         console.log("Server side delete: ",this.params.project_id,this.params.process_id);
-        // validate
-        yield processes.deleteProcess(this.params.project_id,this.params.process_id);
+        let errors = yield validateDeleteProcess(this.params.project_id,this.params.process_id);
+        if (errors != null) {
+            this.status = status.BAD_REQUEST;
+            this.body = errors;
+        } else {
+            let rv = yield processes.deleteProcess(this.params.project_id,this.params.process_id);
+            if (rv.error) {
+                this.status = status.BAD_REQUEST;
+                this.body = rv;
+            } else {
+                this.body = rv.val;
+            }
+        }
         yield next;
+    }
+
+    function* validateDeleteProcess(project_id,process_id) {
+        console.log("Validate args for delete: ",project_id,process_id);
+        let datasets = yield processes.datasetsForProcess(process_id);
+        console.log('dataset2process', datasets.length);
+        if (datasets.length > 0) {
+            let datasetNames = datasets.map(ds => ds.title);
+            let message = "Process can not be deleted when it is in a dataset; remove from: " + datasetNames.join(", ");
+            let error = {error: message};
+            console.log('dataset2process',error);
+            return error;
+        }
+        return null;
     }
 
     function* updateProcess(next) {
