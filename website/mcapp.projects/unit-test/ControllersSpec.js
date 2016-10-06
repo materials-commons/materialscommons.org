80c8f36515d5eaae08e7ed1f7117e3c804805411
Terry Weymouth <weymouth@umich.edu>
2016-10-06 06:49:12 -0400

    playing around with karma, mocha, jasmin, other testing stuff

--- website/mcapp.projects/unit-test/ControllersSpec.js
+++ website/mcapp.projects/unit-test/ControllersSpec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 describe('HelpController', function(){
-    it('should create list with 3 names', inject(function($controller){
-        var scope = {},
-            ctrl = $controller('HelpController', {$scope: scope});
-        expect(scope.list.length).toBe(3);
-    }));
+//    it('should create list with 3 names', inject(function($controller){
+//        var scope = {},
+//            ctrl = $controller('HelpController', {$scope: scope});
+//        expect(scope.list.length).toBe(3);
+//    }));
 });
 
 
