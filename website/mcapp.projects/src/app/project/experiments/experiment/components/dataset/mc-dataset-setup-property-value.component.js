610e998f748034677100d4558641d3584d5224c3
V. Glenn Tarcea <gtarcea@umich.edu>
2016-10-12 13:47:33 -0400

    Show dataset as an outline.

---
+++ website/mcapp.projects/src/app/project/experiments/experiment/components/dataset/mc-dataset-setup-property-value.component.js
@@ -0,0 +1,6 @@
+angular.module('materialscommons').component('mcDatasetSetupPropertyValue', {
+    templateUrl: 'app/project/experiments/experiment/components/dataset/mc-dataset-setup-property-value.html',
+    bindings: {
+        property: '<'
+    }
+});
