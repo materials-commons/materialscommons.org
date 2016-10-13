af69f540d68b67d0076729744be607375b33b5de
V. Glenn Tarcea <gtarcea@umich.edu>
2016-10-13 12:52:46 -0400

    Add processGraph service to construct the graph for a set of processes. Change mc-processes-workflow-graph component to use the processGraph service.

---
+++ website/mcapp.projects/src/app/global.services/process-graph.service.js
@@ -0,0 +1,103 @@
+class ProcessGraphService {
+    /*@ngInject*/
+    constructor() {}
+
+    build(processes) {
+        let sample2InputProcesses = {};
+        let sample2OutputProcesses = {};
+
+        processes.forEach(p => {
+            p.input_samples.forEach(s => {
+                let id = `${s.id}/${s.property_set_id}`;
+                if (!(id in sample2InputProcesses)) {
+                    sample2InputProcesses[id] = [];
+                }
+                sample2InputProcesses[id].push(p);
+            });
+
+            p.output_samples.forEach(s => {
+                let id = `${s.id}/${s.property_set_id}`;
+                if (!(id in sample2OutputProcesses)) {
+                    sample2OutputProcesses[id] = [];
+                }
+                sample2OutputProcesses[id].push(p);
+            })
+        });
+
+        // Draw all processes
+        let elements = processes.map(p => {
+            return {
+                data: {
+                    id: p.id,
+                    name: p.name,
+                    color: ProcessGraphService.processColor(p),
+                    shape: ProcessGraphService.processShape(p)
+                }
+            };
+        });
+        processes.filter(p => p.does_transform).forEach(p => {
+            p.output_samples.forEach(s => {
+                let id = `${s.id}/${s.property_set_id}`;
+                let processes = sample2InputProcesses[id];
+                if (processes && processes.length) {
+                    processes.forEach(proc => {
+                        elements.push({
+                            data: {
+                                id: `${proc.id}_${p.id}`,
+                                source: p.id,
+                                target: proc.id,
+                                name: s.name
+                            }
+                        });
+                    });
+                }
+            });
+        });
+
+        let sampleName2Sample = {};
+        processes.forEach(p => {
+            p.input_samples.forEach(s => {
+                sampleName2Sample[s.name] = s;
+            });
+            p.output_samples.forEach(s => {
+                sampleName2Sample[s.name] = s;
+            });
+        });
+
+        let samples = _.values(sampleName2Sample);
+
+        return {elements, samples};
+    }
+
+    static processColor(p) {
+        switch (p.process_type) {
+            case "transform":
+                return p.destructive ? "#d32f2f" : "#fbc02d";
+            case "measurement":
+                return p.destructive ? "#d32f2f" : "#cfd8dc";
+            case "analysis":
+                return "#d1c4e9";
+            case "create":
+                return "#ffecb3";
+            case "import":
+                return "#b2dfdb";
+        }
+    }
+
+    static processShape(p) {
+        switch (p.process_type) {
+            case "transform":
+                return "triangle";
+            case "measurement":
+                return "ellipse";
+            case "analysis":
+                return "roundrectangle";
+            case "create":
+                return "diamond";
+            case "import":
+                return "diamond";
+        }
+    }
+}
+
+angular.module('materialscommons').service('processGraph', ProcessGraphService);
