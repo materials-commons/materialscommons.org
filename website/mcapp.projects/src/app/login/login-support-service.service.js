fe38c01ef7662e36725435024efc1dbbd873fdca
Terry Weymouth <weymouth@umich.edu>
2016-10-13 05:07:15 -0400

    Refactoring reset of lost password to use user record - phase one delete reset flag from user on sucessful login

--- website/mcapp.projects/src/app/login/login-support-service.service.js
+++ website/mcapp.projects/src/app/login/login-support-service.service.js
@@ -1,3 +1,15 @@
-/**
- * Created by weymouth on 10/12/16.
- */
+class LoginSupportService {
+    /*@ngInject*/
+    constructor(apiService) {
+        this.apiService = apiService;
+    }
+
+    clearPasswordResetRequest(user) {
+        console.log("client - login - clearPasswordResetRequest: " + user.id);
+        return this.apiService('accounts').one('clear-reset-password').customPOST({
+            id: user.id
+        });
+    }
+}
+
+angular.module('materialscommons').service('loginSupportService', LoginSupportService);
