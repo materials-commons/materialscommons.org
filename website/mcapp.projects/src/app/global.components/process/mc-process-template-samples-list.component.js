77ddf2e8a47e84ee7436be5753ef09465ad25776
V. Glenn Tarcea <gtarcea@umich.edu>
2016-10-18 13:42:43 -0400

    Move displaying files and samples for a process into separate directives. Cleanup how they are laid out. This removes duplicated code from the 3 directives for displaying processes.

---
+++ website/mcapp.projects/src/app/global.components/process/mc-process-template-samples-list.component.js
@@ -0,0 +1,7 @@
+angular.module('materialscommons').component('mcProcessTemplateSamplesList', {
+    templateUrl: 'app/global.components/process/mc-process-template-samples-list.html',
+    bindings: {
+        inputSamples: '<',
+        outputSamples: '<'
+    }
+});
