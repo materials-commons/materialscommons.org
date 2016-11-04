e9528d629033f51c4992b4fcc7c15874dc502994
V. Glenn Tarcea <gtarcea@umich.edu>
2016-11-04 16:20:41 -0400

    Add additional definitions: Dataset, Setup, ExperimentNote and ExperimentTask. Tighten up some type definitions.

--- backend/servers/mcapi/model/index.js
+++ backend/servers/mcapi/model/index.js
@@ -10,6 +10,15 @@ const Project = db.createModel("projects", {
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
+/*
+ * A Process also has:
+ *    input_samples
+ *    output_samples
+ *    input_files
+ *    output_files
+ *
+ *    TODO: Need to add setups
+ */
 const Process = db.createModel("processes", {
     id: db.type.string(),
     _type: db.type.string().default("process"),
@@ -25,6 +34,21 @@ const Process = db.createModel("processes", {
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
+const Setup = db.createModel("setups", {
+    id: db.type.string(),
+    _type: db.type.string().default("settings"),
+    name: db.type.string(),
+    attribute: db.type.string(),
+    birthtime: db.type.date().default(r.now())
+});
+
+/*
+ * A Sample may also have:
+ *    property_set_id
+ *    files
+ *    processes
+ *    measurements
+ */
 const Sample = db.createModel("samples", {
     id: db.type.string(),
     _type: db.type.string().default("sample"),
@@ -43,12 +67,45 @@ const Experiment = db.createModel("experiments", {
     owner: db.type.string(),
     note: db.type.string(),
     status: db.type.string().default("active"),
-    citations: [db.type.string()],
-    collaborators: [db.type.string()],
-    funding: [db.type.string()],
-    goals: [db.type.string()],
-    papers: [db.type.string()],
-    publications: [db.type.string()],
+    citations: db.type.array().schema(db.type.string()),
+    collaborators: db.type.array().schema(db.type.string()),
+    funding: db.type.array().schema(db.type.string()),
+    goals: db.type.array().schema(db.type.string()),
+    papers: db.type.array().schema(db.type.string()),
+    publications: db.type.array().schema(db.type.string()),
+    birthtime: db.type.date.default(r.now()),
+    mtime: db.type.date().default(function() { return this.birthtime; })
+});
+
+const Dataset = db.createModel("datasets", {
+    id: db.type.string(),
+    _type: db.type.string().default("dataset"),
+    title: db.type.string(),
+    description: b.type.string(),
+    owner: db.type.string(),
+    doi: db.type.string(),
+    institution: db.type.string(),
+    authors: db.type.array().schema(db.type.object().schema({
+        firstname: db.type.string(),
+        lastname: db.type.string(),
+        affiliation: db.type.string()
+    }).removeExtra()),
+    papers: db.type.array().schema(db.type.object().schema({
+        abstract: db.type.string(),
+        authors: db.type.string(),
+        doi: db.type.string(),
+        link: db.type.string(),
+        title: db.type.string()
+    }).removeExtra()),
+    license: db.type.object().schema({
+        link: db.type.string(),
+        name: db.type.string()
+    }).removeExtra(),
+    funding: db.type.string(),
+    published: db.type.boolean().default(false),
+    published_date: db.type.date(),
+    embargo_date: db.type.date(),
+    keywords: db.type.array().schema(db.type.string()),
     birthtime: db.type.date.default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
@@ -64,10 +121,9 @@ const Datafile = db.createModel("datafiles", {
     checksum: db.type.string(),
     uploaded: db.type.integer(),
     usesid: db.type.string(),
-    mediatype: {
-        description: db.type.string(),
-        mime: db.type.string()
-    },
+    mediatype: db.type.object().schema({
+        description: db.type.string(), mime: db.type.string()
+    }).removeExtra(),
     birthtime: db.type.date.default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
@@ -83,11 +139,46 @@ const Datadir = db.createModel("datadirs", {
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
+const ExperimentNote = db.createModel("experimentnotes", {
+    id: db.type.string(),
+    _type: db.type.string().default("note"),
+    owner: db.type.string(),
+    note: db.type.string(),
+    name: db.type.string(),
+    birthtime: db.type.date.default(r.now()),
+    mtime: db.type.date().default(function() { return this.birthtime; })
+});
+
+const ExperimentTask = db.createModel("experimenttasks", {
+    id: db.type.string(),
+    _type: db.type.string().default("experiment_task"),
+    name: db.type.string(),
+    note: db.type.string(),
+    owner: db.type.string(),
+    process_id: db.type.string(),
+    parent_id: db.type.string(),
+    template_id: db.type.string(),
+    template_name: db.type.string(),
+    index: db.type.integer(),
+    flags: db.type.object().schema({
+        done: db.type.boolean(),
+        flagged: db.type.boolean(),
+        onhold: db.type.boolean(),
+        starred: db.type.boolean()
+    }).removeExtra(),
+    birthtime: db.type.date.default(r.now()),
+    mtime: db.type.date().default(function() { return this.birthtime; })
+});
+
 module.exports = {
     Project,
     Process,
+    Setup,
     Sample,
     Experiment,
+    Dataset,
     Datafile,
-    Datadir
+    Datadir,
+    ExperimentNote,
+    ExperimentTask
 };
