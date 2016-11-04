ebbb722f6fb7f33dbfc77005483fe58c1a42eadf
V. Glenn Tarcea <gtarcea@umich.edu>
2016-11-04 17:04:10 -0400

    Cleanup python code.

--- backend/mcapi/user/account.py
+++ backend/mcapi/user/account.py
@@ -55,6 +55,7 @@ def complete_registration(validation_id):
     del user['password']
     return json.dumps(j)
 
+
 @app.route('/user/rvalidate/<validation_id>/finish', methods=['POST'])
 def finish_reset_password(validation_id):
     cursor = r.table("users").get_all(validation_id,index='validate_uuid').run(g.conn)
@@ -76,6 +77,7 @@ def finish_reset_password(validation_id):
     del user['reset_password']
     return jsonify({'data': user})
 
+
 @app.route('/user/<user>/password', methods=['PUT'])
 @apikey
 @crossdomain(origin='*')
@@ -85,6 +87,7 @@ def change_password(user):
     hash = make_password_hash(newpw)
     rv = r.table('users').get(user).update({'password': hash}).run(g.conn)
     return jsonify(rv)
+
 
 @app.route('/user/<user>/validate/<validation_id>/password', methods=['POST'])
 def reset_password_validate(user,validation_id):
@@ -104,6 +107,7 @@ def reset_password_validate(user,validation_id):
     rv = r.table('users').get(user_id).update({'password': hash}).run(g.conn)
     return jsonify(rv)
 
+
 @app.route('/user/<user>/apikey/reset', methods=['PUT'])
 @apikey
 @crossdomain(origin='*')
@@ -113,93 +117,3 @@ def reset_apikey(user):
     access.remove_user(user)
     return jsonify({'apikey': new_apikey})
 
-
-@app.route('/user/<user>/usergroups', methods=['GET'])
-@apikey
-@jsonp
-def list_usergroups_for_user(user):
-    res = list(r.table('usergroups').filter(r.row['users'].contains(user))
-               .run(g.conn, time_format='raw'))
-    return json.dumps(res)
-
-
-@app.route('/users/<user>', methods=['PUT'])
-@apikey
-@crossdomain(origin='*')
-def update(user):
-    j = request.get_json()
-    fullname = dmutil.get_optional('fullname', j)
-    if fullname:
-        rv = r.table('users').get(user).update({'fullname': fullname})\
-                                       .run(g.conn)
-        return jsonify(rv)
-    return error.bad_request("Expected field not specified")
-
-
-@app.route('/user/<user>/preferred_templates', methods=['GET'])
-@apikey
-@jsonp
-def preferred_templates(user):
-    u_obj = r.table('users').get(user).pluck('preferences').run(g.conn)
-    return args.json_as_format_arg(u_obj)
-
-
-@app.route('/user/<user>/templates', methods=['PUT'])
-@apikey
-@crossdomain(origin='*')
-def update_preferred_templates(user):
-    j = request.get_json()
-    list = dmutil.get_required('templates', j)
-    rv = r.table('users').get(user).update(
-        {'preferences': {'templates': list}}).run(g.conn)
-    return jsonify(rv)
-
-
-@app.route('/user/<user>/tags', methods=['GET'])
-@apikey
-@jsonp
-def get_user_tags(user):
-    user_obj = r.table('users').get(user).pluck('preferences').run(g.conn)
-    return args.json_as_format_arg(user_obj)
-
-
-@app.route('/user/<user>/tags', methods=['PUT'])
-@apikey
-@crossdomain(origin='*')
-def update_tags(user):
-    user_obj = r.table('users').get(user).run(g.conn)
-    existing_tags = user_obj['preferences']['tags']
-    j = request.get_json()
-    tag = dict()
-    tag['name'] = dmutil.get_required('name', j)
-    tag['color'] = dmutil.get_required('color', j)
-    tag['icon'] = dmutil.get_required('icon', j)
-    tag['description'] = dmutil.get_optional('description', j)
-    existing_tags.append(tag)
-    rr = r.table('users').get(user).update(
-        {'preferences': {'tags': existing_tags}}).run(g.conn)
-    return jsonify(rr)
-
-
-@app.route('/user/ui', methods=['GET'])
-@apikey
-@jsonp
-def get_user_ui_setup():
-    user = access.get_user()
-    ui = r.table('ui').get(user).run(g.conn, time_format='raw')
-    if ui is None:
-        return error.not_found("No state for user")
-    return jsonify(ui)
-
-
-@app.route('/user/ui', methods=['POST'])
-@apikey
-def update_user_ui_setup():
-    user = access.get_user()
-    j = request.get_json()
-    exists = r.table('ui').get(user).run(g.conn)
-    if exists is None:
-        r.table('ui').insert(j).run(g.conn)
-    else:
-        r.table('ui').get(user).replace(j).run(g.conn)
-    return jsonify({'id': user})
