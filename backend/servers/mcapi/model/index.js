433cc1b8cf75367d1ab10f95c8d94fc95e478a4d
Glenn Tarcea <glenn.tarcea@gmail.com>
2016-11-05 18:33:56 -0400

    Update to get type definitions to build. Our _type field will need to be changed. I've renamed it to _mctype.

--- backend/servers/mcapi/model/index.js
+++ backend/servers/mcapi/model/index.js
@@ -1,8 +1,9 @@
 const db = require('./db');
+const r = db.r;
 
 const Project = db.createModel("projects", {
     id: db.type.string(),
-    _type: db.type.string().default("project"),
+    _mctype: db.type.string().default("project"),
     name: db.type.string(),
     description: db.type.string(),
     owner: db.type.string(),
@@ -21,7 +22,7 @@ const Project = db.createModel("projects", {
  */
 const Process = db.createModel("processes", {
     id: db.type.string(),
-    _type: db.type.string().default("process"),
+    _mctype: db.type.string().default("process"),
     name: db.type.string(),
     description: db.type.string(),
     owner: db.type.string(),
@@ -36,7 +37,7 @@ const Process = db.createModel("processes", {
 
 const Setup = db.createModel("setups", {
     id: db.type.string(),
-    _type: db.type.string().default("settings"),
+    _mctype: db.type.string().default("settings"),
     name: db.type.string(),
     attribute: db.type.string(),
     birthtime: db.type.date().default(r.now())
@@ -51,7 +52,7 @@ const Setup = db.createModel("setups", {
  */
 const Sample = db.createModel("samples", {
     id: db.type.string(),
-    _type: db.type.string().default("sample"),
+    _mctype: db.type.string().default("sample"),
     name: db.type.string(),
     description: db.type.string(),
     owner: db.type.string(),
@@ -61,7 +62,7 @@ const Sample = db.createModel("samples", {
 
 const Experiment = db.createModel("experiments", {
     id: db.type.string(),
-    _type: db.type.string().default("experiment"),
+    _mctype: db.type.string().default("experiment"),
     name: db.type.string(),
     description: db.type.string(),
     owner: db.type.string(),
@@ -73,15 +74,15 @@ const Experiment = db.createModel("experiments", {
     goals: db.type.array().schema(db.type.string()),
     papers: db.type.array().schema(db.type.string()),
     publications: db.type.array().schema(db.type.string()),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
 const Dataset = db.createModel("datasets", {
     id: db.type.string(),
-    _type: db.type.string().default("dataset"),
+    _mctype: db.type.string().default("dataset"),
     title: db.type.string(),
-    description: b.type.string(),
+    description: db.type.string(),
     owner: db.type.string(),
     doi: db.type.string(),
     institution: db.type.string(),
@@ -106,52 +107,52 @@ const Dataset = db.createModel("datasets", {
     published_date: db.type.date(),
     embargo_date: db.type.date(),
     keywords: db.type.array().schema(db.type.string()),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
 const Datafile = db.createModel("datafiles", {
     id: db.type.string(),
-    _type: db.type.string().default("datafile"),
+    _mctype: db.type.string().default("datafile"),
     name: db.type.string(),
     description: db.type.string(),
     owner: db.type.string(),
     parent: db.type.string(),
-    size: db.type.integer(),
+    size: db.type.number().integer(),
     checksum: db.type.string(),
-    uploaded: db.type.integer(),
+    uploaded: db.type.number().integer(),
     usesid: db.type.string(),
     mediatype: db.type.object().schema({
         description: db.type.string(), mime: db.type.string()
     }).removeExtra(),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
 const Datadir = db.createModel("datadirs", {
     id: db.type.string(),
-    _type: db.type.string().default("datadir"),
+    _mctype: db.type.string().default("datadir"),
     name: db.type.string(),
     owner: db.type.string(),
     parent: db.type.string(),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     atime: db.type.date().default(function() { return this.birthtime; }),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
 const ExperimentNote = db.createModel("experimentnotes", {
     id: db.type.string(),
-    _type: db.type.string().default("note"),
+    _mctype: db.type.string().default("note"),
     owner: db.type.string(),
     note: db.type.string(),
     name: db.type.string(),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
 const ExperimentTask = db.createModel("experimenttasks", {
     id: db.type.string(),
-    _type: db.type.string().default("experiment_task"),
+    _mctype: db.type.string().default("experiment_task"),
     name: db.type.string(),
     note: db.type.string(),
     owner: db.type.string(),
@@ -159,14 +160,14 @@ const ExperimentTask = db.createModel("experimenttasks", {
     parent_id: db.type.string(),
     template_id: db.type.string(),
     template_name: db.type.string(),
-    index: db.type.integer(),
+    index: db.type.number().integer(),
     flags: db.type.object().schema({
         done: db.type.boolean(),
         flagged: db.type.boolean(),
         onhold: db.type.boolean(),
         starred: db.type.boolean()
     }).removeExtra(),
-    birthtime: db.type.date.default(r.now()),
+    birthtime: db.type.date().default(r.now()),
     mtime: db.type.date().default(function() { return this.birthtime; })
 });
 
