16aabef09df2d207fa3db2064ce4a9b4a9571222
Terry Weymouth <weymouth@umich.edu>
2016-11-29 12:26:44 -0500

    Initital, failing, attempt.

--- website/mcapp.projects/src/app/global.components/index.module.js
+++ website/mcapp.projects/src/app/global.components/index.module.js
@@ -15,3 +15,4 @@ import './process/mc-process-template.component';
 import './process/process-settings2.directive';
 import './process/mc-process-template-files-list.component';
 import './process/mc-process-template-samples-list.component';
+import './process/process-settings2-number.directive'
