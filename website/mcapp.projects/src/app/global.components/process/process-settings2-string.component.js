8f1c82c6c4bf43b5a196392ec954a51c04a53e47
Terry Weymouth <weymouth@umich.edu>
2016-11-30 15:47:41 -0500

    Added new feature to ptoject - update with string from attribute in process from template - first pass

---
+++ website/mcapp.projects/src/app/global.components/process/process-settings2-string.component.js
@@ -0,0 +1,46 @@
+class ProcessSettings2NumberComponentController {
+    /*@ngInject*/
+    constructor(experimentsService, toast, $stateParams) {
+        this.experimentsService = experimentsService;
+        this.toast = toast;
+        this.$stateParams = $stateParams;
+        this.projectId = $stateParams.project_id;
+        this.experimentId = $stateParams.experiment_id;
+    }
+
+    updateSettingProperty(property) {
+
+        if (!property.value) {
+            console.log("No value -> ", property);
+            return;
+        }
+
+        if (property.otype != "string") {
+            console.log("Not a string -> ", property);
+            return;
+        }
+
+        property.setup_attribute = this.attribute;
+        let propertyArgs = {
+            template_id: this.templateId,
+            properties: [property]
+        };
+
+        this.experimentsService.updateProcess(this.projectId, this.experimentId, this.processId, propertyArgs)
+            .then(
+                () => null,
+                () => this.toast.error('Unable to update property')
+            );
+    }
+}
+
+angular.module('materialscommons').component('processSettings2String', {
+    templateUrl: 'app/global.components/process/process-settings2-string.html',
+    controller: ProcessSettings2StringComponentController,
+    bindings: {
+        setting: '<',
+        templateId: '<',
+        attribute: '<',
+        processId: '<'
+    }
+});
