eb89db6bc5055d581440881d63ea548349ab3bf3
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-12-01 15:14:25 -0500

    Add call to get process templates.

--- backend/servers/mcapi/resources/index.js
+++ backend/servers/mcapi/resources/index.js
@@ -1,11 +1,17 @@
 const router = require('koa-router')();
 const projects = require('./projects');
 const users = require('./users');
+const templates = require('./templates');
 
 function createResources() {
     let projectsResource = projects.createResource();
     router.use('/projects', projectsResource.routes(), projectsResource.allowedMethods());
+
+    let templatesResource = templates.createResource();
+    router.use('/templates', templatesResource.routes(), templatesResource.allowedMethods());
+
     users.createResource(router);
+
     return router;
 }
 
