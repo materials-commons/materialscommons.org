1bd5d8312840f211866e533ad753420044d91060
Terry Weymouth <weymouth@umich.edu>
2016-12-02 10:08:00 -0500

    refactor DatePicker out of main settings2 (directive) into the Date component

--- website/mcapp.projects/src/app/global.components/process/process-settings2.directive.js
+++ website/mcapp.projects/src/app/global.components/process/process-settings2.directive.js
@@ -17,46 +17,5 @@ function processSettings2Directive() {
 
 /*@ngInject*/
 function ProcessSettings2DirectiveController(experimentsService, toast, $stateParams) {
-    var ctrl = this;
 
-    ctrl.datePickerOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-    };
-    ctrl.openDatePicker = openDatePicker;
-
-    ctrl.updateSettingProperty = (property) => {
-        if (!property.value) {
-            return;
-        }
-
-        if (property.otype === "date") {
-            console.dir(property);
-            return;
-        }
-
-        if (property.units && property.units.length && !property.unit) {
-            return;
-        }
-
-        property.setup_attribute = ctrl.attribute;
-        let propertyArgs = {
-            template_id: ctrl.templateId,
-            properties: [property]
-        };
-
-        experimentsService.updateProcess($stateParams.project_id, $stateParams.experiment_id, ctrl.processId, propertyArgs)
-            .then(
-                () => null,
-                () => toast.error('Unable to update property')
-            );
-    };
-
-    ///////////////////////////////////////
-
-    function openDatePicker($event, prop) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        prop.opened = true;
-    }
 }
