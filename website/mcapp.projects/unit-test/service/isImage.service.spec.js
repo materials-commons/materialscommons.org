efb1f19a3f73f3bc74417c06429ec09e760ecb29
Terry Weymouth <weymouth@umich.edu>
2016-12-08 09:31:59 -0500

    Completed test of idImage service

--- website/mcapp.projects/unit-test/service/isImage.service.spec.js
+++ website/mcapp.projects/unit-test/service/isImage.service.spec.js
@@ -15,5 +15,27 @@ describe('MC Show Process Component Controller', function() {
         expect(service).toBeDefined();
     });
 
+    it('should show image mime values as true', function(){
+        var imageMimeValues = [
+            'image/gif', 'image/jpeg', 'image/png',
+            'image/tiff', 'image/x-ms-bmp', 'image/bmp'
+        ];
+        imageMimeValues.forEach( (mime) => {
+            expect(service(mime)).toBe(true);
+        });
+    });
+
+    it('should show non-image mime values as false', function(){
+        var imageMimeValues = [
+            'text/plain', 'text/html', 'text/css', 'text/javascript',
+            'application/octet-stream', 'application/pkcs12',
+            'application/vnd.mspowerpoint', 'application/xhtml+xml',
+            'application/xml',  'application/pdf'
+        ];
+        imageMimeValues.forEach( (mime) => {
+            expect(service(mime)).toBe(false);
+        });
+    });
+
 });
 
