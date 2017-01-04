e4a0b2221be90c4241d7a370fb103c7b9eeeffd8
Terry Weymouth <weymouth@umich.edu>
2017-01-04 15:25:41 -0500

    Fixed example test

--- backend/tests/mcapi/specs/reset-lost-password-spec.js
+++ backend/tests/mcapi/specs/reset-lost-password-spec.js
@@ -10,15 +10,15 @@ const r = require('rethinkdbdash')({
     port: process.env.MCDB_PORT || 30815
 });
 
-const model = require('../../../servers/mcapi/db/model')(r);
-const schema = require('../../../servers/mcapi/schema')(model);
-const experiments = require('../../../servers/mcapi/resources/experiments')(model.experiments, model.samples, schema);
-const resourcesUsers = require('../../../servers/mcapi/resources/users')(model.users, model.experiments, schema);
-
-const dbModelUsers = require('../../../servers/mcapi/db/model/users')(r);
+//const model_users = require('../../../servers/mcapi/db/model/users');
+//const model_experiments = require('../../../servers/mcapi/db/model/experiments');
+//const schema = require('../../../servers/mcapi/schema')(model);
+const experiments = require('../../../servers/mcapi/resources/projects/experiments');
+//const resourcesUsers = require('../../../servers/mcapi/resources/users');
+const dbModelUsers = require('../../../servers/mcapi/db/model/users');
 
 const user1Id = 'thisIsAUserForTestingONLY!@mc.org';
-const fullname = "Test User"
+const fullname = "Test User";
 
 beforeEach(function*() {
     let results = yield r.db('materialscommons').table('users').insert({
