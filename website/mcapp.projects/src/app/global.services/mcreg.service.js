a7ca6d340ab52e35af4c41010129c12997c95b28
V. Glenn Tarcea <gtarcea@umich.edu>
2017-01-05 15:07:51 -0500

    Separate variable names into a separate service.

--- website/mcapp.projects/src/app/global.services/mcreg.service.js
+++ website/mcapp.projects/src/app/global.services/mcreg.service.js
@@ -1,8 +1,9 @@
 class MCRegService {
     /*@ngInject*/
-    constructor(mcbus) {
+    constructor(mcbus, mcregvars) {
         this.registry = {};
         this.mcbus = mcbus;
+        this.mcregvars = mcregvars;
     }
 
     get(name) {
@@ -41,116 +42,111 @@ class MCRegService {
     ///// current$process /////
 
     registerCurrent$process(name, fn) {
-        this.mcbus.register(MCRegService.CURRENT$PROCESS, name, fn);
+        this.mcbus.register(this.mcregvars.CURRENT$PROCESS, name, fn);
     }
 
     unregisterCurrent$process(name) {
-        this.mcbus.unregister(MCRegService.CURRENT$PROCESS, name);
+        this.mcbus.unregister(this.mcregvars.CURRENT$PROCESS, name);
     }
 
     unregisterAllCurrent$process() {
-        this.mcbus.unregisterEvent(MCRegService.CURRENT$PROCESS);
+        this.mcbus.unregisterEvent(this.mcregvars.CURRENT$PROCESS);
     }
 
     changedCurrent$process() {
-        let p = this.get(MCRegService.CURRENT$PROCESS);
-        this.mcbus.send(MCRegService.CURRENT$PROCESS, p);
+        let p = this.get(this.mcregvars.CURRENT$PROCESS);
+        this.mcbus.send(this.mcregvars.CURRENT$PROCESS, p);
     }
 
     get current$process() {
-        return this.get(MCRegService.CURRENT$PROCESS);
+        return this.get(this.mcregvars.CURRENT$PROCESS);
     }
 
     set current$process(value) {
-        this.set(MCRegService.CURRENT$PROCESS, value);
+        this.set(this.mcregvars.CURRENT$PROCESS, value);
     }
 
     ///// current$sample /////
 
     registerCurrent$sample(name, fn) {
-        this.mcbus.register(MCRegService.CURRENT$SAMPLE, name, fn);
+        this.mcbus.register(this.mcregvars.CURRENT$SAMPLE, name, fn);
     }
 
     unregisterCurrent$sample(name) {
-        this.mcbus.unregister(MCRegService.CURRENT$SAMPLE, name);
+        this.mcbus.unregister(this.mcregvars.CURRENT$SAMPLE, name);
     }
 
     unregisterAllCurrent$sample() {
-        this.mcbus.unregisterEvent(MCRegService.CURRENT$SAMPLE);
+        this.mcbus.unregisterEvent(this.mcregvars.CURRENT$SAMPLE);
     }
 
     changedCurrent$sample() {
-        let p = this.get(MCRegService.CURRENT$SAMPLE);
-        this.mcbus.send(MCRegService.CURRENT$SAMPLE, p);
+        let p = this.get(this.mcregvars.CURRENT$SAMPLE);
+        this.mcbus.send(this.mcregvars.CURRENT$SAMPLE, p);
     }
 
     get current$sample() {
-        return this.get(MCRegService.CURRENT$SAMPLE);
+        return this.get(this.mcregvars.CURRENT$SAMPLE);
     }
 
     set current$sample(value) {
-        this.set(MCRegService.CURRENT$SAMPLE, value);
+        this.set(this.mcregvars.CURRENT$SAMPLE, value);
     }
 
     ///// current$project /////
 
     registerCurrent$project(name, fn) {
-        this.mcbus.register(MCRegService.CURRENT$PROJECT, name, fn);
+        this.mcbus.register(this.mcregvars.CURRENT$PROJECT, name, fn);
     }
 
     unregisterCurrent$project(name) {
-        this.mcbus.unregister(MCRegService.CURRENT$PROJECT, name);
+        this.mcbus.unregister(this.mcregvars.CURRENT$PROJECT, name);
     }
 
     unregisterAllCurrent$project() {
-        this.mcbus.unregisterEvent(MCRegService.CURRENT$PROJECT);
+        this.mcbus.unregisterEvent(this.mcregvars.CURRENT$PROJECT);
     }
 
     changedCurrent$project() {
-        let p = this.get(MCRegService.CURRENT$PROJECT);
-        this.mcbus.send(MCRegService.CURRENT$PROJECT, p);
+        let p = this.get(this.mcregvars.CURRENT$PROJECT);
+        this.mcbus.send(this.mcregvars.CURRENT$PROJECT, p);
     }
 
     get current$project() {
-        return this.get(MCRegService.CURRENT$PROJECT);
+        return this.get(this.mcregvars.CURRENT$PROJECT);
     }
 
     set current$project(value) {
-        this.set(MCRegService.CURRENT$PROJECT, value);
+        this.set(this.mcregvars.CURRENT$PROJECT, value);
     }
 
     ///// current$experiment /////
 
     registerCurrent$experiment(name, fn) {
-        this.mcbus.register(MCRegService.CURRENT$EXPERIMENT, name, fn);
+        this.mcbus.register(this.mcregvars.CURRENT$EXPERIMENT, name, fn);
     }
 
     unregisterCurrent$experiment(name) {
-        this.mcbus.unregister(MCRegService.CURRENT$EXPERIMENT, name);
+        this.mcbus.unregister(this.mcregvars.CURRENT$EXPERIMENT, name);
     }
 
     unregisterAllCurrent$experiment() {
-        this.mcbus.unregisterEvent(MCRegService.CURRENT$EXPERIMENT);
+        this.mcbus.unregisterEvent(this.mcregvars.CURRENT$EXPERIMENT);
     }
 
     changedCurrent$experiment() {
-        let p = this.get(MCRegService.CURRENT$EXPERIMENT);
-        this.mcbus.send(MCRegService.CURRENT$EXPERIMENT, p);
+        let p = this.get(this.mcregvars.CURRENT$EXPERIMENT);
+        this.mcbus.send(this.mcregvars.CURRENT$EXPERIMENT, p);
     }
 
     get current$experiment() {
-        return this.get(MCRegService.CURRENT$EXPERIMENT);
+        return this.get(this.mcregvars.CURRENT$EXPERIMENT);
     }
 
     set current$experiment(value) {
-        this.set(MCRegService.CURRENT$EXPERIMENT, value);
+        this.set(this.mcregvars.CURRENT$EXPERIMENT, value);
     }
 }
-
-MCRegService.CURRENT$PROCESS = 'CURRENT$PROCESS';
-MCRegService.CURRENT$SAMPLE = 'CURRENT$SAMPLE';
-MCRegService.CURRENT$PROJECT = 'CURRENT$PROJECT';
-MCRegService.CURRENT$EXPERIMENT = 'CURRENT$EXPERIMENT';
 
 angular.module('materialscommons').service('mcreg', MCRegService);
 
