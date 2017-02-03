d22e67e8628eef14d6c8d005be27fad4aecdefb3
Terry Weymouth <weymouth@umich.edu>
2017-02-03 09:23:39 -0500

    More Projects test

--- backend/tests/mcapi/specs/projects-spec.js
+++ backend/tests/mcapi/specs/projects-spec.js
@@ -63,7 +63,7 @@ describe('Feature - projects: ', function() {
             let attrs = {
                 name: project_name,
                 description: "This is a test project for automated testing."
-            }
+            };
             let ret = yield projects.createProject(user,attrs);
             let project = ret.val;
             assert.equal(project.otype, "project");
@@ -80,13 +80,13 @@ describe('Feature - projects: ', function() {
             let attrs = {
                 name: project_name,
                 description: "This is a test project for automated testing."
-            }
+            };
             let ret = yield projects.createProject(user,attrs);
             let project = ret.val;
             assert.equal(project.name, project_name);
             assert.equal(project.users[0].user_id, user1Id);
             let project_list = yield projects.all();
-            var found_project;
+            let found_project = null;
             project_list.forEach(function(p){
                 if (p.name == project_name) {
                     found_project = p;
@@ -97,7 +97,35 @@ describe('Feature - projects: ', function() {
             assert.equal(found_project.owner, user.id);
             assert.equal(found_project.owner, user1Id);
         });
-        it ('create project and find project by user', function*(){
+        it ('create project and find project by user', function*() {
+            let user = yield dbModelUsers.getUser(user1Id);
+            let project_name = random_name();
+            assert.isNotNull(user, "test user exists");
+            let attrs = {
+                name: project_name,
+                description: "This is a test project for automated testing."
+            };
+            let ret = yield projects.createProject(user, attrs);
+            let project = ret.val;
+            assert.equal(project.name, project_name);
+            assert.equal(project.users[0].user_id, user1Id);
+            let project_list = yield projects.forUser(user);
+            let found_project = null;
+            project_list.forEach(function (p) {
+                if (p.name == project_name) {
+                    found_project = p;
+                }
+            });
+            assert.isNotNull(found_project);
+            assert.equal(found_project.otype, "project");
+            assert.equal(found_project.owner, user.id);
+            assert.equal(found_project.owner, user1Id);
+            assert.equal(found_project.users.length, 1);
+            // NOTE: field is user, here, but user_id in test above!!!
+            // is this the correct behaivor???
+            assert.equal(found_project.users[0].user, user1Id);
+        });
+        it ('create project, find by user, has full set of properties', function*(){
             let user = yield dbModelUsers.getUser(user1Id);
             let project_name = random_name();
             assert.isNotNull(user,"test user exists");
@@ -107,10 +135,8 @@ describe('Feature - projects: ', function() {
             };
             let ret = yield projects.createProject(user,attrs);
             let project = ret.val;
-            assert.equal(project.name, project_name);
-            assert.equal(project.users[0].user_id, user1Id);
             let project_list = yield projects.forUser(user);
-            let found_project;
+            let found_project = null;
             project_list.forEach(function(p){
                 if (p.name == project_name) {
                     found_project = p;
@@ -121,9 +147,44 @@ describe('Feature - projects: ', function() {
             assert.equal(found_project.owner, user.id);
             assert.equal(found_project.owner, user1Id);
             assert.equal(found_project.users.length,1);
-            // NOTE: field is user, here, but user_id in test above!!!
-            // is this the correct behaivor???
             assert.equal(found_project.users[0].user, user1Id);
+            assert.isTrue(found_project.hasOwnProperty('events'));
+            assert.equal(found_project.events.length,0);
+            assert.isTrue(found_project.hasOwnProperty('processes'));
+            assert.isTrue(found_project.hasOwnProperty('samples'));
+            assert.isTrue(found_project.hasOwnProperty('files'));
+            assert.isTrue(found_project.hasOwnProperty('experiments'));
+            assert.equal(found_project.processes,0);
+            assert.equal(found_project.samples,0);
+            assert.equal(found_project.files,0);
+            assert.equal(found_project.experiments,0);
+        });
+    });
+    describe('Update project', function() {
+        it('create project, update name and description', function*() {
+            let user = yield dbModelUsers.getUser(user1Id);
+            let project_name = random_name();
+            assert.isNotNull(user, "test user exists");
+            let attrs = {
+                name: project_name,
+                description: "This is a test project for automated testing."
+            };
+            let ret = yield projects.createProject(user, attrs);
+            let project = ret.val;
+            assert.equal(project.otype, "project");
+            assert.equal(project.name, project_name);
+            assert.equal(project.owner, user1Id);
+            let name = "Another Name";
+            let description = "An alternate description";
+            let update_attrs = {
+                name: name,
+                description: description
+            };
+            let updated_project = yield projects.update(project.id, update_attrs);
+            assert.equal(updated_project.otype, "project");
+            assert.equal(updated_project.owner, user1Id);
+            assert.equal(updated_project.name, name);
+            assert.equal(updated_project.description, description);
         });
     });
 });
