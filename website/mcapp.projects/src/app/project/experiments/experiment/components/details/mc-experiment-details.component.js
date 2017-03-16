44ac806f16bfa8ca816c78cf1f388e1ec63e476e
gtarcea <glenn.tarcea@gmail.com>
2017-03-16 11:25:25 -0400

    Set timeout to 1 second for saving content. This is a compromise to make sure the content is updated before retrieving new content.

--- website/mcapp.projects/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
+++ website/mcapp.projects/src/app/project/experiments/experiment/components/details/mc-experiment-details.component.js
@@ -9,12 +9,14 @@ angular.module('materialscommons').component('mcExperimentDetails', {
 
 /*@ngInject*/
 function MCExperimentDetailsComponentController($stateParams, experimentsAPI, toast, $scope, editorOpts) {
-    let ctrl = this;
+    let ctrl = this,
+        projectId = $stateParams.project_id,
+        experimentId = this.experiment.id;
 
     $scope.editorOptions = editorOpts({height: 67, width: 41});
 
     ctrl.updateName = () => {
-        experimentsAPI.updateForProject($stateParams.project_id, $stateParams.experiment_id, {name: ctrl.experiment.name})
+        experimentsAPI.updateForProject(projectId, experimentId, {name: ctrl.experiment.name})
             .then(
                 () => null,
                 () => toast.error('Failed to update experiment name')
@@ -22,7 +24,7 @@ function MCExperimentDetailsComponentController($stateParams, experimentsAPI, to
     };
 
     ctrl.updateStatus = () => {
-        experimentsAPI.updateForProject($stateParams.project_id, $stateParams.experiment_id, {status: ctrl.experiment.status})
+        experimentsAPI.updateForProject(projectId, experimentId, {status: ctrl.experiment.status})
             .then(
                 () => null,
                 () => toast.error('Failed to update experiment status')
@@ -30,7 +32,7 @@ function MCExperimentDetailsComponentController($stateParams, experimentsAPI, to
     };
 
     ctrl.updateDescription = () => {
-        experimentsAPI.updateForProject($stateParams.project_id, $stateParams.experiment_id,
+        experimentsAPI.updateForProject(projectId, experimentId,
                 {description: ctrl.experiment.description})
             .then(
                 () => null,
@@ -43,7 +45,7 @@ function MCExperimentDetailsComponentController($stateParams, experimentsAPI, to
             return;
         }
 
-        experimentsAPI.updateForProject($stateParams.project_id, $stateParams.experiment_id, {note: ctrl.experiment.note})
+        experimentsAPI.updateForProject(projectId, experimentId, {note: ctrl.experiment.note})
             .then(
                 () => null,
                 () => toast.error('Failed to update note')
