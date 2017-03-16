8c81fef5747b6e7ea292d7d971f5ed90bac665c9
gtarcea <glenn.tarcea@gmail.com>
2017-03-16 13:39:23 -0400

    Rename test to match component. Update test to use projectsAPI rather than projectsService (projectsService was renamed to projectsAPI). Fix warnings/errors found in inspection. Format code.

--- website/mcapp.projects/unit-test/global.components/mc-show-process.component.spec.js
+++ website/mcapp.projects/unit-test/global.components/mc-show-process.component.spec.js
@@ -1,9 +1,9 @@
-describe('MC Component - controller for: mc-show-process', function() {
+describe('MC Component - controller for: mc-show-process', function () {
     // ref: src/app/global.components/mc-show-process.component.js
 
     let testingProcessId = "056fe1ed-6c1c-48b3-ba63-18d1060e585a";
     let testingProjectId = "123999";
-    let testingFakeOwner = "someone@somewhere.edu"
+    let testingFakeOwner = "someone@somewhere.edu";
     let now = new Date();
 
     let testProcess = {
@@ -20,17 +20,16 @@ describe('MC Component - controller for: mc-show-process', function() {
         template_name: "Ultrasonic Fatigue"
     };
 
-    var testResults;
-
-    var controller;
-    var element;
-    var scope;
+    let testResults,
+        controller,
+        element,
+        scope;
 
     beforeEach(() => {
         module('materialscommons');
 
         // mock $stateParams
-        module(function($provide) {
+        module(function ($provide) {
             $stateParams = {};
             $stateParams.project_id = testingProjectId;
             $provide.value('$stateParams', $stateParams);
@@ -38,8 +37,8 @@ describe('MC Component - controller for: mc-show-process', function() {
 
 
         // mock projectsService
-        module(function($provide) {
-            let projectsService = {};
+        module(function ($provide) {
+            let projectsAPI = {};
             let getProjectProcess = jasmine.createSpy(['getProjectProcess']);
             getProjectProcess.and.callFake(
                 (projectId, processId) => {
@@ -47,19 +46,19 @@ describe('MC Component - controller for: mc-show-process', function() {
                     testResults.id = processId;
 
                     // punting on promise instance!!!
-                    let dummyPromise = {}
-                    dummyPromise.then = (okFn, errFN) => {
+                    let dummyPromise = {};
+                    dummyPromise.then = (okFn) => {
                         okFn(testResults);
                     };
                     return dummyPromise;
                 }
             );
-            projectsService.getProjectProcess = getProjectProcess;
-            $provide.value('projectsService', projectsService);
+            projectsAPI.getProjectProcess = getProjectProcess;
+            $provide.value('projectsAPI', projectsAPI);
         });
 
         // mock toastr
-        module(function($provide) {
+        module(function ($provide) {
             toastr = {};
 
             toastr.info = jasmine.createSpy();
@@ -69,8 +68,8 @@ describe('MC Component - controller for: mc-show-process', function() {
             $provide.value('toastr', toastr);
         });
 
-        inject(function($rootScope, $compile) {
-            var usage = '<mc-show-process process-id="pid"></mc-show-process>';
+        inject(function ($rootScope, $compile) {
+            let usage = '<mc-show-process process-id="pid"></mc-show-process>';
 
             // refs:
             // https://www.airpair.com/angularjs/posts/unit-testing-angularjs-applications
@@ -83,28 +82,28 @@ describe('MC Component - controller for: mc-show-process', function() {
         });
     });
 
-    beforeEach(function() {
+    beforeEach(function () {
         controller = element.controller('mcShowProcess');
     });
 
-    it('should be defined', function() {
+    it('should be defined', function () {
         expect(controller).toBeDefined();
     });
 
-    it('project id should be set in the controller', function() {
+    it('project id should be set in the controller', function () {
         expect(controller.projectId).toEqual(testingProjectId);
     });
 
-    it('process id should be set in the controller', function() {
+    it('process id should be set in the controller', function () {
         expect(controller.processId).toEqual(testingProcessId);
         // above should actually be controller.processId
     });
 
-    it('process should be set in the controller', function() {
+    it('process should be set in the controller', function () {
         expect(controller.process).toBeDefined();
     });
 
-    it('process id should be the same as testprocess', function() {
+    it('process id should be the same as testprocess', function () {
         expect(controller.process.id).toEqual(testingProcessId);
         // above should actually be controller.process.id
     });
