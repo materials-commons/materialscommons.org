7b08620148eb9e2683c97cbe57890a9d2ca14358
gtarcea <glenn.tarcea@gmail.com>
2017-03-18 17:41:15 -0400

    Unit test for mc-projects
    This is the start of a set of unit tests for the mc-projects component.

--- website/mcapp.projects/unit-test/components/projects/mc-projects.component.spec.js
+++ website/mcapp.projects/unit-test/components/projects/mc-projects.component.spec.js
@@ -1,4 +1,6 @@
 describe('mc-projects tests', () => {
+    let element = angular.element('<mc-projects></mc-projects>');
+
     beforeEach(() => {
         module('materialscommons');
 
@@ -18,8 +20,16 @@ describe('mc-projects tests', () => {
 
         // mock $mdDialog
         module($provide => {
-
+            let mdDialog = {};
+            mdDialog.show = jasmine.createSpy(['show']);
+            mdDialog.show.and.callFake(() => {
+                let p = {};
+                p.then = (ok) => ok();
+                return p;
+            });
+            $provide.value('$mdDialog', mdDialog);
         });
     });
 
+    //it('should get all user projects when instantiated', )
 });
