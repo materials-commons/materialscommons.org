ad650b2a202febf2ef8ab2c3697e7c444f4d3a15
V. Glenn Tarcea <gtarcea@umich.edu>
2017-03-20 14:40:58 -0400

    Update overview to remove details, samples and overview and just have a 'card' with some interesting details on an experiment.

--- website/mcapp.projects/src/app/project/home/mc-experiment-overview-details.component.js
+++ website/mcapp.projects/src/app/project/home/mc-experiment-overview-details.component.js
@@ -9,6 +9,30 @@ class MCExperimentOverviewDetailsComponentController {
         this.publishedDatasets = this.experiment.datasets.filter(ds => ds.published);
         this.completedTasks = this.experiment.tasks.filter(t => t.flags.done);
     }
+
+    pubPlural() {
+        return this.experiment.publications.length === 1 ? "Publication" : "Publications";
+    }
+
+    samplePlural() {
+        return this.experiment.samples.length === 1 ? "Sample" : "Samples";
+    }
+
+    processPlural() {
+        return this.experiment.processes.length === 1 ? "Process" : "Processes";
+    }
+
+    datasetPlural() {
+        return this.experiment.datasets.length === 1 ? "Dataset" : "Datasets";
+    }
+
+    filePlural() {
+        return this.experiment.files.length === 1 ? "File" : "Files";
+    }
+
+    notePlural() {
+        return this.experiment.notes.length === 1 ? "Note" : "Notes";
+    }
 }
 
 angular.module('materialscommons').component('mcExperimentOverviewDetails', {
