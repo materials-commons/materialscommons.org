186ce20f915997e662d8374392555ae406896147
Terry Weymouth <weymouth@umich.edu>
2017-04-14 15:19:20 -0400

    rough framming for project delete - nothing workd

---
+++ backend/tests/mcapi/Database-Level/specs/project-delete-spec.js
@@ -0,0 +1,141 @@
+'use strict';
+require('mocha');
+import {it} from 'mocha';
+require('co-mocha');
+const _ = require('lodash');
+const chai = require('chai');
+const assert = chai.assert;
+const should = chai.should();
+
+const r = require('rethinkdbdash')({
+    db: process.env.MCDB || 'materialscommons',
+    port: process.env.MCDB_PORT || 30815
+});
+
+const mcapi_base = '../../../../servers/mcapi';
+const backend_base = mcapi_base + "/db/model";
+
+const dbModelUsers = require(backend_base + '/users');
+const projects = require(backend_base + '/projects');
+
+const testHelpers = require('./test-helpers');
+
+const experimentDelete = require(backend_base + '/experiment-delete');
+
+const base_project_name = "Test directory";
+
+let random_name = function () {
+    let number = Math.floor(Math.random() * 10000);
+    return base_project_name + number;
+};
+
+let userId = "test@test.mc";
+let user = null;
+
+let project = null;
+let experiment = null;
+let processList = null;
+let sampleList = null;
+let fileList = null;
+let datasetList = null;
+let experimentNote = null;
+let experimentTask = null;
+let reviews_count = 0;
+let notes_count = 0;
+
+before(function*() {
+
+    user = yield dbModelUsers.getUser(userId);
+    assert.isOk(user, "No test user available = " + userId);
+    assert.equal(userId, user.id);
+});
+
+describe('Feature - Experiments: ', function () {
+    describe('Delete Experiment: ', function () {
+        it('does not delete an project with any published datasets', function*() {
+
+            this.timeout(8000); // test take up to 8 seconds
+
+            yield setup();
+
+            let project_id = project.id;
+            assert.isOk(project_id);
+            let experiment_id = experiment.id;
+            assert.isOk(experiment_id);
+
+            let idList = [];
+            for (let i = 0; i < datasetList.length; i++) {
+                idList.push(datasetList[i].id);
+            }
+
+            yield testDatasets({assertExists: true});
+
+            // publish one of the datasets
+            let datasetId = idList[0];
+            let results = yield experimentDatasets.publishDataset(datasetId);
+            assert.isOk(results);
+            assert.isOk(results.val);
+            assert.isOk(results.val.published);
+            assert(results.val.published);
+
+            // delete project - fails
+            results = yield experimentDelete.deleteProject(project_id);
+            assert.isOk(results);
+            assert.isOk(results.error);
+
+            yield testDatasets({assertExists: true});
+
+        });
+    });
+});
+
+
+function* setup() {
+    let valOrError = yield testHelpers.createDemoTestProject(user);
+    assert.isUndefined(valOrError.error, "Unexpected error from createDemoProjectForUser: " + valOrError.error);
+    let results = valOrError.val;
+    project = results.project;
+    experiment = results.experiment;
+    processList = results.processList;
+    sampleList = results.sampleList;
+    fileList = results.fileList;
+
+    assert.equal(project.otype, "project");
+    assert.equal(project.owner, userId);
+
+    datasetList = yield testHelpers.createDatasetList(experiment, processList, userId);
+
+    // Note: create fake sample that is not part of a process for testing
+    results = yield r.table('samples').insert({'name': 'fake sample', 'otype': 'sample', 'owner': 'noone'});
+    let key = results.generated_keys[0];
+    yield r.table('experiment2sample').insert({sample_id: key, experiment_id: experiment.id});
+    yield r.table('project2sample').insert({sample_id: key, project_id: project.id});
+
+}
+
+function* testDatasets(options) {
+
+    let count = 0;
+    if (options && options.assertExists) {
+        count = 2;
+    }
+
+    let idList = [];
+    for (let i = 0; i < datasetList.length; i++) {
+        idList.push(datasetList[i].id);
+    }
+
+    let check = yield experimentDatasets.getDatasetsForExperiment(experiment.id);
+    let dataset_list = check.val;
+    assert.isOk(dataset_list);
+    assert.equal(dataset_list.length, count);
+
+    check = yield r.table('dataset2process').getAll(r.args([...idList]), {index: 'dataset_id'});
+    assert.isOk(check);
+    assert.equal(check.length, count);
+
+    check = yield r.table('experiment2dataset').getAll(r.args([...idList]), {index: 'dataset_id'});
+    assert.isOk(check);
+    assert.equal(check.length, count);
+}
+
