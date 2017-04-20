0704bed99763f7c6bd04af93932f220c9a8bb08f
Terry Weymouth <weymouth@umich.edu>
2017-04-20 08:48:16 -0400

    non-working draft of delete-file tests

---
+++ backend/tests/mcapi/Database-Level/specs/files-delete-spec.js
@@ -0,0 +1,184 @@
+'use strict';
+require('mocha');
+import {it} from 'mocha';
+require('co-mocha');
+const chai = require('chai');
+const assert = require('chai').assert;
+
+const r = require('rethinkdbdash')({
+    db: process.env.MCDB || 'materialscommons',
+    port: process.env.MCDB_PORT || 30815
+});
+
+const backend_base = '../../../..';
+const lib_base = backend_base + '/servers/lib/'
+
+const db_model = backend_base + '/servers/mcapi/db/model/';
+const dbModelUsers = require(db_model + 'users');
+const projects = require(db_model + 'projects');
+const directories = require(db_model + 'directories');
+const files = require(db_model + 'files');
+
+const fileUtils = require(lib_base + 'create-file-utils');
+
+const testHelpers = require('./test-helpers');
+
+const base_project_name = "Test file project - ";
+
+let random_name = function(){
+    let number = Math.floor(Math.random()*10000);
+    return base_project_name + number;
+};
+
+let userId = "test@test.mc";
+let user = null;
+
+before(function*() {
+
+    user = yield dbModelUsers.getUser(userId);
+    assert.isOk(user, "No test user available = " + userId);
+    assert.equal(userId, user.id);
+
+});
+
+describe('Feature - Files: ', function() {
+    describe('Delete', function () {
+        it('Will delete a file that has the uses id set', function* () {
+
+            let project = yield setupProject();
+
+            let newName = 'Another name for photo.jpg';
+            let oldName = 'L124_photo.jpg';
+
+            let file1 = yield testHelpers.createFileFromDemoFileSet(project,1,user);
+            assert.isOk(file1);
+            assert.equal(file1.owner,userId);
+            assert.equal(file1.name,oldName);
+
+            let updateArgs = {
+                name: newName
+            };
+
+            let valOrError = yield files.update(file1.id,project.id,user,updateArgs);
+            assert.isOk(valOrError);
+            assert.isOk(valOrError.val);
+            let probe = valOrError.val;
+            assert.isOk(probe);
+            assert.equal(probe.owner,userId);
+            assert.equal(probe.name,newName);
+            assert.equal(probe.id,file1.id);
+
+            let file2 = yield testHelpers.createFileFromDemoFileSet(project,1,user);
+            assert.isOk(file2);
+            assert.equal(file2.owner,userId);
+            assert.equal(file1.checksum, file2.checksum);
+
+            let usesid = file1.id;
+            if (file1.usesid) {
+                usesid = file1.usesid;
+            }
+            assert.equal(file2.usesid, usesid);
+
+            valOrError = yield files.deleteFile(file2.id);
+            assert.isOk(valOrError);
+            assert.isOk(valOrError.val);
+            let fileDeleted = valOrError.val;
+
+            assert.equal(fileDeleted.id,file2.id);
+
+            let message = `Physical file for id ${file2.id} unexpectedly missing.`;
+            assert(fileUtils.datafilePathExists(file2.id),message)
+        });
+        it('Will delete a file but keep physical file when needed', function* () {
+
+            let project = yield setupProject();
+
+            let newName = 'Another name for photo.jpg';
+            let oldName = 'L124_photo.jpg';
+
+            let file1 = yield testHelpers.createFileFromDemoFileSet(project,1,user);
+            assert.isOk(file1);
+            assert.equal(file1.owner,userId);
+            assert.equal(file1.name,oldName);
+
+            let updateArgs = {
+                name: newName
+            };
+
+            let valOrError = yield files.update(file1.id,project.id,user,updateArgs);
+            assert.isOk(valOrError);
+            assert.isOk(valOrError.val);
+            let probe = valOrError.val;
+            assert.isOk(probe);
+            assert.equal(probe.owner,userId);
+            assert.equal(probe.name,newName);
+            assert.equal(probe.id,file1.id);
+
+            let file2 = yield testHelpers.createFileFromDemoFileSet(project,1,user);
+            assert.isOk(file2);
+            assert.equal(file2.owner,userId);
+            assert.equal(file1.checksum, file2.checksum);
+
+            let continueFlag = true;
+            let checksum = file1.checksum;
+
+            while (continueFlag) {
+                let results = yield files.getAllByChecksum(checksum);
+                assert.isOk(results);
+                let fetchedFileList = [...results];
+                assert.isOk(fetchedFileList);
+
+                assert(fetchedFileList.length > 0);
+                let fileNumber1 = fetchedFileList.length;
+                let target = fetchedFileList[0];
+
+                valOrError = yield files.deleteFile(target.id);
+                assert.isOk(valOrError);
+                assert.isOk(valOrError.val);
+                let fileDeleted = valOrError.val;
+
+                assert.equal(fileDeleted.id,target.id);
+                results = yield files.getAllByChecksum(checksum);
+                assert.isOk(results);
+                fetchedFileList = [...results];
+                assert.isOk(fetchedFileList);
+
+                let fileNumber2 = fetchedFileList.length;
+
+                assert.equal((fileNumber1-fileNumber2),1);
+
+                console.log("Before check on path: ",target.id);
+                let isDeleted = yield fileUtils.datafilePathExists(target.id);
+                console.log("After check on path: ",target.id);
+
+                if (fileNumber2 > 0) {
+                    let message = `Physical file for id ${target.id} unexpectedly missing.`;
+                    assert(isDeleted,message);
+                    continueFlag = true;
+                } else {
+                    let message = `Physical file for id ${target.id} was not deleted.`;
+                    assert.isFalse(isDeleted,message);
+                    continueFlag = false;
+                }
+
+                console.log("Number of files left: ", fileNumber2);
+
+            }
+            console.log("done")
+        });
+    });
+});
+
+function* setupProject() {
+
+    let results = yield testHelpers.createProject(random_name(),user);
+    assert.isOk(results);
+    assert.isOk(results.val);
+    let project = results.val;
+    assert.equal(project.owner,userId);
+
+    console.log("Project Name: ", project.name);
+
+    return project;
+
+}
