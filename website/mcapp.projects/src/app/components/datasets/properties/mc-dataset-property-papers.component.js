0915b9b0743867d6aee84255c8a8623aa046f3c4
gtarcea <glenn.tarcea@gmail.com>
2017-06-06 12:42:29 -0400

    Convert dataset overview
    Begin conversion of dataset from mcpub. Add additional property fields to layout dataset properties.

---
+++ website/mcapp.projects/src/app/components/datasets/properties/mc-dataset-property-papers.component.js
@@ -0,0 +1,18 @@
+angular.module('materialscommons').component('mcDatasetPropertyPapers', {
+    template: `
+        <label>Papers</label>
+        <ul>
+            <li ng-if="paper.title" ng-repeat="paper in $ctrl.dataset.papers">
+                <div class="md-subtitle">
+                    <b>{{paper.title}}</b>
+                    <a ng-if="paper.link" href="{{paper.link}}">
+                        <i class="fa fa-fw fa-file-text-o"></i>
+                    </a>
+                </div>
+            </li>
+        </ul>
+    `,
+    bindings: {
+        dataset: '<'
+    }
+});
