97d67ce982f63557754736d0fdbfb3d84627e29f
V. Glenn Tarcea <gtarcea@umich.edu>
2017-06-15 14:47:49 -0400

    Add tags and search to datasets home
    Add placeholder for search. Add popular tags.

--- website/mcapp.projects/src/app/data/components/home/mc-data-home.component.js
+++ website/mcapp.projects/src/app/data/components/home/mc-data-home.component.js
@@ -1,3 +1,6 @@
 angular.module('materialscommons').component('mcDataHome', {
-    templateUrl: 'app/data/components/home/mc-data-home.html'
+    templateUrl: 'app/data/components/home/mc-data-home.html',
+    bindings: {
+        tags: '<'
+    }
 });
