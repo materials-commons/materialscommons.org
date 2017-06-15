1aa7a22e535ef5832766cb10421e59a697bc7a37
V. Glenn Tarcea <gtarcea@umich.edu>
2017-06-15 15:23:47 -0400

    Implement search in datasets
    Search is using the angularJS filter function. Maybe later this can use Elasticsearch.

--- website/mcapp.projects/src/app/components/datasets/mc-dataset-list/mc-dataset-list.component.js
+++ website/mcapp.projects/src/app/components/datasets/mc-dataset-list/mc-dataset-list.component.js
@@ -1,17 +1,16 @@
 class MCDatasetListComponentController {
     /*@ngInject*/
-    constructor($stateParams, datasetsAPI, User) {
-        this.projectId = $stateParams.project_id;
-        this.experimentId = 'e83207f4-c29f-4672-8035-cdfeae30a7fd';
-        this.datasetsAPI = datasetsAPI;
-        this.isAuthenticated = User.isAuthenticated();
-        console.log('this.detailsRoute', this.detailsRoute);
+    constructor(mcbus) {
+        this.mcbus = mcbus;
+        this.myName = 'MCDatasetListComponentController';
     }
 
     $onInit() {
-        // this.datasetsAPI.getDatasetsForExperiment('e634ee47-b217-4547-a345-5007cd146dbd', '511930bd-96a5-4678-9626-ef79aceb75b5').then(
-        //     (datasets) => this.datasets = datasets
-        // );
+        this.mcbus.subscribe('MCDATA$SEARCH', this.myName, (query) => this.query = query);
+    }
+
+    $onDestroy() {
+        this.mcbus.leave('MCDATA$SEARCH', this.myName);
     }
 }
 
