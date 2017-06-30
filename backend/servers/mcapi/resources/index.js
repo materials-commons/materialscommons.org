b7dadd13e0b66037e5bcbb6741a639c6b4c7060d
Terry Weymouth <weymouth@umich.edu>
2017-06-30 14:44:02 -0400

    User profile backend - still to do - python tests

--- backend/servers/mcapi/resources/index.js
+++ backend/servers/mcapi/resources/index.js
@@ -2,6 +2,7 @@ const router = require('koa-router')();
 const projects = require('./projects');
 const users = require('./users');
 const templates = require('./templates');
+const profiles = require('./user_profiles');
 
 function createResources() {
     let projectsResource = projects.createResource();
@@ -9,6 +10,9 @@ function createResources() {
 
     let templatesResource = templates.createResource();
     router.use('/templates', templatesResource.routes(), templatesResource.allowedMethods());
+
+    let templatesResource = templates.createResource();
+    router.use('/profiles', templatesResource.routes(), profiles.allowedMethods());
 
     users.createResource(router);
 
--- backend/tests/mcapi/Database-Level/specs/users-spec.js
+++ backend/tests/mcapi/Database-Level/specs/users-spec.js
@@ -11,62 +11,45 @@ const r = require('rethinkdbdash')({
 
 const backend_base = '../../../..';
 const dbModelUsers = require(backend_base + '/servers/mcapi/db/model/users');
+const dbModelProfile = require(backend_base + '/servers/mcapi/db/model/user_profiles');
 const projects = require(backend_base + '/servers/mcapi/db/model/projects');
 const directories = require(backend_base + '/servers/mcapi/db/model/directories');
 
-const base_user_id = 'thisIsAUserForTestingONLY!';
-const fullname = "Test User";
-const base_project_name = "Test project - test 1: ";
+let userId = "test@test.mc";
+let user = null;
 
-let random_name = function(){
+let random_name = function(prefix){
     let number = Math.floor(Math.random()*10000);
-    return base_project_name + number;
+    return prefix + number;
 };
-
-let random_user = function(){
-    let number = Math.floor(Math.random()*10000);
-    return base_user_id + number + "@mc.org";
-};
-
-let user1Id = random_user();
 
 before(function*() {
-    let user = yield dbModelUsers.getUser(user1Id);
-    if (!user) {
-        let results = yield r.table('users').insert({
-            admin: false,
-            affiliation: "",
-            avatar: "",
-            description: "",
-            email: user1Id,
-            fullname: fullname,
-            homepage: "",
-            id: user1Id,
-            name: fullname,
-            preferences: {
-                tags: [],
-                templates: []
-            }
-        });
-        assert.equal(results.inserted, 1, "The User was correctly inserted");
-    } else {
-        assert.equal(user.id,user1Id, "Wrong test user, id = " + user.id);
-    }
+    user = yield dbModelUsers.getUser(userId);
+    assert.isOk(user, "No test user available = " + userId);
+    assert.equal(userId, user.id);
 });
 
 describe('Feature - Users: ', function() {
-    describe('Function level', function () {
-        it('individual test level');
+    describe('User Profile', function () {
+        it('can store values', function* (){
+            let name = random_name("test value - ");
+            let value = name;
+            let probe = yield dbModelProfile.storeInUserProfile(userId, name, value);
+            assert.isOk(probe);
+            assert.equal(probe,name);
+            probe = yield dbModelProfile.storeInUserProfile(userId, name, value);
+            assert.isOk(probe);
+            assert.equal(probe,name);
+            probe = yield dbModelProfile.getFromUserProfile(userId, name);
+            assert.isOk(probe);
+            assert.equal(probe,name);
+            probe = yield dbModelProfile.clearFromUserProfile(userId, name);
+            assert.isOk(probe);
+            assert.equal(probe,name);
+            probe = yield dbModelProfile.clearFromUserProfile(userId, name);
+            assert.isNull(probe);
+            probe = yield dbModelProfile.getFromUserProfile(userId, name);
+            assert.isNull(probe)
+        });
     });
 });
-
-after(function*() {
-    let user = yield dbModelUsers.getUser(user1Id);
-    if (user) {
-        let results = yield r.table('users').get(user1Id).delete();
-        assert.equal(results.deleted,1, "The User was correctly deleted");
-    } else {
-        assert.isNull(user,"The user still exists at end");
-    }
-});
-
