080fbeb25ed6242d71a9fac515c1b32bec509e08
V. Glenn Tarcea <gtarcea@umich.edu>
2017-07-06 15:30:38 -0400

    Remove unused code

--- website/mcapp.projects/src/app/project/experiments/experiment/components/workflow/mc-processes-workflow.component.js
+++ website/mcapp.projects/src/app/project/experiments/experiment/components/workflow/mc-processes-workflow.component.js
@@ -1,62 +1,33 @@
 class MCProcessesWorkflowComponentController {
     /*@ngInject*/
-    constructor(experimentsAPI, $stateParams, toast, experimentProcessesService, datasetsAPI,
-                workflowService, mcstate, mcbus, User, workflowState) {
-        this.myName = 'MCProcessesWorkflow';
-        this.experimentsAPI = experimentsAPI;
-        this.toast = toast;
-        this.projectId = $stateParams.project_id;
-        this.experimentId = $stateParams.experiment_id;
-        this.datasetId = $stateParams.dataset_id;
-        this.selectedProcess = null;
-        this.showGraphView = true;
-        this.currentTab = 0;
-        this.experimentProcessesService = experimentProcessesService;
-        this.datasetsAPI = datasetsAPI;
-        this.workflowService = workflowService;
+    constructor($stateParams, mcstate, mcbus, User, workflowState, experimentsAPI) {
         this.mcstate = mcstate;
         this.mcbus = mcbus;
+        this.workflowState = workflowState;
+        this.experimentsAPI = experimentsAPI;
+
         workflowState.setDataset(this.dataset);
-        //this.datasetProcesses = this.dataset ? _.indexBy(this.dataset.processes, 'id') : {};
+
+        this.projectId = $stateParams.project_id;
+        this.experimentId = $stateParams.experiment_id;
+        this.myName = 'MCProcessesWorkflow';
+        this.showGraphView = true;
+        this.currentTab = 0;
         this.showWorkspace = true;
         this.isBetaUser = User.isBetaUser();
     }
 
     $onInit() {
-        let onChangeCB = () => this.onChange();
-        this.mcstate.set(this.mcstate.SELECTED$PROCESS, null);
-        this.mcbus.subscribe('WORKFLOW$CHANGE', this.myName, onChangeCB);
-
         this.mcbus.subscribe('WORKFLOW$VIEW', this.myName, (whichView) => {
             this.showGraphView = whichView === 'graph';
         });
+
+        this.workflowState.subscribeSelectedProcess(this.myName, (process) => this.selectedProcess = process);
     }
 
     $onDestroy() {
-        this.mcbus.leave('WORKFLOW$CHANGE', this.myName);
         this.mcbus.leave('WORKFLOW$VIEW', this.myName);
-        this.mcstate.set(this.mcstate.SELECTED$PROCESS, null);
-    }
-
-    setSelectedProcess(processId, hasChildren) {
-        if (processId) {
-            this.experimentsAPI.getProcessForExperiment(this.projectId, this.experimentId, processId)
-                .then(
-                    (process) => {
-                        process.hasChildren = hasChildren;
-                        this.selectedProcess = process;
-                        this.mcstate.set(this.mcstate.SELECTED$PROCESS, process);
-                    },
-                    () => {
-                        this.toast.error('Unable to retrieve process details');
-                        this.selectedProcess = null;
-                        this.mcstate.set(this.mcstate.SELECTED$PROCESS, null);
-                    }
-                );
-        } else {
-            this.selectedProcess = null;
-            this.mcstate.set(this.mcstate.SELECTED$PROCESS, null);
-        }
+        this.workflowState.leaveSelectedProcess(this.myName);
     }
 
     onChange() {
