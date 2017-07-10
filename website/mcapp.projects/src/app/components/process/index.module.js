fe098f85f63205d74cd8f8da53131d0bbe130826
V. Glenn Tarcea <gtarcea@umich.edu>
2017-07-10 13:09:11 -0400

    Add additional components for read only process display

--- website/mcapp.projects/src/app/components/process/index.module.js
+++ website/mcapp.projects/src/app/components/process/index.module.js
@@ -1,1 +1,3 @@
 import './mc-process-details-ro/mc-process-details-ro.component';
+import './mc-process-details-ro/mc-process-details-ro-setup.component';
+import './mc-process-details-ro/mc-process-details-ro-setup-property-value.component';
