a64c73e4b1d57f4baa6d3e308bb9327a81d4d357
V. Glenn Tarcea <gtarcea@umich.edu>
2017-07-24 12:50:46 -0400

    Remove building mcapp.data

--- website/deploy.sh
+++ website/deploy.sh
@@ -2,17 +2,9 @@
 
 rm -rf mcpub mcapp
 echo
-echo Building mcapp.data...
-echo
-cd mcapp.data
-./buildmcapp.data.sh
-mv mcpub ..
-echo
-echo Done.
-echo
 echo Building mcapp.projects
 echo
-cd ../mcapp.projects
+cd mcapp.projects
 ./buildmcapp.projects.sh
 mv mcapp ..
 echo
