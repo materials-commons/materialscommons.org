1473f0d7f42d477955483c801ae61c461c306875
V. Glenn Tarcea <gtarcea@umich.edu>
2017-08-24 14:03:01 -0400

    Upgrade packages
    Changes in script to support newest webpack

--- website/gulp/scripts.js
+++ website/gulp/scripts.js
@@ -11,58 +11,60 @@ var $ = require('gulp-load-plugins')();
 
 
 function webpackWrapper(watch, test, callback) {
-  var webpackOptions = {
-    watch: watch,
-    module: {
-      preLoaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'}],
-      loaders: [{ test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate', 'babel-loader']}]
-    },
-    output: { filename: 'index.module.js' }
-  };
+    var webpackOptions = {
+        watch: watch,
+        module: {
+            rules: [
+                {enforce: 'pre', test: /\.js$/, exclude: /node_modules/, loader: 'eslint-loader'},
+                {test: /\.js$/, exclude: /node_modules/, loaders: ['ng-annotate-loader', 'babel-loader']}
+            ]
+        },
+        output: {filename: 'index.module.js'}
+    };
 
-  if(watch) {
-    webpackOptions.devtool = 'inline-source-map';
-  }
-
-  var webpackChangeHandler = function(err, stats) {
-    if(err) {
-      conf.errorHandler('Webpack')(err);
+    if (watch) {
+        webpackOptions.devtool = 'inline-source-map';
     }
-    $.util.log(stats.toString({
-      colors: $.util.colors.supportsColor,
-      chunks: false,
-      hash: false,
-      version: false
-    }));
-    browserSync.reload();
-    if(watch) {
-      watch = false;
-      callback();
+
+    var webpackChangeHandler = function(err, stats) {
+        if (err) {
+            conf.errorHandler('Webpack')(err);
+        }
+        $.util.log(stats.toString({
+            colors: $.util.colors.supportsColor,
+            chunks: false,
+            hash: false,
+            version: false
+        }));
+        browserSync.reload();
+        if (watch) {
+            watch = false;
+            callback();
+        }
+    };
+
+    var sources = [path.join(conf.paths.src, '/app/app.js')];
+    if (test) {
+        sources.push(path.join(conf.paths.src, '/app/**/*.spec.js'));
     }
-  };
 
-  var sources = [ path.join(conf.paths.src, '/app/app.js') ];
-  if (test) {
-    sources.push(path.join(conf.paths.src, '/app/**/*.spec.js'));
-  }
-
-  return gulp.src(sources)
-    .pipe(webpack(webpackOptions, null, webpackChangeHandler))
-    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
+    return gulp.src(sources)
+        .pipe(webpack(webpackOptions, null, webpackChangeHandler))
+        .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app')));
 }
 
-gulp.task('scripts', function () {
-  return webpackWrapper(false, false);
+gulp.task('scripts', function() {
+    return webpackWrapper(false, false);
 });
 
-gulp.task('scripts:watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, false, callback);
+gulp.task('scripts:watch', ['scripts'], function(callback) {
+    return webpackWrapper(true, false, callback);
 });
 
-gulp.task('scripts:test', function () {
-  return webpackWrapper(false, true);
+gulp.task('scripts:test', function() {
+    return webpackWrapper(false, true);
 });
 
-gulp.task('scripts:test-watch', ['scripts'], function (callback) {
-  return webpackWrapper(true, true, callback);
+gulp.task('scripts:test-watch', ['scripts'], function(callback) {
+    return webpackWrapper(true, true, callback);
 });
