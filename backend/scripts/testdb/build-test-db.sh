8b9a20b1c94e398570896e6c0738fd13f16f3e12
Terry Weymouth <weymouth@umich.edu>
2017-08-24 11:01:18 -0400

    Script to build test database

---
+++ backend/scripts/testdb/build-test-db.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# abort if TEST_ACCOUNT_PW not set and non-empty
+if [ -z "$TEST_ACCOUNT_PW" ]; then
+    echo "TEST_ACCOUNT_PW must be set to a non-empty string"
+    exit
+fi
+
+# pushd location of script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $DIR
+
+# check that MCDB_PORT is available
+if [ -z "$MCDB_PORT" ]; then
+    echo "MCDB_PORT must be set to the RETHINKDB port number"
+    echo "Setting MCDB_PORT to default value for tests: 30815"
+    export MCDB_PORT=30815
+fi
+echo "using MCDB_PORT = $MCDB_PORT"
+
+# build basic (e.g 'empty') DB; using standard script
+../dbcreate.py --port $MCDB_PORT
+
+# add templates
+pushd '../templates/'
+./run.sh
+popd
+
+# add test users
+./makeUsersForTests.py --port $MCDB_PORT --password $TEST_ACCOUNT_PW
+
+popd
+echo "Done."
