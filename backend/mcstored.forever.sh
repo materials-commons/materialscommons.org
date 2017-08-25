1033d72ad2b2898adeec3181471d6977b8d45f4f
gtarcea <glenn.tarcea@gmail.com>
2017-08-25 11:39:41 -0400

    Write a shell script to keep mcstored up and running
    unit version restarts 3 times. Production/test restarts 20 times.

---
+++ backend/mcstored.forever.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+shift
+> ${MC_LOG_DIR}/mcstored.out.${SERVERTYPE}
+MAX_RETRIES=20
+CURRENT_RETRY=0
+if [ "${SERVERTYPE}" = "unit" ]; then
+    MAX_RETRIES=3
+fi
+LOG_FILE=${MC_LOG_DIR}/mcstored.out.${SERVERTYPE}
+while [ 1 ];
+do
+    ${MCSTOREDBIN} $* >> ${LOG_FILE} 2>&1
+    CURRENT_RETRY=$(($CURRENT_RETRY+1))
+    if [ $CURRENT_RETRY -eq ${MAX_RETRIES} ]; then
+        echo "============================" >> ${LOG_FILE}
+        echo "  ${MCSTOREDBIN} MAX RETRIES (${MAX_RETRIES}) REACHED... EXITING..." >> ${LOG_FILE}
+        echo "============================" >> ${LOG_FILE}
+        break
+    fi
+    echo "============================" >> ${LOG_FILE}
+    echo "Restarting ${MCSTOREDBIN} Retry ${CURRENT_RETRY} of ${MAX_RETRIES}" >> ${LOG_FILE}
+    echo "============================" >> ${LOG_FILE}
+done
