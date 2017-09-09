d220f87e5c1427103a75f20529733640d1e0067d
gtarcea <glenn.tarcea@gmail.com>
2017-09-08 20:10:09 -0400

    Remove console statement

--- website/mc-web/src/api/datasetsAPI.js
+++ website/mc-web/src/api/datasetsAPI.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export function getTopPublicDatasets() {
     return axios.get('/api/pub/datasets/filter/views').then(
         (result) => {
-            console.log(result);
             return result.data;
         }
     )
