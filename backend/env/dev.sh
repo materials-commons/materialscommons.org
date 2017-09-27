6651f804540b5c97325e306c6552e1374c517d89
Terry Weymouth <weymouth@umich.edu>
2017-09-27 12:08:36 -0400

    Initial pass at setting up environments

---
+++ backend/env/dev.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+export MC_SERVICE_PORT=5002
+export MC_API_SERVICE_PORT=5004
+export MC_PUB_SERVICE_PORT=5026
+export MCDB_PORT=30815
+export MCDB_CONNECTION="localhost:$MCDB_PORT"
+export MCDB_DIR=~/testdb
+export RETHINKDB_HTTP_PORT=8090
+export RETHINKDB_CLUSTER_PORT=31815
+export MCSTOREDBIN=testbin/mcstored
+export MC_ES_URL="http://localhost:9500"
+export MC_ES_NAME="mc-es-test"
+export MC_LOG_DIR=/tmp
+if [ "$MCDB_FILE" = "" ]; then
+    export MCDB_FILE=../test_data/rethinkdb_dump_test_data.tar.gz
+fi
+export MCDIR=~/mcdir/mcfs/data/test:/mcfs/data/materialscommons
+export MCFS_HTTP_PORT=5012
+
+if [ -f /etc/materialscommons/config.dev ]; then
+    . /etc/materialscommons/config.dev
+fi
+
+if [ ! -d ${MCDB_DIR} ]; then
+    mkdir ${MCDB_DIR}
+fi
+
+if [ "$REINIT" = "t" ]; then
+    (cd ${MCDB_DIR}; rm -rf rethinkdb_data)
+fi
