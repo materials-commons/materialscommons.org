88c5cfe08add80de18d35aae6ae51629eab88f0d
Terry Weymouth <weymouth@umich.edu>
2017-10-13 15:23:13 -0400

    Minor front end changes to dataset and comments component, to expose tab for comments and check for comments on dataset

--- website/src/app/components/comments/mc-comments.component.js
+++ website/src/app/components/comments/mc-comments.component.js
@@ -1,52 +1,58 @@
 class MCCommentsComponentController {
     constructor(User) {
         this.user = User.u();
-        this.comments = [
-            {
-                comment: 'First Comment!',
-                owner: 'tradiasa@umich.edu',
-                owner_details: {
-                    fullname: 'Tracy Berman'
+        console.log(this.comments)
+        if (! this.comments) {
+            this.comments = [
+                {
+                    comment: 'First Comment!',
+                    owner: 'tradiasa@umich.edu',
+                    owner_details: {
+                        fullname: 'Tracy Berman'
+                    },
+                    birthtime: Date.now(),
+                    mtime: Date.now()
                 },
-                birthtime: Date.now(),
-                mtime: Date.now()
-            },
-            {
+                {
 
-                comment: 'This is a comment',
-                owner: 'gtarcea@umich.edu',
-                owner_details: {
-                    fullname: 'Glenn Tarcea'
+                    comment: 'This is a comment',
+                    owner: 'gtarcea@umich.edu',
+                    owner_details: {
+                        fullname: 'Glenn Tarcea'
+                    },
+                    birthtime: Date.now(),
+                    mtime: Date.now()
                 },
-                birthtime: Date.now(),
-                mtime: Date.now()
-            },
-            {
-                comment: 'It works!',
-                owner: 'bpuchala@umich.edu',
-                owner_details: {
-                    fullname: 'Brian Puchala'
+                {
+                    comment: 'It works!',
+                    owner: 'bpuchala@umich.edu',
+                    owner_details: {
+                        fullname: 'Brian Puchala'
+                    },
+                    birthtime: Date.now(),
+                    mtime: Date.now()
                 },
-                birthtime: Date.now(),
-                mtime: Date.now()
-            },
-            {
-                comment: 'Another comment',
-                owner: 'johnea@umich.edu',
-                owner_details: {
-                    fullname: 'John Allison'
-                },
-                birthtime: Date.now(),
-                mtime: Date.now()
-            }
-        ]
+                {
+                    comment: 'Another comment',
+                    owner: 'johnea@umich.edu',
+                    owner_details: {
+                        fullname: 'John Allison'
+                    },
+                    birthtime: Date.now(),
+                    mtime: Date.now()
+                }
+            ]
+        }
 
     }
 }
 
 angular.module('materialscommons').component('mcComments', {
     templateUrl: 'app/components/comments/mc-comments.html',
-    controller: MCCommentsComponentController
+    controller: MCCommentsComponentController,
+    bindings: {
+        comments: '<'
+    }
 });
 
 // angular.module('materialscommons').directive('mcCommentsButton', mcCommentsButtonDirective);
