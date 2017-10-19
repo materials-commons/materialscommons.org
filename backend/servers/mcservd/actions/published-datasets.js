f58be4412b50de594feb81de1ccd71d389898da0
gtarcea <glenn.tarcea@gmail.com>
2017-10-19 07:19:45 -0400

    Ignore files and explore sub directories in actions space

--- backend/servers/mcservd/actions/published-datasets.js
+++ backend/servers/mcservd/actions/published-datasets.js
@@ -1,4 +1,4 @@
-const {Action, api} = require('actionhero');
+const {Action} = require('actionhero');
 const datasets = require('../model/datasets');
 
 module.exports = class AllPublishedDatasetsAction extends Action {
@@ -10,41 +10,5 @@ module.exports = class AllPublishedDatasetsAction extends Action {
 
     async run({response}) {
         response.users = await datasets.getAll();
-    }
-};
-
-module.exports = class HelloAction extends Action {
-    constructor() {
-        super();
-        this.name = 'hello';
-        this.description = 'Return hello';
-    }
-
-    async run({response}) {
-        response.hello = {msg: 'hello'};
-    }
-};
-
-module.exports = class Hello2Action extends Action {
-    constructor() {
-        super();
-        this.name = 'hello2';
-        this.description = 'Return hello';
-    }
-
-    async run({response}) {
-        response.hello = {msg: 'hello2'};
-    }
-};
-
-module.exports = class Hello3Action extends Action {
-    constructor() {
-        super();
-        this.name = 'hello3';
-        this.description = 'Return hello';
-    }
-
-    async run({response}) {
-        response.hello = {msg: 'hello changed'};
     }
 };
--- backend/servers/mcservd/model/datasets.js
+++ backend/servers/mcservd/model/datasets.js
@@ -1,11 +1,32 @@
-const {api} = require('actionhero');
+const r = require('actionhero').api.r;
 
 module.exports.getAll = async () => {
-    return await api.r.table('datasets').filter({published: true}).merge((ds) => {
+    return await r.table('datasets').filter({published: true}).merge((ds) => {
         return {
-            'file_count': api.r.db('mcpub').table('dataset2datafile').getAll(ds('id'), {index: 'dataset_id'}).count(),
-            'appreciations': api.r.db('mcpub').table('appreciations').getAll(ds('id'), {index: 'dataset_id'}).count(),
-            'views': api.r.db('mcpub').table('views').getAll(ds('id'), {index: 'dataset_id'}).count()
+            'file_count': r.db('mcpub').table('dataset2datafile').getAll(ds('id'), {index: 'dataset_id'}).count(),
+            'appreciations': r.db('mcpub').table('appreciations').getAll(ds('id'), {index: 'dataset_id'}).count(),
+            'views': r.db('mcpub').table('views').getAll(ds('id'), {index: 'dataset_id'}).count()
         }
     });
+};
+
+// module.exports.getDatasetProcess = async () => {
+//     let rv = yield processCommon.getProcess(r, this.params.process_id);
+//     this.body = rv.val;
+//     yield next;
+// };
+
+module.exports.getAllCount = async () => {
+    return await r.db('materialscommons').table('datasets').filter({published: true}).count();
+};
+
+module.exports.getRecent = async () => {
+    return await r.db('materialscommons').table('datasets').filter({published: true})
+        .orderBy(r.desc('birthtime')).merge((ds) => {
+            return {
+                'file_count': r.db('mcpub').table('dataset2datafile').getAll(ds('id'), {index: 'dataset_id'}).count(),
+                'appreciations': r.db('mcpub').table('appreciations').getAll(ds('id'), {index: 'dataset_id'}).count(),
+                'views': r.db('mcpub').table('views').getAll(ds('id'), {index: 'dataset_id'}).count()
+            }
+        }).limit(10);
 };
