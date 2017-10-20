4bb1d9c5576adcf6dfc5bdec3d662aaacf7bf8c1
gtarcea <glenn.tarcea@gmail.com>
2017-10-20 17:00:52 -0400

    Add additional actions

--- backend/servers/mcservd/actions/published-datasets.js
+++ backend/servers/mcservd/actions/published-datasets.js
@@ -9,6 +9,47 @@ module.exports = class AllPublishedDatasetsAction extends Action {
     }
 
     async run({response}) {
-        response.users = await datasets.getAll();
+        response.val = await datasets.getAll();
+    }
+};
+
+model.exports = class TopViewedPublishedDatasetsAction extends Action {
+    constructor() {
+        super();
+        this.name = 'topViewedPublishedDatasets';
+        this.description = 'Returns the most viewed datasets';
+    }
+
+    async run({response}) {
+        response.val = await datasets.getTopViews();
+    }
+};
+
+model.exports = class RecentlyPublishedDatasetsAction extends Action {
+    constructor() {
+        super();
+        this.name = 'recentlyPublishedDatasets';
+        this.description = 'Returns most recently published datasets';
+    }
+
+    async run({response}) {
+        response.val = await datasets.getRecent();
+    }
+};
+
+model.exports = class GetPublishedDatasetAction extends Action {
+    constructor() {
+        super();
+        this.name = 'getPublishedDataset';
+        this.description = 'Returns the published dataset';
+        this.inputs = {
+            dataset_id: {
+                required: true
+            }
+        }
+    }
+
+    async run({response, params}) {
+        response.val = await datasets.getDataset(params.dataset_id);
     }
 };
