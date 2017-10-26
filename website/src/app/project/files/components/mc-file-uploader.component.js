7c9c98724bce8c468f0dfdec11a67ff2ff26737d
gtarcea <glenn.tarcea@gmail.com>
2017-10-25 22:38:03 -0400

    Update uploader with additional features
    * Delete files
    * Preview files
    * Upload multiple files
    * Change layout and update formatting

--- website/src/app/project/files/components/mc-file-uploader.component.js
+++ website/src/app/project/files/components/mc-file-uploader.component.js
@@ -1,18 +1,32 @@
 class MCFileUploaderComponentController {
     /*@ngInject*/
-    constructor($stateParams, Upload, User) {
+    constructor($stateParams, Upload, User, mcprojstore, gridFiles) {
         this.$stateParams = $stateParams;
         this.Upload = Upload;
         this.User = User;
+        this.mcprojstore = mcprojstore;
+        this.gridFiles = gridFiles;
         this.files = [];
+    }
+
+    $onInit() {
+        let project = this.mcprojstore.currentProject;
+        const entry = this.gridFiles.findEntry(project.files[0], this.$stateParams.directory_id);
+        this.path = entry.model.data.path;
+    }
+
+    remove(index) {
+        this.files.splice(index, 1);
     }
 
     submit() {
         let {project_id, directory_id} = this.$stateParams;
-        this.Upload.upload({
-            url: `api/v2/projects/${project_id}/directories/${directory_id}/fileupload?apikey=${this.User.apikey()}`,
-            data: {file: this.files[0]}
-        });
+        for (let i = 0; i < this.files.length; i++) {
+            this.Upload.upload({
+                url: `api/v2/projects/${project_id}/directories/${directory_id}/fileupload?apikey=${this.User.apikey()}`,
+                data: {file: this.files[i]}
+            });
+        }
     }
 }
 
