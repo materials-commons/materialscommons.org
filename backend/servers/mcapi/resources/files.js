313e6f1fb3610b9ba826de198c096d83e3e5625d
V. Glenn Tarcea <gtarcea@umich.edu>
2017-10-30 13:23:26 -0400

    Search mcdir path for file

--- backend/servers/mcapi/resources/files.js
+++ backend/servers/mcapi/resources/files.js
@@ -8,26 +8,34 @@ const ra = require('./resource-access');
 function* downloadFile(next) {
     let file = this.reqctx.file;
     let mediatype = '';
+    let found = false;
 
-    const filePath = fileUtils.datafilePathFromFile(file, this.params.original);
-    if (yield fsExtra.pathExists(filePath)) {
-        if (this.params.original) {
-            mediatype = file.mediatype.mime;
-        } else if (fileUtils.isOfficeDoc(file.mediatype.mime)) {
-            mediatype = 'application/pdf';
-        } else if (fileUtils.isConvertedImage(file.mediatype.mime)) {
-            mediatype = 'image/jpeg';
-        } else {
-            mediatype = file.mediatype.mime;
+    const filePaths = fileUtils.datafilePathsFromFile(file, this.params.original);
+    for (let i = 0; i < filePaths.length; i++) {
+        let filePath = filePaths[i];
+        if (yield fsExtra.pathExists(filePath)) {
+            if (this.params.original) {
+                mediatype = file.mediatype.mime;
+            } else if (fileUtils.isOfficeDoc(file.mediatype.mime)) {
+                mediatype = 'application/pdf';
+            } else if (fileUtils.isConvertedImage(file.mediatype.mime)) {
+                mediatype = 'image/jpeg';
+            } else {
+                mediatype = file.mediatype.mime;
+            }
+
+            if (mediatype === 'application/pdf' && !this.params.original) {
+                this.set('Content-Disposition', `inline; filename="filename.pdf"`)
+            }
+
+            this.set('Content-Type', mediatype);
+            this.body = fs.createReadStream(filePath);
+            found = true;
+            break;
         }
+    }
 
-        if (mediatype === 'application/pdf' && !this.params.original) {
-            this.set('Content-Disposition', `inline; filename="filename.pdf"`)
-        }
-
-        this.set('Content-Type', mediatype);
-        this.body = fs.createReadStream(filePath);
-    } else {
+    if (!found) {
         this.status = httpStatus.NOT_FOUND;
         this.body = {error: 'File not found: ' + filename};
     }
