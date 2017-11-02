f47eaa9c8ab202c405cdcaa651b36c2d6dae4702
Terry Weymouth <weymouth@umich.edu>
2017-11-02 13:31:02 -0400

    added user full name to new comment query

--- backend/servers/mcpub/db/comments.js
+++ backend/servers/mcpub/db/comments.js
@@ -1,7 +1,15 @@
 const r = require('./../dash');
 
 module.exports.get = function*(next) {
-    var rv = yield r.db('materialscommons').table('comments').getAll(this.query.target, {index: 'item_id'});
+    var rv = yield  r.db('materialscommons').table('comments')
+                    .getAll(this.query.target, {index:'item_id'}).merge((c) => {
+                        return {
+                            user: r.db('materialscommons').table('users')
+                                .get(c('owner')).pluck('fullname')
+                            };
+                        }
+                    )
+                    .orderBy(r.desc('birthtime'))
     if (rv.error) {
         this.status = httpStatus.INTERNAL_SERVER_ERROR;
         this.body = rv;
