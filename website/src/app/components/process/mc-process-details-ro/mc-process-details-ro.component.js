32323b12e7c415a9587fdcbdb260fae03b46bd57
gtarcea <glenn.tarcea@gmail.com>
2017-11-09 13:47:17 -0500

    Process details don't change when user clicks on a different process

--- website/src/app/components/process/mc-process-details-ro/mc-process-details-ro.component.js
+++ website/src/app/components/process/mc-process-details-ro/mc-process-details-ro.component.js
@@ -1,5 +1,18 @@
+class MCProcessDetailsRoComponentController {
+    /*@ngInject*/
+    constructor() {
+    }
+
+    $onChanges(changes) {
+        if (!changes.process.isFirstChange()) {
+            this.process = changes.process.currentValue;
+        }
+    }
+}
+
 angular.module('materialscommons').component('mcProcessDetailsRo', {
     templateUrl: 'app/components/process/mc-process-details-ro/mc-process-details-ro.html',
+    controller: MCProcessDetailsRoComponentController,
     bindings: {
         process: '<'
     }
