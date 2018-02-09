d8c3be54e87253079eef76de59fc495bb1420069
Terry Weymouth <weymouth@umich.edu>
2018-02-09 08:50:18 -0500

    experiment etl metadata in database - python API support - working

--- backend/servers/mcapi/db/model/experiments-etl-metadata.js
+++ backend/servers/mcapi/db/model/experiments-etl-metadata.js
@@ -6,6 +6,15 @@ const model = require('./model');
 // GET/PUT/POST rest-path/project/id/experiment/id/metadata/[id]
 
 function* create(user_id, experiment_id, metadata_json) {
+    let probe = yield dbExec(r.table("experiment_etl_metadata")
+        .getAll(experiment_id, {index: 'experiment_id'}));
+    if (probe && probe.length > 0) {
+        // for now, only one metadata record per experiment
+        let previous = probe[0];
+        let id = previous.id;
+        let updated = yield update(id, metadata_json);
+        return updated;
+    }
     let metadata = yield db.insert('experiment_etl_metadata',
         new model.ExperimentEtlMetadata(experiment_id, metadata_json, user_id));
     let ret = {error: `could not add experiment_etl_metadata for experiment_id = ${experiment_id}`};
--- backend/servers/mcapi/resources/experiment-etl-metadata.js
+++ backend/servers/mcapi/resources/experiment-etl-metadata.js
@@ -8,43 +8,48 @@ function* createEtlExperimentMetadata(next) {
     let user = this.reqctx.user;
     let data = yield parse(this);
     let experiment_id = this.params.experiment_id;
-    this.body = yield metadata.create(user.id, experiment_id, data.json)
+    this.body = yield metadata.create(user.id, experiment_id, data.json);
     yield next;
 }
 
 function* getEtlExperimentMetadata(next) {
-    this.status = status.METHOD_NOT_ALLOWED;
+    let id = this.params.id;
+    this.body = yield metadata.get(id);
     yield next;
 }
 
 function* updateEtlExperimentMetadata(next) {
+    let id = this.params.id;
     let data = yield parse(this);
-    this.status = status.METHOD_NOT_ALLOWED;
+    this.body = yield metadata.update(id, data.json);
     yield next;
 }
 
 function* deleteEtlExperimentMetadata(next) {
-    this.status = status.METHOD_NOT_ALLOWED;
+    let id = this.params.id;
+    results = yield metadata.remove(id);
+    this.body = '';
     yield next;
 }
 
 function* getEtlExperimentMetadataByExperiment(next) {
-    this.status = status.METHOD_NOT_ALLOWED;
+    let experiment_id = this.params.experiment_id;
+    this.body = yield metadata.getByExperimentId(experiment_id);
     yield next;
 }
 
 function createResource() {
     const router = new Router();
 
-    router.post('/create/:experiment_id', createEtlExperimentMetadata);
+    router.put('/create/:experiment_id', createEtlExperimentMetadata);
 
     // basic operations
     router.get('/metadata/:id', getEtlExperimentMetadata);
-    router.put('/metadata/:id', updateEtlExperimentMetadata);
+    router.post('/metadata/:id', updateEtlExperimentMetadata);
     router.delete('/metadata/:id', deleteEtlExperimentMetadata);
 
     // get ETL metadata for experiment
-    router.get('experiment/:experiment_id/metadata',getEtlExperimentMetadataByExperiment);
+    router.get('/experiment/:experiment_id/metadata',getEtlExperimentMetadataByExperiment);
 
     return router;
 }
