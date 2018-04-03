6c85ef9532fddd66b55388126f50b8f558ca5bab
gtarcea <glenn.tarcea@gmail.com>
2018-04-03 11:33:47 -0400

    General code cleanup and moving commented out code we need to refer to in a bit to bottom of file

--- website/src/app/components/workflow/mc-workflow-as-table/mc-workflow-as-table.component.js
+++ website/src/app/components/workflow/mc-workflow-as-table/mc-workflow-as-table.component.js
@@ -1,42 +1,21 @@
 class MCWorkflowAsTableComponentController {
     /*@ngInject*/
-    constructor(mcprojstore, projectsAPI, $mdDialog) {
-        this.mcprojstore = mcprojstore;
-        this.projectsAPI = projectsAPI;
+    constructor($mdDialog) {
         this.$mdDialog = $mdDialog;
-        this.project = mcprojstore.currentProject;
-        //console.log('this.project', this.project);
         this.grouped = false;
         this.editTable = false;
     }
 
     $onInit() {
-        // this.projectsAPI.getProjectSamples(this.project.id).then(
-        //     (samples) => {
-        //         this.samples = samples;
-        //         this.samples.forEach(s => this.addProcessListTimeLine(s));
-        //         let uniqueProcesses = this.computeUniqueProcesses();
-        //         this.createHeaders(uniqueProcesses);
-        //         //console.log('this.samples', samples);
-        //     }
-        // );
+        this.createDemoData();
+    }
 
+    createDemoData() {
         this.headers = [
             "Heat Treatment",
-            "Heat Treatment",
-            "SEM",
             "SEM",
             "Low Cycle Fatigue",
             "EBSD",
-            "EBSD",
-            "Tension",
-            "EBSD",
-            "TEM",
-            "TEM",
-            "TEM",
-            "Cogging",
-            "Cogging",
-            "Cogging",
             "Tension",
             "EBSD",
             "TEM",
@@ -46,9 +25,9 @@ class MCWorkflowAsTableComponentController {
             "TEM",
         ];
 
-        this.samples2 = [];
+        this.samples = [];
         for (let i = 0; i < 10; i++) {
-            this.samples2.push({
+            this.samples.push({
                 selected: false,
                 name: "Sample_" + i,
                 processes: this.fillRandomProcesses(this.headers.length)
@@ -70,8 +49,8 @@ class MCWorkflowAsTableComponentController {
     }
 
     handleDeleteSampleClick(index) {
-        this.samples2.splice(index, 1);
-        this.samples2 = angular.copy(this.samples2);
+        this.samples.splice(index, 1);
+        this.samples = angular.copy(this.samples);
     }
 
     handleDeleteProcess(index) {
@@ -80,61 +59,21 @@ class MCWorkflowAsTableComponentController {
     }
 
     handleUpdateStateOfProcessInSample(sampleIndex, processIndex, state) {
-        this.samples2[sampleIndex].processes[processIndex].selected = state;
-        this.samples2[sampleIndex].processes[processIndex].active = state;
+        this.samples[sampleIndex].processes[processIndex].selected = state;
+        this.samples[sampleIndex].processes[processIndex].active = state;
     }
 
     finishEditingTable() {
         this.editTable = false;
-        this.samples2 = angular.copy(this.samples2);
+        this.samples = angular.copy(this.samples);
     }
 
     handleSelectSample(sampleIndex, selectState) {
-        console.log('handleSelectSample', sampleIndex, selectState);
-        this.samples2[sampleIndex].selected = selectState;
+        this.samples[sampleIndex].selected = selectState;
     }
 
     handleRemoveSelectedSamples() {
-        this.samples2 = this.samples2.filter(s => !s.selected);
-    }
-
-    addProcessListTimeLine(sample) {
-        let processes = _.indexBy(sample.processes, 'process_id');
-        sample.processesInTimeline = sample.processes.filter(
-            (p) => processes[p.process_id].property_set_id === p.property_set_id
-        ).filter(p => {
-            if (p.template_name == 'Create Samples') {
-                return false;
-            } else if (p.template_name == 'Sectioning') {
-                return false;
-            }
-
-            return true;
-        }).map(p => ({
-            name: p.template_name,
-            process_id: p.process_id,
-            seen: false
-        }));
-    }
-
-    computeUniqueProcesses() {
-        const allProcesses = [];
-        this.samples.forEach(s => allProcesses.push(s.processesInTimeline));
-        let combinedProcessTimeline = _.uniq([].concat.apply([], allProcesses), 'process_id');
-        let combinedProcessTimelineMap = _.indexBy(combinedProcessTimeline, 'process_id');
-        return {combinedProcessTimelineMap, combinedProcessTimeline};
-    }
-
-    createHeaders(uniqueProcesses) {
-        // uniqueProcesses.combinedProcessTimeline.forEach(p => {
-        //     //console.log(p.name);
-        // })
-        // let headers = [];
-        // let first = this.samples.processesInTimeline[0];
-        // for (let i = 1; i < this.samples.length; i++) {
-        //
-        // }
-         uniqueProcesses.combinedProcessTimeline.forEach(() => null);
+        this.samples = this.samples.filter(s => !s.selected);
     }
 
     chooseExistingSamples() {
@@ -157,7 +96,10 @@ class MCWorkflowAsTableComponentController {
             }
         }).then(
             samplesChosen => {
-                this.samples2 = this.samples2.concat(samplesChosen);
+                // TODO: Update headers using header determination algorithm with added samples
+                // Once the algorithm for creating the headers is written then we need to
+                // use that algorithm here to account for new samples
+                this.samples = this.samples.concat(samplesChosen);
             }
         );
     }
@@ -191,3 +133,51 @@ class ChooseExistingSamplesDialogController {
         this.$mdDialog.cancel();
     }
 }
+
+// this.projectsAPI.getProjectSamples(this.project.id).then(
+//     (samples) => {
+//         this.samples = samples;
+//         this.samples.forEach(s => this.addProcessListTimeLine(s));
+//         let uniqueProcesses = this.computeUniqueProcesses();
+//         this.createHeaders(uniqueProcesses);
+//         //console.log('this.samples', samples);
+//     }
+// );
+// addProcessListTimeLine(sample) {
+//     let processes = _.indexBy(sample.processes, 'process_id');
+//     sample.processesInTimeline = sample.processes.filter(
+//         (p) => processes[p.process_id].property_set_id === p.property_set_id
+//     ).filter(p => {
+//         if (p.template_name == 'Create Samples') {
+//             return false;
+//         } else if (p.template_name == 'Sectioning') {
+//             return false;
+//         }
+//
+//         return true;
+//     }).map(p => ({
+//         name: p.template_name,
+//         process_id: p.process_id,
+//         seen: false
+//     }));
+// }
+//
+// computeUniqueProcesses() {
+//     const allProcesses = [];
+//     this.samples.forEach(s => allProcesses.push(s.processesInTimeline));
+//     let combinedProcessTimeline = _.uniq([].concat.apply([], allProcesses), 'process_id');
+//     let combinedProcessTimelineMap = _.indexBy(combinedProcessTimeline, 'process_id');
+//     return {combinedProcessTimelineMap, combinedProcessTimeline};
+// }
+//
+// createHeaders(uniqueProcesses) {
+//     // uniqueProcesses.combinedProcessTimeline.forEach(p => {
+//     //     //console.log(p.name);
+//     // })
+//     // let headers = [];
+//     // let first = this.samples.processesInTimeline[0];
+//     // for (let i = 1; i < this.samples.length; i++) {
+//     //
+//     // }
+//      uniqueProcesses.combinedProcessTimeline.forEach(() => null);
+// }
