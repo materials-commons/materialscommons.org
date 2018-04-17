0218d645ba3d2cb3ef1cc181675ff3e14026ed2c
Terry Weymouth <weymouth@umich.edu>
2018-04-17 14:49:37 -0400

    Added backend erver to sanity check

--- backend/servers/etlserver/check_sanity.sh
+++ backend/servers/etlserver/check_sanity.sh
@@ -120,6 +120,16 @@ check_worker() {
     fi
 }
 
+check_etlserver() {
+    R=$(ps -eo "pid,command" | grep -v grep | grep python | grep start_etlserver)
+    if [ "$R" = "" ]; then
+        echo "Backend server, etlserver, does not appear to be running <-----"
+        ALL_OK=-1
+    else
+        echo "Backend server, etlserver, appears to be running"
+    fi
+}
+
 check_mc_endpoint() {
     echo "Checking conditions for running Globus clients"
     pushd ${SERVERS}
@@ -166,6 +176,8 @@ check_env_variables
 check_faktory
 # is ELT/Globus worker running
 check_worker
+# is the backend server running
+check_etlserver
 # is MaterialsCommons confidential client endpoint available
 check_mc_endpoint
 
