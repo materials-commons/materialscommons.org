597ce4924ef7bdb2ec3f2ab1d1accecf2357f405
Terry Weymouth <weymouth@umich.edu>
2018-04-27 07:49:58 -0400

    Fixed immediate upload of excel file

---
+++ backend/servers/etlserver/UploadUtility.py
@@ -0,0 +1,99 @@
+import os
+import logging
+from werkzeug.utils import secure_filename
+from flask import request
+from flask_api import status
+
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+
+class UploadUtility:
+    def __init__(self):
+        self.log = logging.getLogger(__name__ + "." + self.__class__.__name__)
+        self.log.info("Starting UploadUtility")
+
+    @staticmethod
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    def get_file(self):
+        # noinspection PyUnusedLocal
+        try:
+            self.log.info("Starting get_file")
+            upload_folder = self.getTmpUploadDir()
+            self.log.info("upload_folder = {}".format(upload_folder))
+            if not os.path.exists(upload_folder):
+                message = "etl file upload - no upload folder: " + upload_folder
+                self.log.info(message)
+                return message, status.HTTP_503_SERVICE_UNAVAILABLE
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                message = "etl file upload - no file"
+                self.log.info(message)
+                return message, status.HTTP_400_BAD_REQUEST
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submits a empty part without filename
+            if file.filename == '':
+                message = "etl file upload - empty file"
+                self.log.info(message)
+                return message, status.HTTP_400_BAD_REQUEST
+            name = request.form.get('name')
+            project_id = request.form.get("project_id")
+            description = request.form.get("description")
+            self.log.info("etl file upload - request data")
+            self.log.info(name)
+            self.log.info(project_id)
+            self.log.info(description)
+            if not name:
+                message = "etl file upload - experiment name missing, required"
+                self.log.info(message)
+                return message, status.HTTP_400_BAD_REQUEST
+            if not project_id:
+                message = "etl file upload - project_id missing, required"
+                self.log.info(message)
+                return message, status.HTTP_400_BAD_REQUEST
+            if not self.allowed_file(file.filename):
+                message = "etl file upload - wrong file extension, must be '*.xlsx'"
+                message += ": " + file.filename
+                self.log.info("etl file upload - file accepted")
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(upload_folder, filename)
+            file.save(file_path)
+            self.log.info("etl file upload - done")
+            return file_path, None
+        except Exception as e:
+            self.log.info("Unexpected exception...", exc_info=True)
+            message = str(e)
+            return message, status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    def getTmpUploadDir(self):
+        base = os.environ['MCDIR']
+        dir = base.split(':')[0]
+        self.log.info("MCDIR = {}".format(dir))
+        return dir
+
+
+
+
+
+
+
+
+
+
+# function getFileStoreDirs() {
+#     let base = process.env.MCDIR;
+#     return base.split(':');
+# }
+#
+# // NOTE: dir for temp uploads should be in same file system
+# // as dir for file store; a rename is used to position the final
+# // version of the upload file - this is called at build time;
+# // ref: line 12 in directories.js
+# function getTmpUploadDir() {
+#     let path = getFileStoreDir() + "uploadTmp/";
+#     mkdirpSync(path);
+#     return path;
+# }
--- backend/servers/etlserver/etl_api_app.py
+++ backend/servers/etlserver/etl_api_app.py
@@ -1,11 +1,10 @@
-import os
 import logging
 import json
 import pkg_resources
 from flask import Flask, request
 from flask_api import status
-from werkzeug.utils import secure_filename
 
+from .UploadUtility import UploadUtility
 from .DatabaseInterface import DatabaseInterface
 from .globus_etl.task_library import startup_and_verify
 from .globus_etl.BuildProjectExperiment import BuildProjectExperiment
@@ -19,7 +18,6 @@ log = logging.getLogger(__name__)
 log.debug("Starting Flask with {}".format(__name__.split('.')[0]))
 
 app = Flask(__name__.split('.')[0])
-ALLOWED_EXTENSIONS = {'xlsx'}
 
 
 def format_as_json_return(what):
@@ -152,63 +150,3 @@ def upload_file():
         message = str(e)
         return message, status.HTTP_500_INTERNAL_SERVER_ERROR
 
-class UploadUtility:
-    def __init__(self):
-        self.log = logging.getLogger(__name__ + "." + self.__class__.__name__)
-        self.log.info("Starting UploadUtility")
-
-    @staticmethod
-    def allowed_file(filename):
-        return '.' in filename and \
-               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-    def get_file(self):
-        # noinspection PyUnusedLocal
-        try:
-            self.log.info("Starting get_file")
-            upload_folder = '/tmp/upload'
-            self.log.info("upload_folder = {}".format(upload_folder))
-            if not os.path.exists(upload_folder):
-                message = "etl file upload - no upload folder: " + upload_folder
-                self.log.info(message)
-                return message, status.HTTP_503_SERVICE_UNAVAILABLE
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                message = "etl file upload - no file"
-                self.log.info(message)
-                return message, status.HTTP_400_BAD_REQUEST
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submits a empty part without filename
-            if file.filename == '':
-                message = "etl file upload - empty file"
-                self.log.info(message)
-                return message, status.HTTP_400_BAD_REQUEST
-            name = request.form.get('name')
-            project_id = request.form.get("project_id")
-            description = request.form.get("description")
-            self.log.info("etl file upload - request data")
-            self.log.info(name)
-            self.log.info(project_id)
-            self.log.info(description)
-            if not name:
-                message = "etl file upload - experiment name missing, required"
-                self.log.info(message)
-                return message, status.HTTP_400_BAD_REQUEST
-            if not project_id:
-                message = "etl file upload - project_id missing, required"
-                self.log.info(message)
-                return message, status.HTTP_400_BAD_REQUEST
-            if not self.allowed_file(file.filename):
-                message = "etl file upload - wrong file extension, must be '*.xlsx'"
-                message += ": " + file.filename
-                self.log.info("etl file upload - file accepted")
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(upload_folder, filename)
-            file.save(file_path)
-            self.log.info("etl file upload - done")
-            return file_path, None
-        except Exception as e:
-            self.log.info("Unexpected exception...", exc_info=True)
-            message = str(e)
-            return message, status.HTTP_500_INTERNAL_SERVER_ERROR
