84b960949264f5a0f09d3c1ea5ed7eb44dc7e0e4
gtarcea <glenn.tarcea@gmail.com>
2018-05-02 11:00:21 -0400

    Pass in files to dataset files
    Start with example files if the dataset has no files so we can test that all the pieces are working

--- website/src/app/views/projects/project.datasets.dataset/project-dataset-view-container.component.js
+++ website/src/app/views/projects/project.datasets.dataset/project-dataset-view-container.component.js
@@ -10,6 +10,40 @@ class MCProjectDatasetViewContainerComponentController {
 
     $onInit() {
         this.state.dataset = angular.copy(this.mcdsstore.getDataset(this.$stateParams.dataset_id));
+        if (!this.state.dataset.files.length) {
+            this.state.dataset.files = [
+                {
+                    selected: false,
+                    name: 'hardeningdata.xls',
+                    path: 'project1/hardening tests',
+                    samples: 'E1XKG'
+                },
+                {
+                    selected: false,
+                    name: 'crack.tiff',
+                    path: 'project1/hardening tests',
+                    samples: 'S1XKG'
+                },
+                {
+                    selected: false,
+                    name: 'sem.odg',
+                    path: 'project1/sem runs/hardening',
+                    samples: 'S1XKG_400'
+                },
+                {
+                    selected: false,
+                    name: 'sem graphs.xls',
+                    path: 'project1/sem runs/hardening',
+                    samples: 'S1XKG_400'
+                },
+                {
+                    selected: false,
+                    name: 'final_results.xls',
+                    path: 'project1/hardening tests/results',
+                    samples: 'S1XKG, S1XKG_400'
+                },
+            ]
+        }
     }
 }
 
