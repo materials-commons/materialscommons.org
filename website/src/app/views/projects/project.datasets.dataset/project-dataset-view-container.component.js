796dbe79caa5b52a6314502a0c61540d8dafd618
gtarcea <glenn.tarcea@gmail.com>
2018-05-03 14:28:15 -0400

    Add logic for adding/deleting files

--- website/src/app/views/projects/project.datasets.dataset/project-dataset-view-container.component.js
+++ website/src/app/views/projects/project.datasets.dataset/project-dataset-view-container.component.js
@@ -1,73 +1,48 @@
 class MCProjectDatasetViewContainerComponentController {
     /*@ngInject*/
-    constructor($stateParams, mcdsstore) {
-        this.$stateParams = $stateParams;
-        this.mcdsstore = mcdsstore;
+    constructor ($stateParams, mcdsstore, datasetsAPI) {
+        this.$stateParams = $stateParams
+        this.mcdsstore = mcdsstore
+        this.datasetsAPI = datasetsAPI
         this.state = {
             dataset: null
-        };
-    }
-
-    $onInit() {
-        this.state.dataset = angular.copy(this.mcdsstore.getDataset(this.$stateParams.dataset_id));
-        if (!this.state.dataset.files || !this.state.dataset.files.length) {
-            this.state.dataset.files = [
-                {
-                    selected: false,
-                    id: 1,
-                    name: 'hardeningdata.xls',
-                    path: 'project1/hardening tests',
-                    samples: 'E1XKG'
-                },
-                {
-                    selected: false,
-                    id: 2,
-                    name: 'crack.tiff',
-                    path: 'project1/hardening tests',
-                    samples: 'S1XKG'
-                },
-                {
-                    selected: false,
-                    id: 3,
-                    name: 'sem.odg',
-                    path: 'project1/sem runs/hardening',
-                    samples: 'S1XKG_400'
-                },
-                {
-                    selected: false,
-                    id: 4,
-                    name: 'sem graphs.xls',
-                    path: 'project1/sem runs/hardening',
-                    samples: 'S1XKG_400'
-                },
-                {
-                    selected: false,
-                    id: 5,
-                    name: 'final_results.xls',
-                    path: 'project1/hardening tests/results',
-                    samples: 'S1XKG, S1XKG_400'
-                },
-                {
-                    selected: false,
-                    id: 6,
-                    name: 'final_results_22.xls',
-                    path: 'project1/hardening tests/results',
-                    samples: 'S1XKG, S1XKG_400'
-                },
-            ];
         }
     }
 
-    handleDeleteFiles(filesToDelete) {
-        // Figure out if this object needs to keep track of changes
-        // let filesMap = _.indexBy(filesToDelete, 'id');
-        // this.state.dataset.files = this.state.dataset.files.filter(f => (!(f.id in filesMap)));
+    $onInit () {
+        this.state.dataset = angular.copy(this.mcdsstore.getDataset(this.$stateParams.dataset_id))
+    }
+
+    handleDeleteFiles (filesToDelete) {
+        let filesMap = _.indexBy(filesToDelete, 'id'),
+            fileIds = filesToDelete.map(f => f.id)
+        this.datasetsAPI.deleteFilesFromProjectDataset(this.$stateParams.project_id, this.$stateParams.dataset_id, fileIds)
+            .then(
+                () => {
+                    this.state.dataset.files = this.state.dataset.files.filter(f => (!(f.id in filesMap)))
+                    this.state.dataset = angular.copy(this.state.dataset)
+                }
+            )
+    }
+
+    handleAddFiles (filesToAdd) {
+        let existingFiles = _.indexBy(this.state.dataset.files, 'id'),
+            newFiles = filesToAdd.filter(f => (!(f.id in existingFiles))),
+            newFileIds = newFiles.map(f => f.id)
+        this.datasetsAPI.addFilesToProjectDataset(this.$stateParams.project_id, this.$stateParams.dataset_id, newFileIds)
+            .then(
+                () => {
+                    this.state.dataset.files = this.state.dataset.files.concat(newFiles)
+                    this.state.dataset = angular.copy(this.state.dataset)
+                }
+            )
     }
 }
 
 angular.module('materialscommons').component('mcProjectDatasetViewContainer', {
-    template: `
-        <mc-project-dataset-view dataset="$ctrl.state.dataset" layout-fill ng-if="$ctrl.state.dataset">
-        </mc-project-dataset-view>`,
+    template: `<mc-project-dataset-view dataset="$ctrl.state.dataset" 
+                                    on-delete-files="$ctrl.handleDeleteFiles(filesToDelete)"
+                                    on-add-files="$ctrl.handleAddFiles(filesToAdd)"
+                                    layout-fill ng-if="$ctrl.state.dataset"></mc-project-dataset-view>`,
     controller: MCProjectDatasetViewContainerComponentController
-});
+})
