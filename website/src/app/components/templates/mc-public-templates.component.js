628350d7e57cbaf5a096a11810ed50db8a7ec3df
Terry Weymouth <weymouth@umich.edu>
2018-05-07 11:55:05 -0400

    Working version of public tempates - except for backend - login is still required for backend to work

--- website/src/app/components/templates/mc-public-templates.component.js
+++ website/src/app/components/templates/mc-public-templates.component.js
@@ -1,8 +1,9 @@
 class MCPublicTemplatesComponentController {
     /*@ngInject*/
-    constructor(templatesAPI) {
+    constructor(templatesAPI,$mdDialog) {
         console.log("MCPublicTemplatesComponentController - constructor");
         this.templatesAPI = templatesAPI;
+        this.$mdDialog = $mdDialog;
         this.templates = null;
         this.templateLoaded = false;
     }
@@ -14,6 +15,8 @@ class MCPublicTemplatesComponentController {
                 console.log("MCPublicTemplatesComponentController - templates", templates.length);
                 let index = 0;
                 for (var i = 0; i < templates.length; i++) {
+                    templates[i].n_setup = templates[i].setup[0].properties.length;
+                    templates[i].n_measurement = templates[i].measurements.length;
                     if (templates[i].name === "SEM") {
                         index = i;
                     }
@@ -26,7 +29,24 @@ class MCPublicTemplatesComponentController {
     }
 
     showTemplateDetails(template) {
-        console.log("Show details for", template.name);
+        console.log("Show details for", template.name, template.otype);
+        for (var i = 0; i < template.setup[0].properties.length; i++) {
+            let attribute = template.setup[0].properties[i];
+            console.log("t", attribute.attribute, attribute.choices, attribute.units);
+        }
+        for (i = 0; i < template.measurements.length; i++) {
+            let attribute = template.measurements[i];
+            console.log("m", attribute.attribute, attribute.choices, attribute.units);
+        }
+        this.$mdDialog.show({
+            templateUrl: 'app/modals/view-template-attributes-dialog.html',
+            controller: ViewPublicTemplateDialogController,
+            controllerAs: '$ctrl',
+            bindToController: true,
+            locals: {
+                template: template
+            }
+        });
     }
 
 }
@@ -35,3 +55,14 @@ angular.module('materialscommons').component('mcPublicTemplates', {
     template: require('./mc-public-templates.html'),
     controller: MCPublicTemplatesComponentController
 });
+
+class ViewPublicTemplateDialogController {
+    /*@ngInject*/
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+    }
+
+    dismiss() {
+        this.$mdDialog.hide();
+    }
+}
