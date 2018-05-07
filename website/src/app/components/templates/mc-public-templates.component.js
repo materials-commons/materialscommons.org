f8aa6a4122c43829b6cee72cbacf7d25e6b591b0
Terry Weymouth <weymouth@umich.edu>
2018-05-07 08:37:31 -0400

    First pass a public template view

--- website/src/app/components/templates/mc-public-templates.component.js
+++ website/src/app/components/templates/mc-public-templates.component.js
@@ -3,16 +3,30 @@ class MCPublicTemplatesComponentController {
     constructor(templatesAPI) {
         console.log("MCPublicTemplatesComponentController - constructor");
         this.templatesAPI = templatesAPI;
+        this.templates = null;
+        this.templateLoaded = false;
     }
 
     $onInit() {
         console.log("MCPublicTemplatesComponentController - init");
         this.templatesAPI.getAllTemplates().then(
             (templates) => {
-                console.log("MCPublicTemplatesComponentController - templates", templates);
+                console.log("MCPublicTemplatesComponentController - templates", templates.length);
+                let index = 0;
+                for (var i = 0; i < templates.length; i++) {
+                    if (templates[i].name === "SEM") {
+                        index = i;
+                    }
+                }
+                console.log(index, templates[index]);
                 this.templates = templates;
+                this.templateLoaded = true;
             }
         );
+    }
+
+    showTemplateDetails(template) {
+        console.log("Show details for", template.name);
     }
 
 }
