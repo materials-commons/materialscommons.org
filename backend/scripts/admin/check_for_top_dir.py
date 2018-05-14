ce479c238f13fe66b2c6d132fc3edcce8b2e14c8
Terry Weymouth <weymouth@umich.edu>
2018-05-14 14:35:24 -0400

    Added flag to include delete files in search for top_level_dir mismatch - excluded by default

--- backend/scripts/admin/check_for_top_dir.py
+++ backend/scripts/admin/check_for_top_dir.py
@@ -9,7 +9,7 @@ def run(rql):
         return None
 
 
-def main(port):
+def main(port, include_deleted):
     conn = r.connect('localhost', port, db='materialscommons')
     cursor = r.table('project2datadir')\
         .eq_join('datadir_id',r.table('datadirs'))\
@@ -23,6 +23,9 @@ def main(port):
     for doc in cursor:
         project_name = doc['name']
         dir_name = doc['name2']
+        owner = doc['owner']
+        if (owner == 'delete@materialscommons.org') and not include_deleted:
+            continue
         if len(dir_name.split('/')) == 1:
             if not project_name == dir_name:
                 print("Project '{}'({})".format(project_name, doc['project_id']))
@@ -32,10 +35,20 @@ def main(port):
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-P", "--port", dest="port", type="int", help="rethinkdb port", default=30815)
+    parser.add_option("-I", "--include-deleted", dest="incd", action="store_true", help="include deleted files", default=False)
+
     (options, args) = parser.parse_args()
+
+    include_deleted = options.incd
     port = options.port
     print("Using database port = {}".format(port))
-    main(port)
+
+    if include_deleted:
+        print("Including deleted files in search")
+    else:
+        print("Excluding deleted files from search")
+
+    main(port, include_deleted)
 
 
 
