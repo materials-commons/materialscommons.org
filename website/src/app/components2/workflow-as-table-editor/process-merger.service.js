78fbb5b065d00139ca92e8e093b894f316b6dacd
gtarcea <glenn.tarcea@gmail.com>
2018-07-10 13:10:33 -0400

    Add code for merging two trees

--- website/src/app/components2/workflow-as-table-editor/process-merger.service.js
+++ website/src/app/components2/workflow-as-table-editor/process-merger.service.js
@@ -159,20 +159,24 @@ class ProcessMergerService {
 
         let workflows = {};
         roots.forEach(tree => {
-            //console.log('=======tree=============')
             let s = '';
             tree.walk(node => {
-                //console.log(node.model.model.template_id);
                 s += node.model.model.template_id;
+                node.model.model.node_id = s.slice(0);
             });
             const hash = this.hashCode(s);
-            //console.log('hash', hash);
+            tree.hash = hash;
             workflows[hash] = tree;
         });
 
-        //console.log('roots length', roots.length);
-        //console.log('workflows length', _.size(workflows));
+        roots.forEach(tree => {
+            if (tree !== workflows[tree.hash]) {
+                let treeToAddTo = workflows[tree.hash];
+                this.addMissingSamples(tree, treeToAddTo);
+            }
+        });
 
+        console.log('workflows', workflows);
         return workflows;
     }
 
@@ -181,6 +185,78 @@ class ProcessMergerService {
             a = ((a << 5) - a) + b.charCodeAt(0);
             return a & a;
         }, 0);
+    }
+
+    addMissingSamples(fromTree, toTree) {
+        console.log('addMissingSamples');
+        fromTree.walk(node => {
+            let id = node.model.model.node_id;
+            let addTo = toTree.first(n => id === n.model.model.node_id);
+            let samplesToAdd = [];
+            node.model.model.input_samples.forEach(s => {
+                if (_.findIndex(addTo.model.model.input_samples, sample => (sample.id === s.id && sample.property_set_id === s.property_set_id)) === -1) {
+                    samplesToAdd.push(s);
+                }
+            });
+            samplesToAdd.forEach(s => addTo.model.model.input_samples.push(s));
+
+            samplesToAdd.length = 0;
+            node.model.model.output_samples.forEach(s => {
+                if (_.findIndex(addTo.model.model.output_samples, sample => (sample.id === s.id && sample.property_set_id === s.property_set_id)) === -1) {
+                    samplesToAdd.push(s);
+                }
+            });
+            samplesToAdd.forEach(s => addTo.model.model.output_samples.push(s));
+            console.log('addTo', addTo);
+        });
+    }
+
+    //=====================================
+
+// Update the size of n1 to that of n2
+    updateSize(n1, n2) {
+        if (n2.model.size) {
+            n1.model.size = n2.model.size;
+        } else {
+            delete n1.model.size;
+        }
+    }
+
+// Check if n1 and n2 have the same id
+    idEq(n1) {
+        return function(n2) {
+            return n1.model.id === n2.model.id;
+        };
+    }
+
+// Check if n1 contains the given n2 child
+    hasChild(n1) {
+        return function(n2Child) {
+            return n1.children.some(idEq(n2Child));
+        };
+    }
+
+    mergeNodes(n1, n2) {
+        var n1HasN2Child, i, n2Child, n1Child;
+
+        // Update the sizes
+        this.updateSize(n1, n2);
+
+        // Check which n2 children are present in n1
+        n1HasN2Child = n2.children.map(this.hasChild(n1));
+
+        // Iterate over n2 children
+        for (i = 0; i < n1HasN2Child.length; i++) {
+            n2Child = n2.children[i];
+            if (n1HasN2Child[i]) {
+                // n1 already has this n2 child, so lets merge them
+                n1Child = n1.first({strategy: 'breadth'}, this.idEq(n2Child));
+                this.mergeNodes(n1Child, n2Child);
+            } else {
+                // n1 does not have this n2 child, so add it
+                n1.addChild(n2Child);
+            }
+        }
     }
 
 }
