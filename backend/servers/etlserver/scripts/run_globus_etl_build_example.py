6347bdc3295797d2dffcbb28b9b87e88b7602f71
Terry Weymouth <weymouth@umich.edu>
2018-07-10 07:00:39 -0400

    Working copy of all Globus backend parts

--- backend/servers/etlserver/scripts/run_globus_etl_build_example.py
+++ backend/servers/etlserver/scripts/run_globus_etl_build_example.py
@@ -1,22 +1,31 @@
-import logging
 import sys
-import argparse
 import time
+import logging
+import argparse
 
 from ..common.TestProject import TestProject
-from ..globus_non_etl_upload.non_etl_task_library import startup_and_verify
 from ..database.BackgroundProcess import BackgroundProcess
 from ..database.DatabaseInterface import DatabaseInterface
 
-def main(project, endpoint):
-    main_log = logging.getLogger("main")
-    main_log.info("Starting all file Globus upload. Project = {} ({})".
-                  format(project.name, project.id))
-    main_log.info("... Globus endpoint id = {}".format(endpoint))
 
-    results = startup_and_verify(project.owner, project.id, endpoint)
+def main(project, globus_endpoint, excel_file_path, data_dir_path):
+    from ..globus_etl.etl_task_library import startup_and_verify
+    main_log = logging.getLogger("top_level_run_ELT_example")
 
-    main_log.info("Startup and verify results = {}".format(results))
+    user_id = "test@test.mc"
+    experiment_name = "Test from excel"
+    experiment_description = "An experiment built via etl from test data"
+
+    main_log.info("user_id = " + user_id)
+    main_log.info("project = '{}' ({})".format(project.name, project.id))
+    main_log.info("experiment_name = " + experiment_name)
+    main_log.info("experiment_description = " + experiment_description)
+    main_log.info("globus_endpoint = " + globus_endpoint)
+    main_log.info("excel_file_path = " + excel_file_path)
+    main_log.info("data_dir_path = " + data_dir_path)
+
+    results = startup_and_verify(user_id, project.id, experiment_name, experiment_description,
+                                 globus_endpoint, excel_file_path, data_dir_path)
 
     if not results['status'] == "SUCCESS":
         main_log.error("Setup failed.")
@@ -38,7 +47,6 @@ def main(project, endpoint):
 
 
 if __name__ == "__main__":
-
     root = logging.getLogger()
     root.setLevel(logging.INFO)
 
@@ -61,8 +69,11 @@ if __name__ == "__main__":
         logging.getLogger(name).setLevel(logging.ERROR)
 
     argv = sys.argv
-    parser = argparse.ArgumentParser(description='Test of Globus non-ETL upload')
-    parser.add_argument('--endpoint', type=str, help="Globus shared endpoint id")
+    parser = argparse.ArgumentParser(
+        description='Run the Globus/ETL process with test data - in a predefined test endpoint')
+    parser.add_argument('--endpoint', type=str, help="Globus Endpoint")
+    parser.add_argument('--input', type=str, help="Input Spreadsheet File (path relative to endpoint)")
+    parser.add_argument('--data', type=str, help="Data Directory (path relative to endpoint)")
     args = parser.parse_args(argv[1:])
 
     if not args.endpoint:
@@ -70,10 +81,21 @@ if __name__ == "__main__":
         parser.print_help()
         exit(-1)
 
-    project = TestProject().get_project()
+    if not args.input:
+        print("You must specify the path for the spreadsheet file (relative to the endpoint). Argument not found.")
+        parser.print_help()
+        exit(-1)
 
-    startup_log.info("args: endpoint = {}".format(args.endpoint))
+    if not args.endpoint:
+        print("You must specify the path for the data directory (relative to the endpoint). Argument not found.")
+        parser.print_help()
+        exit(-1)
+
+    generated_project = TestProject().get_project()
+
+    startup_log.info("args: endpoint = {}; input = {}; data={}"
+                     .format(args.endpoint, args.input, args.data))
     startup_log.info("generated test project - name = {}; id = {}".
-                     format(project.name, project.id))
+                     format(generated_project.name, generated_project.id))
 
-    main(project, args.endpoint)
+    main(generated_project, args.endpoint, args.input, args.data)
