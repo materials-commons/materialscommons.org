05081c7d1def169d13711061124608cb49e24720
Terry Weymouth <weymouth@umich.edu>
2018-08-02 10:08:09 -0400

    Snapshot

--- backend/servers/etlserver/scripts/check_mc_globus_status.py
+++ backend/servers/etlserver/scripts/check_mc_globus_status.py
@@ -2,44 +2,68 @@ import os
 import logging
 import sys
 import argparse
+from random import randint
 from materials_commons.api import get_all_projects
 from ..common.MaterialsCommonsGlobusInterface import MaterialsCommonsGlobusInterface
 from ..common.GlobusInfo import GlobusInfo
+from ..common.McdirHelper import McdirHelper
 from globus_sdk.exc import GlobusAPIError
 
 
-def main(project, globus_user_id, globus_endpoint_id, datadir):
+def fake_name(prefix):
+    number = "%05d" % randint(0, 99999)
+    return prefix + number
 
-    mcdir = os.environ.get('MCDIR')
-    mcdb_port = os.environ.get('MCDB_PORT')
-    client_user = os.environ.get('MC_CONFIDENTIAL_CLIENT_USER')
-    client_token = os.environ.get('MC_CONFIDENTIAL_CLIENT_PW')
-    mc_target_upload_ep_id = os.environ.get('MC_CONFIDENTIAL_CLIENT_ENDPOINT')
-    mc_target_download_ep_id = os.environ.get('MC_DOWNLOAD_ENDPOINT_ID')
 
-    if (not mcdir) or (not mcdb_port) \
-            or (not client_user) or (not client_token) \
-            or (not mc_target_upload_ep_id) or (not mc_target_download_ep_id):
-        missing = []
-        if not mcdir:
-            missing.append('MCDIR')
-        if not mcdb_port:
-            missing.append('MCDB_PORT')
-        if not client_user:
-            missing.append('MC_CONFIDENTIAL_CLIENT_USER')
-        if not client_token:
-            missing.append('MC_CONFIDENTIAL_CLIENT_PW')
-        if not mc_target_upload_ep_id:
-            missing.append("MC_CONFIDENTIAL_CLIENT_ENDPOINT")
-        if not mc_target_download_ep_id:
-            missing.append("MC_DOWNLOAD_ENDPOINT_ID")
+def main(project, globus_user_id, globus_endpoint_id, local_endpoint_dir, test_file_path):
+
+    env_list = ['MCDIR', 'MCDB_PORT', 'MC_CONFIDENTIAL_CLIENT_USER', 'MC_CONFIDENTIAL_CLIENT_PW',
+                'MC_CONFIDENTIAL_CLIENT_ENDPOINT', 'MC_DOWNLOAD_ENDPOINT_ID', 'MC_API_URL']
+
+    print("")
+    env_values = {}
+    missing = []
+    for env_name in env_list:
+        value = os.environ.get(env_name)
+        env_values[env_name] = value
+        if not value:
+            missing.append(env_name)
+    if missing:
         message = "Missing environment values: {}".format(", ".join(missing))
         print(message)
         exit(-1)
+    print("Environment variables:")
+    for key in env_values:
+        print("  {} = {}".format(key, env_values[key]))
 
-    interface = MaterialsCommonsGlobusInterface("does-not-matter")
+    print("")
     try:
-        interface.set_transfer_client()
+        source = GlobusInfo()
+        returned_info = source.get_all()
+        print("Basic Globus Information:")
+        for key in returned_info:
+            print("  Details from info: {} = {}". format(key, returned_info[key]))
+    except GlobusAPIError as e1:
+        http_status = e1.http_status
+        code = e1.code
+        details = e1.message
+        message = "Unable to connect to the Globus Connection server (based on configuration information): "
+        message += " http_status = " + str(http_status)
+        message += ", code = " + code
+        message += ", message = " + details
+        print(message)
+        exit(-1)
+
+    print("")
+    interface = MaterialsCommonsGlobusInterface(project.owner)
+    try:
+        status = interface.set_transfer_client()
+        if status['status'] == 'ok':
+            print("Globus Transfer client is available; checks: " +
+                  "MC_CONFIDENTIAL_CLIENT_USER, and MC_CONFIDENTIAL_CLIENT_PW")
+        else:
+            print("Some problem with creating the Globus Transfer Client!")
+            exit(-1)
     except GlobusAPIError as e:
         http_status = e.http_status
         code = e.code
@@ -51,16 +75,18 @@ def main(project, globus_user_id, globus_endpoint_id, datadir):
         print(message)
         exit(-1)
 
+    print("")
+    print("Testing upload...")
+    print("  Copy test file to local endpoint dir")
+    # get file and type
+    # copy and rename file
+    # 
     try:
-        source = GlobusInfo()
-        returned_info = source.get_all()
-        print("\nBasic Globus Information:")
-        for key in returned_info:
-            print("Details from info: {} = {}". format(key, returned_info[key]))
-    except GlobusAPIError as e1:
-        http_status = e1.http_status
-        code = e1.code
-        details = e1.message
+        pass
+    except GlobusAPIError as e:
+        http_status = e.http_status
+        code = e.code
+        details = e.message
         message = "Unable to connect to the Globus Connection server (based on configuration information): "
         message += " http_status = " + str(http_status)
         message += ", code = " + code
@@ -97,11 +123,18 @@ if __name__ == "__main__":
 
     argv = sys.argv
     parser = argparse.ArgumentParser(description='Test of configuration of Materials Commons Globus interface')
-    parser.add_argument('--name', type=str, help="The unique name of a Materials Commons Project. Partial name ok.")
-    parser.add_argument('--userid', type=str, help="A Globus user id/name - for download testing")
-    parser.add_argument('--endpoint', type=str, help="A Globus source endpoint that is shared transfer client - for upload testing")
-    parser.add_argument('--apikey', type=str, help="Materials Commons apikey, the owner of the project")
-    parser.add_argument('--testdata', type=str, help="The path to a directory with a single small file for transfer testing")
+    parser.add_argument('--name', type=str,
+                        help="The unique name of a Materials Commons Project. Partial name ok.")
+    parser.add_argument('--userid', type=str,
+                        help="A Globus user id/name - for download testing")
+    parser.add_argument('--endpoint', type=str,
+                        help="A Globus source endpoint that is shared with transfer client - for upload testing")
+    parser.add_argument('--localdir', type=str,
+                        help="The path to the directory of --endpoint")
+    parser.add_argument('--apikey', type=str,
+                        help="Materials Commons apikey, the owner of the project")
+    parser.add_argument('--testdata', type=str,
+                        help="The path to a directory with a single small file for transfer testing")
     args = parser.parse_args(argv[1:])
 
     local_log.info("Searching for project with name-match = {}".format(args.name))
@@ -111,14 +144,38 @@ if __name__ == "__main__":
     try:
         datadir = os.path.abspath(args.testdata)
         if not os.path.isdir(datadir):
-            print("The given path is not a directory = {}".format(datadir))
+            print("The given path for --testdata is not a directory = {}".format(datadir))
             datadir = None
     except TypeError:
-        print("Coould not convert into path; test data = {}".format(args.testdata))
+        print("Could not convert into path; test data = {}".format(args.testdata))
         datadir = None
 
     if not datadir:
         parser.print_help()
+        exit(-1)
+
+    local_endpoint_path = None
+    try:
+        local_endpoint_path = os.path.abspath(args.localdir)
+        if not os.path.isdir(local_endpoint_path):
+            print("The given path for --localdir is not a directory = {}".format(local_endpoint_path))
+            local_endpoint_path = None
+    except TypeError:
+        print("Could not convert into path; test data = {}".format(args.localdir))
+        local_endpoint_path = None
+
+    if not local_endpoint_path:
+        parser.print_help()
+        exit(-1)
+
+    datafile_path = None
+    content = os.listdir(datadir)
+    for entry in content:
+        if os.path.isfile(os.path.join(datadir, entry)):
+            datafile_path = os.path.join(datadir, entry)
+            break
+    if not datafile_path:
+        print("Could not find a file in the datadir: {}".format(datadir))
         exit(-1)
 
     project_selected = None
@@ -143,6 +200,7 @@ if __name__ == "__main__":
     local_log.info("Additional test values: ")
     local_log.info("   globus user name   = {}".format(args.name))
     local_log.info("   globus endpoint id = {}".format(args.endpoint))
-    local_log.info("   globus user name = {}".format(datadir))
+    local_log.info("   local path to globus endpoint = {}".format(args.localdir))
+    local_log.info("   path to test file = {}".format(datafile_path))
 
-    main(project_selected, args.name, args.endpoint, datadir)
+    main(project_selected, args.name, args.endpoint, local_endpoint_path, datafile_path)
