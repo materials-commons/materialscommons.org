c06e207d94e665667b2a293cad1c6876fe913c8a
Terry Weymouth <weymouth@umich.edu>
2018-08-14 10:01:09 -0400

    Admin pages - added user == admin guard to admin info page dispaly to forstall direct access with url, e.g. http://host/#/admin/info/users

--- website/src/app/components/admin/mc-admin-info/mc-admin-info.component.js
+++ website/src/app/components/admin/mc-admin-info/mc-admin-info.component.js
@@ -1,3 +1,17 @@
+class MCAdminInfoComponentController {
+    /*@ngInject*/
+    constructor(User, toast) {
+        console.log("Constructor - MCAdminInfoComponentController");
+        this.isAdmin = false;
+        if (User.isAuthenticated()) {
+            this.isAdmin = User.attr().admin;
+        }
+        console.log(this.isAdmin);
+        this.toast = toast;
+    }
+}
+
 angular.module('materialscommons').component('mcAdminInfo', {
     template: require('./mc-admin-info.html'),
+    controller: MCAdminInfoComponentController
 });
