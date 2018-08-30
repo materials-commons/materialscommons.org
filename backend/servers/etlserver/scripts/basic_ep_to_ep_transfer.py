e76f4efbf2216512e9cf66800c2dfcfcedfd0ee4
gtarcea <glenn.tarcea@gmail.com>
2018-08-29 22:26:04 -0400

    Add acl setting/deleting support

--- backend/servers/etlserver/scripts/basic_ep_to_ep_transfer.py
+++ backend/servers/etlserver/scripts/basic_ep_to_ep_transfer.py
@@ -12,13 +12,16 @@ from ..common.McdirHelper import McdirHelper
 from ..common.MaterialsCommonsGlobusInterfaceNew import MaterialsCommonsGlobusInterfaceNew
 from ..database.DatabaseInterface import DatabaseInterface
 
-SOURCE_ENDPOINT = '2567c5aa-aaca-11e8-9704-0a6d4e044368'  # 'e1a3e368-aa26-11e8-9704-0a6d4e044368'  # '85908598-a7cb-11e8-9700-0a6d4e044368'
+# 1960* is the endpoint on titan laptop
+SOURCE_ENDPOINT = '1960ad4c-aaf2-11e8-970a-0a6d4e044368'  # '2567c5aa-aaca-11e8-9704-0a6d4e044368'  # 'e1a3e368-aa26-11e8-9704-0a6d4e044368'  # '85908598-a7cb-11e8-9700-0a6d4e044368'
+
 
 class EpEpTransferHelper:
     def __init__(self, source_endpoint, project_id, user_id):
         self.source_endpoint = source_endpoint
         self.project_id = project_id
         self.user_id = user_id
+        self.dest_path = ""
         self.log = logging.getLogger(self.__class__.__name__)
         self.worker_base_path = McdirHelper().get_upload_dir()
         self.mc_interface = MaterialsCommonsGlobusInterfaceNew(user_id)
@@ -48,6 +51,8 @@ class EpEpTransferHelper:
         random_key = "{0:04d}".format(randint(0, 10000))
         transfer_dir = self.make_transfer_dir(random_key)
         transfer_base_path = os.path.join(base_path, transfer_dir)
+        self.dest_path = "/{}".format(transfer_dir)
+        self.log.info("dest_path is {}".format(self.dest_path))
         os.mkdir(transfer_base_path)
         self.log.info("transfer_base_path = {}".format(transfer_base_path))
 
@@ -74,11 +79,28 @@ class EpEpTransferHelper:
                                      label='Test-transfer-for-{}'.format(transfer_dir))
         self.log.info("Object transfer_data = {}".format(transfer_data))
         transfer_data.add_item(source_path='/',
-                               destination_path="/{}".format(transfer_dir),
+                               destination_path=self.dest_path,
                                recursive=True)
         self.log.info("Object transfer_data = {}".format(transfer_data))
         self.transfer_client.endpoint_autoactivate(self.source_endpoint)
         self.transfer_client.endpoint_autoactivate(self.mc_target_ep_id)
+
+        try:
+            self.transfer_client.add_endpoint_acl_rule(
+                self.mc_target_ep_id,
+                dict(principal=self.user_id,
+                     principal_type='identity', path=self.dest_path, permissions='rw'),
+            )
+        except TransferAPIError as error:
+            # PermissionDenied can happen if a new Portal client is swapped
+            # in and it doesn't have endpoint manager on the dest_ep.
+            # The /portal/processed directory has been set to to read/write
+            # for all users so the subsequent operations will succeed.
+            if error.code == 'PermissionDenied':
+                pass
+            elif error.code != 'Exists':
+                raise
+
         self.log.info("Before submit transfer")
         results = self.transfer_client.submit_transfer(transfer_data)
         self.log.info("After submit transfer: {}".format(results))
@@ -89,6 +111,22 @@ class EpEpTransferHelper:
             status = task['status']
             self.log.info("Current task status = {}".format(status))
             time.sleep(5)
+
+        try:
+            acl = next(acl for acl in self.transfer_client.endpoint_acl_list(self.mc_target_ep_id)
+                       if self.dest_path == acl['path'])
+        except StopIteration:
+            pass
+        except TransferAPIError as ex:
+            # PermissionDenied can happen if a new Portal client is swapped
+            # in and it doesn't have endpoint manager on the dest_ep.
+            # The /portal/processed directory has been set to to writeable
+            # for all users so the delete task will succeed even if an ACL
+            # can't be set.
+            if ex.code == 'PermissionDenied':
+                pass
+        else:
+            self.transfer_client.delete_endpoint_acl_rule(self.mc_target_ep_id, acl['id'])
 
     def get_transfer_client(self, transfer_tokens, endpoint_id, endpoint_path='/'):
         materials_commons_client = self.mc_interface.get_auth_client()
