5e4bc8a636236e11cd06faac4f73f3f248038d6e
Terry Weymouth <weymouth@umich.edu>
2018-09-10 15:03:24 -0400

    Pulled out for ACL Rule test: basic_set_acl.py; updated background work

--- backend/servers/etlserver/scripts/basic_ep_to_ep_transfer.py
+++ backend/servers/etlserver/scripts/basic_ep_to_ep_transfer.py
@@ -2,156 +2,160 @@
 import os
 import sys
 import logging
-import time
+# import time
 import argparse
-from random import randint
+# from random import randint
 
 from globus_sdk import (RefreshTokenAuthorizer, TransferClient, TransferAPIError,
                         TransferData)
 from ..utils.LoggingHelper import LoggingHelper
-from ..common.McdirHelper import McdirHelper
-from ..common.MaterialsCommonsGlobusInterfaceNew import MaterialsCommonsGlobusInterfaceNew
 from ..database.DatabaseInterface import DatabaseInterface
+from ..common.GlobusAccess import GlobusAccess, CONFIDENTIAL_CLIENT_APP_AUTH
 
-
-# SOURCE_ENDPOINT = '1960ad4c-aaf2-11e8-970a-0a6d4e044368' # titan laptop
-# SOURCE_ENDPOINT = '2567c5aa-aaca-11e8-9704-0a6d4e044368'
-# SOURCE_ENDPOINT = 'e1a3e368-aa26-11e8-9704-0a6d4e044368'
-# SOURCE_ENDPOINT = '85908598-a7cb-11e8-9700-0a6d4e044368'
-# SOURCE_ENDPOINT = '52d6a200-ab99-11e8-9710-0a6d4e044368'
-
+DESTINATION_BASE_PATH = '/mcdir/landing_for_test/'
 
 
 class EpEpTransferHelper:
-    def __init__(self, mc_user_id, globus_source_endpoint, dest_endpoint):
+    def __init__(self, mc_user_id, globus_users_source_endpoint, globus_users_source_path, dest_endpoint):
         self.log = logging.getLogger(self.__class__.__name__)
         self.user_id = mc_user_id
-        self.source_endpoint = globus_source_endpoint
+        self.source_endpoint = globus_users_source_endpoint
+        self.source_path = globus_users_source_path
 
-        self.worker_base_path = McdirHelper().get_upload_dir()
-        self.mc_interface = MaterialsCommonsGlobusInterfaceNew(mc_user_id)
+        self.globus_access = GlobusAccess(use_implementation=CONFIDENTIAL_CLIENT_APP_AUTH)
+
         self.client_user = os.environ.get('MC_CONFIDENTIAL_CLIENT_USER')
         self.client_token = os.environ.get('MC_CONFIDENTIAL_CLIENT_PW')
-        self.mc_target_ep_id = dest_endpoint
-        self.transfer_client = None
+        self.target_endpoint = dest_endpoint
         self.source_user_globus_id = None
-        self.dest_path = ""
 
     def do_transfer(self):
         self.log.info('Transfer for user_id = {}'.format(self.user_id))
-        if (not self.client_user) or (not self.client_token) or (not self.mc_target_ep_id):
+        if (not self.client_user) or (not self.client_token) or (not self.target_endpoint):
             missing = []
             if not self.client_user:
                 missing.append('MC_CONFIDENTIAL_CLIENT_USER')
             if not self.client_token:
                 missing.append('MC_CONFIDENTIAL_CLIENT_PW')
-            if not self.mc_target_ep_id:
+            if not self.target_endpoint:
                 missing.append("MC_CONFIDENTIAL_CLIENT_ENDPOINT")
             message = "Missing environment values: {}".format(", ".join(missing))
             raise EnvironmentError(message)
 
         self.log.info('Confidential Client, user_id = {}'.format(self.client_user))
+
+        base_path = DESTINATION_BASE_PATH
+        # random_key = "{0:04d}".format(randint(0, 10000))
+        # transfer_dir = self.make_transfer_dir(random_key)
+        # transfer_base_path = os.path.join(base_path, transfer_dir)
+        # destination_path = "{}/".format(transfer_base_path)
+        destination_path = base_path
+
         self.log.info('Source endpoint = {}'.format(self.source_endpoint))
-        self.log.info('Target endpoint = {}'.format(self.mc_target_ep_id))
+        self.log.info('Source path = {}'.format(self.source_path))
+        self.log.info('Target endpoint = {}'.format(self.target_endpoint))
+        self.log.info('Target path = {}'.format(destination_path))
 
-        base_path = self.worker_base_path
-        random_key = "{0:04d}".format(randint(0, 10000))
-        transfer_dir = self.make_transfer_dir(random_key)
-        transfer_base_path = os.path.join(base_path, transfer_dir)
-        # self.dest_path = "/{}/".format(transfer_dir)
-        self.dest_path = "/"
-        self.log.info("dest_path is {}".format(self.dest_path))
-        os.mkdir(transfer_base_path)
-        self.log.info("transfer_base_path = {}".format(transfer_base_path))
-
-        records = DatabaseInterface().get_globus_auth_info_records_by_user_id(self.user_id)
-        # Only the latest
-        record = (records[0] if len(records) > 0 else None)
-        if not record:
-            self.log.info("Globus auth info record does not exist; logged out?")
+        cc_transfer_client = self.globus_access.get_transfer_client()
+        user_transfer_client = self.get_user_transfer_client() # also sets self.source_user_globus_id
+        if not user_transfer_client:
+            self.log.error("Unable to create User's Globus Transfer Client")
             return
 
-        # else
-        self.source_user_globus_id = record['globus_id']
-        transfer_tokens = record['tokens']['transfer.api.globus.org']
-        self.log.info("transfer.api.globus.org tokens = {}".format(transfer_tokens))
-        self.transfer_client = \
-            self.get_transfer_client(transfer_tokens, self.source_endpoint)
-        if not self.transfer_client:
-            self.log.error("Transfer Client is not available; abort")
-            return
+        cc_transfer_client.endpoint_autoactivate(self.target_endpoint)
+        ep = cc_transfer_client.get_endpoint(self.target_endpoint)
 
-        # else
-        transfer_data = TransferData(transfer_client=self.transfer_client,
-                                     source_endpoint=self.source_endpoint,
-                                     destination_endpoint=self.mc_target_ep_id,
-                                     label='Test-transfer-for-{}'.format(transfer_dir))
-        self.log.info("Object transfer_data = {}".format(transfer_data))
-        transfer_data.add_item(source_path='/',
-                               destination_path=self.dest_path,
-                               recursive=True)
-        self.log.info("Object transfer_data = {}".format(transfer_data))
-        self.transfer_client.endpoint_autoactivate(self.source_endpoint)
-        self.transfer_client.endpoint_autoactivate(self.mc_target_ep_id)
+        self.log.info(ep['acl_available'])
 
-        self.log.info("==== Just prior to add_endpoint_acl_rule ====")
-        self.log.info("|")
-        self.log.info("|  target endpoint = {}".format(self.mc_target_ep_id))
-        self.log.info("|  principal = {} ({})".format(self.source_user_globus_id))
-        self.log.info("|  path = {}".format(self.dest_path))
-        self.log.info("| ")
-        self.log.info("==== ")
+        self.log.info("Just before call to add_endpoint_acl_rule:")
+        self.log.info("  target_endpoint = {}".format(self.target_endpoint))
+        self.log.info("  path = {}".format(destination_path))
+        self.log.info("  globus user id = {}".format(self.source_user_globus_id))
 
         try:
-            self.transfer_client.add_endpoint_acl_rule(
-                self.mc_target_ep_id,
+            cc_transfer_client.add_endpoint_acl_rule(
+                self.target_endpoint,
                 dict(principal=self.source_user_globus_id,
-                     principal_type='identity', path=self.dest_path, permissions='rw'),
+                     principal_type='identity', path=destination_path, permissions='rw')
             )
         except TransferAPIError as error:
             # PermissionDenied can happen if a new Portal client is swapped
             # in and it doesn't have endpoint manager on the dest_ep.
             # The /portal/processed directory has been set to to read/write
             # for all users so the subsequent operations will succeed.
+            self.log.info(error)
             if error.code == 'PermissionDenied':
-                self.log.info(error)
-            elif error.code != 'Exists':
-                raise
-            else:
-                self.log.info(error)
-
-        self.log.info("Before submit transfer")
-        results = self.transfer_client.submit_transfer(transfer_data)
-        self.log.info("After submit transfer: {}".format(results))
-        task_id = results['task_id']
-        status = "STARTED"
-        while not status == "SUCCEEDED" and not status == "FAILED":
-            task = self.transfer_client.get_task(task_id)
-            status = task['status']
-            self.log.info("Current task status = {}".format(status))
-            time.sleep(5)
-
-        try:
-            acl = next(acl for acl in self.transfer_client.endpoint_acl_list(self.mc_target_ep_id)
-                       if self.dest_path == acl['path'])
-        except StopIteration:
-            pass
-        except TransferAPIError as ex:
-            # PermissionDenied can happen if a new Portal client is swapped
-            # in and it doesn't have endpoint manager on the dest_ep.
-            # The /portal/processed directory has been set to to writeable
-            # for all users so the delete task will succeed even if an ACL
-            # can't be set.
-            if ex.code == 'PermissionDenied':
                 pass
-        else:
-            self.transfer_client.delete_endpoint_acl_rule(self.mc_target_ep_id, acl['id'])
+            elif error.code != 'Exists':
+                pass
+
+    def get_user_transfer_client(self):
+        self.log.info("Getting MC User's globus infomation")
+        records = DatabaseInterface().get_globus_auth_info_records_by_user_id(self.user_id)
+        # Only the latest
+        record = (records[0] if len(records) > 0 else None)
+        if not record:
+            self.log.info("Globus auth info record for MC user {} does not exist; logged out?".format(self.user_id))
+            return None
+
+        # else
+        self.log.info("Got MC User's globus information; getting tokens")
+        self.source_user_globus_id = record['globus_id']
+        transfer_tokens = record['tokens']['transfer.api.globus.org']
+        self.log.info("Got transfer.api.globus.org tokens; keys = {}".format(transfer_tokens.keys()))
+        transfer_client = \
+            self.get_transfer_client(transfer_tokens, self.source_endpoint)
+        if not transfer_client:
+            self.log.error("Transfer Client is not available; abort")
+            return None
+        return transfer_client
+
+
+    #     self.user_transfer_client.endpoint_autoactivate(self.target_endpoint)
+    #     transfer_data = TransferData(transfer_client=self.user_transfer_client,
+    #                                  source_endpoint=self.source_endpoint,
+    #                                  destination_endpoint=self.target_endpoint,
+    #                                  label='Test-transfer-for-{}'.format(transfer_dir))
+    #     self.log.info("Object transfer_data = {}".format(transfer_data))
+    #     transfer_data.add_item(source_path='/',
+    #                            destination_path=destination_path,
+    #                            recursive=True)
+    #     self.log.info("Object transfer_data = {}".format(transfer_data))
+    #
+    #
+    #
+    #     self.log.info("Before submit transfer")
+    #     results = self.user_transfer_client.submit_transfer(transfer_data)
+    #     self.log.info("After submit transfer: {}".format(results))
+    #     task_id = results['task_id']
+    #     status = "STARTED"
+    #     while not status == "SUCCEEDED" and not status == "FAILED":
+    #         task = self.user_transfer_client.get_task(task_id)
+    #         status = task['status']
+    #         self.log.info("Current task status = {}".format(status))
+    #         time.sleep(5)
+    #
+    #     try:
+    #         acl = next(acl for acl in self.user_transfer_client.endpoint_acl_list(self.target_endpoint)
+    #                    if self.destination_path == acl['path'])
+    #     except StopIteration:
+    #         pass
+    #     except TransferAPIError as ex:
+    #         # PermissionDenied can happen if a new Portal client is swapped
+    #         # in and it doesn't have endpoint manager on the dest_ep.
+    #         # The /portal/processed directory has been set to to writeable
+    #         # for all users so the delete task will succeed even if an ACL
+    #         # can't be set.
+    #         if ex.code == 'PermissionDenied':
+    #             pass
+    #     else:
+    #         self.user_transfer_client.delete_endpoint_acl_rule(self.target_endpoint, acl['id'])
+    #
 
     def get_transfer_client(self, transfer_tokens, endpoint_id, endpoint_path='/'):
-        materials_commons_client = self.mc_interface.get_auth_client()
         authorizer = RefreshTokenAuthorizer(
             transfer_tokens['refresh_token'],
-            materials_commons_client,
+            self.globus_access.get_auth_client(),
             access_token=transfer_tokens['access_token'],
             expires_at=transfer_tokens['expires_at_seconds'])
 
@@ -171,22 +175,11 @@ class EpEpTransferHelper:
         self.log.info("Endpoint - display_name = {}".format(ep['display_name']))
         self.log.info("Endpoint - owner_string = {}".format(ep['owner_string']))
 
-        https_server = ep['https_server']
-        endpoint_uri = https_server + endpoint_path if https_server else None
-        self.log.info("endpoint_uri = {}".format(endpoint_uri))
         return transfer
 
     @staticmethod
     def make_transfer_dir(status_record_id):
         return "ep2ep-{}".format(status_record_id)
-
-
-def main(mc_user_id, source_endpoint, dest_endpoint):
-    log = logging.getLogger("main")
-    log.info("Starting: main()")
-    transfer_helper = EpEpTransferHelper(mc_user_id, source_endpoint, dest_endpoint)
-    transfer_helper.do_transfer()
-
 
 if __name__ == '__main__':
     LoggingHelper().set_root()
@@ -197,10 +190,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test of Globus non-ETL upload')
     parser.add_argument('-u', '--user_id', type=str, dest="user_id", help="Materials Commons user id; fallback MC_USER_ID env variable; required")
     parser.add_argument('-s', '--source', type=str, dest="source", help="source endpoint; fallback SOURCE_ENDPOINT env var; required")
+    parser.add_argument('-p', '--path', type=str, dest="path", help="source endpoint path; fallback SOURCE_ENDPOINT_PATH env var; required")
     parser.add_argument('-d', '--destination', type=str, dest="destination", help="destination endpoint; fallback DESTINATION_ENDPOINT env var; required")
     args = parser.parse_args(argv[1:])
 
-    # user_id
+    # Materials Commons User id
     user_id = None
     if args.user_id:
         user_id = args.user_id
@@ -212,7 +206,7 @@ if __name__ == '__main__':
         parser.print_help()
         exit(-1)
 
-    # endpoint
+    # Globus user's source endpoint
     source_endpoint = None
     if args.source:
         source_endpoint = args.source
@@ -224,19 +218,39 @@ if __name__ == '__main__':
         parser.print_help()
         exit(-1)
 
-    dest_endpoint = None
+    # Globus user's source endpoint path
+    source_path = None
+    if args.path:
+        source_path = args.path
+    elif os.environ.get('SOURCE_ENDPOINT_PATH'):
+        source_path = os.environ.get('SOURCE_ENDPOINT_PATH')
+        startup_log.info("Getting source Globus endpoint from SOURCE_ENDPOINT_PATH env var = {}".format(source_endpoint))
+    else:
+        print("You must specify a source endpoint path. Argument not found.")
+        parser.print_help()
+        exit(-1)
+
+    # Materials Commons Globus Connect Server endpoint
+    destination_endpoint = None
     if args.destination:
-        dest_endpoint = args.destination
+        destination_endpoint = args.destination
     elif os.environ.get('DESTINATION_ENDPOINT'):
-        dest_endpoint = os.environ.get('DESTINATION_ENDPOINT')
-        startup_log.info("Getting destination Globus endpoint from DESTINATION_ENDPOINT env var = {}".format(dest_endpoint))
+        destination_endpoint = os.environ.get('DESTINATION_ENDPOINT')
+        startup_log.info("Getting destination Globus endpoint from DESTINATION_ENDPOINT env var = {}".format(destination_endpoint))
     elif os.environ.get('MC_CONFIDENTIAL_CLIENT_ENDPOINT'):
-        dest_endpoint = os.environ.get('MC_CONFIDENTIAL_CLIENT_ENDPOINT')
-        startup_log.info("Getting destination Globus endpoint from MC_CONFIDENTIAL_CLIENT_ENDPOINT env var = {}".format(dest_endpoint))
+        destination_endpoint = os.environ.get('MC_CONFIDENTIAL_CLIENT_ENDPOINT')
+        startup_log.info("Getting destination Globus endpoint from MC_CONFIDENTIAL_CLIENT_ENDPOINT env var = {}".format(destination_endpoint))
     else:
         print("You must specify a destination endpoint. Argument not found.")
         parser.print_help()
         exit(-1)
 
-    startup_log.info("user_id = {}, source_endpoint = {}".format(user_id, source_endpoint))
-    main(user_id, source_endpoint, dest_endpoint)
+    startup_log.info("Input parameters:")
+    startup_log.info("  Materials Commons user_id = {}".format(user_id))
+    startup_log.info("  User source_endpoint = {}".format(source_endpoint))
+    startup_log.info("  User source_endpoint path = {}".format(source_path))
+    startup_log.info("  Globus Connect Server destination_endpoint = {}".format(destination_endpoint))
+
+    transfer_helper = EpEpTransferHelper(user_id, source_endpoint, source_path, destination_endpoint)
+    transfer_helper.do_transfer()
+
--- backend/servers/etlserver/scripts/basic_set_acl.py
+++ backend/servers/etlserver/scripts/basic_set_acl.py
@@ -1,31 +1,116 @@
 import logging
+import os
 from pathlib import Path
 
 from ..utils.LoggingHelper import LoggingHelper
-from ..common.GlobusAccess import GlobusAccess, NATIVE_APP_AUTH, CONFIDENTIAL_CLIENT_APP_AUTH
+from globus_sdk import (ConfidentialAppAuthClient, ClientCredentialsAuthorizer, TransferClient)
 
-TOKEN_FILE_PATH = Path(Path.home(), '.globus', 'refresh-testing-tokens.json')
-REDIRECT_URI = 'https://auth.globus.org/v2/web/auth-code'
 SCOPES = ('openid email profile '
           'urn:globus:auth:scope:transfer.api.globus.org:all')
-USE_CONFIDENTIAL_CLIENT = False
+
+
+class AuthenticationException(BaseException):
+    def __init__(self, attr):
+        self.attr = str(attr)
+
+
+class GlobusHelper:
+    def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.info("in __init__")
+        self.client_user = None
+        self.client_token = None
+        self.auth_client = None
+        self.transfer_client = None
+
+    def setup(self):
+        self.client_user = os.environ.get('MC_CONFIDENTIAL_CLIENT_USER')
+        self.client_token = os.environ.get('MC_CONFIDENTIAL_CLIENT_PW')
+
+        if (not self.client_user) or (not self.client_token):
+            missing = []
+            if not self.client_user:
+                missing.append('MC_CONFIDENTIAL_CLIENT_USER')
+            if not self.client_token:
+                missing.append('MC_CONFIDENTIAL_CLIENT_PW')
+            message = "Missing environment values: {}".format(", ".join(missing))
+            raise EnvironmentError(message)
+
+        self.log.info("Env variables are ok")
+        self.log.info("  -- MC_CONFIDENTIAL_CLIENT_USER = {}".format(self.client_user))
+
+        auth_client = ConfidentialAppAuthClient(
+            client_id=self.client_user, client_secret=self.client_token)
+        if not auth_client:
+            error = "No Authentication Client"
+            self.log.error("Error: " + str(error))
+            raise AuthenticationException(error)
+        self.log.info("set_transfer_client - auth_client = {}".format(auth_client.client_id))
+        scopes = "urn:globus:auth:scope:transfer.api.globus.org:all"
+        cc_authorizer = ClientCredentialsAuthorizer(auth_client, scopes)
+        transfer_client = TransferClient(authorizer=cc_authorizer)
+        self.log.debug("get_transfer_interface - transfer_client")
+        self.log.debug(transfer_client)
+        if not transfer_client:
+            error = "No transfer interface"
+            self.log.error("Error: " + str(error))
+            raise AuthenticationException(error)
+
+        self.log.debug(" init - done")
+        self.auth_client = auth_client
+        self.transfer_client = transfer_client
+
+    def get_globus_user(self, user_name):
+        ret = self.auth_client.get_identities(usernames=[user_name])
+        globus_user = None
+        if ret and ret['identities'] and len(ret['identities']) > 0:
+            globus_user = ret['identities'][0]
+        return globus_user
+
+    def get_endpoint(self, endpoint_id):
+        return self.transfer_client.get_endpoint(endpoint_id)
+
+    def set_acl_rule(self, ep_id, path, globus_user_id, permissions):
+        transfer_client = self.transfer_client
+        results = transfer_client.endpoint_acl_list(ep_id)
+        for rule in results["DATA"]:
+            if rule['path'] == path and rule['principal'] == globus_user_id:
+                return rule
+        rule_data = {
+            "DATA_TYPE": "access",
+            "principal_type": "identity",
+            "principal": globus_user_id,
+            "path": path,
+            "permissions": permissions
+        }
+        results = transfer_client.add_endpoint_acl_rule(ep_id, rule_data)
+        if not results['code'] == "Created":
+            return None
+        results = transfer_client.endpoint_acl_list(ep_id)
+        for rule in results["DATA"]:
+            if rule['path'] == path and rule['principal'] == globus_user_id:
+                return rule
+        return None
 
 
 class BasicACLExample:
-    def __init__(self, globus_access):
+    def __init__(self):
         self.log = logging.getLogger(self.__class__.__name__)
         self.log.info("BasicACLExample.__init__()")
-        self.globus_access = globus_access
+        self.globus_access = GlobusHelper()
+        self.globus_access.setup()
 
-    def do_it(self, globus_user_name, ep_id, path, permissions):
-        if permissions == 'delete':
-            pass # here we delete the acl
-        else:
-            ret = self.globus_access.get_globus_user(globus_user_name)
-            globus_user_id = ret['id']
-            results = self.globus_access.set_acl_rule(
-                ep_id, path, globus_user_id, permissions)
-            self.log.info("Results from setting acl rule = {}".format(results))
+    def do_it(self, globus_user_name, ep_id, path):
+        ret = self.globus_access.get_globus_user(globus_user_name)
+        globus_user_id = ret['id']
+        self.log.info("Globus user = {} ({})".format(globus_user_name, globus_user_id))
+
+        ep = self.globus_access.get_endpoint(ep_id)
+        self.log.info("Endpoint 'acl_available' flag = {}".format(ep['acl_available']))
+
+        results = self.globus_access.set_acl_rule(
+            ep_id, path, globus_user_id, 'rw')
+        self.log.info("Results from setting acl rule = {}".format(results))
 
 
 if __name__ == '__main__':
@@ -35,18 +120,12 @@ if __name__ == '__main__':
 
     # ---- These should be command line params ----
     # endpoint_id is the endpoint on which to set acl (a shared endpoint)
-    endpoint_id = 'caeb35c2-ad1e-11e8-823c-0a3b7ca8ce66'
+    endpoint_id = '3df15436-b2c2-11e8-8240-0a3b7ca8ce66'
     # endpoint_path
-    endpoint_path = '/'
+    endpoint_path = '/mcdir/landing_for_test/'
     # globus_user_id - the globus user that is in the ACL rule
-    globus_user_name = "glenn.tarcea@gmail.com"
+    globus_user_name = "materialscommonstestuser1@globusid.org"
     # permissions for the ACL Rule ("r" "rw" or 'delete' to delete that acl rule)
     permissions = "rw"
 
-    impl = NATIVE_APP_AUTH
-    if USE_CONFIDENTIAL_CLIENT:
-        impl = CONFIDENTIAL_CLIENT_APP_AUTH
-
-    globus_access = GlobusAccess(use_implementation=impl)
-
-    BasicACLExample(globus_access).do_it(globus_user_name, endpoint_id, endpoint_path, permissions)
+    BasicACLExample().do_it(globus_user_name, endpoint_id, endpoint_path)
