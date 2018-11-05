6f1f2a0a33a0aaa95a9765a96cbb95656785690b
Terry Weymouth <weymouth@umich.edu>
2018-11-05 12:23:37 -0500

    Snapshot

--- website/src/app/models/api/globus-interface-api.service.js
+++ website/src/app/models/api/globus-interface-api.service.js
@@ -6,11 +6,13 @@ class GlobusInterfaceAPIService {
 
     setupUploadEndpoint(userId, projectId) {
         console.log('setupDownloadUrl');
-        let route = this.globusInterfaceRoute('setupUploadEndpoint');
+        let route = this.globusInterfaceRoute('createGlobusUploadRequest');
         let data = {
             user_id: userId,
             project_id: projectId
         };
+        console.log('setupDownloadUrl', route);
+        console.log('setupDownloadUrl', data);
         return route.customPOST(data).then(
             r => {
                 console.log('setupUploadEndpoint - normal return');
