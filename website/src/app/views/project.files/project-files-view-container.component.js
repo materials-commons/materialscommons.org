2a0ddd13b2a6ffcae4b0c0f888c84aafd7c45104
gtarcea <glenn.tarcea@gmail.com>
2018-11-20 14:49:52 -0500

    Don't show file count and start working on tree

--- website/src/app/views/project.files/project-files-view-container.component.js
+++ website/src/app/views/project.files/project-files-view-container.component.js
@@ -12,13 +12,14 @@ class MCProjectFilesViewContainerComponentController {
     }
 
     $onInit() {
+        console.log('this.state.project', this.state.project);
         if (this.state.fileTree == null) {
-            this.projectFileTreeAPI.getProjectRoot(this.state.project.id).then(
-                (files) => {
-                    console.log('files', files);
-                    files[0].data.childrenLoaded = true;
-                    files[0].expand = true;
-                    this.state.fileTree = files;
+            this.projectFileTreeAPI.getDirectoryForProject(this.state.project.id, this.state.project.root_dir[0].id).then(
+                (dirs) => {
+                    console.log('dirs', dirs);
+                    // files[0].data.childrenLoaded = true;
+                    // files[0].expand = true;
+                    this.state.fileTree = dirs;
                     this.mcStateStore.updateState('project:file-tree', this.state.fileTree);
                 },
                 () => this.toast.error('Unable to retrieve project files')
