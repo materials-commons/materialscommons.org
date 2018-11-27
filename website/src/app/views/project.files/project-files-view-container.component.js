60a1cf7e794fb25ae35c8bdb7d0a8c8e0e792a22
gtarcea <glenn.tarcea@gmail.com>
2018-11-27 14:34:21 -0500

    Partially working file tree

--- website/src/app/views/project.files/project-files-view-container.component.js
+++ website/src/app/views/project.files/project-files-view-container.component.js
@@ -8,23 +8,35 @@ class MCProjectFilesViewContainerComponentController {
         this.state = {
             project: this.mcStateStore.getState('project'),
             fileTree: this.mcStateStore.getState('project:file-tree'),
-        }
+        };
     }
 
     $onInit() {
         console.log('this.state.project', this.state.project);
         if (this.state.fileTree == null) {
-            this.projectFileTreeAPI.getDirectoryForProject(this.state.project.id, this.state.project.root_dir[0].id).then(
-                (dirs) => {
-                    console.log('dirs', dirs);
-                    // files[0].data.childrenLoaded = true;
-                    // files[0].expand = true;
-                    this.state.fileTree = dirs;
-                    this.mcStateStore.updateState('project:file-tree', this.state.fileTree);
+            this.projectFileTreeAPI.getProjectRoot(this.state.project.id).then(
+                files => {
+                    files[0].data.childrenLoaded = true;
+                    files[0].expand = true;
+                    this.state.fileTree = angular.copy(files);
+                    console.log('this.state.fileTree', this.state.fileTree);
                 },
-                () => this.toast.error('Unable to retrieve project files')
+                () => this.toast.error('Unable to retrieve project root')
             );
         }
+    }
+
+    handleLoadDir(node) {
+        this.projectFileTreeAPI.getDirectory(this.state.project.id, dirId).then(
+            files => {
+                console.log('projectFileTreeAPI.getDirectory', files);
+                file.children = files;
+                file.active = true;
+                file.data.childrenLoaded = true;
+                file.expand = !file.expand;
+            },
+            () => this.toast.error('unable to retrieve directory')
+        );
     }
 }
 
--- website/src/app/views/project.files/project-files-view.component.js
+++ website/src/app/views/project.files/project-files-view.component.js
@@ -3,6 +3,7 @@ class MCProjectFilesViewComponentController {
     constructor() {
         this.state = {
             root: null,
+            all: []
         }
     }
 
@@ -21,3 +22,34 @@ angular.module('materialscommons').component('mcProjectFilesView', {
         root: '<'
     }
 });
+
+class MCFileTreeDirectoryDirectiveController {
+    /*ngInject*/
+    constructor() {
+        console.log('mcFileTreeDirectory', this.file);
+        this.state = {
+            files: this.file.children
+        };
+    }
+}
+
+/*@ngInject*/
+function mcFileTreeDirectoryDirective(RecursionHelper) {
+    return {
+        restrict: 'E',
+        scope: {
+            file: '='
+        },
+        controller: MCFileTreeDirectoryDirectiveController,
+        replace: true,
+        controllerAs: '$ctrl',
+        bindToController: true,
+        template: require('./mc-file-tree-directory.html'),
+        compile: function(element) {
+            return RecursionHelper.compile(element, function() {
+            });
+        }
+    };
+}
+
+angular.module('materialscommons').directive('mcFileTreeDirectory', mcFileTreeDirectoryDirective);
