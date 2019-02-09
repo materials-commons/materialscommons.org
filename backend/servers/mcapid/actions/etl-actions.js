391c2d4f5809c64b6fb4ce4b4fdfc4daee70a6c9
gtarcea <glenn.tarcea@gmail.com>
2019-02-09 16:20:12 -0500

    Add example queuing call

---
+++ backend/servers/mcapid/actions/etl-actions.js
@@ -0,0 +1,15 @@
+const {Action, api} = require('actionhero');
+
+module.exports.QueueETLJobAction = class QueueETLJobAction extends Action {
+    constructor() {
+        super();
+        this.name = "queueETLJob";
+        this.description = "Queues a spreadsheet for ETL processing"
+        this.do_not_authenticate = true;
+    }
+
+    async run({response}) {
+        const enqueued = await api.tasks.enqueue('etlSpreadsheetProcessorTask', { arg: "arg1" }, 'default');
+        response.data = {enqueued: enqueued};
+    }
+}
---
+++ backend/servers/mcapid/tasks/etl-tasks.js
@@ -0,0 +1,16 @@
+const {api, Task} = require('actionhero')
+
+module.exports.ETLSpreadsheetProcessorTask = class ETLSpreadsheetProcessorTask extends Task {
+    constructor() {
+        super();
+        this.name = "etlSpreadsheetProcessorTask";
+        this.description = "Performs ETL on a spreadsheet";
+        this.frequency = 0;
+        this.queue = "etl";
+    }
+
+    async run(data) {
+        api.log("data = ", 'info', data);
+        api.log(`etlSpreadsheetProcessTask ${data.arg}`);
+    }
+}
