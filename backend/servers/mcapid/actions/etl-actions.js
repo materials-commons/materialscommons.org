0222bdd15743c74406706f39e588cc0fd614f5a2
gtarcea <glenn.tarcea@gmail.com>
2019-02-09 16:21:33 -0500

    Format code

--- backend/servers/mcapid/actions/etl-actions.js
+++ backend/servers/mcapid/actions/etl-actions.js
@@ -3,13 +3,13 @@ const {Action, api} = require('actionhero');
 module.exports.QueueETLJobAction = class QueueETLJobAction extends Action {
     constructor() {
         super();
-        this.name = "queueETLJob";
-        this.description = "Queues a spreadsheet for ETL processing"
+        this.name = 'queueETLJob';
+        this.description = 'Queues a spreadsheet for ETL processing';
         this.do_not_authenticate = true;
     }
 
     async run({response}) {
-        const enqueued = await api.tasks.enqueue('etlSpreadsheetProcessorTask', { arg: "arg1" }, 'default');
+        const enqueued = await api.tasks.enqueue('etlSpreadsheetProcessorTask', {arg: 'arg1'}, 'default');
         response.data = {enqueued: enqueued};
     }
-}
+};
--- backend/servers/mcapid/tasks/etl-tasks.js
+++ backend/servers/mcapid/tasks/etl-tasks.js
@@ -1,16 +1,16 @@
-const {api, Task} = require('actionhero')
+const {api, Task} = require('actionhero');
 
 module.exports.ETLSpreadsheetProcessorTask = class ETLSpreadsheetProcessorTask extends Task {
     constructor() {
         super();
-        this.name = "etlSpreadsheetProcessorTask";
-        this.description = "Performs ETL on a spreadsheet";
+        this.name = 'etlSpreadsheetProcessorTask';
+        this.description = 'Performs ETL on a spreadsheet';
         this.frequency = 0;
-        this.queue = "etl";
+        this.queue = 'etl';
     }
 
     async run(data) {
-        api.log("data = ", 'info', data);
+        api.log('data = ', 'info', data);
         api.log(`etlSpreadsheetProcessTask ${data.arg}`);
     }
-}
+};
