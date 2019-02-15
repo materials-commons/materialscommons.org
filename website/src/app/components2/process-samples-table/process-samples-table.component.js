53abf846cf243e5b265e380ea45d42ee1b1682b5
gtarcea <glenn.tarcea@gmail.com>
2019-02-15 15:43:28 -0500

    Goto full page sample rather than a popup

--- website/src/app/components2/process-samples-table/process-samples-table.component.js
+++ website/src/app/components2/process-samples-table/process-samples-table.component.js
@@ -1,7 +1,6 @@
 class MCProcessSamplesTableComponentController {
     /*@ngInject*/
     constructor($mdDialog) {
-        this.$mdDialog = $mdDialog;
         this.state = {
             samples: [],
         };
@@ -11,31 +10,6 @@ class MCProcessSamplesTableComponentController {
         if (changes.samples) {
             this.state.samples = angular.copy(changes.samples.currentValue);
         }
-    }
-
-    showSample(sample) {
-        this.$mdDialog.show({
-            templateUrl: 'app/modals/show-sample-dialog.html',
-            controllerAs: '$ctrl',
-            controller: ShowSampleDialogController,
-            bindToController: true,
-            multiple: true,
-            clickOutsideToClose: true,
-            locals: {
-                sample: sample
-            }
-        });
-    }
-}
-
-class ShowSampleDialogController {
-    /*@ngInject*/
-    constructor($mdDialog) {
-        this.$mdDialog = $mdDialog;
-    }
-
-    done() {
-        this.$mdDialog.cancel();
     }
 }
 
