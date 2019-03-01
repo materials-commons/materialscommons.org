4be233251a810e6a74c3ffa271378e91e0148dca
gtarcea <glenn.tarcea@gmail.com>
2019-03-01 12:57:20 -0500

    Add tests for moving a file

---
+++ backend/servers/mcapid/lib/dal/__tests__/files-tests.js
@@ -0,0 +1,31 @@
+const r = require('@lib/r');
+const files = require('@dal/files')(r);
+const tutil = require('@lib/test-utils')(r);
+const dirUtils = require('@dal/dir-utils')(r);
+
+describe('Test moveFileToDirectory', () => {
+    let project, dir1, fileInDir1;
+
+    beforeAll(async() => {
+        project = await tutil.createTestProject();
+        [dir1, dir11] = await dirUtils.createDirsFromParent('dir1/dir11', project.root_dir.id, project.id);
+        fileInDir1 = await tutil.createFile('file1.txt', dir1.id, project.id);
+    });
+
+    afterAll(async() => {
+        tutil.deleteProject(project.id);
+    });
+
+    test('it should throw an error if file directory does not exist', async() => {
+        await expect(files.moveFileToDirectory(fileInDir1.id, 'does-not-exist', dir11.id)).rejects.toThrow();
+    });
+
+    test('it should throw an error if file does not exist', async() => {
+        await expect(files.moveFileToDirectory('does-not-exist', dir1.id, dir11.id)).rejects.toThrow();
+    });
+
+    test('it should move the file', async() => {
+        let file = await files.moveFileToDirectory(fileInDir1.id, dir1.id, dir11.id);
+        expect(file.directory.id).toBe(dir11.id);
+    });
+});
