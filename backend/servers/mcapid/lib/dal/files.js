e197f10b72a8f3dea6d25d06c4c902e6c7d80ad8
gtarcea <glenn.tarcea@gmail.com>
2019-03-01 12:57:10 -0500

    Throw an error if nothing is changed on a file move

--- backend/servers/mcapid/lib/dal/files.js
+++ backend/servers/mcapid/lib/dal/files.js
@@ -8,8 +8,11 @@ module.exports = function(r) {
     };
 
     const moveFileToDirectory = async(fileId, oldDirectoryId, newDirectoryId) => {
-        await r.table('datadir2datafile').getAll([oldDirectoryId, fileId], {index: 'datadir_datafile'})
+        let rv = await r.table('datadir2datafile').getAll([oldDirectoryId, fileId], {index: 'datadir_datafile'})
             .update({datadir_id: newDirectoryId});
+        if (!rv.replaced) {
+            throw new Error(`Unable to move file ${fileId} in directory ${oldDirectoryId} into directory ${newDirectoryId}`);
+        }
         return await getFile(fileId);
     };
 
