4b14f7745c20c489cb8ee554ee98fbcde2058832
gtarcea <glenn.tarcea@gmail.com>
2019-03-01 16:17:31 -0500

    Add log shortcuts instead of having to specify the log level if a parameter is added

--- backend/servers/mcapid/initializers/api.js
+++ backend/servers/mcapid/initializers/api.js
@@ -19,6 +19,16 @@ module.exports = class ApiInitializer extends Initializer {
             samples: require('@dal/samples')(r),
             templates: require('@dal/templates')(r),
             files: require('@dal/files')(r),
+            log: {
+                debug: (msg, params) => api.log(msg, 'debug', params),
+                info: (msg, params) => api.log(msg, 'info', params),
+                notice: (msg, params) => api.log(msg, 'notice', params),
+                warning: (msg, params) => api.log(msg, 'warning', params),
+                error: (msg, params) => api.log(msg, 'error', params),
+                critical: (msg, params) => api.log(msg, 'crit', params),
+                alert: (msg, params) => api.log(msg, 'alert', params),
+                emergency: (msg, params) => api.log(msg, 'emerg', params),
+            }
         };
     }
 };
