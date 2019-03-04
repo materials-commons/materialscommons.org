81dc7ba145a367515e837f2c0e9b794ddca57d1b
gtarcea <glenn.tarcea@gmail.com>
2019-03-04 16:16:23 -0500

    Converted access to other dal modules that is passed r in

--- backend/servers/mcapid/lib/dal/access.js
+++ backend/servers/mcapid/lib/dal/access.js
@@ -1,29 +1,29 @@
-const r = require('@lib/r');
-
-function allByProject() {
-    return r.table('access').run().then(function (allAccess) {
-        let byProject = {};
-        allAccess.forEach(function (a) {
-            if (!(a.project_id in byProject)) {
-                byProject[a.project_id] = [];
-            }
-            byProject[a.project_id].push(a);
+module.exports = function(r) {
+    function allByProject() {
+        return r.table('access').run().then(function(allAccess) {
+            let byProject = {};
+            allAccess.forEach(function(a) {
+                if (!(a.project_id in byProject)) {
+                    byProject[a.project_id] = [];
+                }
+                byProject[a.project_id].push(a);
+            });
+            return byProject;
         });
-        return byProject;
-    });
-}
+    }
 
-async function projectAccessList(projectId) {
-    return await r.table('access').getAll(projectId, {index: 'project_id'});
-}
+    async function projectAccessList(projectId) {
+        return await r.table('access').getAll(projectId, {index: 'project_id'});
+    }
 
-function adminUsers() {
-    return r.table('users').filter({admin: true}).run();
-}
+    function adminUsers() {
+        return r.table('users').filter({admin: true}).run();
+    }
 
-module.exports = {
-    allByProject: allByProject,
-    adminUsers: adminUsers,
-    projectAccessList
+    return {
+        allByProject: allByProject,
+        adminUsers: adminUsers,
+        projectAccessList
+    };
 };
 
