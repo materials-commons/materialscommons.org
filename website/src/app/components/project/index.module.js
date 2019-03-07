331570b0703e4ba6455b746fddfce6b0aeae2126
gtarcea <glenn.tarcea@gmail.com>
2019-03-07 15:23:32 -0500

    Remove mc-p-project* and mc-project-dataset components as they aren't used anywhere

--- website/src/app/components/project/index.module.js
+++ website/src/app/components/project/index.module.js
@@ -1,5 +1,1 @@
 import './mc-project-notes/mc-project-notes.component';
-import './mc-project-dataset/mc-project-dataset.component';
-import './mc-project-dataset/mc-p-project-dataset-grouped.component';
-import './mc-project-dataset/mc-p-project-dataset-ungrouped.component';
-import './mc-project-dataset/mc-p-project-dataset-details.component';
