77a9ad114674d687fe261eb63d2d7d115578d8d8
gtarcea <glenn.tarcea@gmail.com>
2019-03-19 15:07:20 -0400

    Merge process-actions and processes-actions into processes-actions

--- backend/servers/mcapid/actions/processes-actions.js
+++ backend/servers/mcapid/actions/processes-actions.js
@@ -75,6 +75,40 @@ module.exports.GetProcessAction = class GetProcessAction extends Action {
     }
 };
 
+module.exports.CreateProcessAction = class CreateProcessAction extends Action {
+    constructor() {
+        super();
+        this.name = 'createProcess';
+        this.description = 'creates a new process';
+        this.inputs = {
+            project_id: {
+                required: true,
+            },
+
+            name: {
+                required: true,
+            },
+
+            experiment_id: {
+                required: true,
+            },
+
+            attributes: {
+                default: [],
+            }
+        };
+    }
+
+    async run({response, params, user}) {
+        let process = await dal.tryCatch(async() => await api.mc.processes.createProcess(params.project_id, params.experiment_id, params.name, user.id, params.attributes));
+        if (!process) {
+            throw new Error(`Unable to create process`);
+        }
+
+        response.data = process;
+    }
+};
+
 const processExample = {
     'data': {
         'birthtime': '2019-02-08T21:14:08.250Z',
