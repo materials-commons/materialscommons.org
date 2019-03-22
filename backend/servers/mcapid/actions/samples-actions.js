b31ec01f685b4b9ffe4280eff2adb705eadcbaaa
gtarcea <glenn.tarcea@gmail.com>
2019-03-22 18:03:33 -0400

    Start filling out actions and dal for creating samples, adding attributes, etc...

--- backend/servers/mcapid/actions/samples-actions.js
+++ backend/servers/mcapid/actions/samples-actions.js
@@ -1,5 +1,6 @@
 const {Action, api} = require('actionhero');
 const dal = require('@dal');
+const _ = require('lodash');
 
 module.exports.GetSamplesForProjectAction = class GetSamplesForProjectAction extends Action {
     constructor() {
@@ -37,11 +38,72 @@ module.exports.GetSampleAction = class GetSampleAction extends Action {
     }
 
     async run({response, params, user}) {
-        const results = await dal.tryCatch(async() => await api.mc.samples.getSample(user.id, params.sample_id));
+        const results = await dal.tryCatch(async() => await api.mc.samples.getSample(params.sample_id, user.id));
         if (!results) {
             throw new Error(`Unable to retrieve samples ${params.sample_id}`);
         }
         response.data = results;
+    }
+};
+
+module.exports.CreateSampleInProcessAction = class CreateSampleInProcessAction extends Action {
+    constructor() {
+        super();
+        this.name = 'createSampleInProcess';
+        this.description = 'Creates a new sample in the given process';
+        this.inputs = {
+            project_id: {
+                required: true,
+            },
+
+            experiment_id: {
+                required: true,
+            },
+
+            process_id: {
+                required: true,
+            },
+
+            name: {
+                required: true,
+            },
+
+            description: {
+                required: true,
+            },
+
+            attributes: {
+                default: [],
+                validator: (param) => {
+                    if (!_.isArray(param)) {
+                        throw new Error('attributes must be an array');
+                    }
+                }
+            }
+        };
+    }
+
+    async run({response, params, user}) {
+        const {project_id, experiment_id, process_id, name, description, attributes} = params;
+        let sample = await dal.tryCatch(async() => await api.mc.samples.createSampleInProcess(name, description, user.id, process_id, project_id));
+        if (!sample) {
+            throw new Error(`Unable to create sample ${name} for process ${process_id} in project ${project_id}/experiment ${experiment_id}`);
+        }
+
+        await api.mc.samples.addSampleToExperiment(sample.id, experiment_id);
+
+        if (attributes.length !== 0) {
+            let sample2 = await dal.tryCatch(async() => await api.mc.samples.addAttributesToSampleInProcess(attributes, sample.id, process_id));
+
+            if (sample2 !== null) {
+                sample = sample2;
+            } else {
+                api.mc.log.info(`Unable to add attributes to sample ${name}/${sample.id}`);
+                sample.error = `Unable to add attributes to sample`;
+            }
+        }
+
+        response.data = sample;
     }
 };
 
@@ -59,22 +121,129 @@ module.exports.CreateSampleAction = class CreateSampleAction extends Action {
                 required: true,
             },
 
-            process_id: {
-                default: '',
-            },
-
             name: {
                 required: true,
             },
 
+            description: {
+                default: '',
+            },
+
             attributes: {
                 default: [],
+                validator: (param) => {
+                    if (!_.isArray(param)) {
+                        throw new Error('attributes must be an array');
+                    }
+                }
             }
         };
     }
 
-    async run() {
+    async run({response, params, user}) {
+        const {project_id, experiment_id, name, description, attributes} = params;
 
+        let process = await dal.tryCatch(async() => await api.mc.processes.createProcess(project_id, experiment_id, 'Create Samples', user.id, []));
+        if (!process) {
+            throw new Error(`Unable to create process to add sample ${name} to`);
+        }
+
+        let sample = await dal.tryCatch(async() => await api.mc.samples.createSampleInProcess(name, description, user.id, process.id, project_id));
+
+        if (!sample) {
+            throw new Error(`Unable to create sample ${name} in project ${project_id}/experiment ${experiment_id}`);
+        }
+
+        await api.mc.samples.addSampleToExperiment(sample.id, experiment_id);
+
+        if (attributes.length !== 0) {
+            sample = await dal.tryCatch(async() => await api.mc.samples.addAttributesToSampleInProcess(attributes, sample.id, process.id));
+        }
+
+        response.data = sample;
+    }
+};
+
+module.exports.AddSampleToProcessAction = class AddSampleToProcessAction extends Action {
+    constructor() {
+        super();
+        this.name = 'addSampleToProcess';
+        this.description = 'Add sample to process. Optionally create a new attribute set.';
+        this.inputs = {
+            project_id: {
+                required: true,
+            },
+
+            experiment_id: {
+                required: true,
+            },
+
+            process_id: {
+                required: true,
+            },
+
+            sample_id: {
+                required: true,
+            },
+
+            property_set_id: {
+                required: true,
+            },
+
+            transform: {
+                default: false,
+            }
+        };
+    }
+
+    async run({response, params}) {
+        const {sample_id, property_set_id, process_id, transform} = params;
+        const sample = await dal.tryCatch(async() => await api.mc.samples.addSampleToProcess(sample_id, property_set_id, process_id, transform));
+        if (!sample) {
+            throw new Error(`Unable to add sample ${sample_id}/${property_set_id} to process ${process_id}`);
+        }
+
+        response.data = sample;
+    }
+};
+
+module.exports.AddMeasurementsToSampleInProcessAction = class AddMeasurementsToSampleInProcessAction extends Action {
+    constructor() {
+        super();
+        this.name = 'addMeasurementsToSampleInProcess';
+        this.description = 'Adds measurements to the sample in the process';
+        this.inputs = {
+            project_id: {
+                required: true,
+            },
+
+            experiment_id: {
+                required: true,
+            },
+
+            process_id: {
+                required: true,
+            },
+
+            attributes: {
+                required: true,
+                validator: (param) => {
+                    if (!_.isArray(param)) {
+                        throw new Error('attributes must be an array');
+                    }
+                }
+            }
+        };
+    }
+
+    async run({response, params}) {
+        const {sample_id, property_set_id, process_id, attributes} = params;
+        const sample = await dal.tryCatch(async() => await api.mc.samples.addMeasurementsToSampleInProcess(attributes, sample_id, property_set_id, process_id));
+        if (!sample) {
+            throw new Error(`Unable to add measurements to sample ${sample_id}/${property_set_id} in process ${process_id}`);
+        }
+
+        response.data = sample;
     }
 };
 
--- backend/servers/mcapid/lib/dal/samples.js
+++ backend/servers/mcapid/lib/dal/samples.js
@@ -1,4 +1,8 @@
+const model = require('@lib/model');
+
 module.exports = function(r) {
+
+    const db = require('./db')(r);
 
     async function getSamplesForProject(projectId) {
         return await r.table('project2sample').getAll(projectId, {index: 'project_id'})
@@ -17,7 +21,7 @@ module.exports = function(r) {
         };
     }
 
-    async function getSample(userId, sampleId) {
+    async function getSample(sampleId, userId) {
         return await r.table('access').getAll(userId, {index: 'user_id'})
             .eqJoin([r.row('project_id'), sampleId], r.table('project2sample'), {index: 'project_sample'}).zip().limit(1)
             .eqJoin('sample_id', r.table('samples')).zip()
@@ -64,18 +68,51 @@ module.exports = function(r) {
             }).coerceTo('array');
     }
 
-    async function createSample() {
-
+    async function addSampleToProject(sampleId, projectId) {
+        const proj2sample = new model.Project2Sample(projectId, sampleId);
+        await db.insert('project2sample', proj2sample);
     }
 
-    async function addSampleToProcess() {
+    async function addSampleToExperiment(sampleId, experimentId) {
+        const e2s = new model.Experiment2Sample(experimentId, sampleId);
+        await db.insert('experiment2sample', e2s);
+    }
 
+    async function createSampleInProcess(name, description, owner, processId, projectId) {
+        let pset = new model.PropertySet(true);
+        let createdPSet = await db.insert('propertysets', pset);
+        let s = new model.Sample(name, description, owner);
+        let createdSample = await db.insert('samples', s);
+        let s2ps = new model.Sample2PropertySet(createdSample.id, createdPSet.id, true);
+        await db.insert('sample2propertyset', s2ps);
+        let proc2sample = new model.Process2Sample(processId, createdSample.id, createdPSet.id, 'out');
+        let proj2sample = new model.Project2Sample(projectId, createdSample.id);
+        await db.insert('process2sample', proc2sample);
+        await db.insert('project2sample', proj2sample);
+
+        return createdSample;
+    }
+
+    async function addSampleToProcess(sampleId, propertySetId, processId, transform) {
+        return {};
+    }
+
+    async function addMeasurementsToSampleInProcess(attributes, sampleId, propertySetId, processId) {
+        return {};
+    }
+
+    async function addAttributesToSampleInProcess(attributes, sampleId, processId) {
+        return {};
     }
 
     return {
         getSamplesForProject,
         getSample,
-        createSample,
+        addSampleToProject,
+        addSampleToExperiment,
+        createSampleInProcess,
         addSampleToProcess,
+        addMeasurementsToSampleInProcess,
+        addAttributesToSampleInProcess,
     };
 };
