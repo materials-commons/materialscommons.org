9f2bbd9670b86ce43816b041a4a975180cf7a898
gtarcea <glenn.tarcea@gmail.com>
2019-03-29 16:47:49 -0400

    Add name2Attr

--- backend/servers/mcapid/lib/util.js
+++ backend/servers/mcapid/lib/util.js
@@ -1,9 +1,18 @@
 const _ = require('lodash');
 
-module.exports.removeExistingItemsIn = function removeExistingItemsIn(items, matchingEntries, property) {
+function removeExistingItemsIn(items, matchingEntries, property) {
     if (items.length) {
         let matchingEntriesByProperty = _.keyBy(matchingEntries, property);
         return items.filter(item => (!(item[property] in matchingEntriesByProperty)));
     }
     return items;
+}
+
+function nameToAttr(name) {
+    return name.replace(/\s+/g, '_').replace(/\//g, '_').replace(/-/g, '_').toLowerCase();
+}
+
+module.exports = {
+    removeExistingItemsIn,
+    nameToAttr,
 };
