33b95059a79d4516f49eb4fed57b6cdd124126c9
gtarcea <glenn.tarcea@gmail.com>
2019-04-05 15:10:06 -0400

    Formatting

--- backend/servers/mcapid/lib/dal/samples.js
+++ backend/servers/mcapid/lib/dal/samples.js
@@ -125,6 +125,7 @@ module.exports = function(r) {
                 await addAttributeToExistingSampleInProcess(attr, sampleId, propertySetId, processId);
             }
         }
+
         return true;
     }
 
