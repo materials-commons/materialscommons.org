31b6291c5564ba905bb0952c9ba91b44397fabf7
gtarcea <glenn.tarcea@gmail.com>
2019-04-22 14:43:16 -0400

    Add utility package for running etl jobs - in this case check

---
+++ backend/servers/mcapid/lib/etl.js
@@ -0,0 +1,55 @@
+const {spawn} = require('child_process');
+let {api} = require('actionhero');
+
+async function spawnJob(cmd, args, path) {
+    return new Promise((resolve, reject) => {
+        api.mc.log.info(cmd, args);
+        let child = spawn(cmd, args);
+        let log = '';
+        child.stderr.on('data', data => {
+            let entry = '' + data;
+            log = log + entry;
+            api.mc.log.info(entry);
+        });
+        child.stdout.on('data', data => {
+            let entry = '' + data;
+            log = log + entry;
+            api.mc.log.info(entry);
+        });
+        child.on('close', exitCode => {
+            api.mc.log.info('exitCode', exitCode);
+            if (exitCode === 0) {
+                resolve(log);
+            } else {
+                reject(log);
+            }
+        });
+
+        child.on('error', err => {
+            log.push(`Failed to start processing job for file ${path}: ${err}`);
+            api.mc.log.info(log);
+            reject(log);
+        });
+    });
+}
+
+async function checkSpreadsheet(path, hasParent) {
+    let args = ['check', '--file', path], output, success;
+    if (hasParent) {
+        args.push('--has-parent');
+    }
+    try {
+        output = await spawnJob('../../prodbin/mcetl', args, path);
+        success = true;
+    } catch (e) {
+        api.mc.log.info('spawnJob failed', e);
+        output = e;
+        success = false;
+    }
+
+    return {output, success};
+}
+
+module.exports = {
+    checkSpreadsheet,
+};
