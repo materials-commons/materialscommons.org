7f3f9d854a0550e1961e40a940aec1541cf90b77
gtarcea <glenn.tarcea@gmail.com>
2019-05-02 10:46:39 -0400

    Changed name of API - update to new name

--- backend/servers/mcapid/actions/samples-actions.js
+++ backend/servers/mcapid/actions/samples-actions.js
@@ -190,7 +190,7 @@ module.exports.CreateSampleAction = class CreateSampleAction extends Action {
     async run({response, params, user}) {
         const {project_id, experiment_id, name, description, attributes} = params;
 
-        let process = await dal.tryCatch(async() => await api.mc.processes.createProcess(project_id, experiment_id, 'Create Samples', user.id, []));
+        let process = await dal.tryCatch(async() => await api.mc.processes.createTransformProcessFromTemplate(project_id, experiment_id, 'Create Samples', user.id, []));
         if (!process) {
             throw new Error(`Unable to create process to add sample ${name} to`);
         }
