a2621e2e12c48b2660371cc8ae9722d7aa82d202
gtarcea <glenn.tarcea@gmail.com>
2019-07-24 11:58:27 -0400

    Add unit tests for getDatasetSamplesAndProcesses

--- backend/servers/mcapid/__tests__/dataset-actions-tests.js
+++ backend/servers/mcapid/__tests__/dataset-actions-tests.js
@@ -160,27 +160,130 @@ describe('getDataset action tests', () => {
     });
 });
 
-describe('getDatasetFiles action tests', () => {
-    let api;
-    beforeAll(async() => api = await actionhero.start());
-    afterAll(async() => {
-        await actionhero.stop();
-    });
-
-    test('It should ', () => {
-
-    });
-});
+//
+// With file selection this call is obsolete and probably going away...
+//
+// describe('getDatasetFiles action tests', () => {
+//     let api, ds, proj, proj2;
+//
+//     beforeAll(async() => {
+//         api = await actionhero.start();
+//         proj = await tutil.createTestProject();
+//         proj2 = await tutil.createTestProject();
+//         let title = uuid();
+//         let params = {
+//             title: title,
+//             project_id: proj.id,
+//             apikey: 'totally-bogus',
+//         };
+//         let result = await api.specHelper.runAction('createDataset', params);
+//         expect(result.error).toBeFalsy();
+//         ds = result.data;
+//     });
+//
+//     afterAll(async() => {
+//         await actionhero.stop();
+//         let params = {
+//             project_id: proj.id,
+//             dataset_id: ds.id,
+//             apikey: 'totally-bogus'
+//         };
+//         let result = await api.specHelper.runAction('deleteDataset', params);
+//         expect(result.error).toBeFalsy();
+//
+//         await tutil.deleteProject(proj.id);
+//         await tutil.deleteProject(proj2.id);
+//     });
+//
+//     test('It should ', () => {
+//
+//     });
+// });
 
 describe('getDatasetSamplesAndProcesses action tests', () => {
-    let api;
-    beforeAll(async() => api = await actionhero.start());
-    afterAll(async() => {
-        await actionhero.stop();
+    let api, ds, proj, proj2;
+
+    beforeAll(async() => {
+        api = await actionhero.start();
+        proj = await tutil.createTestProject();
+        proj2 = await tutil.createTestProject();
+        let title = uuid();
+        let params = {
+            title: title,
+            project_id: proj.id,
+            apikey: 'totally-bogus',
+        };
+        let result = await api.specHelper.runAction('createDataset', params);
+        expect(result.error).toBeFalsy();
+        ds = result.data;
     });
 
-    test('It should ', () => {
+    afterAll(async() => {
+        await actionhero.stop();
+        let params = {
+            project_id: proj.id,
+            dataset_id: ds.id,
+            apikey: 'totally-bogus'
+        };
+        let result = await api.specHelper.runAction('deleteDataset', params);
+        expect(result.error).toBeFalsy();
 
+        await tutil.deleteProject(proj.id);
+        await tutil.deleteProject(proj2.id);
+    });
+
+    test('It should return the datasets processes and samples', async() => {
+        let params = {
+            project_id: proj.id,
+            dataset_id: ds.id,
+            apikey: 'totally-bogus',
+        };
+
+        let result = await api.specHelper.runAction('getDatasetSamplesAndProcesses', params);
+        expect(result.error).toBeFalsy();
+        expect(result.data.id).toEqual(ds.id);
+    });
+
+    test('It should error when no project is included', async() => {
+        let params = {
+            dataset_id: ds.id,
+            apikey: 'totally-bogus',
+        };
+
+        let result = await api.specHelper.runAction('getDatasetSamplesAndProcesses', params);
+        expect(result.error).toBeTruthy();
+    });
+
+    test('It should error when no apikey is given', async() => {
+        let params = {
+            project_id: proj.id,
+            dataset_id: ds.id,
+        };
+
+        let result = await api.specHelper.runAction('getDatasetSamplesAndProcesses', params);
+        expect(result.error).toBeTruthy();
+    });
+
+    test('It should error when wrong project id is given', async() => {
+        let params = {
+            project_id: proj2.id,
+            dataset_id: ds.id,
+            apikey: 'totally-bogus',
+        };
+
+        let result = await api.specHelper.runAction('getDatasetSamplesAndProcesses', params);
+        expect(result.error).toBeTruthy();
+    });
+
+    test('It should error when dataset id does not exist', async() => {
+        let params = {
+            project_id: proj.id,
+            dataset_id: 'does-not-exist',
+            apikey: 'totally-bogus',
+        };
+
+        let result = await api.specHelper.runAction('getDatasetSamplesAndProcesses', params);
+        expect(result.error).toBeTruthy();
     });
 });
 
